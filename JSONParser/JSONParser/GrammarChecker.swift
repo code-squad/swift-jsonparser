f7b16e1e3f2046508588f4b12828bd2c94c1828c
yang hee jung <puuurm@naver.com>
2017-11-02 17:57:51 +0900

    Create GrammarChecker structure

---
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -0,0 +1,13 @@
+//
+//  GrammarChecker.swift
+//  JSONParser
+//
+//  Created by yangpc on 2017. 11. 2..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct GrammarChecker {
+    
+}
