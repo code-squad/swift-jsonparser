2e2ad4060ed7a568c7d644df000924610386f1ca
undervine <undervine@naver.com>
2017-11-06 17:12:09 +0900

    Implement init() of JSONData

--- JSONParser/JSONParser/JSONData.swift
+++ JSONParser/JSONParser/JSONData.swift
@@ -15,17 +15,26 @@ struct JSONData {
         case Float
         case Double
     }
-    var string: [String]
-    var number: [Number]
-    var bool: [Bool]
+    var string: [String]?
+    var number: [Number]?
+    var bool: [Bool]?
 }
 
 extension JSONData {
     init?(parsedData: [Any]) {
-        return nil
+        for each in parsedData {
+            switch each {
+            case is Number: self.number?.append(each as! Number)
+            case is String: self.string?.append(each as! String)
+            case is Bool: self.bool?.append(each as! Bool)
+            default: break
+            }
+        }
     }
     
     static func parse(from rawData: String) -> [Any] {
         return []
     }
 }
+
+
