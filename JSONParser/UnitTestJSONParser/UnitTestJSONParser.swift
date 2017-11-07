ff1a9faa3dc3114d143b9d1afa2f2952e6ca60e8
yang hee jung <puuurm@naver.com>
2017-11-07 17:55:02 +0900

    Modify test case

--- JSONParser/UnitTestJSONParser/UnitTestJSONParser.swift
+++ JSONParser/UnitTestJSONParser/UnitTestJSONParser.swift
@@ -10,43 +10,56 @@ import XCTest
 @testable import JSONParser
 
 class UnitTestJSONParser: XCTestCase {
-    var nestedInObject: String!
-    var nestedInArray: String!
+    var nestedObjectInObject: String!
+    var nestedArrayInObject: String!
+    var nestedObjectInArray: String!
+    var nestedArrayInArray: String!
+    
     var nestedArrayMoreThanTwo: String!
-    var nestedOnlyObjectInArray: String!
+    var nestedObjectsInArray: String!
     var catchErrorSpellingTrue: String!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        // 객체 내부의 key: value 에서 value에 객체, 배열, 원시 타입이 존재하는 경우 && 띄어씌기 무시
-        nestedInObject = "{ \"name\" : \"KIM JUNG\", \"alias\" : {\"code squad\" : \"JK\", \"git hub\" : \"godrm\"}, \"level\" : 5, \"children\" : [\"hana\", \"hayul\", \"haun\"] }"
-        // 배열 내부에 객체, 배열 있는 경우
-        nestedInArray = "[ { \"name\" : \"master's course\", \"opened\" : true }, [ \"java\", \"javascript\", \"swift\" ] ]"
+        
+        nestedObjectInObject = "{ \"alias\" : {\"code squad\" : \"JK\", \"git hub\" : \"godrm\"} }"
+        nestedArrayInObject = "{ \"children\" : [\"hana\", \"hayul\", \"haun\"] }"
+
+        nestedObjectInArray = "[ { \"name\" : \"master's course\", \"opened\" : true } ]"
+        nestedArrayInArray = "[ [ \"java\", \"javascript\", \"swift\" ] ]"
+
         // 2차 이상 중첩인 경우
         nestedArrayMoreThanTwo = "[[false, true, {\"name\":\"heejung\"}]]"
-        // 배열 내부에 객체만 있는 경우
-        nestedOnlyObjectInArray = "[ { \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }, { \"name\" : \"YOON JISU\", \"alias\" : \"crong\", \"level\" : 4, \"married\" : true } ]"
+        
+        // 배열 내부에 객체 하나 이상 있는 경우
+        nestedObjectsInArray = "[ { \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }, { \"name\" : \"YOON JISU\", \"alias\" : \"crong\", \"level\" : 4, \"married\" : true } ]"
         catchErrorSpellingTrue = "[ {\"visit\":ture} ]"
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-        nestedInObject = nil
-        nestedInArray = nil
+        nestedObjectInObject = nil
+        nestedArrayInObject = nil
+        nestedObjectInArray = nil
+        nestedArrayInArray = nil
+        
         nestedArrayMoreThanTwo = nil
-        nestedOnlyObjectInArray = nil
+        nestedObjectsInArray = nil
         catchErrorSpellingTrue = nil
     }
     
     func testMakeObject() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedInObject))
-        XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedInArray))
+        XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedObjectInObject))
+        XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedArrayInObject))
+        XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedObjectInArray))
+        XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedArrayInArray))
+        
         XCTAssertThrowsError(try JSONAnalyzer.makeObject(with: nestedArrayMoreThanTwo))
-        XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedOnlyObjectInArray))
+        XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedObjectsInArray))
         XCTAssertThrowsError(try JSONAnalyzer.makeObject(with: catchErrorSpellingTrue))
     }
     
