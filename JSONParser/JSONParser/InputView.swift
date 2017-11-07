164da3c76714d6b155f87c0fb9185f27c90c9d82
undervine <undervine@naver.com>
2017-11-07 18:50:42 +0900

    Add askFor() func and isJSONPattern() func
    Add askFor() func that prints user menu and get user input.
    Add isJSONPattern() func that check if the user input conforms JSON pattern.

--- JSONParser/JSONParser/InputView.swift
+++ JSONParser/JSONParser/InputView.swift
@@ -10,4 +10,23 @@ import Foundation
 
 struct InputView {
     
+    private(set) static var jsonPattern = "\\[{1}(.+[^\\[\\]\\s,]),*\\]{1}?"
+    
+    // 사용자 입력 메뉴 출력.
+    static func askFor(message: String) throws -> String? {
+        // 요구 메시지 출력.
+        print("\(message)", terminator: " ")
+        // 요구 메시지가 nil 인 경우, isNil 에러 처리.
+        guard let inputLine = readLine(), try isJSONPattern(inputLine) else { return nil }
+        // q 또는 quit 입력 시 종료.
+        guard inputLine != "q" && inputLine != "quit" else { return nil }
+        return inputLine
+    }
+    
+    // 사용자 입력 문자열이 JSON 규격인지 검사.
+    private static func isJSONPattern(_ message: String) throws -> Bool {
+        guard try message.split(by: jsonPattern).count > 0 else { return false }
+        return true
+    }
+    
 }
