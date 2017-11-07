b5c80f1a11ec64a3ba2e616ea75ce216727ac2fc
yang hee jung <puuurm@naver.com>
2017-11-07 19:57:48 +0900

    Add Test Case

--- JSONParser/UnitTestJSONParser/UnitTestJSONParser.swift
+++ JSONParser/UnitTestJSONParser/UnitTestJSONParser.swift
@@ -12,8 +12,16 @@ import XCTest
 class UnitTestJSONParser: XCTestCase {
     var nestedObjectInObject: String!
     var nestedArrayInObject: String!
+    var formatErrorNestedArrayInObject: String!
+    
     var nestedObjectInArray: String!
     var nestedArrayInArray: String!
+    var nestedArrayAndObjectInArray: String!
+    var nestedArrayAndObjectInArrayNoComma: String!
+    
+    var primitiveTypeInArray: String!
+    var primitiveTypeInArrayNoComma: String!
+    var primitiveTypeInArraywithLastDataComma: String!
     
     var nestedArrayMoreThanTwo: String!
     var nestedObjectsInArray: String!
@@ -23,17 +31,39 @@ class UnitTestJSONParser: XCTestCase {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
+        // [Not Nil] 오브젝트 내부에 중첩 오브젝트
         nestedObjectInObject = "{ \"alias\" : {\"code squad\" : \"JK\", \"git hub\" : \"godrm\"} }"
+        // [Not Nil] 오브젝트 내부에 중첩 배열
         nestedArrayInObject = "{ \"children\" : [\"hana\", \"hayul\", \"haun\"] }"
 
+        // [throw Error] 오브젝트 내부 형식이 잘 못 됐을 때
+        formatErrorNestedArrayInObject = "{ \"children\" : {\"hana\", \"hayul\", \"haun\"} }"
+        
+        // [Not Nil] 배열 내부에 객체
         nestedObjectInArray = "[ { \"name\" : \"master's course\", \"opened\" : true } ]"
+        
+        // [Not Nil] 배열 내부에 배열
         nestedArrayInArray = "[ [ \"java\", \"javascript\", \"swift\" ] ]"
+        
+        // [Not Nil] 배열 내부에 객체와 배열
+        nestedArrayAndObjectInArray = "[ [20, \"hee jung\"], { \"code squad\" : \"level 2\" }]"
+        
+        // [throw Error] 배열 내부 객체와 배열 사이에 쉼표가 없을 때
+        nestedArrayAndObjectInArrayNoComma = "[ [20, \"hee jung\"]  { \"code squad\" : \"level 2\" }]"
 
-        // 2차 이상 중첩인 경우
+        // [Not Nil] 기본 배열에 원시 타입 있는 경우
+        primitiveTypeInArray = "[ 4, false, \"hello world\" ]"
+        // [throw Error] 기본 배열 원시 타입 사이에 쉼표가 없는 경우
+        primitiveTypeInArrayNoComma = "[ 4 false, \"hello world\" ]"
+        // [throw Error] 기본 배열 원시 타입들의 가장 마지막에 쉼표가 있는 경우
+        primitiveTypeInArraywithLastDataComma = "[ 4, false, \"hello world\", ]"
+        
+        // [throw Error] 2차 이상 중첩인 경우
         nestedArrayMoreThanTwo = "[[false, true, {\"name\":\"heejung\"}]]"
         
-        // 배열 내부에 객체 하나 이상 있는 경우
+        // [Not Nil] 배열 내부에 객체 하나 이상 있는 경우
         nestedObjectsInArray = "[ { \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }, { \"name\" : \"YOON JISU\", \"alias\" : \"crong\", \"level\" : 4, \"married\" : true } ]"
+        // [throw Error] 스팰링 에러
         catchErrorSpellingTrue = "[ {\"visit\":ture} ]"
     }
     
@@ -42,8 +72,17 @@ class UnitTestJSONParser: XCTestCase {
         super.tearDown()
         nestedObjectInObject = nil
         nestedArrayInObject = nil
+        
+        formatErrorNestedArrayInObject = nil
+        
         nestedObjectInArray = nil
         nestedArrayInArray = nil
+        nestedArrayAndObjectInArray = nil
+        nestedArrayAndObjectInArrayNoComma = nil
+        
+        primitiveTypeInArray = nil
+        primitiveTypeInArrayNoComma = nil
+        primitiveTypeInArraywithLastDataComma = nil
         
         nestedArrayMoreThanTwo = nil
         nestedObjectsInArray = nil
@@ -55,8 +94,17 @@ class UnitTestJSONParser: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedObjectInObject))
         XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedArrayInObject))
+        
+        XCTAssertThrowsError(try JSONAnalyzer.makeObject(with: formatErrorNestedArrayInObject))
+        
         XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedObjectInArray))
         XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedArrayInArray))
+        XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedArrayAndObjectInArray))
+        XCTAssertThrowsError(try JSONAnalyzer.makeObject(with: nestedArrayAndObjectInArrayNoComma))
+        
+        XCTAssertNotNil(try JSONAnalyzer.makeObject(with: primitiveTypeInArray))
+        XCTAssertThrowsError(try JSONAnalyzer.makeObject(with: primitiveTypeInArrayNoComma))
+        XCTAssertThrowsError(try JSONAnalyzer.makeObject(with: primitiveTypeInArraywithLastDataComma))
         
         XCTAssertThrowsError(try JSONAnalyzer.makeObject(with: nestedArrayMoreThanTwo))
         XCTAssertNotNil(try JSONAnalyzer.makeObject(with: nestedObjectsInArray))
