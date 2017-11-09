f7d3e9e619950b742ae20d23d9fd5bd03658b8c0
undervine <undervine@naver.com>
2017-11-09 16:42:13 +0900

    Add DataType enum and type property that stores DataType
    DataType enum indicates whether type of data is Array or Object.

--- JSONParser/JSONParser/JSONData.swift
+++ JSONParser/JSONParser/JSONData.swift
@@ -10,10 +10,29 @@ import Foundation
 
 struct JSONData {
     typealias Number = Int
-    var string: [String]
-    var number: [Number]
-    var bool: [Bool]
+    enum DataType {
+        case array
+        case object
+    }
+    private(set) var type: DataType
+    private(set) var string: [String:String] = [:]
+    private(set) var number: [String:Number] = [:]
+    private(set) var bool: [String:Bool] = [:]
     var count: Int {
         return self.number.count + self.string.count + self.bool.count
     }
+    
+    init(_ data: [String:Any], ofType type: DataType) {
+        self.type = type
+        for (key, value) in data {
+            switch value {
+            case is String: self.string.updateValue(value as! String, forKey: key)
+            case is Number: self.number.updateValue(value as! Number, forKey: key)
+            case is Bool: self.bool.updateValue(value as! Bool, forKey: key)
+            default: return
+            }
+        }
+    }
+    
 }
+
