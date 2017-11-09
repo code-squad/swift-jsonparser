69d4cc2dca463133c1941130644d0749857278ee
undervine <undervine@naver.com>
2017-11-09 18:25:37 +0900

    Modify getOverallReport() func
    Make getOverallReport() to print the number of objects and the number of arrays separately.

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -11,16 +11,33 @@ import Foundation
 struct OutputView {
     
     static func printDataReport(of jsonDatas: [JSONData]) {
-        print(jsonDatas.count)
-        let report = jsonDatas.count == 1 ? getSpecificReport(of: jsonDatas[0]) : getOverAllReport(of: jsonDatas)
+        // 데이터 blob이 1개밖에 없으면 세부 레포트를 하고, 2개 이상이면 전체 레포트를 한다.
+        let report = jsonDatas.count == 1 ? getSpecificReport(of: jsonDatas[0]) : getOverallReport(of: jsonDatas)
         print(report)
     }
     
-    private static func getOverAllReport(of jsonDatas: [JSONData]) -> String {
-        let message = "총 \(jsonDatas.count)개의 배열 데이터 중에 객체 \(jsonDatas.count)개가 포함되어 있습니다."
+    // 전체 레포트
+    private static func getOverallReport(of jsonDatas: [JSONData]) -> String {
+        var numberOfObjects = 0
+        var numberOfArrays = 0
+        var message = "총 \(jsonDatas.count)개의 배열 데이터 중에 "
+        for jsonData in jsonDatas {
+            switch jsonData.type {
+            case .object: numberOfObjects += 1
+            case .array: numberOfArrays += 1
+            }
+        }
+        if numberOfObjects > 0 {
+            message += "객체 \(numberOfObjects)개와 "
+        }
+        if numberOfArrays > 0 {
+            message += "배열 \(numberOfArrays)개"
+        }
+        message += "가 포함되어 있습니다."
         return message
     }
     
+    // 세부 레포트
     private static func getSpecificReport(of data: JSONData) -> String {
         var result = "총 \(data.count)개의 데이터 중에"
         if data.string.count > 0 {
