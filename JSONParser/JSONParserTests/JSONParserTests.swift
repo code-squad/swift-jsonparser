e26886f0a01cb713c66a322f82178fdc12481752
undervine <undervine@naver.com>
2017-11-09 18:53:24 +0900

    Add unit tests

--- JSONParser/JSONParserTests/JSONParserTests.swift
+++ JSONParser/JSONParserTests/JSONParserTests.swift
@@ -11,46 +11,71 @@ import XCTest
 
 class JSONParserTests: XCTestCase {
     
-    func testParsingJSONData() {
-        do {
-            let jsonString: String = "[ 10, \"jk\", 4, \"314\", 99, \"crong\", false ]"
-            try JSONParser.parse(jsonString)
-        }catch {
-            XCTAssertNoThrow(error, error.localizedDescription)
-        }
+    func testParsingJSONData() throws {
+        let jsonString: String = "{\"name\":\"KIM JUNG\",\"alias\":\"JK\",\"level\":5,\"married\":true}"
+        XCTAssertNotNil(try JSONParser.parse(jsonString))
     }
     
     func testStringDataIsNotNil() {
-        let parsedData = JSONData(["name":"KIM JUNG"])
+        let parsedData = JSONData(["name":"KIM JUNG"], ofType: .array)
         XCTAssertNotNil(parsedData)
     }
     
     func testNumberDataIsNotNil() {
-        let parsedData = JSONData(["level" :5])
+        let parsedData = JSONData(["level" :5], ofType: .array)
         XCTAssertNotNil(parsedData)
     }
     
     func testBoolDataIsNotNil() {
-        let parsedData = JSONData(["married": true])
+        let parsedData = JSONData(["married": true], ofType: .array)
         XCTAssertNotNil(parsedData)
     }
     
-    func testCountOfNumberIsRight() {
-        let jsonString: String = "[ 10, \"jk\", 4, \"314\", 99, \"crong\", false ]"
+    func testCountOfNumberIsRight() throws {
+        let jsonString: String = "{\"name\":\"KIM JUNG\",\"alias\":\"JK\",\"level\":5,\"married\":true}"
         let parsedData = try JSONParser.parse(jsonString)
-        XCTAssertEqual(parsedData[0].number.count, 3)
-    }
-    
-    func testCountOfStringIsRight() {
-        let jsonString: String = "[ 10, \"jk\", 4, \"314\", 99, \"crong\", false ]"
-        let parsedData = try JSONParser.parse(jsonString)
-        XCTAssertEqual(parsedData[0].string.count, 3)
-    }
-    
-    func testCountOfBoolIsRight() {
-        let jsonString: String = "[ 10, \"jk\", 4, \"314\", 99, \"crong\", false ]"
-        let parsedData = try JSONParser.parse(jsonString)
+        XCTAssertEqual(parsedData[0].number.count, 1)
+        XCTAssertEqual(parsedData[0].string.count, 2)
         XCTAssertEqual(parsedData[0].bool.count, 1)
+    }
+    
+    func testCountOfAnArrayIsRight() throws {
+        let jsonString: String = "{\"name\":\"KIMJUNG\",\"alias\":\"JK\",\"level\":5,\"married\":true}"
+        let parsedData = try JSONParser.parse(jsonString)
+        XCTAssertEqual(parsedData.count, 1)
+    }
+    
+    func testCountOfAnObjectIsRight() throws {
+        let jsonString: String = "[\"asdf\":\"asdf\",\"hrtdg\":234,\"ertgf\":true]"
+        let parsedData = try JSONParser.parse(jsonString)
+        XCTAssertEqual(parsedData.count, 1)
+    }
+    
+    func testCountOfArraysIsRight() throws {
+        let jsonString: String = "{\"asdf\":\"asdf\",\"hrtdg\":234},{\"ertgf\":true}"
+        let parsedData = try JSONParser.parse(jsonString)
+        XCTAssertEqual(parsedData.count, 2)
+    }
+    
+    func testCountOfObjectsIsRight() throws {
+        let jsonString: String = "[\"asdf\":\"asdf\",\"hrtdg\":234],[\"ertgf\":true]],[\"ertgf\":23456]"
+        let parsedData = try JSONParser.parse(jsonString)
+        XCTAssertEqual(parsedData.count, 3)
+    }
+    
+    func testCountOfObjectsAndArraysIsRight() throws {
+        let jsonString: String = "{\"name\":\"KIMJUNG\",\"alias\":\"JK\",\"level\":5,\"married\":true},[\"name\":\"YOONJISU\",\"alias\":\"crong\",\"level\":4,\"married\":true],{\"asdf\":\"asdf\",\"hrtdg\":234},{\"ertgf\":true},[\"asdf\":\"asdf\",\"hrtdg\":234,\"ertgf\":true]"
+        let parsedData = try JSONParser.parse(jsonString)
+        var arrayCount = 0
+        var objectCount = 0
+        for data in parsedData {
+            switch data.type {
+            case .array: arrayCount += 1
+            case .object: objectCount += 1
+            }
+        }
+        XCTAssertEqual(arrayCount, 3)
+        XCTAssertEqual(objectCount, 2)
     }
     
 }
