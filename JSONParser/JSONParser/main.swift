a1435764be5dd0ccb553d8d6ec0b440db1148f35
yang hee jung <puuurm@naver.com>
2017-11-13 11:26:58 +0900

    Create readIO() and loadJSON() on InputView
    Modify the InputView so that it can pass argument values while running in the terminal.

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -9,20 +9,21 @@
 import Foundation
 
 // 단순한 JSON 배열(array) 문자열을 입력하면 내부 요소들을 분석해서 배열로 저장하도록 구현한다.
-while(true) {
-    // 입력 받기
-    guard let jsonString = InputView.read() else { break }
-    do {
-        // json string 을 json object 로 변환
-        let analyzedArray = try JSONAnalyzer.makeObject(with: jsonString)
-        
-        // 분석한 결과 출력
-        OutputView.printJSON(analyzedArray)
-        
-    } catch GrammarChecker.FormatError.notFormatted{
-        print(GrammarChecker.FormatError.notFormatted.rawValue)
-    } catch GrammarChecker.FormatError.invalidDataType{
-        print(GrammarChecker.FormatError.invalidDataType.rawValue)
-    }
+// 입력 받기
+let FileNames = InputView.readIO()
+do {
+    // file을 읽고 json String을 반환
+    let jsonString = try InputView.loadJSON(FileNames.inputFileName)
+    // json string 을 json object 로 변환
+    let analyzedArray = try JSONAnalyzer.makeObject(with: jsonString)
+    
+    // 분석한 결과 출력
+    OutputView.printJSON(analyzedArray)
+    
+} catch InputView.InputError.notFindFile {
+    print(InputView.InputError.notFindFile.rawValue)
+} catch GrammarChecker.FormatError.notFormatted{
+    print(GrammarChecker.FormatError.notFormatted.rawValue)
+} catch GrammarChecker.FormatError.invalidDataType{
+    print(GrammarChecker.FormatError.invalidDataType.rawValue)
 }
-
