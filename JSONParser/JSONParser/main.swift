0609687f1ebb1f7a3e1927043613fe206becce8d
DongJin Lee <comets195@gmail.com>
2017-11-13 15:20:59 +0900

    Add main, Edit OutputView.

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -8,3 +8,19 @@
 
 import Foundation
 
+func executeJSONParser() {
+    let inputView = InputView()
+    let outputView = OutputView()
+    let parser = JSONParser()
+    var repeatCondition = true
+    
+    while repeatCondition {
+        print("분석할 JSON 데이터를 입력하세요.")
+        if let rawJSONData = inputView.read() {
+            outputView.printJSONAnalysis(jsonData: parser.makeJSONData(rawJSONData))
+            repeatCondition = false
+        }
+    }
+}
+
+executeJSONParser()
