675a93097eff93d76ec91047b87a25e709222f63
DongJin Lee <comets195@gmail.com>
2017-11-13 14:46:51 +0900

    Add JSONData.

--- JSONParser/JSONParser/JSONData.swift
+++ JSONParser/JSONParser/JSONData.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+struct JSONData {
+    private var jsonData: [Any]
+    private (set) var boolTypeCount: Int
+    private (set) var stringTypeCount: Int
+    private (set) var intTypeCount: Int
+    
+    init() {
+        self.jsonData = [Any]()
+        self.boolTypeCount = 0
+        self.stringTypeCount = 0
+        self.intTypeCount = 0
+    }
+    
+    init(_ value: [Any]) {
+        self.init()
+        self.jsonData = value
+        countJSONData()
+    }
+    
+    private mutating func countJSONData() {
+        for indexOfJSONData in jsonData {
+            if indexOfJSONData is Int {
+                intTypeCount += 1
+            } else if indexOfJSONData is String {
+                stringTypeCount += 1
+            } else {
+                boolTypeCount += 1
+            }
+        }
+    }
+}
