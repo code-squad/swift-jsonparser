397d70301f3fa895a589febb6d49be02d5b009ff
DongJin Lee <comets195@gmail.com>
2017-11-13 14:46:29 +0900

    Add OutputView.

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+struct OutputView {
+    func printJSONAnalysis(jsonData: JSONData) {
+        let numberOfData = jsonData.boolTypeCount +
+                            jsonData.intTypeCount +
+                            jsonData.stringTypeCount
+        print("총 \(numberOfData)개의 데이터 중에", terminator: "")
+        
+        for _ in 0..<numberOfData {
+            if jsonData.stringTypeCount > 0 {
+                print(" 문자열 \(jsonData.stringTypeCount)")
+            } else if jsonData.intTypeCount > 0 {
+                print(" 숫자 \(jsonData.intTypeCount)")
+            } else if jsonData.boolTypeCount > 0 {
+                print(" 부울 \(jsonData.boolTypeCount)")
+            }
+            if numberOfData > 0 { print(",") }
+        }
+        print("가 포함되어 있습니다.")
+    }
+}
