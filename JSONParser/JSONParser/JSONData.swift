3c8de8b5d7a82b00af2d0303c286ef0b9d9d92c4
DongJin Lee <comets195@gmail.com>
2017-11-13 14:32:41 +0900

    Add JSON Parser.

---
+++ JSONParser/JSONParser/JSONData.swift
@@ -0,0 +1,9 @@
+//
+//  JSONData.swift
+//  JSONParser
+//
+//  Created by Mrlee on 2017. 11. 13..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import Foundation
---
+++ JSONParser/JSONParser/JSONParser.swift
@@ -0,0 +1,49 @@
+//
+//  JSONParser.swift
+//  JSONParser
+//
+//  Created by Mrlee on 2017. 11. 13..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct JSONParser {
+    func makeJSONData(_ value: String) -> JSONData{
+        var rawJSON = value
+        rawJSON.removeFirst()
+        rawJSON.removeLast()
+        let separateJSON = rawJSON.components(separatedBy: ",")
+        let removedWhiteSpaceJSON = separateJSON.map {
+            (value: String) -> String in
+            return value.trimmingCharacters(in: .whitespacesAndNewlines)
+        }
+        let jsonData = JSONData(sortJSONType(removedWhiteSpaceJSON))
+        return jsonData
+    }
+    
+    private func sortJSONType(_ value: [String]) -> [Any] {
+        var jsonData = [Any]()
+        for sortedJSON in value {
+            jsonData.append(sortJSONData(sortedJSON))
+        }
+        return jsonData
+    }
+    
+    private func sortJSONData(_ value: String) -> Any {
+        if let boolType = Bool(value) {
+            return boolType
+        } else if let intType = Int(value) {
+            return intType
+        }
+        return hasStringType(value)
+    }
+    
+    private func hasStringType(_ value: String) -> String{
+        var pureString = value
+        pureString.removeFirst()
+        pureString.removeLast()
+        return pureString
+    }
+    
+}
