f1b0fdd0290e0b7c3d81a56759247b854ad1987e
DongJin Lee <comets195@gmail.com>
2017-11-14 12:27:18 +0900

    Add UnitTest Case4 for Error Throw Msg.

--- JSONParser/UnitTestJSONParser/UnitTestJSONParser.swift
+++ JSONParser/UnitTestJSONParser/UnitTestJSONParser.swift
@@ -10,10 +10,10 @@ import XCTest
 @testable import JSONParser
 
 class UnitTestJSONParser: XCTestCase {
-    let parserTest = JSONParser()
-
+    
     override func setUp() {
         super.setUp()
+
     }
     
     override func tearDown() {
@@ -21,12 +21,14 @@ class UnitTestJSONParser: XCTestCase {
     }
     
     func testJSONParserCASE1() {
+        let parserTest = JSONParser()
         let testRawJSONData = "[ 10, 21, 4, 314, 99, 0, 72 ]"
         XCTAssertEqual(parserTest.makeJSONData(testRawJSONData).intTypeCount, 7)
         XCTAssertEqual(parserTest.makeJSONData(testRawJSONData).sumOfData, 7)
     }
     
     func testJSONParserCASE2() {
+        let parserTest = JSONParser()
         let testRawJSONData = "[ 10, \"jk\", 4, \"314\", 99, \"crong\", false ]"
         XCTAssertEqual(parserTest.makeJSONData(testRawJSONData).stringTypeCount, 3)
         XCTAssertEqual(parserTest.makeJSONData(testRawJSONData).intTypeCount, 3)
@@ -35,10 +37,17 @@ class UnitTestJSONParser: XCTestCase {
     }
     
     func testJSONParserCASE3() {
+        let parserTest = JSONParser()
         let testRawJSONData = "[ 10, \"jk\", 4, \"314\", 99, \"crong\", false, true, \"lee\", 1024]"
         XCTAssertEqual(parserTest.makeJSONData(testRawJSONData).stringTypeCount, 4)
         XCTAssertEqual(parserTest.makeJSONData(testRawJSONData).intTypeCount, 4)
         XCTAssertEqual(parserTest.makeJSONData(testRawJSONData).boolTypeCount, 2)
         XCTAssertEqual(parserTest.makeJSONData(testRawJSONData).sumOfData, 10)
     }
+    
+    func testJSONParserCASE4() {
+        let testRawJSONData = " 10, 21, 4, 314, 99, 0, 72 ]"
+        XCTAssertThrowsError(try JSONParser.check(testRawJSONData))
+    }
+    
 }
