c1c37b473c8400edf21fce45adc0c6ebe3324076
undervine <undervine@naver.com>
2017-11-16 17:57:36 +0900

    Separate regular pattern and add comments

--- JSONParser/JSONParser/GrammarChecker.swift
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -15,25 +15,35 @@ struct GrammarChecker {
     }
     
     // 중첩 불가 정규식.
-    static let jsonObjectPattern = "(^\\{(\\s*\"[^,:\\\"\\s]+?\"\\s*:\\s*(\"[^\\\"]+?\"|true|false|\\d+)\\s*,\\s*)*(\\s*\"[^,:\\\"\\s]+?\"\\s*:\\s*(\"[^\\\"]+?\"|true|false|\\d+)\\s*,?\\s*)\\})"
-    static let objectValues = "\\s*(\"[^\\\"\\{\\[\\]\\}]+?\"|true|false|\\d+|\\[(\\s*(\"[^\\\"\\{\\[\\]\\}]+?\"|true|false|\\d+)\\s*,?\\s*)+\\]|\\{" + GrammarChecker.jsonObjectPattern + "+\\})\\s*,"
-    static let jsonArrayPattern = "(^\\[(" + GrammarChecker.objectValues + "\\s*)*(" + GrammarChecker.objectValues + "?\\s*)\\])"
-    static let jsonPattern = GrammarChecker.jsonObjectPattern + "|" + GrammarChecker.jsonArrayPattern
+    // 키 패턴. (문자열)
+    private static let keyPattern = "\"[^,:\\\"\\s]+?\""
+    // 기본형 값 패턴. 문자열|부울|숫자)
+    private static let basicValuePattern = "(\"[^\\\"\\{\\[\\]\\}]+?\"|true|false|\\d+)"
+    // 콤마필수 객체형 패턴.
+    private static let objectValueWithComma = "(\\s*" + GrammarChecker.keyPattern + "\\s*:\\s*" + GrammarChecker.basicValuePattern + "\\s*,"
+    // 콤마선택 객체형 패턴. (중간 데이터는 콤마 필수, 마지막 데이터는 선택)
+    private static let objectValues = "(\\{" + GrammarChecker.objectValueWithComma + "\\s*)*" + GrammarChecker.objectValueWithComma + "?\\s*)\\})"
+    // 중첩 객체형 패턴.
+    private static let nestedObjectPattern = "\\{" + GrammarChecker.objectValues + "+\\}"
+    // 콤마필수 배열형 패턴.
+    private static let arrayValueWithComma = "(\\s*" + GrammarChecker.basicValuePattern + "\\s*,"
+    // 콤마선택 배열형 패턴. (중간 데이터는 콤마 필수, 마지막 데이터는 선택)
+    private static let arrayValues = "(\\[" + GrammarChecker.arrayValueWithComma + "\\s*)*" + GrammarChecker.arrayValueWithComma + "?\\s*)\\])"
+    // 중첩 배열형 패턴.
+    static let nestedArrayPattern = "\\[" + GrammarChecker.arrayValues + "+\\]"
+    // 배열 내 삽입가능 값 패턴.
+    static let arrayValuePattern = GrammarChecker.basicValuePattern+"|"+GrammarChecker.arrayValues+"|"+GrammarChecker.objectValues
+    // 콤마필수 종합객체형 패턴. (중첩 객체값 가능.)
+    private static let jsonObjectWithComma = "\\s*(" + GrammarChecker.basicValuePattern + "|" + GrammarChecker.nestedArrayPattern + "|" + GrammarChecker.nestedObjectPattern + ")\\s*,"
+    // 콤마선택 종합객체형 패턴. (중간 데이터는 콤마 필수, 마지막 데이터는 선택)
+    private static let jsonObjectPattern = "(" + GrammarChecker.jsonObjectWithComma + "\\s*)*(" + GrammarChecker.jsonObjectWithComma + "?\\s*)"
+    // 종합배열형 패턴.
+    private static let jsonArrayPattern = "(^\\[" + GrammarChecker.jsonObjectPattern + "\\])"
+    // 전체 json 규격.
+    static let jsonPattern = GrammarChecker.nestedObjectPattern + "|" + GrammarChecker.jsonArrayPattern
     // 가장 바깥쪽 괄호만 제거한 패턴.
-    static let JSONArrayPattern = "((?<=\\[)(\\s*(\"[^\\\"\\{\\[\\]\\}]+?\"|true|false|\\d+|\\[(\\s*(\"[^\\\"\\{\\[\\]\\}]+?\"|true|false|\\d+)\\s*,?\\s*)+\\]|\\{(\\s*\"[^,:\\\"]+?\"\\s*:\\s*(\"[^\\\"]+?\"|true|false|\\d+)\\s*,?\\s*)+\\})\\s*,?\\s*)+(?=\\]))"
-    // Object 데이터 패턴. - {"key": value, "key": value, ...}
-    static let JSONObjectPattern  = "(?<=\\{)\\s?(\".+?\"\\s?\\:\\s?.+?)\\s?(?=\\})"
-    // 재귀패턴으로 만들었으나, 안 먹히는 듯함.
-    //static let jsonObjectPattern = "(\\{?\\s*\"[^,:\"\\\\s\\[\\]]+?\"\\s*:\\s*((\"[^\\\"]+?\"|true|false|\\d+|(\\[\\s*(?2)+?\\s*\\])|(?1)+?)\\s*,?)\\s*\\}?\\s*,?)"
-    //static let jsonArrayPattern = "(\\[\\s*((\"[^\"\\]+?\"|true|false|\\d+)\\s*,?\\s*)*?\\s*\\])"
-    // object의 key 패턴.
-    static let objectKeyPattern = "((\"[^\"\\,]+?\")|true|false|\\d+)\\s*(?=:)"
-    // object의 value 패턴. (딕셔너리 값. ':' 기준으로 나뉨)
-    static let objectValuePattern = "(((?<=\\:)(\"[^\"\\,]+?\"))|true|false|\\d+)|\\[((\"[^\"\\,]+?\")|true|false|\\d+).+?\\]"
-    // array의 value 패턴. (내부 [], {}을 1뎁스까지 매칭 가능.)
-    static let arrayValuePattern = "((\"[^\"\\,]+?\")|true|false|\\d+|\\[(\\s*((\"[^\"\\,]+?\")|true|false|\\d+)+?\\s*,?\\s*)+?\\]|\\{(\\s*(\"[^\\\"\\s]+?\"\\s*:\\s*((\"[^\"\\,]+?\")|true|false|\\d+)+?)\\s*,?\\s*)+?\\})"
-    static let innerArrayValuePattern = "(\"[^\"\\]+?\"|true|false|\\d+)"
-    static let innerObjectValuePattern = "(?<=:)\\s*(\"[^\"\\]+?\"|true|false|\\d+)+?"
+    static let arrayDataWithoutBracket = "(?<=\\[).+(?=\\])"
+    static let objectDataWithoutBracket  = "(?<=\\{).+(?=\\})"
     
     // 사용자 입력 문자열이 JSON 규격인지 검사.
     static func isJSONPattern(_ message: String) throws -> Bool {
--- JSONParser/JSONParser/JSONParser.swift
+++ JSONParser/JSONParser/JSONParser.swift
@@ -9,94 +9,154 @@
 import Foundation
 
 struct JSONParser {
-    enum JsonError: String, Error {
-        case invalidPattern = "지원하지 않는 형식을 포함하고 있습니다."
-    }
     
     // 문자열을 JSONData로 파싱하여 배열로 반환.
-    static func parse(_ rawData: String) throws -> [JSONData] {
-        // 배열 데이터와 객체 데이터로 나눔.
-        let objects = try extractObjectBlobs(from: rawData)
-        let arrays = try extractArrayBlobs(from: rawData)
-        // 배열 blob, 객체 blob에 따라 각각 JSONData 객체들을 만들어 배열로 받음.
-        let jsonObjects = try generateJSONData(from: objects, ofType: JSONData.DataType.object)
-        let jsonArrays = try generateJSONData(from: arrays, ofType: JSONData.DataType.array)
+    static func parse(_ rawData: String) throws -> JSONData {
+        var rawDataWithoutBracket: [String] = []
+        var jsonData = JSONData()
+        // 각 데이터 추출한 문자열 데이터. (object, array는 하나로 묶음. 1뎁스까지만 추출.)
+        if try isObjectType(rawData) {
+            rawDataWithoutBracket = try rawData.splitPattern(by: GrammarChecker.JSONObjectPattern)
+            // JSONData 객체 생성. (Object Type, Array Type 객체로 분리.)
+            jsonData = try generateJSONData(from: rawDataWithoutBracket, ofType: JSONData.DataType.object)
+        }else {
+            rawDataWithoutBracket = try rawData.splitPattern(by: GrammarChecker.JSONArrayPattern)
+            jsonData = try generateJSONData(from: rawDataWithoutBracket, ofType: JSONData.DataType.array)
+        }
         // 모든 JSONData 배열 반환.
-        return jsonObjects+jsonArrays
+        return jsonData
     }
     
-    private static let JSONObjectPattern = "\\[\\s?(\".+?\"\\s?\\:\\s?.+?)\\s?\\]"
-    private static let JSONArrayPattern  = "\\{\\s?(\".+?\"\\s?\\:\\s?.+?)\\s?\\}"
+    private static func isObjectType(_ rawData: String) throws -> Bool {
+        guard rawData.first == "{" else { return false }
+        return true
+    }
     
     // blob 단위 JSONData 배열 반환.
-    private static func generateJSONData(from blobs: [String], ofType type: JSONData.DataType) throws -> [JSONData] {
-        var jsonDataCollection: [JSONData] = []
-        // blob = [], {} 들의 배열
-        let blob = blobs.map { $0.trimmingCharacters(in: [","]) }
-        // data = [], {} 각각의 내부 데이터
-        for data in blob {
-            // datum = 내부 데이터들의 배열
-            let datum = data.split(separator: ",").map { return String($0) }
-            jsonDataCollection.append(JSONData(convertToDictionary(from: datum), ofType: type))
+    private static func generateJSONData(from blobs: [String], ofType type: JSONData.DataType) throws -> JSONData {
+        var jsonDataCollection = JSONData()
+        // blobs = array 또는 object 문자열 배열들.
+        // blob = 각 데이터들의 문자열 배열. 콤마 기준으로 나눔.
+        for blob in blobs {
+            // 타입에 따라 딕셔너리 형태로 추출할지, 배열 형태로 추출할지 결정.
+            switch type {
+            case .object: // 딕셔너리로 변환한 후, JSONData 객체 생성하여 추가.
+                let objectData = try convertToDictionary(from: blob)
+                jsonDataCollection.setData(objectData)
+            case .array:
+                let arrayData = try convertToArray(from: blob)
+                jsonDataCollection.setData(arrayData)
+            }
         }
         return jsonDataCollection
     }
     
     // 각 blob 내부 데이터들로 딕셔너리 생성하여 반환.
-    private static func convertToDictionary(from datum: [String]) -> [String:Any] {
-        var result: [String:Any] = [:]
-        // element = 각 내부 데이터
-        for element in datum {
-            let key = generateKey(from: element)
-            guard let value = generateValue(from: element) else { break }
-            result.updateValue(value, forKey: key)
+    private static func convertToDictionary(from data: String) throws -> [String:Any] {
+        var dictionaries: [String:Any] = [:]
+        let keys = try generateKey(from: data)
+        let values = try generateValue(from: data, type: .object)
+        for i in 0..<keys.count {
+            dictionaries.updateValue(values[i], forKey: keys[i])
         }
-        return result
+        return dictionaries
     }
     
-    // 문자열 딕셔너리에서 key 값 추출.
-    private static func generateKey(from data: String) -> String {
-        let keyRange = data.startIndex..<data.index(of: ":")!
-        let trimmedKey = String(data[keyRange]).trimmingCharacters(in: .whitespaces).trimmingCharacters(in: ["\""])
-        return trimmedKey
+    // 각 blob 내부 데이터들로 배열 생성하여 반환.
+    private static func convertToArray(from data: String) throws -> [Any] {
+        var arrays: [Any] = []
+        arrays = try generateValue(from: data, type: .array)
+        return arrays
     }
     
-    // 문자열 딕셔너리에서 value 값 추출 및 실제 value 타입으로 캐스팅.
-    private static func generateValue(from data: String) -> Any? {
-        let indexAfterColon = data.index(after: data.index(of: ":")!)
-        let valueRange = indexAfterColon..<data.endIndex
-        let trimmedValue = String(data[valueRange]).trimmingCharacters(in: .whitespaces)
-        if trimmedValue.contains("\"") {
-            return trimmedValue.trimmingCharacters(in: ["\""])
-        }else if let numberElement = JSONData.Number(trimmedValue) {
-            return numberElement
-        }else if let boolElement = Bool(trimmedValue) {
-            return boolElement
-        }else {
-            return nil
+    // 문자열에서 key 값 추출.
+    private static func generateKey(from data: String) throws -> [String] {
+        var keys: [String] = []
+        // 키 패턴만 추출. 따옴표 제거.
+        let elements = data.split(separator: ",").map { String($0) }
+        for element in elements {
+            let keyRange = element.startIndex...element.index(before: element.index(of: ":")!)
+            let key = element[keyRange].trimmingCharacters(in: [" ", "\""])
+            keys.append(key)
         }
+        return keys
     }
     
-    // 문자열에서 객체 패턴([..])인 문자열 배열 반환.
-    private static func extractObjectBlobs(from rawData: String) throws -> [String] {
-        // 객체 패턴으로 문자열데이터 자름.
-        let objectBlobs = try rawData.splitPattern(by: JSONObjectPattern)
-        let objectBlobsWithoutSquareBracket = objectBlobs.map { return removeBrackets(of: $0) }
-        return objectBlobsWithoutSquareBracket
+    // 문자열에서 value 값 추출 및 실제 value 타입으로 캐스팅.
+    private static func generateValue(from data: String, type: JSONData.DataType) throws -> [Any] {
+        // 값 패턴만 추출.
+        var stringValues: [String] = []
+        switch type {
+        case .object:
+            let elements = data.split(separator: ",").map { String($0) }
+            for element in elements {
+                let valueRange = element.index(after: element.index(of: ":")!)..<element.endIndex
+                let value = String(element[valueRange])
+                stringValues.append(value)
+            }
+        case .array:
+            stringValues = try data.splitPattern(by: GrammarChecker.arrayValuePattern)
+        }
+        // 문자열 값을 실제 값으로 변환.
+        let realValues = try makeRealData(from: stringValues)
+        return realValues
     }
     
-    // 문자열에서 배열 패턴({..})인 문자열 배열 반환.
-    private static func extractArrayBlobs(from rawData: String) throws -> [String] {
-        // 배열 패턴으로 문자열데이터 자름.
-        let arrayBlobs = try rawData.splitPattern(by: JSONArrayPattern)
-        let arrayBlobsWithoutParenthesis = arrayBlobs.map { return removeBrackets(of: $0) }
-        return arrayBlobsWithoutParenthesis
+    // 문자열 데이터를 실제 값 데이터로 변환.
+    private static func makeRealData(from stringValues: [String]) throws -> [Any] {
+        var resultValues: [Any] = []
+        for value in stringValues {
+            if value.contains("[") {    // 객체 형태의 데이터인 경우.
+                let convertedArray = try convertStringToAnyArray(from: value, ofType: .array)
+                resultValues.append(convertedArray)
+            }else if value.contains("{") {
+                let convertedObject = try convertStringToAnyArray(from: value, ofType: .object)
+                resultValues.append(convertedObject)
+            }else {                     // 기본 데이터인 경우.
+                guard let convertedValue = try convertStringToAny(from: value) else { break }
+                resultValues.append(convertedValue)
+            }
+        }
+        return resultValues
     }
     
-    // 괄호 제거({} 또는 []) - 데이터 자체에 괄호가 포함돼 있을 수 있으므로 양끝만 제거.
-    private static func removeBrackets(of eachData: String) -> String {
-        let rangeWithoutParenthesis = eachData.index(after: eachData.startIndex)..<eachData.index(before: eachData.endIndex)
-        return String(eachData[rangeWithoutParenthesis])
+    // 객체 형태 문자열 데이터를 실제 객체로 변환.
+    private static func convertStringToAnyArray(from data: String, ofType type: JSONData.DataType) throws -> [Any] {
+        var arrayValue: [Any] = []
+        // data = 내부배열. elements = 내부배열 내의 각 문자열 데이터.(정규표현식을 쓰려고 했으나 오류남)
+        var elements: [String] = []
+        switch type {
+        case .array:
+            elements = data.trimmingCharacters(in: ["[","]"]).split(separator: ",").map { $0.trimmingCharacters(in: .whitespaces) }
+            for element in elements {
+                // 내부배열 내의 각 문자열 데이터를 실제 데이터로 변환.
+                guard let convertedValue = try convertStringToAny(from: element) else { break }
+                arrayValue.append(convertedValue)
+            }
+        case .object:
+            var convertedValue: [String:Any] = [:]
+            elements = data.trimmingCharacters(in: ["{","}"]).split(separator: ",").map { $0.trimmingCharacters(in: .whitespaces) }
+            for element in elements {
+                convertedValue = try convertToDictionary(from: element)
+                arrayValue.append(convertedValue.popFirst()!)
+            }
+        }
+        return arrayValue
+    }
+    
+    // 기본값 형태 문자열 데이터를 실제 값으로 변환.
+    private static func convertStringToAny(from data: String) throws -> Any? {
+        var data = data
+        data = data.trimmingCharacters(in: [" "])
+        var value: Any?
+        if data.contains("\"") {
+            value = data.trimmingCharacters(in: ["\""])
+        }else if let numberElement = JSONData.Number(data) {
+            value = numberElement
+        }else if let boolElement = Bool(data) {
+            value = boolElement
+        }
+        return value
     }
     
 }
