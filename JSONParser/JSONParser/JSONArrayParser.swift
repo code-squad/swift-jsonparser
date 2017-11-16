edb61dbc76958cf60e9227d70336a3ee29ef3b16
DongJin Lee <comets195@gmail.com>
2017-11-16 18:25:00 +0900

    Remove overlaping Code..

--- JSONParser/JSONParser/JSONArrayParser.swift
+++ JSONParser/JSONParser/JSONArrayParser.swift
@@ -8,46 +8,49 @@
 
 import Foundation
 // JSONParser for Array
+typealias JSONTypeAndIdx = (object: JSONType, elementIdx: String.Index)
 extension JSONParser {
     func makeArrayJSONData(_ value: String) throws -> JSONData {
         var resultOfParser = [JSONType]()
         let removedBracket = supportingJSON.processBeforeMakingJSON(value)
         var rawJSON = removedBracket.trimmingCharacters(in: .whitespacesAndNewlines)
-        var checkEndElement = true
-        while checkEndElement {
+        var checkLastElement = true
+        while checkLastElement {
             if rawJSON.hasPrefix("{") {
                 let rawObjects = try makeObjectsInArray(rawJSON)
                 resultOfParser.append(rawObjects.object)
-                rawJSON.removeSubrange(rawJSON.startIndex...rawObjects.elementIdx)
-                if rawJSON.hasPrefix(",") {
-                    rawJSON.removeSubrange(rawJSON.startIndex..<rawJSON.index(rawJSON.startIndex, offsetBy: 2))
-                } else {
-                    checkEndElement = false
-                }
+                rawJSON = adjustRawJSON(separatingIdx: rawObjects.elementIdx, rawJSON)
             } else {
                 let rawArrays = try makeJSONInArray(rawJSON)
                 resultOfParser.append(rawArrays.object)
-                rawJSON.removeSubrange(rawJSON.startIndex..<rawArrays.elementIdx)
-                if rawJSON.hasPrefix(",") {
-                    rawJSON.removeSubrange(rawJSON.startIndex..<rawJSON.index(rawJSON.startIndex, offsetBy: 2))
-                } else {
-                    checkEndElement = false
-                }
+                rawJSON = adjustRawJSON(separatingIdx: rawArrays.elementIdx, rawJSON)
             }
+            if rawJSON.isEmpty { checkLastElement = false }
         }
         return JSONData(resultOfParser)
     }
     
+    private func adjustRawJSON(separatingIdx: String.Index, _ rawJSON: String) -> String {
+        var preRemoveJSON = rawJSON
+        if preRemoveJSON.contains(",") {
+            preRemoveJSON.removeSubrange(rawJSON.startIndex...separatingIdx)
+            if !preRemoveJSON.isEmpty { preRemoveJSON.removeSubrange(preRemoveJSON.startIndex..<preRemoveJSON.index(preRemoveJSON.startIndex, offsetBy: 2)) }
+        } else {
+            preRemoveJSON.removeAll()
+        }
+        return preRemoveJSON
+    }
+    
     // [ { } ] 처리로직
-    private func makeObjectsInArray(_ value: String) throws -> (object: JSONType, elementIdx: String.Index) {
+    private func makeObjectsInArray(_ value: String) throws -> JSONTypeAndIdx {
         let rawJSON = value
         var jsonObjects: JSONType
-        if rawJSON.hasPrefix("{") {
-            let objectAndEndBrace = try makeObjectInArray(rawJSON)
-            jsonObjects = JSONType.objectType(objectAndEndBrace.object)
-            return (jsonObjects, objectAndEndBrace.endBraceIndex)
+        guard let endBrace = rawJSON.index(of: "}") else {
+            throw ErrorCode.invalidJSONStandard
         }
-        throw ErrorCode.invalidJSONStandard
+        let rawObject = String(rawJSON.prefix(through: endBrace))
+        jsonObjects = JSONType.objectType(rawObject)
+        return (jsonObjects, endBrace)
     }
     
     private func makeObjectInArray(_ value: String) throws -> (object: String, endBraceIndex: String.Index) {
@@ -61,13 +64,14 @@ extension JSONParser {
     }
     
     // [ , ] 처리로직
-    private func makeJSONInArray(_ value: String) throws -> (object: JSONType, elementIdx: String.Index) {
+    private func makeJSONInArray(_ value: String) throws -> JSONTypeAndIdx {
         let rawJSON = value
         var jsonType: JSONType
         if let endOfElementIdx = rawJSON.index(of: ",") {
-            let element = String(rawJSON.prefix(through: endOfElementIdx))
+            var element = String(rawJSON.prefix(through: endOfElementIdx))
+            element.removeLast()
             jsonType = supportingJSON.sortJSONData(element)
-            return (jsonType, endOfElementIdx)
+            return (jsonType, rawJSON.index(before: endOfElementIdx))
         } else {
             let element = String(rawJSON.prefix(through: rawJSON.index(before: rawJSON.endIndex)))
             jsonType = supportingJSON.sortJSONData(element)
