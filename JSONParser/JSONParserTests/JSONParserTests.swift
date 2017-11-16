197c46aeecc76433eb499c9f3dedc1ed17ded748
undervine <undervine@naver.com>
2017-11-16 18:06:46 +0900

    Add test cases
    Add nested test cases.
    Add invalid test cases including whitespaces of object's key, values without commas, values without outermost brackets, etc.

--- JSONParser/JSONParserTests/JSONParserTests.swift
+++ JSONParser/JSONParserTests/JSONParserTests.swift
@@ -11,92 +11,82 @@ import XCTest
 
 class JSONParserTests: XCTestCase {
     
-    func testParsingJSONData() throws {
-        let jsonString: String = "{\"name\":\"KIM JUNG\",\"alias\":\"JK\",\"level\":5,\"married\":true}"
-        XCTAssertNotNil(try JSONParser.parse(jsonString))
+    func testBasicArrayPattern() throws {
+        let data: String = "[ \"sdf\" , 234 , true ]"
+        let parsedData = try JSONParser.parse(data)
+        XCTAssertEqual(parsedData.arrayString.count, 1)
+        XCTAssertEqual(parsedData.arrayNumber.count, 1)
+        XCTAssertEqual(parsedData.arrayBool.count, 1)
     }
     
-    func testStringDataIsNotNil() {
-        let parsedData = JSONData(["name":"KIM JUNG"], ofType: .array)
-        XCTAssertNotNil(parsedData)
+    func testBasicObjectPattern() throws {
+        let data: String = "{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\",\"level\":5,\"married\":true}"
+        let parsedData = try JSONParser.parse(data)
+        XCTAssertEqual(parsedData.objectNumber.count, 1)
+        XCTAssertEqual(parsedData.objectString.count, 2)
+        XCTAssertEqual(parsedData.objectBool.count, 1)
     }
     
-    func testNumberDataIsNotNil() {
-        let parsedData = JSONData(["level" :5], ofType: .array)
-        XCTAssertNotNil(parsedData)
+    func testNestedObjectPattern() throws {
+        let data: String = "[{\"asdf\":\"asdf\",\"hrtdg\":234},{\"ertgf\":true}]"
+        let parsedData = try JSONParser.parse(data)
+        XCTAssertEqual(parsedData.innerObjectCount, 2)
     }
     
-    func testBoolDataIsNotNil() {
-        let parsedData = JSONData(["married": true], ofType: .array)
-        XCTAssertNotNil(parsedData)
+    func testNestedArrayPattern() throws {
+        let data: String = "[ \"sdf\" , 234 , true, [\"Sdf\", 234], 234]"
+        let parsedData = try JSONParser.parse(data)
+        XCTAssertEqual(parsedData.arrayString.count, 1)
+        XCTAssertEqual(parsedData.arrayNumber.count, 2)
+        XCTAssertEqual(parsedData.arrayBool.count, 1)
+        XCTAssertEqual(parsedData.innerArrayCount, 1)
     }
     
-    func testCountOfNumberIsRight() throws {
-        let jsonString: String = "{\"name\":\"KIM JUNG\",\"alias\":\"JK\",\"level\":5,\"married\":true}"
-        let parsedData = try JSONParser.parse(jsonString)
-        XCTAssertEqual(parsedData[0].number.count, 1)
-        XCTAssertEqual(parsedData[0].string.count, 2)
-        XCTAssertEqual(parsedData[0].bool.count, 1)
-    }
-    
-    func testCountOfAnArrayIsRight() throws {
-        let jsonString: String = "{\"name\":\"KIMJUNG\",\"alias\":\"JK\",\"level\":5,\"married\":true}"
-        let parsedData = try JSONParser.parse(jsonString)
-        XCTAssertEqual(parsedData.count, 1)
-    }
-    
-    func testCountOfAnObjectIsRight() throws {
-        let jsonString: String = "[\"asdf\":\"asdf\",\"hrtdg\":234,\"ertgf\":true]"
-        let parsedData = try JSONParser.parse(jsonString)
-        XCTAssertEqual(parsedData.count, 1)
-    }
-    
-    func testCountOfArraysIsRight() throws {
-        let jsonString: String = "{\"asdf\":\"asdf\",\"hrtdg\":234},{\"ertgf\":true}"
-        let parsedData = try JSONParser.parse(jsonString)
-        XCTAssertEqual(parsedData.count, 2)
-    }
-    
-    func testCountOfObjectsIsRight() throws {
-        let jsonString: String = "[\"asdf\":\"asdf\",\"hrtdg\":234],[\"ertgf\":true]],[\"ertgf\":23456]"
-        let parsedData = try JSONParser.parse(jsonString)
-        XCTAssertEqual(parsedData.count, 3)
-    }
-    
-    func testCountOfObjectsAndArraysIsRight() throws {
-        let jsonString: String = "{\"name\":\"KIMJUNG\",\"alias\":\"JK\",\"level\":5,\"married\":true},[\"name\":\"YOONJISU\",\"alias\":\"crong\",\"level\":4,\"married\":true],{\"asdf\":\"asdf\",\"hrtdg\":234},{\"ertgf\":true},[\"asdf\":\"asdf\",\"hrtdg\":234,\"ertgf\":true]"
-        let parsedData = try JSONParser.parse(jsonString)
+    func testNestedMultiPattern() throws {
+        let data: String = "[ { \"name\" : \"KIMJUNG\", \"alias\":\"JK\",\"level\":5,\"married\":true}, [true,\"crong\",4,\"married\"], {\"asdf\":\"asdf\",\"hrtdg\":234}, {\"ertgf\":true}, [\"asdf\",234,\"ertgf\"] ]"
+        let parsedData = try JSONParser.parse(data)
         XCTAssertEqual(parsedData.count, 5)
-        var arrayCount = 0
-        var objectCount = 0
-        for data in parsedData {
-            switch data.type {
-            case .array: arrayCount += 1
-            case .object: objectCount += 1
-            }
-        }
-        XCTAssertEqual(arrayCount, 3)
-        XCTAssertEqual(objectCount, 2)
+        XCTAssertEqual(parsedData.innerArrayCount, 2)
+        XCTAssertEqual(parsedData.innerObjectCount, 3)
     }
     
-    func testObjectWithWhitespace() throws {
-        let jsonString: String = "[ \"asdf\": \"asdf\", \"hrtdg\":234, \"ertgf\":true, \"sjdfkl\":234 ]"
-        let parsedData = try JSONParser.parse(jsonString)
-        XCTAssertEqual(parsedData.count, 1)
-        XCTAssertEqual(parsedData[0].bool.count, 1)
-        XCTAssertEqual(parsedData[0].number.count, 2)
-        XCTAssertEqual(parsedData[0].string.count, 1)
+    func testInvalidArrayType() throws {
+        let data: String = "[ \"name\" : \"KIM JUNG\" ]"
+        XCTAssertFalse(try GrammarChecker.isJSONPattern(data))
     }
     
-    func testArrayWithWhitespace() throws {
-        let jsonString: String = "[ { \"asdf\": \"asdf\", \"hrtdg\": 234 }, { \"ertgf\": true} ]"
-        let parsedData = try JSONParser.parse(jsonString)
-        XCTAssertEqual(parsedData.count, 2)
-        XCTAssertEqual(parsedData[0].bool.count, 0)
-        XCTAssertEqual(parsedData[0].number.count, 1)
-        XCTAssertEqual(parsedData[0].string.count, 1)
-        XCTAssertEqual(parsedData[1].bool.count, 1)
-        XCTAssertEqual(parsedData[1].number.count, 0)
-        XCTAssertEqual(parsedData[1].string.count, 0)
+    func testInvalidNestedArrayinObjectPattern() throws {
+        let data: String = "{ \"sdf\":324,\"asdf\":{\"asdf\":234,\"asdf\":true}}"
+        XCTAssertFalse(try GrammarChecker.isJSONPattern(data))
+    }
+    
+    func testInvalid2DepthNestedPattern() throws {
+        let data: String = "[[\"sdf\",\"sadf\", {\"Asdf\":234} ],true,\"sdf\",false,324]"
+        XCTAssertFalse(try GrammarChecker.isJSONPattern(data))
+    }
+    
+    func testObjectKeyWithWhitespace() throws {
+        let data: String = "{\" dsf\":234}"
+        XCTAssertFalse(try GrammarChecker.isJSONPattern(data))
+    }
+    
+    func testWithoutCommaInObject() throws {
+        let data: String = "{ \"asdf\" : 234\"asdf\":true}"
+        XCTAssertFalse(try GrammarChecker.isJSONPattern(data))
+    }
+    
+    func testWithoutCommaInArray() throws {
+        let data: String = "[ \"asdf\"234, true]"
+        XCTAssertFalse(try GrammarChecker.isJSONPattern(data))
+    }
+    
+    func testWithNoOutermostBrackets() throws {
+        let data: String = "\"sdf\":234,\"324\":true"
+        XCTAssertFalse(try GrammarChecker.isJSONPattern(data))
+    }
+    
+    func testInvalidArrayDataInObject() throws {
+        let data: String = "{ \"asdf\" : 324, [\"asdf\",234]}"
+        XCTAssertFalse(try GrammarChecker.isJSONPattern(data))
     }
 }
