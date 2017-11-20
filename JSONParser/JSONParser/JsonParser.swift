5f0c831016bbf29425cd9d12706e57bee093812a
dely2p <lhyal2p@gmail.com>
2017-11-20 10:39:02 +0900

    add JsonParser Struct

---
+++ JSONParser/JSONParser/JsonParser.swift
@@ -0,0 +1,18 @@
+//
+//  JsonParser.swift
+//  JSONParser
+//
+//  Created by Eunjin Kim on 2017. 11. 20..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct JsonParser {
+
+    private var format = ""
+    
+    func startJsonValue() {
+        
+    }
+}
--- JSONParser/JSONParser/JsonScanner.swift
+++ JSONParser/JSONParser/JsonScanner.swift
@@ -9,10 +9,23 @@
 import Foundation
 
 struct JsonScanner {
+    enum token: String{
+        case STARTSQUAREBRACKET = "["
+        case ENDSQUAREBRACKET = "]"
+        case NUMBER = "[0-9]"
+        case STRING = "[a-z,A-Z]"
+        case BOOLEAN = "[true|false]"
+        case COMMA = ","
+    }
     
     func scanOfJsonValue(jsonValue: String) {
         for index in jsonValue.characters {
-            print(index)
+            //print(index)
+            
         }
+    }
+    
+    func getToken() {
+        
     }
 }
