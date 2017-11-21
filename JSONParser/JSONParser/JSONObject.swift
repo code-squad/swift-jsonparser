52ac0751471ce55877930b2c322ccd0717377a1b
undervine <undervine@naver.com>
2017-11-22 00:15:28 +0900

    Make new structs

---
+++ JSONParser/JSONParser/JSONObject.swift
@@ -0,0 +1,13 @@
+//
+//  JSONObject.swift
+//  JSONParser
+//
+//  Created by 심 승민 on 2017. 11. 22..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct JSONObject {
+    
+}
