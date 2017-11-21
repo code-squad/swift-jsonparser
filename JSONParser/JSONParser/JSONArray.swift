7c31d96d3824d05f8e32c06a93601b7eff457bcb
undervine <undervine@naver.com>
2017-11-22 03:03:57 +0900

    Separate JSONData struct to JSONArray and JSONObject structs

--- JSONParser/JSONParser/JSONArray.swift
+++ JSONParser/JSONParser/JSONArray.swift
@@ -8,6 +8,72 @@
 
 import Foundation
 
-struct JSONArray {
+struct JSONArray: JSONData {
+    var data: TYPEArray
+    var dataCountOfEach: (string: Int, number: Int, bool: Int, nestedObject: Int, nestedArray: Int)
+    var prettyData: [Any]
+    var count: Int { return data.count }
+    
+    init(data: TYPEArray) {
+        self.data = []
+        self.dataCountOfEach = (0,0,0,0,0)
+        self.prettyData = []
+        setData(data)
+        setCounts()
+        makePrettyData()
+    }
+    
+    mutating func setData(_ data: TYPEArray) {
+        for value in data {
+            self.data.append(value)
+        }
+    }
+    
+    mutating func setCounts() {
+        for value in data {
+            setEachCounts(of: value)
+        }
+    }
+    
+    mutating func makePrettyData() {
+        self.prettyData.append("[")
+        self.prettyData.append(contentsOf: addValuesOfData())
+        self.prettyData.append("]")
+    }
+    
+    func addValuesOfData() -> [Any] {
+        var temp: [Any] = []
+        var isNotNormalData = false
+        for datum in self.data {
+            switch datum {
+            case let values as [String:Any]:
+                temp.append("{")
+                for (key, value) in values {
+                    let data = makeDataString(from: value)
+                    appendAnys(to: &temp, of: "\n\t\t\"", key, "\": ", data, ",")
+                }
+                temp.append("\n\t},")
+                isNotNormalData = true
+                break
+            case let values as [Any]:
+                let elem = makeDataString(from: values)
+                if self.dataCountOfEach.nestedObject != 0 {
+                    temp.append("\n\t")
+                }
+                appendAnys(to: &temp, of: elem, ",")
+                isNotNormalData = false
+                break
+            default:
+                let elem = makeDataString(from: datum)
+                appendAnys(to: &temp, of: " ", elem, ",")
+                isNotNormalData = false
+                break
+            }
+        }
+        if isNotNormalData {
+            temp.append("\n")
+        }
+        return temp
+    }
     
 }
--- JSONParser/JSONParser/JSONObject.swift
+++ JSONParser/JSONParser/JSONObject.swift
@@ -8,6 +8,64 @@
 
 import Foundation
 
-struct JSONObject {
+struct JSONObject: JSONData {
+    var data: TYPEObject
+    var dataCountOfEach: (string: Int, number: Int, bool: Int, nestedObject: Int, nestedArray: Int)
+    var prettyData: [Any]
+    var count: Int { return data.count }
+    
+    init(data: TYPEObject) {
+        self.data = [:]
+        self.dataCountOfEach = (0,0,0,0,0)
+        self.prettyData = []
+        setData(data)
+        setCounts()
+        makePrettyData()
+    }
+    
+    mutating func setData(_ data: TYPEObject) {
+        for (key, value) in data {
+            self.data.updateValue(value, forKey: key)
+        }
+    }
+    
+    mutating func setCounts() {
+        for value in data.values {
+            setEachCounts(of: value)
+        }
+    }
+
+    mutating func makePrettyData() {
+        self.prettyData.append("{")
+        self.prettyData.append(contentsOf: addValuesOfData())
+        self.prettyData.append("\n")
+        self.prettyData.append("}")
+    }
+    
+    func addValuesOfData() -> [Any] {
+        var temp: [Any] = []
+        for (key, value) in self.data {
+            switch value {
+            case let values as [String:Any]:
+                appendAnys(to: &temp, of: "\n\t\"", key, "\": ", "{")
+                for (key, value) in values {
+                    // 데이터가 문자열이면 따옴표 붙임.
+                    let data = makeDataString(from: value)
+                    appendAnys(to: &temp, of: "\n\t\t\"", key, "\": ", data, ",")
+                }
+                temp.append("\n\t},")
+                break
+            case let values as [Any]:
+                let data = makeDataString(from: values)
+                appendAnys(to: &temp, of: "\n\t\"", key, "\": ", data, ",")
+                break
+            default:
+                let data = makeDataString(from: value)
+                appendAnys(to: &temp, of: "\n\t\"", key, "\": ", data, ",")
+                break
+            }
+        }
+        return temp
+    }
     
 }
