40cd7999afa041c8f586257760758ad20c96028f
undervine <undervine@naver.com>
2017-11-21 14:07:18 +0900

    Remove some funcs and shorten codes

--- JSONParser/JSONParser/JSONParser.swift
+++ JSONParser/JSONParser/JSONParser.swift
@@ -12,17 +12,12 @@ struct JSONParser {
     
     // 문자열을 JSONData로 파싱.
     static func parse(_ rawData: String) throws -> JSONData {
-        var rawDataWithoutBracket: [String] = []
         var jsonData = JSONData()
         // 전체 데이터의 타입(object|array) 확인.
         if try isObjectType(rawData) {
-            // 가장 바깥쪽 괄호를 제거한 전체데이터. (따라서 1개밖에 없음.)
-            rawDataWithoutBracket = try rawData.splitPattern(by: GrammarChecker.objectDataWithoutBracket)
-            // JSONData 객체 생성.
-            jsonData = try generateJSONData(from: rawDataWithoutBracket[0], ofType: JSONData.DataType.object)
+            jsonData = try generateJSONData(from: rawData, ofType: JSONData.DataType.object)
         }else {
-            rawDataWithoutBracket = try rawData.splitPattern(by: GrammarChecker.arrayDataWithoutBracket)
-            jsonData = try generateJSONData(from: rawDataWithoutBracket[0], ofType: JSONData.DataType.array)
+            jsonData = try generateJSONData(from: rawData, ofType: JSONData.DataType.array)
         }
         // 모든 JSONData 반환.
         return jsonData
@@ -62,26 +57,30 @@ struct JSONParser {
     }
     
     // 각 blob 내부 데이터들로 생성한 배열 반환.
-    private static func generateArray(from data: String) throws -> [Any] {
+    private static func generateArray(from data: String) throws -> JSONData.JSONArray {
         var stringValues: [String] = []
-        // array 내부 데이터로 들어갈 수 있는 밸류패턴으로 자름. (문자열, 부울, 숫자, 배열, 객체 패턴.)
-        stringValues = try data.splitPattern(by: GrammarChecker.jsonValuePattern)
+        // 가장 바깥쪽 괄호 제거.
+        let rawDataWithoutBracket = try data.splitPattern(by: GrammarChecker.arrayDataWithoutBracket)
+        // 배열 내부 데이터 단위로 자름.
+        stringValues = try rawDataWithoutBracket[0].splitPattern(by: GrammarChecker.jsonValuePattern)
         // 각 문자열 데이터를 실 데이터로 변환.
-        let resultValues: [Any] = try stringValues.map { try makeRealElement(from: $0) }
+        let resultValues = try stringValues.map { try makeRealElement(from: $0) }
         return resultValues
     }
     
     // 각 blob 내부 데이터들로 생성한 딕셔너리 반환.
-    private static func generateDictionary(from data: String) throws -> [String:Any] {
-        var dictionaries: [String:Any] = [:]
-        var elements: [String] = []
-        elements = try data.splitPattern(by: GrammarChecker.jsonObjectValueWithComma+"?")
+    private static func generateDictionary(from data: String) throws -> JSONData.JSONObject {
+        var dictionaries: JSONData.JSONObject = [:]
+        // 가장 바깥쪽 괄호 제거.
+        let rawDataWithoutBracket = try data.splitPattern(by: GrammarChecker.objectDataWithoutBracket)
+        // 객체 내부 데이터 단위로 자름.
+        let elements = try rawDataWithoutBracket[0].splitPattern(by: GrammarChecker.jsonObjectValueWithComma+"?")
         for element in elements {
             let key = try generateKey(from: element)
             let value = try generateValue(from: element)
             dictionaries.updateValue(value, forKey: key)
         }
-        // 실제 값으로 변환된 [String:Any] 반환.
+        // 실제 값으로 변환된 딕셔너리 반환.
         return dictionaries
     }
     
@@ -102,51 +101,23 @@ struct JSONParser {
         // 밸류값 범위의 문자열. - 문자열, 부울, 숫자, 배열, 객체.
         let value = String(data[valueRange])
         // 실제 값으로 변환하여 리턴.
-        return try makeRealElement(from: value)
+        let realData = try makeRealElement(from: value)
+        return realData
     }
     
     // 개별 문자열 데이터를 실 데이터로 변환.
     private static func makeRealElement(from value: String) throws -> Any {
         if value.contains("[") {
             // 배열 형태의 데이터 형변환.
-            return try generateNestedBlobData(from: value, of: .array)
+            return try generateArray(from: value)
         }else if value.contains("{") {
             // 객체 형태의 데이터 형변환.
-            return try generateNestedBlobData(from: value, of: .object)
+            return try generateDictionary(from: value)
         }else {
             // 기본형 데이터 형변환.
             guard let convertedValue = try generateBasicData(from: value) else { throw GrammarChecker.JsonError.dataOfNil }
             return convertedValue
         }
-    }
-    
-    // 객체 또는 배열 형태의 데이터 형변환.
-    private static func generateNestedBlobData(from data: String, of type: JSONData.DataType) throws -> JSONData.JSONArray {
-        var arrayValue: JSONData.JSONArray = []
-        // data = 내부배열. elements = 내부배열 내의 각 문자열 데이터.(정규표현식을 쓰려고 했으나 오류남)
-        var elements: [String] = []
-        let trimmedData = data.trimmingCharacters(in: .whitespacesAndNewlines).trimmingCharacters(in: [","])
-        switch type {
-        case .array:
-            // 내부 배열 데이터를 기본형 데이터 단위로 분리.
-            elements = trimmedData.trimmingCharacters(in: ["[","]"]).split(separator: ",").map(String.init)
-            for element in elements {
-                // 내부 배열 내의 각 기본형 문자열 데이터를 실제 데이터로 변환.
-                guard let convertedValue = try generateBasicData(from: element) else { break }
-                arrayValue.append(convertedValue)
-            }
-        case .object:
-            var convertedValue: [String:Any] = [:]
-            // 내부 객체 데이터를 기본형 데이터 단위로 분리.
-            elements = trimmedData.trimmingCharacters(in: ["{","}"]).split(separator: ",").map(String.init)
-            for element in elements {
-                // 내부 객체 내의 각 기본형 문자열 데이터를 실제 데이터로 변환.
-                convertedValue = try generateDictionary(from: element)
-                // 딕셔너리 내부 값만 빼서 붙임.
-                arrayValue.append(convertedValue.popFirst()!)
-            }
-        }
-        return arrayValue
     }
     
     // 기본형 문자열 데이터를 실제 값으로 변환.
