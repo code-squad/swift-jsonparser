15617c8c6522cd2c0efd8f3efb55489f6509d6a5
ChocOZerO <nayalth@gmail.com>
2017-11-22 15:28:46 +0900

    [Add]Comman Line input으로 파일 입력하여 처리

--- JSONParser/JSONParser/InputView.swift
+++ JSONParser/JSONParser/InputView.swift
@@ -9,9 +9,54 @@
 import Foundation
 
 struct InputView {
+    let grammarChecker: GrammarChecker
 
-    static func readInput() -> String {
-        return readLine() ?? "[]"
+    init(grammarChecker: GrammarChecker) {
+        self.grammarChecker = grammarChecker
+    }
+
+    enum InputError: Error {
+        case notJSONPattern
+    }
+
+    func readInput() throws -> String {
+        let commandCount: Int = CommandLine.arguments.count
+        var result: String = ""
+        switch commandCount {
+        case 2,3:
+            result = try readFromFile()
+        default:
+            result = readFromConsole()
+        }
+        return result
+    }
+
+    func readFromConsole() -> String {
+        var inputValue: String = ""
+        var isJSONPattern: Bool = false
+        while !isJSONPattern {
+            print("분석할 JSON 데이터를 입력하세요.")
+            inputValue = readLine() ?? "[]"
+            isJSONPattern = grammarChecker.isJSONPattern(target: inputValue)
+            if !isJSONPattern {
+                print("지원하지 않는 형식을 포함하고 있습니다.")
+            }
+        }
+        return inputValue
+    }
+
+    func readFromFile() throws -> String {
+        var inputValue: String = ""
+        let basePath: String = "./MyProject/CodeSquad/Masters/Level2/swift-jsonparser/JSONParser/JSONFile/"
+        let file: String = basePath + CommandLine.arguments[1]
+        if let dir = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first {
+            let fileURL = dir.appendingPathComponent(file)
+            inputValue = try String(contentsOf: fileURL, encoding: .utf8)
+            if !grammarChecker.isJSONPattern(target: inputValue) {
+                throw InputView.InputError.notJSONPattern
+            }
+        }
+        return inputValue.trimmingCharacters(in: .whitespaces)
     }
 
 }
--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -11,22 +11,21 @@ import Foundation
 var inputValue: String = ""
 let grammarChecker: GrammarChecker = GrammarChecker()
 // 입력
-var isJSONPattern: Bool = false
-while !isJSONPattern {
-    print("분석할 JSON 데이터를 입력하세요.")
-    inputValue = InputView.readInput()
-    isJSONPattern = grammarChecker.isJSONPattern(target: inputValue)
-    if !isJSONPattern {
-        print("지원하지 않는 형식을 포함하고 있습니다.")
-    }
+let inputView: InputView = InputView.init(grammarChecker: grammarChecker)
+do {
+    inputValue = try inputView.readInput()
+    // 분석
+    let jsonAnalyser = JSONAnalyser.init(grammarChecker: grammarChecker)
+    let jsonType = jsonAnalyser.getJSONType(inputValue: inputValue)
+    // JSON 형식
+    var jsonPainter = JSONPainter()
+    jsonPainter.paintJSON(jsonType: jsonType)
+    // 출력
+    let outputView = OutputView.init(jsonType: jsonType)
+    outputView.printResult()
+    outputView.printJSON(jsonPainter: jsonPainter)
+} catch InputView.InputError.notJSONPattern {
+    print("지원하지 않는 형식을 포함하고 있습니다.")
+} catch {
+    print("입력이 올바르지 않습니다.")
 }
-// 분석
-let jsonAnalyser = JSONAnalyser.init(grammarChecker: grammarChecker)
-let jsonType = jsonAnalyser.getJSONType(inputValue: inputValue)
-// JSON 형식
-var jsonPainter = JSONPainter()
-jsonPainter.paintJSON(jsonType: jsonType)
-// 출력
-let outputView = OutputView.init(jsonType: jsonType)
-outputView.printResult()
-outputView.printJSON(jsonPainter: jsonPainter)
