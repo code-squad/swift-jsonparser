8c24792d46e7014b46954508e523465727d9395d
DongJin Lee <comets195@gmail.com>
2017-11-22 14:06:19 +0900

    Remove Don't use methods.

--- JSONParser/JSONParser/Utility/JSONSupporting.swift
+++ JSONParser/JSONParser/Utility/JSONSupporting.swift
@@ -18,20 +18,14 @@ extension JSONSupporting {
         return rawJSON
     }
     
-    func sortJSONType(_ value: [String]) -> [Any] {
-        var jsonData = [Any]()
-        for sortedJSON in value {
-            jsonData.append(sortJSONData(sortedJSON))
-        }
-        return jsonData
-    }
-    
     func sortJSONData(_ value: String) -> JSONType {
         var rawJSON = value
         if let boolType = Bool(rawJSON) {
             return JSONType.boolType(boolType)
         } else if let intType = Int(rawJSON) {
             return JSONType.intType(intType)
+        } else if rawJSON.hasPrefix("{") {
+            return JSONType.objectType(rawJSON)
         } else {
             rawJSON.removeFirst()
             rawJSON.removeLast()
