b21e0fc13fbccb1ebce9c0b54523e08951e1ae55
undervine <undervine@naver.com>
2017-11-23 19:14:56 +0900

    Add functions to handle file output
    Add writeData() to write parsing data into a file.
    Modify printPrettyData() and printReport() to getPrettyData() and getReport().
    Add OutputError to throw error during file output.

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -9,17 +9,32 @@
 import Foundation
 
 struct OutputView {
+    enum OutputError: String, Error {
+        case pathError = "파일 경로를 확인해주세요."
+        case fileURLError = "파일 내용을 쓰는 데 실패했습니다."
+    }
+    
+    static func writeData(_ reportData: String, _ prettyData: String, into outputFileName: String) throws {
+        let resultData = reportData + "\n\n" + prettyData
+        do {
+            try resultData.write(toFile: outputFileName, atomically: true, encoding: .utf8)
+        }catch {
+            throw OutputError.fileURLError
+        }
+    }
     
     // JSON 규격에 맞춰 예쁘게 출력.
-    static func printJSONData(_ data: JSONData) {
+    static func getPrettyData(_ data: JSONData) -> String {
+        var resultData = ""
         for datum in data.prettyData {
-            print(datum, terminator: "")
+            resultData += "\(datum) "
         }
-        print("\n")
+        resultData += "\n"
+        return resultData
     }
     
     // 내부 데이터 description 출력.
-    static func printReport(_ data: JSONData) {
+    static func getReport(_ data: JSONData) -> String {
         var dataType: String = ""
         // 전체 데이터 개수 (문자열, 숫자, 부울, 객체, 배열)
         let dataCounts = data.dataCountOfEach
@@ -49,7 +64,7 @@ struct OutputView {
         }
         result.removeLast()             // 마지막 콤마(,) 제거.
         result += "가 포함되어 있습니다."
-        print(result)
+        return result
     }
     
     // JSON 관련 에러 출력.
