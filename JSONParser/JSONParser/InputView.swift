36fbd0ec61580a78d85bb8d08f1438a419cfcd90
undervine <undervine@naver.com>
2017-11-23 19:12:07 +0900

    Add functions to handle file input
    Add terminalMode() that makes arguments from terminal to tuple variables.
    Add getData() to get file input data.
    Add InputError to throw errors during file handling.

--- JSONParser/JSONParser/InputView.swift
+++ JSONParser/JSONParser/InputView.swift
@@ -9,6 +9,36 @@
 import Foundation
 
 struct InputView {
+    enum InputError: String, Error {
+        case fileNameError = "확장자(.json)를 확인해주세요."
+        case pathError = "파일 경로를 확인해주세요."
+        case fileURLError = "파일 내용을 읽는 데 실패했습니다."
+    }
+    
+    static func getData(from inputFileName: String) throws -> String {
+        let path = Bundle.main.path(forResource: inputFileName, ofType: nil)
+        do {
+            guard let fileURL = path else { throw InputError.fileNameError }
+            let content = try String(contentsOfFile: fileURL, encoding: .utf8)
+            return content
+        }catch {
+            throw InputError.fileURLError
+        }
+    }
+    
+    static func terminalMode(manualMessage: String) throws -> (String, String?) {
+        var inputFileName: String
+        var outputFileName: String?
+        // 첫번째 인자 저장.
+        inputFileName = CommandLine.arguments[1]
+        // 인자가 2개 이상인 경우.
+        if CommandLine.argc > 2 {
+            // 두번째 인자 저장.
+            outputFileName = CommandLine.arguments[2]
+        }
+        return (inputFileName, outputFileName)
+    }
+    
     // 사용자 입력값의 json 규격 체크 후 반환.
     static func askFor(message: String) throws -> String? {
         // 요구 메시지 출력.
