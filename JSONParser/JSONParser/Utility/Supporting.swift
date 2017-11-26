d929fa41fe1d3017f3740c2079a3c523dae64ad3
DongJin Lee <comets195@gmail.com>
2017-11-26 13:39:09 +0900

    Add Protocol, To combine the common method. removeBrace(:String).

--- JSONParser/JSONParser/Utility/Supporting.swift
+++ JSONParser/JSONParser/Utility/Supporting.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+protocol JSONSupporting {
+    
+}
+
+extension JSONSupporting {
+    func removeBrace(_ value: String) -> String {
+        var rawJSON = value
+        rawJSON.removeFirst()
+        rawJSON.removeLast()
+        return rawJSON
+    }
+    
+}
