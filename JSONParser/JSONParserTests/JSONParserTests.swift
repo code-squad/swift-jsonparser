7673db5422f7166f6c40e988f1376036f5ca0dc3
yuaming <miennes3@gmail.com>
2017-11-28 17:37:13 +0900

    [수정] JSON Parser 2단계 -> 테스트 케이스 수정

--- JSONParser/JSONParserTests/JSONParserTests.swift
+++ JSONParser/JSONParserTests/JSONParserTests.swift
@@ -11,13 +11,6 @@ import XCTest
 class JSONParserTests: XCTestCase {
     typealias DataTypeName = JSONData.TypeName
     
-    func test_JSONData_배열_요소_분리_확인() {
-        let inputValue = """
-        [ 10, "jk", 4, "314", 99, "crong", false ]
-        """
-        XCTAssertTrue(Utility.removeFromFirstToEnd(in: inputValue).splitUnits().count == 7)
-    }
-    
     func test_JSONData_배열_요소_개수_확인() {
         let inputValue = """
         [ 10, "jk", 4, "314", 99, "crong", false ]
@@ -29,18 +22,11 @@ class JSONParserTests: XCTestCase {
     
     func test_JSONData_배열_안_객체_개수_확인() {
         let inputValue = """
-            [ { "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "married" : true },
-           { "name" : "YOON JISU", "alias" : "crong", "level" : 4, "married" : true } ]
+        [ { "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "married" : true }, { "name" : "YOON JISU", "alias" : "crong", "level" : 4, "married" : true } ]
         """
         XCTAssertTrue(try JSONParser.analyzeJSONData(in: inputValue).objectCount == 2)
     }
     
-    func test_JSONData_객체_요소_분리_확인() {
-        let inputValue = """
-        { "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "married" : true }
-        """
-        XCTAssertTrue(Utility.removeFromFirstToEnd(in: inputValue).splitUnits().count == 4)
-    }
     
     func test_JSONData_객체_요소_개수_확인() {
         let inputValue = """
