8dbaf96dff67bc9b521864e503e9640b2b70d840
DongJin Lee <comets195@gmail.com>
2017-12-04 11:00:09 +0900

    Separate method when Input type is File.

--- JSONParser/JSONParser/JSONAnalyzer.swift
+++ JSONParser/JSONParser/JSONAnalyzer.swift
@@ -9,11 +9,10 @@
 import Foundation
 
 struct JSONAnalyzer {
-    private let location = "/Users/Mrlee/Documents/CodeSquad_Napster/iOS Level 2/swift-jsonparser/JSONParser/"
-
+    private let inputView = InputView()
+    private let outputView = OutputView()
+    
     func make() {
-        let inputView = InputView()
-        let outputView = OutputView()
         var repeatCondition = true
         while repeatCondition {
             print("분석할 JSON 데이터를 입력하세요.")
@@ -31,30 +30,14 @@ struct JSONAnalyzer {
     func make(inputFile: String, outputFile: String?) {
         var repeatCondition = true
         while repeatCondition {
-            guard let rawJSON = readFile(name: inputFile) else {
+            guard let rawJSON = inputView.readFile(name: inputFile) else {
                 OutputView.printErrorMsg(errorCode: .invalidCommandlineArgument)
                 return
             }
             if let vaildJSON = executeParser(rawJSONData: rawJSON) {
-                writeFile(contents: vaildJSON.showJSONData(1), writeFileName: outputFile)
+                outputView.writeFile(contents: vaildJSON.showJSONData(1), writeFileName: outputFile)
                 repeatCondition = false
             }
-        }
-    }
-    
-    private func readFile(name: String) -> String? {
-        let fileContent = try? String(contentsOfFile: location + name, encoding: String.Encoding.utf8)
-        return fileContent
-    }
-    
-    private func writeFile(contents: String, writeFileName: String?) {
-        var fileName = "output.json"
-        if writeFileName != nil { fileName = writeFileName! }
-        do {
-            try contents.write(toFile: location + fileName, atomically: false, encoding: String.Encoding.utf8)
-            OutputView.printSuccessMsg()
-        } catch let error as NSError {
-            OutputView.printAPIErrorMsg(errorCode: error)
         }
     }
     
