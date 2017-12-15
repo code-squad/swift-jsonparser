3644038ac45c04117df23f4de75eadbb39422fc6
waitNseE <yellow.cjh@gmail.com>
2017-12-15 15:56:40 +0900

    step1 테스트 코드 작성

--- JSONParser/TestJsonParser/TestJsonParser.swift
+++ JSONParser/TestJsonParser/TestJsonParser.swift
@@ -10,5 +10,31 @@ import XCTest
 
 class TestJsonParser: XCTestCase {
     
+    func testErrorOfInvalidInput () {
+        let invalidInput = "123,false,\"Jeonghoon\"]"
+        XCTAssertThrowsError(try SyntaxChecker.makeValidString(values: invalidInput)) { (error) -> Void in
+            XCTAssertEqual(error as? SyntaxChecker.ErrorMessage , SyntaxChecker.ErrorMessage.ofNoSquareBracket)
+        }
+    }
+    
+    func testIsSuccessMakingCountingDataInstance () {
+        let uncheckedValues = "[57,\"JeongHoon\",41,\"Korea\",120,\"make\",true,true,\"Choi\",2017]"
+        let checkedValues = try! SyntaxChecker.makeValidString(values: uncheckedValues)
+        let countedType: CountingData = Analyzer.makeCountedTypeInstance(checkedValues)
+        XCTAssertNotNil(countedType)
+    }
+    
+    func testIsSuccessMakingCountedTypeInstance () {
+        let uncheckedValues = "[120,\"minimum\",12,\"Jazz\",2015,true]"
+        let checkedValues = try! SyntaxChecker.makeValidString(values: uncheckedValues)
+        let numberValue: [Int] = [120,12,2015]
+        let boolValue: [Bool] = [true]
+        let stringValue: [String] = ["\"minimum\"","\"Jazz\""]
+        let countedType: CountingData = Analyzer.makeCountedTypeInstance(checkedValues)
+        XCTAssertEqual(countedType.countOfNumericValue, numberValue)
+        XCTAssertEqual(countedType.countOfBooleanValue, boolValue)
+        XCTAssertEqual(countedType.countOfStringValue, stringValue)
+        XCTAssertEqual(countedType.countOfTotalValue, 6)
+    }
     
 }
