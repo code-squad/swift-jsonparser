6b28dca2dae2b49e220f0f6456beee1fa93bed6a
waitNseE <yellow.cjh@gmail.com>
2017-12-15 14:15:02 +0900

    테스트파일 추가

---
+++ JSONParser/TestJsonParser/TestJsonParser.swift
@@ -0,0 +1,14 @@
+//
+//  TestJsonParser.swift
+//  TestJsonParser
+//
+//  Created by Choi Jeong Hoon on 2017. 12. 15..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import XCTest
+
+class TestJsonParser: XCTestCase {
+    
+    
+}
