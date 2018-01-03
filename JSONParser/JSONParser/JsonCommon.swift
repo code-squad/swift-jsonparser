5114644a1142c6c494474787fed394d74c73260b
waitNseE <yellow.cjh@gmail.com>
2018-01-03 14:28:29 +0900

    Any타입을 대체하기 위해 각 타입별 공통행동을 프로토콜로 추상화해서 타입 확장 및 채택

--- JSONParser/JSONParser/JsonCommon.swift
+++ JSONParser/JSONParser/JsonCommon.swift
@@ -14,17 +14,49 @@ protocol JsonDataCommon {
     func getCountedType () -> JsonData
 }
 
-extension Dictionary: JsonDataCommon {
+protocol JSONType {
+   func getJsontype () -> JSONType
+   func makeValueFromObject () -> String
+   func makeValueFromArray () -> String
+}
+
+extension Dictionary: JsonDataCommon, JSONType {
+    func makeValueFromArray() -> String {
+        return JsonPrintingMaker.makeObjectTypeForPrinting(self as! Dictionary<String, JSONType>) + "," + JsonPrintingMaker.makeNewLine()
+    }
+    
+    func makeValueFromObject() -> String {
+        return JsonPrintingMaker.makeObjectTypeForPrinting(self as! Dictionary<String, JSONType>)
+    }
+    
+    
+    
+    func getJsontype() -> JSONType {
+        return self as! Dictionary<String, JSONType>
+    }
+    
     func getCountedType() -> JsonData {
-        return CountingJsonData.getCountedObjectType(self as! Dictionary<String, Any>)
+        return CountingJsonData.getCountedObjectType(self as! Dictionary<String, JSONType>)
     }
     
     func readyforPrinting() -> String{
-        return JsonPrintingMaker.makeObjectTypeForPrinting(self as! Dictionary<String, Any>)
+        return JsonPrintingMaker.makeObjectTypeForPrinting(self as! Dictionary<String, JSONType>)
     }
 }
 
-extension Array: JsonDataCommon {
+extension Array: JsonDataCommon, JSONType {
+    func makeValueFromArray() -> String {
+        return JsonPrintingMaker.makeArrayTypeForPrinting(self as! Array<String>)
+    }
+    
+    func makeValueFromObject() -> String {
+        return JsonPrintingMaker.makeArrayTypeForPrinting(self as! [String])
+    }
+    
+    func getJsontype() -> JSONType {
+        return self as! [String]
+    }
+    
     func getCountedType() -> JsonData {
         return CountingJsonData.getCountedArrayType(self as!  [String])
     }
@@ -33,3 +65,47 @@ extension Array: JsonDataCommon {
         return JsonPrintingMaker.makeArrayTypeForPrinting(self as! [String])
     }
 }
+
+extension Int: JSONType {
+    func makeValueFromArray() -> String {
+        return String(describing: self)
+    }
+    
+    func makeValueFromObject() -> String {
+        return String(describing: self)
+    }
+    
+    func getJsontype() -> JSONType {
+        return Int(self)
+    }
+}
+
+extension String: JSONType {
+    func makeValueFromArray() -> String {
+        return JsonPrintingMaker.insertIndentation() + String(describing: self) + "," + JsonPrintingMaker.makeNewLine()
+    }
+    
+    func makeValueFromObject() -> String {
+        return "\"" + String(describing: self) + "\""
+    }
+    
+    func getJsontype() -> JSONType {
+        return String(self)
+    }
+}
+
+extension Bool: JSONType {
+    func makeValueFromArray() -> String {
+        return String(describing: self)
+    }
+    
+    func makeValueFromObject() -> String {
+        return String(describing: self)
+    }
+    
+    func getJsontype() -> JSONType {
+        return Bool(self)
+    }
+}
+
+
