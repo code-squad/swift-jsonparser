61d0a0b6071e137f9767c0f32465fc312d262ec1
Jack <somedd@naver.com>
2018-01-03 18:38:56 +0900

    Add enum Marks

--- JSONParser/JSONParser/MyData.swift
+++ JSONParser/JSONParser/MyData.swift
@@ -54,6 +54,16 @@ extension Dictionary : MyValues {
 
 extension String : MyValues {
     
+    enum Marks : String {
+        case whiteSpace = " "
+        case leftBracketOfObject = "{"
+        case rightBracketOfObject = "}"
+        case leftBracketOfArray = "["
+        case rightBracketOfArray = "]"
+        case markOfString = "\""
+        case markOfObject = ":"
+    }
+    
     func sliceByString(from:String, to:String) -> String? {
         let startRange = self.range(of: from)
         guard let firstRange = startRange else {return ""}
@@ -64,7 +74,7 @@ extension String : MyValues {
     }
     
     func makeDataType(_ userData: String) -> MyValues {
-        return sliceOneMark(userData, mark: "\"")
+        return sliceOneMark(userData, mark: Marks.markOfString.rawValue)
     }
     
     func checkDataType() -> MyValues {
@@ -81,23 +91,23 @@ extension String : MyValues {
     func sliceData () -> [String] {
         var temp : String = ""
         let dataWithoutMarks = sliceMarks(self)
-        let valuesInArray = dataWithoutMarks.sliceByString(from: "[", to: "]")
-        let valuesInObject = dataWithoutMarks.sliceByString(from: "{", to: "}")
-        temp = dataWithoutMarks.replacingOccurrences(of: valuesInArray ?? "", with: " ")
-        temp = temp.replacingOccurrences(of: valuesInObject ?? "", with: " ")
+        let valuesInArray = dataWithoutMarks.sliceByString(from: Marks.leftBracketOfArray.rawValue, to: Marks.rightBracketOfArray.rawValue)
+        let valuesInObject = dataWithoutMarks.sliceByString(from: Marks.leftBracketOfObject.rawValue, to: Marks.rightBracketOfObject.rawValue)
+        temp = dataWithoutMarks.replacingOccurrences(of: valuesInArray ?? "", with: Marks.whiteSpace.rawValue)
+        temp = temp.replacingOccurrences(of: valuesInObject ?? "", with: Marks.whiteSpace.rawValue)
         var seperatedData = temp.split(separator: ",").map(String.init)
         for index in 0..<seperatedData.count {
             if isObject(seperatedData[index]) {
-                seperatedData[index] = "{\(valuesInObject ?? "")}"
+                seperatedData[index] = "\(Marks.leftBracketOfObject.rawValue)\(valuesInObject ?? "")\(Marks.rightBracketOfObject.rawValue)"
             }
         }
         for index in 0..<seperatedData.count {
-            if isArray(self) && seperatedData[index].contains("[") && isObject(seperatedData[index]) == false {
-                seperatedData[index] = "[" + (valuesInArray ?? "") + "]"
+            if isArray(self) && seperatedData[index].contains(Marks.leftBracketOfArray.rawValue) && isObject(seperatedData[index]) == false {
+                seperatedData[index] = Marks.leftBracketOfArray.rawValue + (valuesInArray ?? "") + Marks.rightBracketOfArray.rawValue
                 continue
             }
             guard isObject(self) == true else { continue }
-            seperatedData[index] = seperatedData[index].replacingOccurrences(of: "]", with: "\(valuesInArray ?? "")]")
+            seperatedData[index] = seperatedData[index].replacingOccurrences(of: Marks.leftBracketOfArray.rawValue, with: "\(valuesInArray ?? "")\(Marks.rightBracketOfArray.rawValue)")
         }
         return seperatedData
     }
@@ -107,7 +117,7 @@ extension String : MyValues {
     }
     
     private func isStringType (_ oneData : String) -> Bool {
-        return oneData.first == "\"" && oneData.last == "\""
+        return oneData.first == Character(Marks.markOfString.rawValue) && oneData.last == Character(Marks.markOfString.rawValue)
     }
     
     private func isBoolType (_ oneData : String) -> Bool {
@@ -115,23 +125,23 @@ extension String : MyValues {
     }
     
     private func isArray (_ oneData : String) -> Bool {
-        return oneData.first == "["
+        return oneData.first == Character(Marks.leftBracketOfArray.rawValue)
     }
     
     private func isObject (_ oneData : String) -> Bool {
-        return oneData.first == "{"
+        return oneData.first == Character(Marks.leftBracketOfObject.rawValue)
     }
     
     func generateOneObjectData() -> (key : String, value : MyValues) {
-        var databeforeSeperating = self.split(separator: ":").map(String.init)
+        var databeforeSeperating = self.split(separator: Character(Marks.markOfObject.rawValue)).map(String.init)
         let tempKey = self.sliceMarks(databeforeSeperating[0])
         let tempValue = self.checkDataType().makeDataType(databeforeSeperating[1])
         return (tempKey, tempValue)
     }
     
-    func sliceOneMark (_ fullString : String, mark : Character) -> String {
+    func sliceOneMark (_ fullString : String, mark : String) -> String {
         var temp = ""
-        let stringsWithoutMark : [String] = fullString.split(separator: mark).map(String.init)
+        let stringsWithoutMark : [String] = fullString.split(separator: Character(mark)).map(String.init)
         for index in 0..<stringsWithoutMark.count {
             temp += stringsWithoutMark[index]
         }
@@ -139,7 +149,7 @@ extension String : MyValues {
     }
     
     func sliceMarks (_ userInput : String) -> String {
-        var userInputWithoutBracket = sliceOneMark(userInput, mark: " ")
+        var userInputWithoutBracket = sliceOneMark(userInput, mark: Marks.whiteSpace.rawValue)
         if isArray(userInputWithoutBracket) || isObject(userInputWithoutBracket) || isStringType(userInputWithoutBracket) {
             userInputWithoutBracket.removeFirst()
             userInputWithoutBracket.removeLast()
