b921431dd862f9a1e25d44a9152bad401d5679a4
youth27 <mayihelpu27@gmail.com>
2018-01-07 00:29:37 +0900

    Add isEnabledToMakeOutput()

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -6,27 +6,9 @@
 //  Copyright © 2017 JK. All rights reserved.
 //
 
-
 let inputView = InputView()
 let arguments = CommandLine.arguments
-var commandLineInput = (input: "", output: "")
-
-switch arguments.count {
-case 1:
-let userInput = inputView.askUserInput(message: "분석할 JSON 문자열을 입력하세요.")
-guard let inputString = userInput else {
-    throw GrammarChecker.FormatError.invalidInput
-    }
-commandLineInput = (input: inputString, output: "")
-//parse(jsonString: commandLineInput.input)
-
-case 2: print(2)
-case 3: print(3)
-default: print("default")
-}
-
-
-// readCommandLine
+var commandLineInput = (input: "", output: "", type: InputView.InputType.console)
 do {
     commandLineInput = try inputView.readCommandLine(arguments)
 } catch let error {
@@ -39,48 +21,33 @@ do {
         print(InputView.InputError.voidFile) //String init()에서의 에러
     }
 }
-
-var convertedValues : JSONData = JSONData.IntegerValue(0)
-var parseTarget = (parseValue: [String](), parseType: Parser.ParseTarget.list)
-
-
-
-// parse - for all cases of input
-func parse(jsonString: String) {
+    
 let grammarChecker = GrammarChecker()
-    do {
-        parseTarget = try grammarChecker.execute(jsonString)
-        convertedValues = try Parser.matchValues(parseTarget)
-    } catch let error as GrammarChecker.FormatError {
-        switch error {
-        case .invalidArray:
-            print(GrammarChecker.FormatError.invalidArray)
-        case .invalidObject:
-            print(GrammarChecker.FormatError.invalidObject)
-        case .invalidInput:
-            print(GrammarChecker.FormatError.invalidInput)
-        }
+var parseTarget = (parseValue: [String](), parseType: Parser.ParseTarget.list)
+var convertedValues : JSONData = JSONData.IntegerValue(0)
+var isEnabledToMakeOutput: Bool = false
+do {
+    parseTarget = try grammarChecker.execute(commandLineInput.input)
+    convertedValues = try Parser.matchValues(parseTarget)
+isEnabledToMakeOutput = true
+} catch let error as GrammarChecker.FormatError {
+    switch error {
+    case .invalidArray:
+        print(GrammarChecker.FormatError.invalidArray)
+    case .invalidObject:
+        print(GrammarChecker.FormatError.invalidObject)
+    case .invalidInput:
+        print(GrammarChecker.FormatError.invalidInput)
     }
-    catch let unKnownError {
-        print("Unknown error occured!: \(unKnownError)")
-    }
 }
 
-
-
-let counter = ValueCounter(targetToCount: convertedValues)
-let countInfo = counter.makeCountInfo()
-let outputView = OutputView()
-let resultData = ResultData()
-let resultText = resultData.make(convertedValues)
-
-
-// outputType
-func showOutputOnConsole() {
-    outputView.showResult(countInfo, parseTarget.parseType, text: resultText)
+if isEnabledToMakeOutput {
+    let counter = ValueCounter(targetToCount: convertedValues)
+    let countInfo = counter.makeCountInfo()
+    let outputView = OutputView(resultType: commandLineInput.type)
+    let resultData = ResultData()
+    let resultText = resultData.make(convertedValues)
+    outputView.showResult(countInfo,parseTarget.parseType,resultText, fileName: commandLineInput.output)
 }
 
-func makeOutputAsFile() {
-    outputView.makeOutputFile(text: resultText, file: commandLineInput.output)
-}
 
