97e037520387bdedafb8032786ff39a098d878d5
youth27 <mayihelpu27@gmail.com>
2018-01-06 18:18:04 +0900

    Add switch-case by coun size of arguments

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -10,6 +10,23 @@
 let inputView = InputView()
 let arguments = CommandLine.arguments
 var commandLineInput = (input: "", output: "")
+
+switch arguments.count {
+case 1:
+let userInput = inputView.askUserInput(message: "분석할 JSON 문자열을 입력하세요.")
+guard let inputString = userInput else {
+    throw GrammarChecker.FormatError.invalidInput
+    }
+commandLineInput = (input: inputString, output: "")
+//parse(jsonString: commandLineInput.input)
+
+case 2: print(2)
+case 3: print(3)
+default: print("default")
+}
+
+
+// readCommandLine
 do {
     commandLineInput = try inputView.readCommandLine(arguments)
 } catch let error {
@@ -22,33 +39,48 @@ do {
         print(InputView.InputError.voidFile) //String init()에서의 에러
     }
 }
-    
-let grammarChecker = GrammarChecker()
-var parseTarget = (parseValue: [String](), parseType: Parser.ParseTarget.list)
-var convertedValues : JSONData = JSONData.IntegerValue(0)
 
-do {
-    parseTarget = try grammarChecker.execute(commandLineInput.input)
-    convertedValues = try Parser.matchValues(parseTarget)
-} catch let error as GrammarChecker.FormatError {
-    switch error {
-    case .invalidArray:
-        print(GrammarChecker.FormatError.invalidArray)
-    case .invalidObject:
-        print(GrammarChecker.FormatError.invalidObject)
-    case .invalidInput:
-        print(GrammarChecker.FormatError.invalidInput)
+var convertedValues : JSONData = JSONData.IntegerValue(0)
+var parseTarget = (parseValue: [String](), parseType: Parser.ParseTarget.list)
+
+
+
+// parse - for all cases of input
+func parse(jsonString: String) {
+let grammarChecker = GrammarChecker()
+    do {
+        parseTarget = try grammarChecker.execute(jsonString)
+        convertedValues = try Parser.matchValues(parseTarget)
+    } catch let error as GrammarChecker.FormatError {
+        switch error {
+        case .invalidArray:
+            print(GrammarChecker.FormatError.invalidArray)
+        case .invalidObject:
+            print(GrammarChecker.FormatError.invalidObject)
+        case .invalidInput:
+            print(GrammarChecker.FormatError.invalidInput)
+        }
+    }
+    catch let unKnownError {
+        print("Unknown error occured!: \(unKnownError)")
     }
 }
+
+
 
 let counter = ValueCounter(targetToCount: convertedValues)
 let countInfo = counter.makeCountInfo()
 let outputView = OutputView()
 let resultData = ResultData()
 let resultText = resultData.make(convertedValues)
-outputView.showResult(countInfo, parseTarget.parseType, text: resultText)
-outputView.makeOutputFile(text: resultText, file: commandLineInput.output)
 
 
+// outputType
+func showOutputOnConsole() {
+    outputView.showResult(countInfo, parseTarget.parseType, text: resultText)
+}
 
+func makeOutputAsFile() {
+    outputView.makeOutputFile(text: resultText, file: commandLineInput.output)
+}
 
