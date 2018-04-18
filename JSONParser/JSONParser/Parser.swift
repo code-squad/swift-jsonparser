1cdce6fa9431f8a0e01c1240b22d73944c4a35b2
Moon <myssun0325@gmail.com>
2018-04-18 14:12:04 +0900

    Modify parse method in Parser Add getList method in Parser
    parse method makes JSONData object from tokens

--- JSONParser/JSONParser/Parser.swift
+++ JSONParser/JSONParser/Parser.swift
@@ -33,18 +33,43 @@ class Parser {
         return token
     }
     
-    func parse() throws {
+    func parse() throws -> JSONData {
+        typealias Data = (numbers: [Int], characters: [String], booleans: [Bool])
+        var data: Data = ([], [], [])
+        
         while let token = getNextToken() {
             switch token {
-            case .list:
-                getList()
+            case .list(let tokens):
+                let list: Data = getList(tokens)
+                data.numbers += list.numbers
+                data.characters += list.characters
+                data.booleans += list.booleans
             default:
                 throw Parser.Error.invalidToken(token)
             }
         }
+        
+        return JSONData(numbers: data.numbers, characters: data.characters, booleans: data.booleans)
     }
     
-    func getList() {
+    func getList(_ tokens: [Token]) -> ([Int], [String], [Bool]) {
+        var numbers: [Int] = [Int]()
+        var characters: [String] = [String]()
+        var booleans: [Bool] = [Bool]()
         
+        for token in tokens {
+            switch token {
+            case .boolean(let value):
+                booleans.append(value)
+            case .characters(let value):
+                characters.append(value)
+            case .number(let value):
+                numbers.append(value)
+            default:
+                break
+            }
+        }
+        
+        return (numbers, characters, booleans)
     }
 }
