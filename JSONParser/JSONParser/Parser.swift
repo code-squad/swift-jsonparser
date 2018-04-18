ca823af981abd7466007ae6dfb342147a45a6ccb
Moon <myssun0325@gmail.com>
2018-04-18 12:38:28 +0900

    Add parse method in Parser

--- JSONParser/JSONParser/Parser.swift
+++ JSONParser/JSONParser/Parser.swift
@@ -10,10 +10,41 @@ import Foundation
 
 class Parser {
     
+    enum Error: Swift.Error {
+        case unexpectedEndOfInput
+        case invalidToken(Token)
+    }
+    
     let tokens: [Token]
     var position = 0
     
     init(tokens: [Token]) {
         self.tokens = tokens
+    }
+    
+    // 토큰이 남았는지 확인 및 토큰 반환
+    func getNextToken() -> Token? {
+        guard position < tokens.count else {
+            return nil
+        }
+        
+        let token = tokens[position]
+        position += 1
+        return token
+    }
+    
+    func parse() throws {
+        while let token = getNextToken() {
+            switch token {
+            case .list:
+                getList()
+            default:
+                throw Parser.Error.invalidToken(token)
+            }
+        }
+    }
+    
+    func getList() {
+        
     }
 }
