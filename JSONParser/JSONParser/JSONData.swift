c1c0c818e62f3a7ef6a1edf05a140b1195adee0c
Moon <myssun0325@gmail.com>
2018-04-18 14:11:48 +0900

    Add JSONData struct

---
+++ JSONParser/JSONParser/JSONData.swift
@@ -0,0 +1,15 @@
+//
+//  JSONData.swift
+//  JSONParser
+//
+//  Created by moon on 2018. 4. 18..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct JSONData {
+    var numbers: [Int]
+    var characters: [String]
+    var booleans: [Bool]
+}
