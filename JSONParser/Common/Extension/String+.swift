9f28f2048afcc7db29a725d1ea3ecd18a6352c90
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-20 18:32:36 +0900

    trim, 정규표현식 구현

--- JSONParser/Common/Extension/String+.swift
+++ JSONParser/Common/Extension/String+.swift
@@ -5,5 +5,18 @@
 //  Created by Jung seoung Yeo on 2018. 4. 19..
 //  Copyright © 2018년 JK. All rights reserved.
 //
-
 import Foundation
+
+extension String {
+    // 공백 제거
+    func trim() -> String {
+        return self.replacingOccurrences(of: " ", with: "")
+    }
+    
+    func pattenMatching(_ patten: String) throws {
+        let regex = try NSRegularExpression(pattern: patten, options: [])
+        if !(regex.matches(in: self, options: [], range: NSMakeRange(0, self.count)).count > 0) {
+            throw JSONPaserErorr.isRegex
+        }
+    }
+}
