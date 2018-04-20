6da01fc676bdb153638f1238e22870442161cb52
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-20 18:31:25 +0900

    에러 처리 경우 구현

--- JSONParser/Common/JSONPaserErorr.swift
+++ JSONParser/Common/JSONPaserErorr.swift
@@ -6,4 +6,11 @@
 //  Copyright © 2018년 JK. All rights reserved.
 //
 
-import Foundation
+enum JSONPaserErorr: String, Error {
+    case isNil = "값이 없습니다."
+    case isJsonLexer = "JSON Lexer가 될 수 없습니다."
+    case isJsonPaser = "JSON Parser가 될 수 없습니다."
+    case isEmpty = "콤마 뒤에 값이 존재 하지 않습니다."
+    case isRegex = "규격이 맞지 않습니다."
+
+}
