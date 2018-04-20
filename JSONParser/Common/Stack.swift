8a8ea812640c620216a1d2935cae61d1dfa2cdd1
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-20 18:30:20 +0900

    stack 구현

--- JSONParser/Common/Stack.swift
+++ JSONParser/Common/Stack.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2018년 JK. All rights reserved.
 //
 
-import Foundation
+class Stack<T> {
+    
+    private var stack: [T]
+    
+    init() {
+        self.stack = [T]()
+    }
+    
+    init(_ stackForamt: [T]) {
+        self.stack = stackForamt
+    }
+    
+    func push(_ value : T) {
+        self.stack.append(value)
+    }
+    
+    func pop() -> T? {
+        return (!empty() ? self.stack.removeLast() : nil)
+    }
+    
+    func peek() -> T? {
+        return (!empty() ? self.stack[self.stack.count - 1] : nil) 
+    }
+    
+    func empty() -> Bool {
+        return stack.count == 0
+    }
+}
