af180e24f96891abd0bb99aa0cbb42af9d41e677
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-20 18:30:41 +0900

    Queue 구현

--- JSONParser/Common/Queue.swift
+++ JSONParser/Common/Queue.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2018년 JK. All rights reserved.
 //
 
-import Foundation
+class Queue<T> {
+    private var queue:[T]
+    
+    init(_ queueFormat: [T]){
+        self.queue = queueFormat
+    }
+    init() {
+        self.queue = [T]()
+    }
+    
+    func enqueue(_ value: T) {
+        self.queue.append(value)
+    }
+    
+    func dequeue() -> T? {
+        return (!empty() ? self.queue.remove(at: 0): nil)
+    }
+    
+    func font() -> T? {
+        return (!empty() ? self.queue[0] : nil)
+    }
+    
+    func empty() -> Bool {
+        return queue.count == 0
+    }
+    
+}
