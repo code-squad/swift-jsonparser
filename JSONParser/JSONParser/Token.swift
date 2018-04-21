bbcd27693c4d1c26da941bc199b84250e12e0b0c
Moon <myssun0325@gmail.com>
2018-04-21 11:42:18 +0900

    Add Token struct

---
+++ JSONParser/JSONParser/Token.swift
@@ -0,0 +1,14 @@
+//
+//  Token.swift
+//  JSONParser
+//
+//  Created by moon on 2018. 4. 20..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct Token {
+    var token: String
+    var objectToken: [String]
+}
