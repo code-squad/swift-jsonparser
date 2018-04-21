fce1488584774f83e5a66c53834cf0aa1030fe52
Moon <myssun0325@gmail.com>
2018-04-22 01:08:40 +0900

    Modify OutputView to reduce the logic in itself

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -11,49 +11,17 @@ import Foundation
 struct OutputView {
     
     static func printJSONData(_ jsonData: JSONPrintable) {
-        var result = "총 \(jsonData.total()) 데이터 중에 "
         
-        if jsonData.countObjects() == 1 {
-            if jsonData.countCharacters() > 0 {
-                result += jsonData.prefixOfCharacters
-                result += "\(jsonData.countCharacters())개,"
-            }
-            
-            if jsonData.countNumbers() > 0 {
-                result += jsonData.prefixOfNumbers
-                result += "\(jsonData.countNumbers())개,"
-            }
-            
-            if jsonData.countBooleans() > 0 {
-                result += jsonData.prefixOfBooleans
-                result += "\(jsonData.countBooleans())개,"
-            }
-        } else {
-            result += jsonData.prefixOfObjects
-            result += "\(jsonData.countObjects())개,"
-            
-            if jsonData.countCharacters() > 0 {
-                result += jsonData.prefixOfCharacters
-                result += "\(jsonData.countCharacters())개,"
-            }
-            
-            if jsonData.countNumbers() > 0 {
-                result += jsonData.prefixOfNumbers
-                result += "\(jsonData.countNumbers())개,"
-            }
-            
-            if jsonData.countBooleans() > 0 {
-                result += jsonData.prefixOfBooleans
-                result += "\(jsonData.countBooleans())개,"
-            }
+        let totalDataType: TotalDataType = jsonData.totalDataType()
+        var result = "총 \(jsonData.total())개의 \(totalDataType.rawValue) 데이터 중에 "
+        switch totalDataType {
+        case .list:
+            result += jsonData.countObjectDescription()
+        case .object:
+            result += jsonData.countValueDescription()
         }
-
         result.removeLast()
         result += "가 포함되어 있습니다."
-        
         print(result)
     }
 }
-
-
-//문자 숫자 부울 순서
