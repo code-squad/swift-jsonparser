a9f6f049cd578ca34787d0138e2d7c171ea1470b
Moon <myssun0325@gmail.com>
2018-05-02 21:37:19 +0900

    OutputView의 JSON문자열출력기능 추가

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -11,12 +11,12 @@ import Foundation
 protocol JSONPrintable {
     func totalDataCountDescription() -> String
     func countDataDescription() -> String
+    func validateJSONData() -> String
 }
 
 struct OutputView {
-    // 총 4개의 객체 데이터 중에 문자열 2개, 숫자 1개, 부울 1개가 포함되어 있습니다.
-    // 총 2개의 배열 데이터 중에 객체 2개가 포함되어 있습니다.
     static func printJSONData(_ jsonData: JSONPrintable) {
         print("\(jsonData.totalDataCountDescription())\(jsonData.countDataDescription())가 포함되어 있습니다.")
+        print(jsonData.validateJSONData())
     }
 }
