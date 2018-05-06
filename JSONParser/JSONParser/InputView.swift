db5cd57a58a8532c44a7640231fe7c8c02dab768
Moon <myssun0325@gmail.com>
2018-05-07 01:42:47 +0900

    InputView에러추가, File로Input읽기 메서드추가

--- JSONParser/JSONParser/InputView.swift
+++ JSONParser/JSONParser/InputView.swift
@@ -14,12 +14,38 @@ enum Question: String {
 
 struct InputView {
     
+    enum Error: Swift.Error {
+        case invalidInputFileName
+        case readFailFromFile
+        
+        var errorMessage: String {
+            switch self {
+            case .invalidInputFileName:
+                return "파일이름이 잘못되었습니다."
+            case .readFailFromFile:
+                return "파일로부터 Input 읽기 실패"
+            }
+        }
+    }
+    
+    private static let defaultOutputFileName: String = "output.json"
+    
     static func readInput(_ question: Question) -> String? {
         print(question.rawValue)
         guard let input = readLine() else {
             return nil
         }
         
+        return input
+    }
+
+    static func readInputFromFile(_ inputFileName: String) throws -> String {
+        guard let fileURL = Bundle.main.path(forResource: inputFileName, ofType: nil) else {
+            throw InputView.Error.invalidInputFileName
+        }
+        guard let input = try? String(contentsOfFile: fileURL, encoding: .utf8) else {
+            throw InputView.Error.readFailFromFile
+        }
         return input
     }
 }
