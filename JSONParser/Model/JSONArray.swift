d98ed68d545523067e6a2ef60fbde937a6c59966
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-05-12 03:16:45 +0900

    JSON 프로토콜을 채택한 모델 생성

--- JSONParser/Model/JSONArray.swift
+++ JSONParser/Model/JSONArray.swift
@@ -6,16 +6,71 @@
 //  Copyright © 2018년 JK. All rights reserved.
 //
 
-struct JSONArray: JSON {
-    private let jsonGenertor = JSONGenerator()
-    var jsonType: JSONType
+struct JsonArray: JSON {
     
-    init?(_ jsonType: JSONType) {
-        self.jsonType = jsonType
-        self.jsonGenertor.upwrapJSON(jsonType)
+    private var array: [Type] = []
+    
+    init(_ array: Type) {
+        if case .array(let arr) =  array {
+            self.array = arr
+        }
     }
     
-    func descriptionShow() -> String {
-        return self.jsonGenertor.descriptionMake()
+    func description() -> String {
+        var descriptionValue = "총 \(array.count) 개의 배열 데이터 중에 "
+        
+        let (stringCount, numberCount, booleanCount, objectCount, arrayCount) = calculationTypeCount()
+        
+        if stringCount > 0 {
+            descriptionValue += "문자열 \(stringCount)개 "
+        }
+        
+        if numberCount > 0 {
+            descriptionValue += "숫자 \(numberCount)개 "
+        }
+        
+        if booleanCount > 0 {
+            descriptionValue += "부울 \(booleanCount)개 "
+        }
+        
+        if objectCount > 0 {
+            descriptionValue += "객체 \(objectCount)개 "
+        }
+        
+        if arrayCount > 0 {
+            descriptionValue += "배열 \(arrayCount)개 "
+        }
+        
+        return descriptionValue + "가 포함되어 있습니다."
+    }
+    
+    func calculationTypeCount() -> (Int, Int, Int, Int, Int) {
+        
+        var stringCount = 0
+        var numberCount = 0
+        var booleanCount = 0
+        var objectCount = 0
+        var arrayCount = 0
+        
+        let array = self.array[0]
+        
+        if case .array(let arr) = array {
+            for value in arr {
+                switch value {
+                    case .string:
+                        stringCount += 1
+                    case .bool:
+                        booleanCount += 1
+                    case .number:
+                        numberCount += 1
+                    case .object:
+                        objectCount += 1
+                    case .array:
+                        arrayCount += 1
+                }
+            }
+        }
+        
+        return (stringCount, numberCount, booleanCount, objectCount, arrayCount)
     }
 }
--- JSONParser/Model/JSONObject.swift
+++ JSONParser/Model/JSONObject.swift
@@ -6,17 +6,65 @@
 //  Copyright © 2018년 JK. All rights reserved.
 //
 
-struct JSONObjectArray: JSON {
+struct JsonObject: JSON {
     
-    private let jsonGenertor = JSONGenerator()
-    private let jsonObjectArray: JSONType
+    private var object: [String: Type] = [:]
     
-    init?(_ jsonType: JSONType)  {
-        self.jsonObjectArray = jsonType
-        self.jsonGenertor.upwrapJSON(jsonObjectArray)
+    init(_ object: Type) {
+        if case .object(let obj) = object {
+            self.object = obj
+        }
     }
     
-    func descriptionShow() -> String {
-        return self.jsonGenertor.descriptionMake()
+    func description() -> String {
+        var descriptionValue = "총 \(object.count) 개의 객체 데이터 중에 "
+        
+        let (stringCount, numberCount, booleanCount, objectCount, arrayCount) = calculationTypeCount()
+        
+        if stringCount > 0 {
+            descriptionValue += "문자열 \(stringCount)개 "
+        }
+        
+        if numberCount > 0 {
+            descriptionValue += "숫자 \(numberCount)개 "
+        }
+        
+        if booleanCount > 0 {
+            descriptionValue += "부울 \(booleanCount)개 "
+        }
+        
+        if objectCount > 0 {
+            descriptionValue += "객체 \(objectCount)개 "
+        }
+        
+        if arrayCount > 0 {
+            descriptionValue += "배열 \(arrayCount)개 "
+        }
+        
+        return descriptionValue + "가 포함되어 있습니다."
+    }
+    
+    func calculationTypeCount() -> (Int, Int, Int, Int, Int) {
+        var stringCount: Int = 0
+        var numberCount: Int = 0
+        var booleanCount: Int = 0
+        var objectCount: Int = 0
+        var arrayCount: Int = 0
+        
+        for value in self.object.values {
+            switch value{
+                case .string:
+                    stringCount += 1
+                case .number:
+                    numberCount += 1
+                case .bool:
+                    booleanCount += 1
+                case .object:
+                    objectCount += 1
+                case .array:
+                    arrayCount += 1
+            }
+        }
+        return (stringCount, numberCount, booleanCount, objectCount, arrayCount)
     }
 }
