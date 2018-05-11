57cb1fa37dae4a969ef43c8308ad6d8f4ff0455c
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-05-12 03:15:09 +0900

    lexer throw 제거

--- JSONParser/JSONParser/Lexer.swift
+++ JSONParser/JSONParser/Lexer.swift
@@ -6,50 +6,46 @@
 //  Copyright © 2018년 JK. All rights reserved.
 //
 
-// parser에게 token을 넘겨주는 역할
-// lexer 의 첫 번째 것으로 판단 하는 것을 보고 Queue를 이용
 struct Lexer {
+
+    private let inputQueue: Queue<Character>
+    private let lexQueue: Queue<String>
     
-    private var lexer: Queue<Character>
-    private let tokens: Queue<String>
-    
-    init(_ lexerFormat: String) {
-        self.lexer = Queue(Array(lexerFormat.trim()))
-        self.tokens = Queue<String>()
+    init(_ inputData: String) {
+        self.inputQueue = Queue(Array(inputData))
+        self.lexQueue = Queue<String>()
     }
     
-    func getToken() throws -> Queue<String> {
-        var tokenValue : String = ""
-        
-        while let token = lexer.dequeue() {
+    func lex() -> Queue<String> {
+        var tokenValue: String = ""
+        while let token = inputQueue.dequeue() {
             switch token {
-                case TokenSplitUnit.startBracket.char:
-                    tokens.enqueue(String(token))
-                case TokenSplitUnit.endBrackert.char:
-                    tokenValue = setTokenValue(tokenValue)
-                    tokens.enqueue(String(token))
-                case TokenSplitUnit.startBrace.char:
-                    tokens.enqueue(String(token))
-                case TokenSplitUnit.endBrace.char:
-                    tokenValue = setTokenValue(tokenValue)
-                    tokens.enqueue(String(token))
-                case TokenSplitUnit.comma.char:
-                    tokenValue = setTokenValue(tokenValue)
-                    tokens.enqueue(String(token))
+                case TokenForm.openBracket.char:
+                    lexQueue.enqueue(TokenForm.openBracket.str)
+                case TokenForm.closeBracket.char:
+                    tokenValue = validTokenValue(tokenValue)
+                    lexQueue.enqueue(TokenForm.closeBracket.str)
+                case TokenForm.openBrace.char:
+                    lexQueue.enqueue(TokenForm.openBrace.str)
+                case TokenForm.closeBrace.char:
+                    tokenValue = validTokenValue(tokenValue)
+                    lexQueue.enqueue(TokenForm.closeBrace.str)
+                case TokenForm.colon.char:
+                    tokenValue = validTokenValue(tokenValue)
+                    lexQueue.enqueue(TokenForm.colon.str)
+                case TokenForm.comma.char:
+                    tokenValue = validTokenValue(tokenValue)
+                    lexQueue.enqueue(TokenForm.comma.str)
             default:
                 tokenValue += String(token)
             }
         }
-        
-        if tokens.isEmpty() {
-            throw JSONPaserErorr.isJsonLexer
-        }
-        return tokens
+        return lexQueue
     }
     
-    private func setTokenValue(_ tokenValue: String) -> String {
+    private func validTokenValue(_ tokenValue: String) -> String {
         if !tokenValue.isEmpty {
-            tokens.enqueue(tokenValue)
+            lexQueue.enqueue(tokenValue)
             return ""
         }
         return tokenValue
