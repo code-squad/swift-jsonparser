420569100723e4be81e110a1f7dc0d7edac02a65
DrakeYang <drake.the.programmer@gmail.com>
2018-05-14 19:04:49 +0900

    입력용 구조체 및 입력함수 , 입력에러함수 추가

---
+++ JSONParser/JSONParser/InputView.swift
@@ -0,0 +1,28 @@
+//
+//  InputView.swift
+//  JSONParser
+//
+//  Created by Yoda Codd on 2018. 5. 14..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    
+    /// 사용자 입력값을 받는 함수
+    func receiveUserInput() -> String{
+        // 입력을 위한 안내 메세지 출력
+        print("분석할 JSON 데이터를 입력하세요.")
+        guard let userInput =  readLine() else {
+            return ""
+        }
+        return userInput
+    }
+    
+    /// 잘못된 입력일 경우 에러메세지 출력
+    func printErrorMessage(){
+        print ("에러입니다")
+    }
+    
+}
