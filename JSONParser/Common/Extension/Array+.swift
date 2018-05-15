7e8e8d5e9ab993b20c24682a4c0e44f7633bece0
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-05-15 14:50:21 +0900

    JsonArray 제거 -> Array+에서 extension으로 구현

---
+++ JSONParser/Common/Extension/Array+.swift
@@ -0,0 +1,70 @@
+//
+//  Array+.swift
+//  JSONParser
+//
+//  Created by Jung seoung Yeo on 2018. 5. 14..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+extension Array: JSON {
+    
+    func description() -> String {
+        
+        var descriptionValue = "총 \(self.count) 개의 배열 데이터 중에 "
+        
+        let (stringCount, booleanCount, numberCount, objectCount, arrayCount) = calculationTypeCount()
+        
+        if stringCount > 0 {
+            descriptionValue += "문자열 \(stringCount)개 "
+        }
+        
+        if numberCount > 0 {
+            descriptionValue += "숫자 \(numberCount)개 "
+        }
+        
+        if booleanCount > 0 {
+            descriptionValue += "부울 \(booleanCount)개 "
+        }
+        
+        if objectCount > 0 {
+            descriptionValue += "객체 \(objectCount)개 "
+        }
+        
+        if arrayCount > 0 {
+            descriptionValue += "배열 \(arrayCount)개 "
+        }
+        
+        return descriptionValue + "가 포함되어 있습니다."
+    }
+    
+    func calculationTypeCount() -> (Int, Int, Int, Int, Int) {
+        var stringOfCount = 0
+        var booleaOfnCount = 0
+        var numberOfCount = 0
+        var objectOfCount = 0
+        var arrayOfCount = 0
+        
+        
+        for element in self {
+            let type = element as? Type
+            
+            if case .array(let arr)? = type {
+                for type in arr {
+                    switch type {                        
+                        case .string(_):
+                            stringOfCount += 1
+                        case .bool(_):
+                            booleaOfnCount += 1
+                        case .number(_):
+                            numberOfCount += 1
+                        case .object(_):
+                            objectOfCount += 1
+                        case .array(_):
+                            arrayOfCount += 1
+                    }
+                }
+            }
+        }
+        return (stringOfCount, booleaOfnCount, numberOfCount, objectOfCount, arrayOfCount)
+    }
+}
---
+++ JSONParser/Common/Extension/Dictionary+.swift
@@ -0,0 +1,9 @@
+//
+//  Dictionary+.swift
+//  JSONParser
+//
+//  Created by Jung seoung Yeo on 2018. 5. 14..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
