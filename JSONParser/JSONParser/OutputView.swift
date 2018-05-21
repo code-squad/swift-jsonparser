a0581dfe18e1fd995f5882feb04072c2eb0514b5
DrakeYang <drake.the.programmer@gmail.com>
2018-05-21 16:13:59 +0900

    출력담당 구조체 작성. JSON 객체를 받아서 각 항목의 숫자를 출력하는 함수 추가

---
+++ JSONParser/JSONParser/OutputView.swift
@@ -0,0 +1,12 @@
+//
+//  OutView.swift
+//  JSONParser
+//
+//  Created by Yoda Codd on 2018. 5. 21..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+struct OutView {
+    
+}
