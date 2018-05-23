2eb3ec3ab2678af057f49286d8fd53b1ca5fe223
DrakeYang <drake.the.programmer@gmail.com>
2018-05-23 17:14:48 +0900

    JSON스타일 문자열인지 체크해주는 함수 추가

--- JSONParser/JSONParser/Checker.swift
+++ JSONParser/JSONParser/Checker.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+
+struct Checker {
+    /// 문자열을 받아서 JSON 타입의 문자형인지 체크
+    static func isLettersForJSON(letter : String) -> Bool {
+        // 문자열의 첫번쨰와 마지막이 " 이면 참
+        return letter[letter.startIndex] == JSON.letterWrapper && letter[letter.endIndex] == JSON.letterWrapper
+    }
+}
