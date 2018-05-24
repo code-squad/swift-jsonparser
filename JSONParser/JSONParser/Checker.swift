ca6b6ab9b1057b1965fbda7e6d5ca8d151821e16
DrakeYang <drake.the.programmer@gmail.com>
2018-05-24 16:55:07 +0900

    마지막 인덱스 앞을 체크하도록 수정

--- JSONParser/JSONParser/Checker.swift
+++ JSONParser/JSONParser/Checker.swift
@@ -12,6 +12,6 @@ struct Checker {
     /// 문자열을 받아서 JSON 타입의 문자형인지 체크
     static func isLettersForJSON(letter : String) -> Bool {
         // 문자열의 첫번쨰와 마지막이 " 이면 참
-        return letter[letter.startIndex] == JSON.letterWrapper && letter[letter.endIndex] == JSON.letterWrapper
+        return letter[letter.startIndex] == JSON.letterWrapper && letter[letter.index(before:letter.endIndex)] == JSON.letterWrapper
     }
 }
