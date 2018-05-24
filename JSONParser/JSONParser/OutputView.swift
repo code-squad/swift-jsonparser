1bc4648c8c628701dea363c27971033c364fe1ad
DrakeYang <drake.the.programmer@gmail.com>
2018-05-24 17:19:34 +0900

    구조체 이름 변경. 입력받은 배열의 항목 타입 카운트하는 함수들 작성

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -7,6 +7,43 @@
 //
 
 import Foundation
-struct OutView {
+struct OutputView {
     
+    
+    /// JSON 데이터중 입력받은 자료형 타입이 몇개인지 리턴
+    private func countThat(targetType : Any.Type, dataSet : [Any] ) -> Int {
+        // 결과값 변수 선언
+        var countOfLetter = 0
+        // JSON 배열을 반복문에 넣는다
+        for data in dataSet {
+            if ObjectIdentifier(type(of:data)) == ObjectIdentifier(targetType) {
+                countOfLetter += 1
+            }
+        }
+        return countOfLetter
+    }
+    
+    /// 숫자 형태 값이 몇개인지 리턴
+    func countNumberType(dataSet : [Any]) -> Int{
+        // 인트 타입 변수
+        let intType : Int.Type = Int.self
+        // 계산함수를 리턴한다
+        return countThat(targetType: intType, dataSet : dataSet)
+    }
+    
+    /// 문자 형태 값이 몇개인지 리턴
+    func countLetterType(dataSet : [Any]) -> Int{
+        // 인트 타입 변수
+        let intType : String.Type = String.self
+        // 계산함수를 리턴한다
+        return countThat(targetType: intType, dataSet : dataSet)
+    }
+    
+    /// Bool 형태 값이 몇개인지 리턴
+    func countBoolType(dataSet : [Any]) -> Int{
+        // 인트 타입 변수
+        let intType : Bool.Type = Bool.self
+        // 계산함수를 리턴한다
+        return countThat(targetType: intType, dataSet : dataSet)
+    }
 }
