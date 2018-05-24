38df917e53a4980e72822a08da0d3552c8ce5a55
DrakeYang <drake.the.programmer@gmail.com>
2018-05-24 19:07:14 +0900

    배열 리턴함수 삭제. 종류별 카운트함수 추가

--- JSONParser/JSONParser/JSON.swift
+++ JSONParser/JSONParser/JSON.swift
@@ -24,8 +24,40 @@ struct JSON {
         self.dataSetOfJSON = dataSetOfJSON
     }
     
-    // 데이터를 리턴한다
-    func getDataOfJSON() -> [Any] {
-        return dataSetOfJSON
+    /// JSON 데이터중 입력받은 자료형 타입이 몇개인지 리턴
+    private func countThat(targetType : Any.Type) -> Int {
+        // 결과값 변수 선언
+        var countOfLetter = 0
+        // JSON 배열을 반복문에 넣는다
+        for data in dataSetOfJSON {
+            if ObjectIdentifier(type(of:data)) == ObjectIdentifier(targetType) {
+                countOfLetter += 1
+            }
+        }
+        return countOfLetter
     }
+    
+    /// 숫자 형태 값이 몇개인지 리턴
+    func countNumberType() -> Int{
+        // 인트 타입 변수
+        let intType : Int.Type = Int.self
+        // 계산함수를 리턴한다
+        return countThat(targetType: intType)
+    }
+    
+    /// 문자 형태 값이 몇개인지 리턴
+    func countLetterType() -> Int{
+        // 인트 타입 변수
+        let intType : String.Type = String.self
+        // 계산함수를 리턴한다
+        return countThat(targetType: intType)
+    }
+    
+    /// Bool 형태 값이 몇개인지 리턴
+    func countBoolType() -> Int{
+        // 인트 타입 변수
+        let intType : Bool.Type = Bool.self
+        // 계산함수를 리턴한다
+        return countThat(targetType: intType)
+    }    
 }
