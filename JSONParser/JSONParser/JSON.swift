412e368e05b14072e9aa4fbee6934be02bbdf40f
DrakeYang <drake.the.programmer@gmail.com>
2018-05-24 17:29:55 +0900

    생성자에 맞게 배열선언문 수정

--- JSONParser/JSONParser/JSON.swift
+++ JSONParser/JSONParser/JSON.swift
@@ -14,39 +14,18 @@ struct JSON {
     // JSON 에서 문자열을 감싸는 단위
     static let letterWrapper : Character = "\""
     // JSON 에서 Bool 타입을 표현하는 문자열의 배열
-    static let booleanType = ["false","true"]
+    static let booleanType = ["false", "true"]
+    
+    // JSON 타입의 배열
+    private let dataSetOfJSON : [Any] 
     
     // 생성자
-    init(countOfNumber : Int, countOfLetters : Int, countOfBool : Int){
-        self.countOfNumber = countOfNumber
-        self.countOfLetters = countOfLetters
-        self.countOfBool = countOfBool
+    init(dataSetOfJSON : [Any]){
+        self.dataSetOfJSON = dataSetOfJSON
     }
     
-    // JSON 의 숫자형 데이터의 개수
-    private var countOfNumber = 0
-    // JSON 의 문자열 데이터의 개수
-    private var countOfLetters = 0
-    // JSON 의 Bool 데이터의 개수
-    private var countOfBool = 0
-    
-    // 각 개수의 설정 함수
-    mutating func setCountOfNumber(number : Int){
-        countOfNumber = number
-    }
-    func getCountOfNumber() -> Int{
-        return countOfNumber
-    }
-    mutating func setCountOfLetters(number : Int){
-        countOfLetters = number
-    }
-    func getCountOfLetters() -> Int{
-        return countOfLetters
-    }
-    mutating func setCountOfBool(number : Int){
-        countOfBool = number
-    }
-    func getCountOfBool() -> Int{
-        return countOfBool
+    // 데이터를 리턴한다
+    func getDataOfJSON() -> [Any] {
+        return dataSetOfJSON
     }
 }
