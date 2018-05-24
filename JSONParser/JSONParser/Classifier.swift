1eddab34a81dae99e8b8f2972adf681b3728fc00
DrakeYang <drake.the.programmer@gmail.com>
2018-05-24 16:54:28 +0900

    중복함수 제거, 인덱스 설정값 수정

--- JSONParser/JSONParser/Classifier.swift
+++ JSONParser/JSONParser/Classifier.swift
@@ -11,7 +11,7 @@ import Foundation
 struct Classifier{
     
     /// 문자열을 받아서 목표문자의 위치 인덱스를 배열로 리턴
-    fileprivate func surveyLetterPositions(letters : String, targetLetter : Character) -> [String.Index]? {
+    private func surveyLetterPositions(letters : String, targetLetter : Character) -> [String.Index]? {
         // 문자가 한개도 없으면 닐 리턴
         guard letters.index(of: targetLetter) != nil else{
             return nil
@@ -44,7 +44,7 @@ struct Classifier{
     }
     
     /// 문자열을 받아서 특정 캐릭터로 둘러쌓인 부분의 인덱스를 배열로 리턴
-    fileprivate func surveyLetterRange(letters : String, targetLetter : Character) -> [Range<String.Index>]? {
+    private func surveyLetterRange(letters : String, targetLetter : Character) -> [Range<String.Index>]? {
         // 문자의 위치를 가진 배열 선언. 목표문자가 한개도 없으면 닐 리턴
         guard let letterIndexList = surveyLetterPositions(letters: letters, targetLetter: targetLetter) else {
             return nil
@@ -84,8 +84,8 @@ struct Classifier{
         return result
     }
     
-    /// 인덱스배열과 레인지배열을 받아서 레인지범위 안에 있는 인덱스를 로외한 인댁스배열을 리턴
-    fileprivate func removeDuplicatedIndexIn(indexRangeList : [Range<String.Index>], targetIndexes : [String.Index]) -> [String.Index]{
+    /// 인덱스배열과 레인지배열을 받아서 레인지범위 안에 있는 인덱스를 제외한 인댁스배열을 리턴
+    private func removeDuplicatedIndexIn(indexRangeList : [Range<String.Index>], targetIndexes : [String.Index]) -> [String.Index]{
         // 리턴용 인덱스 배열
         var resultIndexList : [String.Index] = []
         // 제거할 목표 인덱스 배열을 반복문에 넣는다
@@ -108,8 +108,8 @@ struct Classifier{
         return resultIndexList
     }
     
-    /// 문자열을 받아서 목표문자배열을 받아서 그 목표문자인덱스를 기준으로 나누어 레인지 인덱스 배열로 생성한다
-    fileprivate func separateByIndexes(letters : String, targetIndexes : [String.Index]) -> [Range<String.Index>] {
+    /// 문자열과 인덱스배열을 받아서 그 인덱스를 기준으로 나누어 레인지 인덱스 배열로 생성한다
+    private func separateByIndexes(letters : String, targetIndexes : [String.Index]) -> [Range<String.Index>] {
         // 쉼표 다음지점을 체크하기 위한 플래그
         var indexFlag = letters.startIndex
         // 결과 리턴용 배열
@@ -118,8 +118,8 @@ struct Classifier{
         for targetIndex in targetIndexes {
             // 앞지점과 타겟인덱스 앞칸의 범위를 결과에 저장
             result.append(indexFlag..<targetIndex)
-            // 타겟인덱스 뒤칸을 플래그에 저장
-            indexFlag = letters.index(after:targetIndex)
+            // 타겟인덱스 2칸 뒤를 플래그에 저장. JSON 데이터는 , 뒤에 빈칸을 한칸 두기 때문에 두칸 다음이 다음 데이터의 시작임
+            indexFlag = letters.index(targetIndex, offsetBy:2)
         }
         // 마지막 인덱스가 문자열의 끝이 아니라면
         if indexFlag != letters.endIndex {
@@ -130,35 +130,8 @@ struct Classifier{
         return result
     }
     
-    /// 문자열을 받아서 쉼표를 기준으로 나누어 레인지 인덱스 배열로 생성한다
-    fileprivate func separateByCommaIndexes(letters : String) -> [Range<String.Index>] {
-        // 쉼표 다음지점을 체크하기 위한 플래그
-        var indexFlag = letters.startIndex
-        // 결과 리턴용 배열
-        var result : [Range<String.Index>] = []
-        //입력받은 문자열 전체를 돈다
-        repeat {
-            // 쉼표 위치 변수. 쉼표가 없을경우 문자열의 끝을 표시
-            let commaPosition = letters[indexFlag..<letters.endIndex].index(of: ",") ?? letters.endIndex
-            // 해당 부분을 잘라서 문자형 배열에 추가
-            result.append(indexFlag..<commaPosition)
-            // 쉼표가 뒤에 더 있으면
-            if commaPosition != letters.endIndex {
-                // 쉼표 다음 글자를 가리킨다
-                indexFlag = letters.index(after:commaPosition)
-                // 마지막일 경우
-            } else {
-                // 문자열의 마지막을 가리킨다
-                indexFlag = letters.endIndex
-            }
-            // 위치 체크용 플래그가 문자열의 마지막을 가리키면 반복문 종료
-        } while indexFlag != letters.endIndex
-        // 결과를 리턴한다
-        return result
-    }
-    
     /// JSON 입력값을 받아서 , 기준으로 자르는 함수, " " 로 둘러쌓인 문자열 안의 , 는 자르지 않는다
-        func surveyLettersByJSON(letters : String) -> [String]?{
+        private func surveyLettersByJSON(letters : String) -> [String]?{
             // 결과 리턴용 변수
             var result : [String] = []
             // 문자열의 , 인덱스를 구한다
@@ -181,55 +154,8 @@ struct Classifier{
             return result
         }
     
-    /// 문자형 배열을 받아서 인트형으로 바뀔수 있는 개수를 리턴
-    func countNumberFrom(letters : [String]) -> Int {
-        // 결과값 리턴을 위한 카운트 변수
-        var countNumber = 0
-        // 문자형 배열을 반복한다
-        for number in letters {
-            // 인트형으로 변환이 가능하면
-            if Int(number) != nil {
-                // 결과값 +1 을 한다
-                countNumber += 1
-            }
-        }
-        // 결과값 리턴
-        return countNumber
-    }
-    
-    /// 문자형 배열을 받아서 false, true 에 맞는 항목이 몇개인지 리턴
-    func countBooleanFrom(letters : [String]) -> Int {
-        // 결과값 리턴을 위한 카운트 변수
-        var countBoolean = 0
-        // 문자형 배열을 반복한다
-        for boolean in letters {
-            // true or false 이면
-            if JSON.booleanType.contains(boolean) {
-                // 결과값 +1 을 한다
-                countBoolean += 1
-            }
-        }
-        // 결과값 리턴
-        return countBoolean
-    }
-    
-    /// 문자형 배열을 받아서 \" 로 둘라쌓인 항목이 몇개인지 리턴
-    func countDoubleQuatationLettersFrom(letters : [String]) -> Int {
-        // 결과값 리턴을 위한 카운트 변수
-        var countDoubleQuatation = 0
-        // 문자형 배열을 반복한다
-        for letter in letters {
-            // 처음과 끝이 \" 일 경우
-            if Checker.isLettersForJSON(letter: letter) {
-                // 결과값 +1 을 한다
-                countDoubleQuatation += 1
-            }
-        }
-        // 결과값 리턴
-        return countDoubleQuatation
-    }
     /// 문자열을 받아서 JSON 타입으로 리턴
-    func transformLetterToValueOfJSON(letter:String) -> Any? {
+    private func transformLetterToValueOfJSON(letter:String) -> Any? {
         // 인트형이 가능한지 체크. 변환가능하면 변환해서 추가
         if Int(letter) != nil {
             return Int(letter)!
@@ -243,11 +169,13 @@ struct Classifier{
             return (letter)
         }
         // 어느것도 매칭되지 않는다면 닐 리턴
-        else {return nil}
+        else {
+            return nil
+        }
     }
     
     /// 문자열 배열을 받아서 JSON 객체로 생성. 변환 불가능한 값이 있으면 닐 리턴
-    func transformLettersToJSON(letters:[String]) -> [Any]? {
+    private func transformLettersToJSON(letters:[String]) -> [Any]? {
         // 결과 출력용 배열 선언
         var result :[Any] = []
         // 배열을 반복분에 넣는다
