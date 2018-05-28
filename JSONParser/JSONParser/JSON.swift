437bdaf6979767572b357938157a2ac462defc9b
DrakeYang <drake.the.programmer@gmail.com>
2018-05-28 14:48:33 +0900

    데이터는 JSONData에 담고 JSON 구조체는 생성을 담당. JSON 데이터를 배열과 객체로 분리, 각각의 생성자 추가

--- JSONParser/JSONParser/JSON.swift
+++ JSONParser/JSONParser/JSON.swift
@@ -16,57 +16,12 @@ struct JSON {
     // JSON 에서 Bool 타입을 표현하는 문자열의 배열
     static let booleanType = ["false", "true"]
     // JSON 배열의 처음 문자
-    static let startOfArrayOfJSON = "["
+    static let startOfArrayOfJSON : Character  = "["
     // JSON 배열의 마지막 문자
-    static let endOfArrayOfJSON = "]"
+    static let endOfArrayOfJSON : Character  = "]"
     // JSON 오브젝트의 처음 문자
-    static let startOfObjectOfJSON = "{"
-    // JSON 오브젝트의 마짐가 문자
-    static let endOfObjectOfJSON = "}"
+    static let startOfObjectOfJSON : Character  = "{"
+    // JSON 오브젝트의 마지막 문자
+    static let endOfObjectOfJSON : Character  = "}"
     
-    
-    // JSON 타입의 배열
-    private let dataSetOfJSON : [Any] 
-    
-    // 생성자
-    init(dataSetOfJSON : [Any]){
-        self.dataSetOfJSON = dataSetOfJSON
-    }
-    
-    /// JSON 데이터중 입력받은 자료형 타입이 몇개인지 리턴
-    private func countThat(targetType : Any.Type) -> Int {
-        // 결과값 변수 선언
-        var countOfLetter = 0
-        // JSON 배열을 반복문에 넣는다
-        for data in dataSetOfJSON {
-            if ObjectIdentifier(type(of:data)) == ObjectIdentifier(targetType) {
-                countOfLetter += 1
-            }
-        }
-        return countOfLetter
-    }
-    
-    /// 숫자 형태 값이 몇개인지 리턴
-    func countNumberType() -> Int{
-        // 인트 타입 변수
-        let intType : Int.Type = Int.self
-        // 계산함수를 리턴한다
-        return countThat(targetType: intType)
-    }
-    
-    /// 문자 형태 값이 몇개인지 리턴
-    func countLetterType() -> Int{
-        // 인트 타입 변수
-        let intType : String.Type = String.self
-        // 계산함수를 리턴한다
-        return countThat(targetType: intType)
-    }
-    
-    /// Bool 형태 값이 몇개인지 리턴
-    func countBoolType() -> Int{
-        // 인트 타입 변수
-        let intType : Bool.Type = Bool.self
-        // 계산함수를 리턴한다
-        return countThat(targetType: intType)
-    }    
 }
--- JSONParser/JSONParser/JSONData.swift
+++ JSONParser/JSONParser/JSONData.swift
@@ -15,7 +15,7 @@ protocol JSONCount {
 
 
 /// JSON 배열타입 데이터
-class JSONArray : JSONCount {
+struct JSONArray : JSONCount {
     // 데이터를 배열로 담는다
     private let dataSetOfJSON : [Any]
     
@@ -26,67 +26,23 @@ class JSONArray : JSONCount {
     
     // 프로토콜을 준수한다
     func countValues() -> String {
-        let countOfNumber = countNumberType()
+        let countOfNumber = Checker.countIntFrom(targetArray: dataSetOfJSON)
         // 문자형 타입의 개수를 받을 변수 선언
-        let countOfLetter = countLetterType()
+        let countOfLetter = Checker.countStringFrom(targetArray: dataSetOfJSON)
         // Bool 형 타입의 개수를 받을 변수 선언
-        let countOfBool = countBoolType()
+        let countOfBool = Checker.countBoolFrom(targetArray: dataSetOfJSON)
         // 객체 타입 개수를 받을 변수 선언
-        let countOfObject = countOfObjectType()
+        let countOfObject = Checker.countObjectFrom(targetArray: dataSetOfJSON)
         // 합계 계산
         let totalCount = countOfNumber + countOfLetter + countOfBool + countOfObject
         // 요구조건의 형태대로 출력
         return ("총 \(totalCount)개의 배열 데이터 중에 문자열 \(countOfLetter)개, 숫자 \(countOfNumber)개, 부울 \(countOfBool)개, 객체 \(countOfObject)개가 포함되어 있습니다.")
     }
     
-    /// JSON 데이터중 입력받은 자료형 타입이 몇개인지 리턴
-    private func countThat(targetType : Any.Type) -> Int {
-        // 결과값 변수 선언
-        var countOfLetter = 0
-        // JSON 배열을 반복문에 넣는다
-        for data in dataSetOfJSON {
-            if ObjectIdentifier(type(of: data)) == ObjectIdentifier(targetType) {
-                countOfLetter += 1
-            }
-        }
-        return countOfLetter
-    }
-    
-    /// 숫자 형태 값이 몇개인지 리턴
-    func countOfObjectType() -> Int{
-        // 인트 타입 변수
-        let dictionaryType : [String:Any] = [:]
-        // 계산함수를 리턴한다
-        return countThat(targetType: type(of:dictionaryType))
-    }
-    
-    /// 숫자 형태 값이 몇개인지 리턴
-    func countNumberType() -> Int{
-        // 인트 타입 변수
-        let intType : Int.Type = Int.self
-        // 계산함수를 리턴한다
-        return countThat(targetType: intType)
-    }
-    
-    /// 문자 형태 값이 몇개인지 리턴
-    func countLetterType() -> Int{
-        // 인트 타입 변수
-        let intType : String.Type = String.self
-        // 계산함수를 리턴한다
-        return countThat(targetType: intType)
-    }
-    
-    /// Bool 형태 값이 몇개인지 리턴
-    func countBoolType() -> Int{
-        // 인트 타입 변수
-        let intType : Bool.Type = Bool.self
-        // 계산함수를 리턴한다
-        return countThat(targetType: intType)
-    }
 }
 
 /// JSON Object 타입
-class JSONObject : JSONCount {
+struct JSONObject : JSONCount {
     // 데이터를 딕셔너리로 담는다
     private let dataSetOfJSON : [String : Any]
     
@@ -98,52 +54,15 @@ class JSONObject : JSONCount {
     // 프로토콜을 준수. 데이터 타입을 종류별로 카운트 해서 리턴
     func countValues() -> String {
         // 숫자형 타입의 개수를 받을 변수 선언
-        let countOfNumber = countNumberType()
+        let countOfNumber = Checker.countIntFrom(targetObject: dataSetOfJSON)
         // 문자형 타입의 개수를 받을 변수 선언
-        let countOfLetter = countLetterType()
+        let countOfLetter = Checker.countStringFrom(targetObject: dataSetOfJSON)
         // Bool 형 타입의 개수를 받을 변수 선언
-        let countOfBool = countBoolType()
+        let countOfBool = Checker.countBoolFrom(targetObject: dataSetOfJSON)
         // 합계 계산
         let totalCount = countOfNumber + countOfLetter + countOfBool
         // 요구조건의 형태대로 출력
         return ("총 \(totalCount)개의 데이터 중에 문자열 \(countOfLetter)개, 숫자 \(countOfNumber)개, 부울 \(countOfBool)개가 포함되어 있습니다.")
-    }
-    
-    /// JSON 데이터중 입력받은 자료형 타입이 몇개인지 리턴
-    private func countThat(targetType : Any.Type) -> Int {
-        // 결과값 변수 선언
-        var countOfLetter = 0
-        // JSON 배열을 반복문에 넣는다
-        for keyData in dataSetOfJSON.keys {
-            if ObjectIdentifier(type(of:keyData)) == ObjectIdentifier(targetType) {
-                countOfLetter += 1
-            }
-        }
-        return countOfLetter
-    }
-    
-    /// 숫자 형태 값이 몇개인지 리턴
-    func countNumberType() -> Int{
-        // 인트 타입 변수
-        let intType : Int.Type = Int.self
-        // 계산함수를 리턴한다
-        return countThat(targetType: intType)
-    }
-    
-    /// 문자 형태 값이 몇개인지 리턴
-    func countLetterType() -> Int{
-        // 인트 타입 변수
-        let intType : String.Type = String.self
-        // 계산함수를 리턴한다
-        return countThat(targetType: intType)
-    }
-    
-    /// Bool 형태 값이 몇개인지 리턴
-    func countBoolType() -> Int{
-        // 인트 타입 변수
-        let intType : Bool.Type = Bool.self
-        // 계산함수를 리턴한다
-        return countThat(targetType: intType)
     }
     
 }
