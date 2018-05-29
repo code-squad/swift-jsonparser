5816ac3fe222aa30647c718358dbee17c52c9392
DrakeYang <drake.the.programmer@gmail.com>
2018-05-29 20:45:18 +0900

    JSONData 구조체 생성함수 추가. 사용 함수 외 함수들 private 처리. 입력값이 JSON 객체일 경우 생성함수 추가

--- JSONParser/JSONParser/JSON.swift
+++ JSONParser/JSONParser/JSON.swift
@@ -27,8 +27,150 @@ struct JSON {
     static let separaterForObject : Character = ":"
     
     
-    // 문자열 배열을 받아서 JSON 데이터형으로 바꿔서 리턴한다
-    static func makeJSONData(JSONData : String) -> JSONCount {
-        return JSONArray(dataSetOfJSON: <#T##[Any]#>)
+    /// 문자열을 받아서 JSON 타입으로 리턴
+    private func transformLetterToDataOfJSONObject(letter:String) -> Any? {
+        // 체크 구조체를 선언한다
+        let checker = Checker()
+        // 인트형이 가능한지 체크. 변환가능하면 변환해서 추가
+        if Int(letter) != nil {
+            return Int(letter)!
+        }
+            // Bool 타입인지 체크. 가능하면 변환해서 추가
+        else if JSON.booleanType.contains(letter){
+            return Bool(letter)!
+        }
+            // " 로 둘러쌓인 문자열인지 체크 후 추가
+        else if checker.isLettersForJSON(letter: letter){
+            return (letter)
+        }
+            // 어느것도 매칭되지 않는다면 닐 리턴
+        else {
+            return nil
+        }
+    }
+    
+    /// 키:벨류 로 붙어있는 문자열을 받아서 키부분 리턴
+    private func extractKeyFromObjectLetter(letter: String) -> String {
+        // : 을 기준으로 문자열을 자른다
+        let separatedLetter = letter.split(separator: JSON.separaterForObject)
+        // 키 부분의 공백을 지워주고 변수화 한다
+        let key = separatedLetter[0].trimmingCharacters(in: .whitespaces)
+        // " 을 제외한 나머지 문자열을 내보낸다
+        return key.replacingOccurrences(of: "\"", with: "")
+    }
+    
+    /// 키:벨류 로 붙어있는 문자열을 받아서 벨류 부분 리턴
+    private func extractValueFromObjectLetter(letter: String) -> Any {
+        // : 을 기준으로 문자열을 자른다
+        let separatedLetter = letter.split(separator: JSON.separaterForObject)
+        // 키 부분의 공백을 지워주고 변수화 한다
+        let value = separatedLetter[1].trimmingCharacters(in: .whitespaces)
+        // " 을 제외한 나머지 문자열을 내보낸다. 앞부분에서 체크 했음으로 강제 래핑
+        return transformLetterToDataOfJSONObject(letter: value)!
+    }
+    
+    /// , 로 나눠진 객체형 배열을 객체형 으로 만들어서 리턴
+    private func combineSeparatedJSONObeject(letters: [String]) -> [String:Any] {
+        // 결과용 JSON 객체형을 선언한다
+        var result : [String:Any] = [:]
+        // 각 키와 밸류를 반복문에 넣어서 JSON 타입이 맞는지 체크
+        for object in letters {
+            // 키:벨류 형태의 문자열을 받아서 결과 객체에 추가한다
+            let key = extractKeyFromObjectLetter(letter: String(object))
+            let value = extractValueFromObjectLetter(letter: String(object))
+            result[key] = value
+        }
+        // 결과를 리턴한다
+        return result
+    }
+    
+    /// {} 로 둘러 쌓이지 않은 문자열을 받아서 객체형인지 체크
+    private func transformLetterToJSONObjectWithoutWrapper(letter: String) -> [String:Any]? {
+        // 뒤에서 함수에 사용할 문자형 배열 선언
+        var separatedByComma : [String] = []
+        // , 를 기준으로 나눠서 배열로 만는다
+        let separatedSubSequencesByComma = letter.split(separator: JSON.separater)
+        // 문자열로 전환한다
+        for subSequence in separatedSubSequencesByComma {
+            separatedByComma.append(String(subSequence))
+        }
+        // 각 키와 밸류를 반복문에 넣어서 JSON 타입이 맞는지 체크, 결과를 리턴한다
+        return combineSeparatedJSONObeject(letters: separatedByComma)
+    }
+    
+    /// {} 로 둘러쌓인 문자열을 받아서 JSON 객체형으로 리턴
+    private func transformLetterToJSONObjectWithWrapper(letter: String) -> [String:Any]? {
+        // 앞의 함수에서 체크된 대로 {} 를 제거
+        let withoutWrapper = String(letter[letter.index(letter.startIndex, offsetBy: 1)..<letter.index(letter.endIndex, offsetBy: -1)])
+        // {} 가 없는 객체형 체크 함수를 리턴한다
+        return transformLetterToJSONObjectWithoutWrapper(letter: withoutWrapper)
+    }
+    
+    /// 문자열을 받아서 JSON 타입으로 리턴
+    private func transformLetterToDataOfJSONArray(letter:String) -> Any? {
+        // 체크 구조체를 선언한다
+        let checker = Checker()
+        // {} 로 둘러쌓인 문자열인지 체크 후 추가
+        if checker.checkWrappedObjectForJSON(letter: letter){
+            return transformLetterToJSONObjectWithWrapper(letter: letter)
+        }
+        // 객체형을 제외한 데이터 타입 리턴함수 사용
+        guard let transformedData = transformLetterToDataOfJSONObject(letter: letter) else {
+            // {} 이 외에 어느것도 매칭되지 않는다면 닐 리턴
+            return nil
+        }
+        // 결롸를 리턴
+        return transformedData
+    }    
+    
+    /// 문자열 배열을 받아서 JSON 배열로 생성. 변환 불가능한 값이 있으면 닐 리턴
+    private func transformLettersToJSONArray(letters:[String]) -> [Any]? {
+        // 결과 출력용 배열 선언
+        var result :[Any] = []
+        // 배열을 반복분에 넣는다
+        for letter in letters {
+            // JSON 에 추가 가능한지 체크. 변환가능하면 변환해서 추가
+            guard let transformedValue = transformLetterToDataOfJSONArray(letter: letter) else {
+                return nil
+            }
+            result.append(transformedValue)
+        }
+        // 결과 리턴
+        return result
+    }
+    
+    /// 문자열 배열을 받아서 JSON 배열로 생성. 변환 불가능한 값이 있으면 닐 리턴
+    private func transformLettersToJSONObject(letters:[String]) -> [String:Any]? {
+        return combineSeparatedJSONObeject(letters: letters)
+    }
+    
+    /// 문자열을 받아서 JSON 객체로 생성. 변환 불가능한 값이 있으면 닐 리턴
+    func transformLetterToJSON(letters:[String]) -> JSONCount? {
+        // 첫번째 배열을 받아서 어떤 형태인지 파악한다
+        let typeOfJSON = String(letters[letters.startIndex])
+        // 배열분류자 를 제외한 나머지 배열을 입력받는다
+        var dataOfJSON = letters
+        dataOfJSON.remove(at: 0)
+        // 만약 배열형테면
+        if typeOfJSON == "[" {
+            // 문자열 배열을 JSON 배열 형태로 만든다
+            guard let transformedLetters =  transformLettersToJSONArray(letters: dataOfJSON) else {
+                return nil
+            }
+            // 결과를 리턴한다
+            return JSONArray(dataSetOfJSON: transformedLetters)
+        }
+        // 객체 형태면
+        else if typeOfJSON == "{" {
+            // 문자열 배열을 JSON 객체 형태로 만든다
+            guard let transformedLetters =  transformLettersToJSONObject(letters: dataOfJSON) else {
+                return nil
+            }
+            // 결과를 리턴한다
+            return JSONObject(dataSetOfJSON: transformedLetters)
+        }
+        // 배열, 객체 형태도 아니면 닐 리턴
+        else { return nil }        
     }
 }
+
