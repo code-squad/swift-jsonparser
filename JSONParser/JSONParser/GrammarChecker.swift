e2320304fecf8883bb4e6c116f51f8d785c90bd0
DrakeYang <drake.the.programmer@gmail.com>
2018-05-31 17:00:16 +0900

    문자열 체크를 위한 구조체 추가

---
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -0,0 +1,15 @@
+//
+//  GrammarChecker.swift
+//  JSONParser
+//
+//  Created by Yoda Codd on 2018. 5. 31..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct GrammarChecker {
+    
+    
+    
+}
