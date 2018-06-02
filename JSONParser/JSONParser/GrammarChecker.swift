6ccb343bb89e758036a828dd16f196bfaef2a3c5
DrakeYang <drake.the.programmer@gmail.com>
2018-06-02 11:24:57 +0900

    객체 데이터 타입 체크 함수 추가

--- JSONParser/JSONParser/GrammarChecker.swift
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -21,14 +21,14 @@ struct GrammarChecker {
     static let regexObject = "^\\{ "+regexObjectValue+"(, "+regexObjectValue+")* \\}$"
     
     /// 문자열과 정규식을 받아서 정규식화 된 문자열 배열을 리턴한다
-    func extractRegexed(regexTry : String, originLetter: String) -> [String]? {
+    private static func extractRegexed(regexTry : String, originLetter: String) -> [String]? {
         let regexForm = try! NSRegularExpression(pattern: regexTry, options: [])
         let result =  regexForm.matches(in:originLetter,range: NSRange(originLetter.startIndex..., in: originLetter))
         return result.map { String(originLetter[Range($0.range, in: originLetter)!]) }
     }
     
     /// 입력받은 문자열이 "문자열" 형태인지 체크
-    func checkStringType(letter: String) -> Bool {
+    static func checkStringType(letter: String) -> Bool {
         // 입력받은 문자열을 문자열 정규식에 통과시킨다
         let regexedLetter = extractRegexed(regexTry: GrammarChecker.regexString, originLetter: letter)
         // 제대로 된 문자열이라면 입력값과 첫번째 정규식 항목과 같아야 한다
@@ -36,7 +36,7 @@ struct GrammarChecker {
     }
     
     /// 입력받은 문자열이 인트 형태인지 체크
-    func checkIntType(letter: String) -> Bool {
+    static func checkIntType(letter: String) -> Bool {
         // 입력받은 문자열을 문자열 정규식에 통과시킨다
         let regexedLetter = extractRegexed(regexTry: GrammarChecker.regexInt, originLetter: letter)
         // 제대로 된 문자열이라면 입력값과 첫번째 정규식 항목과 같아야 한다
@@ -44,17 +44,25 @@ struct GrammarChecker {
     }
     
     /// 입력받은 문자열이 참거짓 형태인지 체크
-    func checkBoolType(letter: String) -> Bool {
+    static func checkBoolType(letter: String) -> Bool {
         // 입력받은 문자열을 문자열 정규식에 통과시킨다
         let regexedLetter = extractRegexed(regexTry: GrammarChecker.regexBool, originLetter: letter)
         // 제대로 된 문자열이라면 입력값과 첫번째 정규식 항목과 같아야 한다
         return regexedLetter!.count > 0 && regexedLetter![0] == letter
     }
     
-    /// 입력받은 문자열이 "문자열" 형태인지 체크
-    func checkObjectType(letter: String) -> Bool {
+    /// 입력받은 문자열이 객체 형태인지 체크
+    static func checkObjectType(letter: String) -> Bool {
         // 입력받은 문자열을 문자열 정규식에 통과시킨다
         let regexedLetter = extractRegexed(regexTry: GrammarChecker.regexObject, originLetter: letter)
+        // 제대로 된 문자열이라면 입력값과 첫번째 정규식 항목과 같아야 한다
+        return regexedLetter!.count > 0 && regexedLetter![0] == letter
+    }
+    
+    /// 입력받은 문자열이 객체의 데이터 형태인지 체크
+    static func checkObjectValueType(letter: String) -> Bool {
+        // 입력받은 문자열을 문자열 정규식에 통과시킨다
+        let regexedLetter = extractRegexed(regexTry: GrammarChecker.regexObjectValue, originLetter: letter)
         // 제대로 된 문자열이라면 입력값과 첫번째 정규식 항목과 같아야 한다
         return regexedLetter!.count > 0 && regexedLetter![0] == letter
     }
