f220809fcf44985f6585f00efd051b5643e3117f
DrakeYang <drake.the.programmer@gmail.com>
2018-06-12 16:22:54 +0900

    객체형 체크하는 정규식에 각 데이터 사이에 빈칸 추가

--- JSONParser/JSONParser/GrammarChecker.swift
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct GrammarChecker {
     // 문자열 체크용 정규식
-    static let regexString = "^\\\\\\\".*\\\\\\\"$"
+    static let regexString = "^\\\".*\\\"$"
     // 숫자형 체크용 정규식
     static let regexInt = "^[0-9]*$"
     // 참거짓 체크용 정규식에 쓸 문자열 생성함수
@@ -29,19 +29,9 @@ struct GrammarChecker {
     // 참거짓 체크용 정규식
     static let regexBool = typeOfBool()
     // 객체의 키와 밸류 정규식
-    static let regexObjectValue = " \\\\\\\".+?\\\\\\\" : (true|false|\\\\\\\".+?\\\\\\\"|[0-9]*)"
+    static let regexObjectValue = "\\\".+?\\\" : (true|false|\\\".+?\\\"|[0-9]*)"
     // 객체 체크용 정규식. 키와 벨류 사이에 , 를 추가
-    static let regexObject = "^\\{"+regexObjectValue+"(,"+regexObjectValue+")* \\}$"
-    
-    static let testSpecial = "\\\\\\\""
-    /// 입력받은 문자열이 "문자열" 형태인지 체크
-    static func checktestSpecial(letter: String) -> Bool {
-        // 입력받은 문자열을 문자열 정규식에 통과시킨다
-        let regexedLetter = extractRegexed(regexTry: GrammarChecker.testSpecial, originLetter: letter)
-        // 제대로 된 문자열이라면 입력값과 첫번째 정규식 항목과 같아야 한다
-        return regexedLetter!.count > 0 && regexedLetter![0] == letter
-    }
-    
+    static let regexObject = "^\\{ "+regexObjectValue+"(, "+regexObjectValue+")* \\}$"
     
     /// 문자열과 정규식을 받아서 정규식화 된 문자열 배열을 리턴한다
     static func extractRegexed(regexTry : String, originLetter: String) -> [String]? {
