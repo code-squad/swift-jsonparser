e671010901f78bdb7ca726ac2395a36436e1504d
DrakeYang <drake.the.programmer@gmail.com>
2018-06-12 16:24:41 +0900

    단위테스트 종료

--- JSONParser/JSONTest/JSONTest.swift
+++ JSONParser/JSONTest/JSONTest.swift
@@ -20,33 +20,39 @@ class JSONTest: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        let checker = Checker()
-//        let input = "[{ \"name\" : \"KIM JUNG\", \"level\" : 5, \"married\" : true }, 4, false, \"sd, true, {\"dsf\" : 4} f\"]"
-        let input = "{ \"name\" : \"KIM JUNG\", \"level\" : 5, \"married\" : true }"
-        let classifier = Classifier()
-        
-        let separated = classifier.surveyLettersByJSON(letters: input)!
-        let json = JSON()
-        let result = json.transformLetterToJSON(letters: separated)
-        print (result?.countValues())
-        XCTAssert(separated == separated )
+    func test_JSONParser(){
+//        let str = [" { \"name\" : \"KIM JUNG\", \"level\" : 5, \"married\" : true }"," 4"," false"," \"sd, true, {\"dsf\" : 4} f\" "]
+        let str = [ "[", "{ \"name\" : \"KIM JUNG\", \"level\" : 5, \"married\" : true }"]
+        let js = JSONParser()
+        let flag = js.transform(letters: str)
+//        print( flag)
+        XCTAssert(flag != nil)
     }
+    
+    func testcheckWrappedBy() {
+        let a = "{ \"name\" : \"KIM JUNG\", \"level\" : 5, \"married\" : true }"
+        let regexedLetter = GrammarChecker.extractRegexed(regexTry: GrammarChecker.regexObject, originLetter: a)
+        print(regexedLetter)
+//        let b = GrammarChecker.checkObjectType(letter: a)
+        XCTAssert(true)
+    }
+    
     func testMain() {
-        let input = "[{ \"name\" : \"KIM JUNG\", \"level\" : 5, \"married\" : true }, 4, false, \"sd, true, {\"dsf\" : 4} f\"]"
-//        let input = "{ \"name\" : \"KIM JUNG\", \"level\" : 5, \"married\" : true }"
+//        let input = "[ { \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }, { \"name\" : \"YOON JISU\", \"alias\" : \"crong\", \"level\" : 4, \"married\" : true } ]"
+        let input = "{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }"
         // 입력값을 분류해주는 구조체 선언
         let classifier = Classifier()
         // 입력값을 JSON 스타일로 나눔
-        guard let separatedLetters = classifier.surveyLettersByJSON(letters: input) else {
+        guard let separatedLetters = classifier.surveyForJSON(letters: input) else {
             print("잘못된 입력")
             return ()
         }
-        
+        print (separatedLetters)
         // JSON 구조체 선언
-        let json = JSON()
+        let json = JSONParser()
         // JSON Data 생성
-        guard let dataOfJSON = json.transformLetterToJSON(letters: separatedLetters) else {
+        guard let dataOfJSON = json.transform(letters: separatedLetters) else {
+            print("지원하지 않는 형식을 포함하고 있습니다.")
             return ()
         }
         
@@ -55,6 +61,19 @@ class JSONTest: XCTestCase {
         outputView.printCountOfTypes(json: dataOfJSON)
     }
     
+    func test_Regex(){
+        let str = "{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }"
+        let regexed = GrammarChecker.extractRegexed(regexTry: GrammarChecker.regexObject, originLetter: str)
+        
+//        let str = "true"
+//        let regexed = gram.extractRegexed(regexTry: GrammarChecker.regexBool, originLetter: str)
+        
+        print(regexed)
+        
+//        let result = gram.checkObjectType(letter: str)
+//        XCTAssert(result)
+        
+    }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
