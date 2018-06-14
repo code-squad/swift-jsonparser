ba9ac6bff89f05d72284136f77306f8aed0441dc
DrakeYang <drake.the.programmer@gmail.com>
2018-06-14 17:17:22 +0900

    프로토콜에 객체인지 배열인지 알려줄 문자형 변수 추가. 객체와 배열에서 해당 변수 준수하도록 수정

--- JSONParser/JSONParser/JSONData.swift
+++ JSONParser/JSONParser/JSONData.swift
@@ -10,14 +10,19 @@ import Foundation
 
 /// JSON 데이터들이 준수하게 될 프로토콜 선언
 protocol JSONCount {
-    // 배열의 인트형 개수
+    // 인트형 개수
     var countOfInt : Int { get }
-    // 배열의 문자형 개수
+    // 문자형 개수
     var countOfString : Int { get }
-    // 배열의 Bool형 개수
+    // Bool형 개수
     var countOfBool : Int { get }
-    // 배열의 객체형 개수
-    var countOfObject : Int? { get }
+    // 객체형 개수
+    var countOfObject : Int { get }
+    // 배열형 개수
+    var countOfArray : Int { get }
+    
+    // 배열인지 객체인지 표시하는 문자열
+    let type : String { get }
 }
 
 /// JSON 에서 사용할 데이터 타입들을 선언
@@ -26,10 +31,13 @@ enum JSONData {
     case boolean(Bool)
     case letter(String)
     case object([String:JSONData])
+    case array([JSONData])
 }
 
 /// JSON 배열타입 데이터
 struct JSONArray : JSONCount {
+    // 배열임을 표시한다
+    var type = "배열"
     
     // 데이터를 배열로 담는다
     private let dataSetOfJSON : [JSONData]
@@ -49,7 +57,9 @@ struct JSONArray : JSONCount {
     // 배열의 Bool형 개수
     var countOfBool = 0
     // 배열의 객체형 개수
-    var countOfObject : Int? = 0
+    var countOfObject = 0
+    // 배열의 배열형 개수
+    var countOfArray = 0
     
     /// JSON 배열 데이터중 입력받은 자료형 별로 변수에 추가.
     mutating func countType() -> Bool {
@@ -64,7 +74,9 @@ struct JSONArray : JSONCount {
             // Bool형일 경우
             case .boolean(_) : countOfBool += 1
             // 객체형일 경우
-            case .object(_) : countOfObject! += 1
+            case .object(_) : countOfObject += 1
+            // 배열형의 경우
+            case .array(_) : countOfArray += 1
             }
         }
         return true
@@ -79,6 +91,9 @@ struct JSONArray : JSONCount {
 
 /// JSON Object 타입
 struct JSONObject : JSONCount {
+    // 객체임을 표시한다
+    var type = "겍체"
+    
     // 데이터를 딕셔너리로 담는다
     private let dataSetOfJSON : [String : JSONData]
     
@@ -96,8 +111,10 @@ struct JSONObject : JSONCount {
     var countOfString = 0
     // 객체의 Bool형 개수
     var countOfBool = 0
-    // 객체의 객체형 개수. 있을수 없기 때문에 닐 처리
-    var countOfObject : Int? = nil
+    // 객체의 객체형 개수
+    var countOfObject = 0
+    // 객체의 배열형 개수
+    var countOfArray = 0
     
     /// JSON 배열 데이터중 입력받은 자료형 별로 변수에 추가.
     mutating func countType() -> Bool {
@@ -111,8 +128,10 @@ struct JSONObject : JSONCount {
             case .letter(_) : countOfString += 1
             // Bool형일 경우
             case .boolean(_) : countOfBool += 1
-            // 그 외의 경우
-            default : return false
+            // 객체형일 경우
+            case .object(_) : countOfObject += 1
+            // 배열형의 경우
+            case .array(_) : countOfArray += 1
             }
         }
         return true
