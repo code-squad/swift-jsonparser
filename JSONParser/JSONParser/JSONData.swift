56c483c60c5d8fa33d1827105286fb0e5c89e2e5
DrakeYang <drake.the.programmer@gmail.com>
2018-06-15 16:50:21 +0900

    모든 구조체 클래스로 변경. 카운트 구조체를 수퍼클래스로 변경. 객체,배열형 클래스가 공통부분을 상속. 각 값을 카운트 하는 함수 프로토콜에 추가

--- JSONParser/JSONParser/JSONData.swift
+++ JSONParser/JSONParser/JSONData.swift
@@ -12,8 +12,19 @@ import Foundation
 protocol JSONCount {
     // 배열인지 객체인지 표시하는 문자열
     var type : String { get }
-    // 각 항목을 카운트 해서 저장하는 구조체
-    var jsonCounter : JSONCounter { get }
+    
+    // 인트형 개수
+    var countOfInt : Int { get }
+    // 문자형 개수
+    var countOfString : Int { get }
+    // Bool형 개수
+    var countOfBool : Int { get }
+    // 객체형 개수
+    var countOfObject : Int { get }
+    // 배열형 개수
+    var countOfArray : Int { get }
+    
+    func countType ()
 }
 
 /// JSON 에서 사용할 데이터 타입들을 선언
@@ -26,24 +37,20 @@ enum JSONData {
 }
 
 /// JSON 카운팅을 위한 구조체
-struct JSONCounter {
-    // 배열의 인트형 개수
+class JSONCounter {
+    // 인트형 개수
     var countOfInt = 0
-    // 배열의 문자형 개수
+    // 문자형 개수
     var countOfString = 0
-    // 배열의 Bool형 개수
+    // Bool형 개수
     var countOfBool = 0
-    // 배열의 객체형 개수
+    // 객체형 개수
     var countOfObject = 0
-    // 배열의 배열형 개수
+    // 배열형 개수
     var countOfArray = 0
     
-    func countTypes() -> (int: Int, string: Int, bool: Int, object: Int) {
-        return (countOfInt,countOfString,countOfBool,countOfObject)
-    }
-    
     /// JSON의 value 를 받아서 카운트
-    mutating func countValue(value: JSONData){
+    func countValue(value: JSONData){
         // 각 항목별 체크
         switch value {
         // 인트형일 경우
@@ -58,55 +65,51 @@ struct JSONCounter {
         case .array(_) : countOfArray += 1
         }
     }
+}
+
+/// JSON 배열타입 데이터
+class JSONArray : JSONCounter, JSONCount {
+    // 배열임을 표시한다
+    var type = "배열"
     
-    /// JSON 배열 데이터중 입력받은 자료형 별로 변수에 추가.
-    mutating func countType(dataSetOfJSONArray : [JSONData]){
-        // JSON 배열을 반복문에 넣는다
-        for value in dataSetOfJSONArray {
-            // 각 항목별 체크
-           countValue(value: value)
-        }
+    // 데이터를 배열로 담는다
+    private let dataSetOfJSON : [JSONData]
+    
+    // 생성자
+    init(dataSetOfJSON : [JSONData]){
+        self.dataSetOfJSON = dataSetOfJSON
     }
     
-    ///JSON 객체 데이터중 입력받은 자료형 별로 변수에 추가.
-    mutating func countType(dataSetOfJSONObject : [String : JSONData]){
+    /// JSON 배열 데이터중 입력받은 자료형 별로 변수에 추가.
+    func countType(){
         // JSON 배열을 반복문에 넣는다
-        for value in dataSetOfJSONObject.values {
+        for value in self.dataSetOfJSON {
             // 각 항목별 체크
             countValue(value: value)
         }
     }
 }
 
-/// JSON 배열타입 데이터
-struct JSONArray : JSONCount {
-    // 배열임을 표시한다
-    var type = "배열"
-    
-    // 데이터를 배열로 담는다
-    private let dataSetOfJSON : [JSONData]
-    var jsonCounter = JSONCounter()
-    
-    // 생성자
-    init(dataSetOfJSON : [JSONData]){
-        self.dataSetOfJSON = dataSetOfJSON
-        jsonCounter.countType(dataSetOfJSONArray: dataSetOfJSON)
-    }
-}
-
 /// JSON Object 타입
-struct JSONObject : JSONCount {
+class JSONObject : JSONCounter, JSONCount {
     // 객체임을 표시한다
     var type = "객체"
     
     // 데이터를 딕셔너리로 담는다
     private let dataSetOfJSON : [String : JSONData]
-    var jsonCounter = JSONCounter()
     
     // 생성자
     init(dataSetOfJSON : [String : JSONData]){
         self.dataSetOfJSON = dataSetOfJSON
-        jsonCounter.countType(dataSetOfJSONObject: dataSetOfJSON)
+    }
+    
+    ///JSON 객체 데이터중 입력받은 자료형 별로 변수에 추가.
+    func countType(){
+        // JSON 배열을 반복문에 넣는다
+        for value in self.dataSetOfJSON.values {
+            // 각 항목별 체크
+            countValue(value: value)
+        }
     }
 }
 
