88e53817d7414a9298d9c99feb12f4161aeade20
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-06 10:36:30 +0900

    Main

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -8,3 +8,12 @@
 
 import Foundation
 
+struct Main {
+    static func start(){
+        guard let input = InputView.read() else { return }
+        let jsonParser = JSONParser(elements: input)
+        OutputView.display(json: input, strings: jsonParser.strings, integers: jsonParser.integers, booleans: jsonParser.booleans)
+    }
+}
+
+Main.start()
