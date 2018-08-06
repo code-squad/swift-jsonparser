08ed5c88e10f7dc8735c2287f3c73435bc6863f8
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-06 10:36:07 +0900

    JSON Parser

---
+++ JSONParser/JSONParser/JSONParser.swift
@@ -0,0 +1,28 @@
+//
+//  JSONParser.swift
+//  JSONParser
+//
+//  Created by 이동건 on 03/08/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct JSONParser {
+    private var elements: [String]
+    private (set) var strings: [String] = []
+    private (set) var integers: [Int] = []
+    private (set) var booleans: [Bool] = []
+    
+    
+    init(elements: [String]) {
+        self.elements = elements
+        parse()
+    }
+    
+    mutating private func parse() {
+        strings = elements.filter {$0.contains("\"")}
+        booleans = elements.compactMap {Bool($0)}
+        integers = elements.compactMap {Int($0)}
+    }
+}
