53207113624e0934491a9930b8d89de353969854
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-06 11:17:56 +0900

    Separate Output by type

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -9,7 +9,28 @@
 import Foundation
 
 struct OutputView {
+    
     static func display(json: [String], strings: [String], integers: [Int], booleans: [Bool]) {
-        print("총 \(json.count)개의 데이터 중에 문자열 \(strings.count)개, 숫자 \(integers.count)개, 부울 \(booleans.count)개가 포함되어 있습니다.")
+        print("\(displayPrefix(json) ?? "")\(displayStrings(strings) ?? "")\(displayIntegers(integers) ?? "")\(displayBooleans(booleans) ?? "")\(displayPostFix(json) ?? "")")
+    }
+    
+    private static func displayPrefix(_ json: [String]) -> String? {
+        return json.count != 0 ? "총 \(json.count)개의 데이터 중에" : nil
+    }
+    
+    private static func displayStrings(_ strings: [String]) -> String? {
+        return strings.count != 0 ? "문자열 \(strings.count)개, " : nil
+    }
+    
+    private static func displayIntegers(_ integers: [Int]) -> String? {
+        return integers.count != 0 ? "숫자 \(integers.count)개, " : nil
+    }
+    
+    private static func displayBooleans(_ booleans: [Bool]) -> String? {
+        return booleans.count != 0 ? "부울 \(booleans.count)개" : nil
+    }
+    
+    private static func displayPostFix(_ json: [String]) -> String? {
+        return json.count != 0 ? "가 포함되어 있습니다." : nil
     }
 }
--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -12,7 +12,7 @@ struct Main {
     static func start(){
         guard let input = InputView.read() else { return }
         let jsonParser = JSONParser(elements: input)
-        OutputView.display(json: input, strings: jsonParser.strings, integers: jsonParser.integers, booleans: jsonParser.booleans)
+        OutputView.display(json: jsonParser.elements, strings: jsonParser.strings, integers: jsonParser.integers, booleans: jsonParser.booleans)
     }
 }
 
