db377d33bdd43b8cd1e5ed679fa02c41647a5d9a
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-06 17:50:52 +0900

    {}, [] parsing

--- JSONParser/JSONParser/JSONParser.swift
+++ JSONParser/JSONParser/JSONParser.swift
@@ -8,30 +8,55 @@
 
 import Foundation
 
+enum JSONKey: String {
+    case String = "문자열"
+    case Integer = "숫자"
+    case Boolean = "부울"
+}
+
 struct JSONParser {
     struct JSONParsedResult {
         private (set) var totalDataCounts: Int
-        private (set) var listOfStrings: [String]
-        private (set) var listOfIntegers: [Int]
-        private (set) var listOfBooleans: [Bool]
+        private (set) var resultDict: [JSONKey:Int] = [:]
         
         init(listOfStrings: [String], listOfIntegers: [Int], listOfBooleans: [Bool]) {
+            resultDict[.String] = listOfStrings.count
+            resultDict[.Integer] = listOfIntegers.count
+            resultDict[.Boolean] = listOfBooleans.count
             self.totalDataCounts = listOfStrings.count + listOfIntegers.count + listOfBooleans.count
-            self.listOfStrings = listOfStrings
-            self.listOfIntegers = listOfIntegers
-            self.listOfBooleans = listOfBooleans
         }
     }
     
-    static func result(from data: String) -> JSONParsedResult {
-        return parse(from: removeAccessory(from: data))
+    static func result(from data: String) -> [JSONParsedResult] {
+        let objects = generateObjects(from: data)
+        return objects.map {parse(from: removeAccessory(from: $0))}
     }
     
-    private static func removeAccessory(from data: String) -> [String] {
+    private static func generateObjects(from data: String) -> [String] { // "[{..}, {..}]" ---> ["{..}", "{..}"]
         var target = data
         target = target.replacingOccurrences(of: " ", with: "")
         target = target.replacingOccurrences(of: "[", with: "")
         target = target.replacingOccurrences(of: "]", with: "")
+        return splitByObject(from: target)
+    }
+    
+    private static func splitByObject(from data: String) -> [String] { // "{...},{...},{...}" ---> ["{...}", "{...}", "{...}"]
+        var target = data.components(separatedBy: "},{")
+        
+        if target.count == 1 {
+            return target
+        }
+        
+        target.enumerated().forEach {
+            $0.offset % 2 == 0 ? target[$0.offset].insert("}", at: target[$0.offset].endIndex) : target[$0.offset].insert("{", at: target[$0.offset].startIndex)
+        }
+        return target
+    }
+    
+    private static func removeAccessory(from data: String) -> [String] {
+        var target = data
+        target = target.replacingOccurrences(of: "{", with: "")
+        target = target.replacingOccurrences(of: "}", with: "")
         return target.split(separator: ",").map {String($0)}
     }
     
@@ -39,7 +64,13 @@ struct JSONParser {
         var strings: [String] = []
         var integers: [Int] = []
         var booleans: [Bool] = []
-        elements.forEach {
+        
+        let values:[String] = elements.compactMap {
+            let keyAndValues = $0.split(separator: ":")
+            return keyAndValues.count == 2 ? String(keyAndValues[1]) : nil
+        }
+        
+        values.forEach {
             switch $0 {
             case let value where value.contains("\""):
                 strings.append(value)
@@ -50,6 +81,7 @@ struct JSONParser {
             default: return
             }
         }
+        
         return JSONParsedResult(listOfStrings: strings, listOfIntegers: integers, listOfBooleans: booleans)
     }
 }
