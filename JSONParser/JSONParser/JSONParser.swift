ec38570f34a5c87c86553e1f22b86797edbf3a18
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-07 11:21:00 +0900

    키-값 쌍의 딕셔너리를 값의 타입에 따라 분류하여 저장

--- JSONParser/JSONParser/JSONParser.swift
+++ JSONParser/JSONParser/JSONParser.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+// MARK: 값에 따른 JSON내 키-값 쌍을 분류해주기 위한 Key 
 enum JSONKey: String {
     case String = "문자열"
     case Integer = "숫자"
@@ -15,23 +16,27 @@ enum JSONKey: String {
 }
 
 struct JSONParser {
+    // JSON Parsed Result
     struct JSONParsedResult {
         private (set) var totalDataCounts: Int
-        private (set) var resultDict: [JSONKey:Int] = [:]
+        private (set) var resultDict: [JSONKey:[[String:Any]]] = [:]
         
-        init(listOfStrings: [String], listOfIntegers: [Int], listOfBooleans: [Bool]) {
-            resultDict[.String] = listOfStrings.count
-            resultDict[.Integer] = listOfIntegers.count
-            resultDict[.Boolean] = listOfBooleans.count
+        init(listOfStrings: [[String:String]], listOfIntegers: [[String:Int]], listOfBooleans: [[String:Bool]]) {
+            resultDict[.String] = listOfStrings
+            resultDict[.Integer] = listOfIntegers
+            resultDict[.Boolean] = listOfBooleans
             self.totalDataCounts = listOfStrings.count + listOfIntegers.count + listOfBooleans.count
         }
     }
     
+    // 사용자 인풋으로부터 시작하는 메소드
     static func result(from data: String) -> [JSONParsedResult] {
         let objects = generateObjects(from: data)
         return objects.map {parse(from: removeAccessory(from: $0))}
     }
     
+    // MARK: 배열 속 JSON 객체를 추출
+    // Remove Accessory
     private static func generateObjects(from data: String) -> [String] { // "[{..}, {..}]" ---> ["{..}", "{..}"]
         var target = data
         target = target.replacingOccurrences(of: " ", with: "")
@@ -39,7 +44,7 @@ struct JSONParser {
         target = target.replacingOccurrences(of: "]", with: "")
         return splitByObject(from: target)
     }
-    
+    // Split
     private static func splitByObject(from data: String) -> [String] { // "{...},{...},{...}" ---> ["{...}", "{...}", "{...}"]
         var target = data.components(separatedBy: "},{")
         
@@ -53,35 +58,35 @@ struct JSONParser {
         return target
     }
     
+    //MARK: JSON 객체로부터 키-값 추출
+    // Remove Accessory
     private static func removeAccessory(from data: String) -> [String] {
         var target = data
         target = target.replacingOccurrences(of: "{", with: "")
         target = target.replacingOccurrences(of: "}", with: "")
         return target.split(separator: ",").map {String($0)}
     }
-    
+    // Parsing
     private static func parse(from elements : [String]) -> JSONParsedResult{
-        var strings: [String] = []
-        var integers: [Int] = []
-        var booleans: [Bool] = []
+        // elements - ["asdf:123",  ... ]
+        var stringTypeValues: [[String:String]] = []
+        var integersTypeValues: [[String:Int]] = []
+        var booleansTypeValues: [[String:Bool]] = []
         
-        let values:[String] = elements.compactMap {
-            let keyAndValues = $0.split(separator: ":")
-            return keyAndValues.count == 2 ? String(keyAndValues[1]) : nil
-        }
-        
-        values.forEach {
-            switch $0 {
+        elements.forEach { (element) in
+            let keyAndValue = element.split(separator: ":").map {String($0)}
+            if keyAndValue.count != 2 { return }
+            switch keyAndValue[1] {
             case let value where value.contains("\""):
-                strings.append(value)
+                stringTypeValues.append([keyAndValue[0]:keyAndValue[1]])
             case let value where Int(value) != nil:
-                integers.append(Int(value)!)
+                integersTypeValues.append([keyAndValue[0]:Int(keyAndValue[1])!])
             case let value where Bool(value) != nil:
-                booleans.append(Bool(value)!)
+                booleansTypeValues.append([keyAndValue[0]:Bool(keyAndValue[1])!])
             default: return
             }
         }
         
-        return JSONParsedResult(listOfStrings: strings, listOfIntegers: integers, listOfBooleans: booleans)
+        return JSONParsedResult(listOfStrings: stringTypeValues, listOfIntegers: integersTypeValues, listOfBooleans: booleansTypeValues)
     }
 }
