e25ccac10f138bd8320d9155e6615b1226166aa6
oingbong <oingbong@gmail.com>
2018-08-09 14:15:50 +0900

    Add Check Pattern : Not allow Object & Array

--- JSONParser/JSONParser/GrammarChecker.swift
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -71,6 +71,13 @@ extension String {
         return count
     }
     
+    /*
+     TIP
+     대괄호 \\[ or \\]
+     따옴표 \"
+     모든문자(특수문자,공백포함) .*?
+     */
+    
     // 숫자 검사 : 있으면 return true OR 없으면 return false
     func isNumber() -> Bool {
         if let regex = try? NSRegularExpression(pattern: "^[0-9]*$", options: []){
@@ -109,6 +116,75 @@ extension String {
             guard result else { return true }
         }
         return false
+    }
+    
+    // [] 배열 안에 ":" 형식 검사 : 있으면 return true OR 없으면 return false
+    mutating func hasObjectInArray() -> Bool {
+        self.removeFirst()
+        self.removeLast()
+        
+        let elements = self.components(separatedBy: ",")
+        for element in elements {
+            if let regex = try? NSRegularExpression(pattern: "[:]", options: []){
+                let string = element as NSString
+                let result = regex.matches(in: element, options: [], range: NSRange(location: 0, length: string.length)).isEmpty
+                guard result else { return true }
+            }
+        }
+        return false
+    }
+    
+    // {} 객체 안에 [] or {} 형식 검사 : 있으면 return true OR 없으면 reutrn false
+    mutating func hasArrayInObject() -> Bool {
+        self.removeFirst()
+        self.removeLast()
+        print(self)
+        let elements = self.components(separatedBy: ",")
+        for element in elements {
+            print("element : \(element)")
+            if let regex = try? NSRegularExpression(pattern: "(\\[|\\])", options: []){
+                let string = element as NSString
+                let result = regex.matches(in: element, options: [], range: NSRange(location: 0, length: string.length)).isEmpty
+                guard result else { return true }
+            }
+        }
+        return false
+    }
+    
+    /*
+     [] 배열안에 형식 검사 : 있으면 return true OR 없으면 reutrn false
+     [ "모든문자(특수문자,공백포함)" : "모든문자(특수문자,공백포함)" ]
+     [ "name" : "KIM JUNG" ]
+     
+     ** 띄어쓰기 또한 .*? 를 이용하여 패턴검사하였습니다.
+     
+     정의한 형식이 맞다면 return false OR 아니라면 return false
+     */
+    mutating func unsupportedArrayTypes() -> Bool {
+        var result = false
+        if let regex = try? NSRegularExpression(pattern: "\\[.*?\".*?\".*?:.*?\".*?\".*?\\]", options: .caseInsensitive){
+            let string = self as NSString
+            result = regex.matches(in: self, options: [], range: NSRange(location: 0, length: string.length)).isEmpty
+        }
+        return result
+    }
+    
+    /*
+     {} 객체안에 [] 형식 검사 : 있으면 return true OR 없으면 reutrn false
+     {모든문자:[]}
+     { "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "children" : ["hana", "hayul", "haun"] }
+     
+     ** 띄어쓰기 또한 .*? 를 이용하여 패턴검사하였습니다.
+     
+     정의한 형식이 맞다면 return false OR 아니라면 return false
+     */
+    mutating func unsupportedObjectTypes() -> Bool {
+        var result = false
+        if let regex = try? NSRegularExpression(pattern: "\\{.*?\\[.*?\\].*?\\}", options: .caseInsensitive){
+            let string = self as NSString
+            result = regex.matches(in: self, options: [], range: NSRange(location: 0, length: string.length)).isEmpty
+        }
+        return result
     }
     
 }
--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -13,12 +13,26 @@ func analyzeJson() -> Bool {
     // 입력받기
     let inputValue = InputView.readInput()
     // 입력값 비어있는지 확인
-    guard let input = InputView.isEmpty(to: inputValue) else { return true }
+    guard var input = InputView.isEmpty(to: inputValue) else { return true }
+
+    // 배열 형식 검사
+    guard input.unsupportedArrayTypes() else {
+        print("지원하지 않는 형식을 포함하고 있습니다.")
+        return true
+    }
+    
+    // 객체 형식 검사
+    guard input.unsupportedObjectTypes() else {
+        print("지원하지 않는 형식을 포함하고 있습니다.")
+        return true
+    }
+    
     // {} or [] 쌍 확인
     guard GrammarChecker.checkPair(to: input) else {
         print("{} or [] 개수가 일치하지 않습니다. 다시 입력하세요.")
         return true
     }
+    
     // 패턴 확인
     guard GrammarChecker.checkPattern(to: input) else {
         print("패턴이 일치하지 않습니다. 다시 입력하세요.")
