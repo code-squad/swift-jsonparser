43191db1a163f6658f5c5607a5d0eec7834153a9
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-14 16:30:09 +0900

    Tokenizer 수정

--- JSONParser/JSONParser/Tokenizer.swift
+++ JSONParser/JSONParser/Tokenizer.swift
@@ -42,19 +42,32 @@ enum Components {
 
 struct Tokenizer {
     static func parse(_ target: String) throws -> [String] {
+        var value = target
         var values:[String] = []
         var token = ""
         var isString = false
         var isObject = false
+        var isArray = false
         
-        for particle in target {
+        if target.first == Components.arrayOpener.value && target.last == Components.arrayCloser.value {
+            value.removeFirst()
+            value.removeLast()
+        }
+        
+        for particle in value {
             switch particle {
             case Components.arrayOpener.value:                          // 배열의 시작
-                if isString {
-                    token += String(particle)                           // 문자열의 일부라면 토큰에 추가
+                if !isString {
+                    isArray = !isArray
                 }
-            case Components.arrayCloser.value, Components.comma.value:
-                if isString || isObject {
+                token += String(particle)
+            case Components.arrayCloser.value:
+                token += String(particle)
+                if !isString {
+                    isArray = !isArray
+                }
+            case Components.comma.value:
+                if isString || isObject || isArray {
                     token += String(particle)                           // 문자열의 일부이거나 객체라면 토큰에 추가
                 }else {
                     values.append(token)                                // 배열이 끝났거나 ,로 구분되었으니 토큰을 저장
@@ -70,20 +83,19 @@ struct Tokenizer {
                 if isObject {
                     isObject = !isObject                                // 현재 객체의 토큰 값이 진행중이라면 객체가 끝났다는 것을 의미
                 }
-                if particle == target.last {
-                    values.append(token)                                // 단일 객체라면 }가 마지막, 토큰에 추가되어 있던 값을 저장
-                    token = ""
-                }
             case Components.doubleQuote.value:
                 isString = !isString                                    // 문자열의 시작과 끝을 의미
                 token += String(particle)
             case Components.space.value:
-                if isString || isObject {                               // 문자열의 일부이거나 객체라면 토큰에 추가
+                if isString || isObject || isArray {                    // 문자열의 일부이거나 객체라면 토큰에 추가
                     token += String(particle)
                 }
             default:
                 token += String(particle)
             }
+        }
+        if token != "" {
+            values.append(token)
         }
         if values.count == 0 {
             throw JSONParserError.invalidInput
