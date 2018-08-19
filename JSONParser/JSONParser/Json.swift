b209a46f2d7a93a5e734a061541f2805189d495b
oingbong <oingbong@gmail.com>
2018-08-19 17:16:41 +0900

    Add extention of Jsonable for generateData

--- JSONParser/JSONParser/Json.swift
+++ JSONParser/JSONParser/Json.swift
@@ -10,6 +10,7 @@ import Foundation
 
 protocol Jsonable {
     func countData() -> (Int,Int,Int,Int,Int)
+    func generateData() -> String
 }
 
 enum JsonType:Equatable {
@@ -18,4 +19,66 @@ enum JsonType:Equatable {
     case bool(Bool)
     case object([String:JsonType])
     case array([JsonType])
+}
+
+extension Jsonable {
+    func makeArray(intent : Int, elements:[JsonType]) -> String {
+        var message = "["
+        for value in elements {
+            if message.hasSuffix("}, ") {
+                message += "\n"
+                message += "\t"
+            }
+            switch value {
+            case .string(let element):
+                message += "\(element), "
+            case .int(let element):
+                message += "\(String(element)), "
+            case .bool(let element):
+                message += "\(String(element)), "
+            case .object(let element):
+                message += "\(self.makeObject(intent: intent + 1, elements: element)), "
+            case .array(let element):
+                message += "\(self.makeArray(intent: intent + 1, elements: element)), "
+            }
+        }
+        message.removeLast(2) // for remove last ","
+        if message.hasSuffix("]") {
+            message += "\n"
+        }
+        message += "]"
+        return message
+    }
+    
+    func makeObject(intent : Int, elements : [String:JsonType]) -> String {
+        var message = "{"
+        message += "\n"
+        for (key,value) in elements {
+            for _ in 0..<intent {
+                message += "\t"
+            }
+            switch value {
+            case .string(let element):
+                message += "\(key) : \(element),"
+            case .int(let element):
+                message += "\(key) : \(String(element)),"
+            case .bool(let element):
+                message += "\(key) : \(String(element)),"
+            case .object(let element):
+                message += "\(key) : "
+                message += "\(self.makeObject(intent: intent + 1, elements: element)),"
+            case .array(let element):
+                message += "\(key) : "
+                message += "\(self.makeArray(intent: intent + 1, elements: element)),"
+            }
+            message += "\n"
+        }
+        message.removeLast(2) // for remove last "," (뒤에서 두번째 index를 삭제)
+        message += "\n"
+        for _ in 0..<intent-1 {
+            message += "\t"
+        }
+        message += "}"
+        return message
+    }
 }
