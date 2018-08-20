76d3bd144c4e50d90081b78a3ce0af24d1496b72
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-20 15:53:52 +0900

    OutputView로부터 JSON 포멧 출력 형식 생성 분리

--- JSONParser/JSONParser/JSONType.swift
+++ JSONParser/JSONParser/JSONType.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 protocol JSONType {
+    var text: String { get }
     var values: JSONValueType { set get }
     var prefix: String { get }
     var result: (string:Int, int:Int, bool:Int, object: Int, array: Int) { get }
 }
-
 
 enum JSONValueType {
     case string(String)
@@ -23,8 +23,89 @@ enum JSONValueType {
     case array([JSONValueType])
 }
 
+class PrettyJSONDiscripter {
+    var stageOfArray: Int = 0
+    var stageOfObject: Int = 0
+    
+    func presentArray(from array: [JSONValueType]) -> String {
+        let stage = stageOfArray > 1 ? stageOfArray - 1 : stageOfArray
+        var text = "\(stageOfArray > 0 ? "\n" : "")\(tab(at: stage))["
+        for (index, element) in array.enumerated() {
+            
+            switch element {
+            case .array(_), .object(_):
+                stageOfArray += 1
+            default: break
+            }
+            
+            text += presentValue(from: element)
+            if index != array.count - 1 {
+                text += ","
+            }
+        }
+        text += "]\(stageOfArray > 0 ? "\n" : "")"
+        return text
+    }
+    
+    func presentObject(from objects: [[String:JSONValueType]]) -> String {
+        stageOfObject += 1
+        let currentStage = stageOfObject + stageOfArray
+        
+        var text = "{"
+        for (index, object) in objects.enumerated() {
+            text += presentPair(from: object, stage: currentStage)
+            
+            if index != objects.count - 1 {
+                text += ","
+            }
+        }
+        
+        text += "\n\(tab(at: currentStage - 1))}"
+        
+        return text
+    }
+    
+    func presentPair(from pair: [String:JSONValueType], stage: Int) -> String {
+        var text = ""
+        let keys = pair.keys.map {String($0)}
+        let values = pair.values.map {$0}
+        
+        text += "\n\(tab(at: stage))\(keys[0]) : \(presentValue(from: values[0]))"
+        return text
+    }
+    
+    func presentValue(from value: JSONValueType) -> String {
+        switch value {
+        case .string(let stringValue):
+            return stringValue
+        case .int(let integerValue):
+            return "\(integerValue)"
+        case .bool(let booleanValue):
+            return "\(booleanValue)"
+        case .object(let objectValue):
+            return presentObject(from: objectValue)
+        case .array(let arrayValue):
+            return presentArray(from: arrayValue)
+        }
+    }
+    
+    func tab(at stage: Int) -> String {
+        if stage <= 0 {
+            return ""
+        }
+        
+        var tabs = ""
+        
+        for _ in (0..<stage) {
+            tabs += "\t"
+        }
+        
+        return tabs
+    }
 
-struct JSONArray: JSONType {
+}
+
+class JSONArray: PrettyJSONDiscripter, JSONType {
     var prefix: String {
         return "배열안에는"
     }
@@ -59,12 +140,16 @@ struct JSONArray: JSONType {
         return (string, int, bool, object, array)
     }
     
+    lazy var text: String = {
+        return presentValue(from: values)
+    }()
+    
     init(values: JSONValueType) {
         self.values = values
     }
 }
 
-struct JSONObject: JSONType {
+class JSONObject: PrettyJSONDiscripter, JSONType {
     var prefix: String {
         return "객체안에는"
     }
@@ -99,6 +184,10 @@ struct JSONObject: JSONType {
         // 단일 객체이므로 객체의 값은 0으로 반환
         return (string, int, bool, object, array)
     }
+    
+    lazy var text: String = {
+        return presentValue(from: values)
+    }()
     
     init(values: JSONValueType) {
         self.values = values
--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -14,15 +14,15 @@ struct OutputView {
     private static var stageOfArray: Int = 0
     
     static func display(_ values: JSONType){
-        print(text(from: values))
-        print(presentResult(of: values))
+        print(presentResult(from: values))
+        print(values.text)
     }
     
     static func display(_ error: JSONParserError) {
         print(error)
     }
     
-    private static func text(from value: JSONType) -> String {
+    private static func presentResult(from value: JSONType) -> String {
         var text = ""
         let result = value.result
         text += value.prefix
@@ -50,85 +50,6 @@ struct OutputView {
         text += "가 존재합니다."
         
         return text
-    }
-    
-    private static func presentResult(of json: JSONType) -> String {
-        return presentValue(from: json.values)
-    }
-    
-    private static func presentArray(from array: [JSONValueType]) -> String {
-        var text = "\(stageOfArray > 0 ? "\n" : "")\(tab(at: stageOfArray - 1))["
-        for (index, element) in array.enumerated() {
-            
-            switch element {
-            case .array(_), .object(_):
-                stageOfArray += 1
-            default: break
-            }
-            
-            text += presentValue(from: element)
-            if index != array.count - 1 {
-                text += ","
-            }
-        }
-        text += "]\(stageOfArray > 0 ? "\n" : "")"
-        return text
-    }
-    
-    private static func presentObject(from objects: [[String:JSONValueType]]) -> String {
-        stageOfObject += 1
-        let currentStage = stageOfObject + stageOfArray
-        
-        var text = "{"
-        for (index, object) in objects.enumerated() {
-            text += presentPair(from: object, stage: currentStage)
-            
-            if index != objects.count - 1 {
-                text += ","
-            }
-        }
-        
-        text += "\n\(tab(at: currentStage - 1))}"
-        
-        return text
-    }
-    
-    private static func presentPair(from pair: [String:JSONValueType], stage: Int) -> String {
-        var text = ""
-        let keys = pair.keys.map {String($0)}
-        let values = pair.values.map {$0}
-        
-        text += "\n\(tab(at: stage))\(keys[0]) : \(presentValue(from: values[0]))"
-        return text
-    }
-    
-    private static func presentValue(from value: JSONValueType) -> String {
-        switch value {
-        case .string(let stringValue):
-            return stringValue
-        case .int(let integerValue):
-            return "\(integerValue)"
-        case .bool(let booleanValue):
-            return "\(booleanValue)"
-        case .object(let objectValue):
-            return presentObject(from: objectValue)
-        case .array(let arrayValue):
-            return presentArray(from: arrayValue)
-        }
-    }
-    
-    private static func tab(at stage: Int) -> String {
-        if stage < 0 {
-            return ""
-        }
-        
-        var tabs = ""
-        
-        for _ in (0..<stage) {
-            tabs += "\t"
-        }
-        
-        return tabs
     }
 }
 
