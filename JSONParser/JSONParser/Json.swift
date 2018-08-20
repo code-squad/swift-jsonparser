83f6acd3f4b52fc5eb573e4d580c547f1f91f91b
oingbong <oingbong@gmail.com>
2018-08-20 16:09:09 +0900

    Move countMessage Code : Outputview -> Protocol extension (for common)

--- JSONParser/JSONParser/Json.swift
+++ JSONParser/JSONParser/Json.swift
@@ -37,3 +37,39 @@ enum JsonType:Equatable {
         }
     }
 }
+
+extension Jsonable {
+    func countDataWithMessage() -> String {
+        let (string,int,bool,object,array) = self.countData()
+        let totalCount = string + int + bool + object + array
+        var message = ""
+        
+        if self is JsonArray {
+            message = "총 \(totalCount)개의 배열 데이터 중에 "
+        }
+        
+        if self is JsonObject {
+            message = "총 \(totalCount)개의 객체 데이터 중에 "
+        }
+        
+        if string > 0 {
+            message = message + "문자열 \(string)개,"
+        }
+        if int > 0 {
+            message = message + "숫자 \(int)개,"
+        }
+        if bool > 0 {
+            message = message + "부울 \(bool)개,"
+        }
+        if object > 0 {
+            message = message + "객체 \(object)개,"
+        }
+        if array > 0 {
+            message = message + "배열 \(array)개,"
+        }
+        message.removeLast() // 마지막 , 는 제거 합니다.
+        message = message + "가 포함되어 있습니다."
+        
+        return message
+    }
+}
--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -25,35 +25,6 @@ struct OutputView {
     }
     
     private static func countJson(from data:Jsonable){
-        let (string,int,bool,object,array) = data.countData()
-        
-        let totalCount = string + int + bool + object + array
-        var message = ""
-        
-        if data is JsonObject {
-            message = "총 \(totalCount)개의 객체 데이터 중에 "
-        }
-        if data is JsonArray {
-            message = "총 \(totalCount)개의 배열 데이터 중에 "
-        }
-        
-        if string > 0 {
-            message = message + "문자열 \(string)개,"
-        }
-        if int > 0 {
-            message = message + "숫자 \(int)개,"
-        }
-        if bool > 0 {
-            message = message + "부울 \(bool)개,"
-        }
-        if object > 0 {
-            message = message + "객체 \(object)개,"
-        }
-        if array > 0 {
-            message = message + "배열 \(array)개,"
-        }
-        message.removeLast() // 마지막 , 는 제거 합니다.
-        message = message + "가 포함되어 있습니다."
-        print(message)
+        print(data.countDataWithMessage())
     }
 }
