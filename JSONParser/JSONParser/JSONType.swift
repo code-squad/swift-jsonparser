b71e10c03daa40fe7aeb72c5a0d4f555ee011b07
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-20 21:21:28 +0900

    배열, 객체 출력부를 각각의 객체가 전담하도록 분리

--- JSONParser/JSONParser/JSONType.swift
+++ JSONParser/JSONParser/JSONType.swift
@@ -21,73 +21,26 @@ enum JSONValueType {
     case bool(Bool)
     case object([[String:JSONValueType]])
     case array([JSONValueType])
+    
+    func presentableValue(stage: Int) -> String {
+        switch self {
+        case .string(let stringValue):
+            return stringValue
+        case .int(let intValue):
+            return "\(intValue)"
+        case .bool(let boolValue):
+            return "\(boolValue)"
+        case .array(let array):
+            let json = JSONArray(values: self)
+            return json.presentArray(from: array, stage: stage)
+        case .object(let object):
+            let json = JSONObject(values: self)
+            return json.presentObject(from: object, stage: stage)
+        }
+    }
 }
 
 class PrettyJSONDiscripter {
-    var stageOfArray: Int = 0
-    var stageOfObject: Int = 0
-    
-    func presentArray(from array: [JSONValueType]) -> String {
-        let stage = stageOfArray > 1 ? stageOfArray - 1 : stageOfArray
-        var text = "\(stageOfArray > 0 ? "\n" : "")\(tab(at: stage))["
-        for (index, element) in array.enumerated() {
-            
-            switch element {
-            case .array(_), .object(_):
-                stageOfArray += 1
-            default: break
-            }
-            
-            text += presentValue(from: element)
-            if index != array.count - 1 {
-                text += ","
-            }
-        }
-        text += "]\(stageOfArray > 0 ? "\n" : "")"
-        return text
-    }
-    
-    func presentObject(from objects: [[String:JSONValueType]]) -> String {
-        stageOfObject += 1
-        let currentStage = stageOfObject + stageOfArray
-        
-        var text = "{"
-        for (index, object) in objects.enumerated() {
-            text += presentPair(from: object, stage: currentStage)
-            
-            if index != objects.count - 1 {
-                text += ","
-            }
-        }
-        
-        text += "\n\(tab(at: currentStage - 1))}"
-        
-        return text
-    }
-    
-    func presentPair(from pair: [String:JSONValueType], stage: Int) -> String {
-        var text = ""
-        let keys = pair.keys.map {String($0)}
-        let values = pair.values.map {$0}
-        
-        text += "\n\(tab(at: stage))\(keys[0]) : \(presentValue(from: values[0]))"
-        return text
-    }
-    
-    func presentValue(from value: JSONValueType) -> String {
-        switch value {
-        case .string(let stringValue):
-            return stringValue
-        case .int(let integerValue):
-            return "\(integerValue)"
-        case .bool(let booleanValue):
-            return "\(booleanValue)"
-        case .object(let objectValue):
-            return presentObject(from: objectValue)
-        case .array(let arrayValue):
-            return presentArray(from: arrayValue)
-        }
-    }
     
     func tab(at stage: Int) -> String {
         if stage <= 0 {
@@ -141,11 +94,31 @@ class JSONArray: PrettyJSONDiscripter, JSONType {
     }
     
     lazy var text: String = {
-        return presentValue(from: values)
+        let stage = result.int > 0 || result.bool > 0 || result.int > 0 ? 0 : 1
+        return values.presentableValue(stage: stage)
     }()
     
     init(values: JSONValueType) {
         self.values = values
+    }
+    
+    func presentArray(from array: [JSONValueType], stage: Int) -> String {
+        var text = "["
+        for (index, item) in array.enumerated() {
+            switch item {
+            case .array(_):
+                text += "\(stage > 0 ? "\n" : "")\(tab(at: stage))\(item.presentableValue(stage: stage + 1))\(stage > 0 ? "\n" : "")"
+            case .object(_):
+                text += "\(item.presentableValue(stage: stage + 1))"
+            default:
+                text += item.presentableValue(stage: stage)
+            }
+            if index != array.count - 1 {
+                text += ","
+            }
+        }
+        text += "]"
+        return text
     }
 }
 
@@ -186,10 +159,38 @@ class JSONObject: PrettyJSONDiscripter, JSONType {
     }
     
     lazy var text: String = {
-        return presentValue(from: values)
+        return values.presentableValue(stage: 1)
     }()
     
     init(values: JSONValueType) {
         self.values = values
+    }
+    
+    func presentObject(from objects: [[String:JSONValueType]], stage: Int) -> String {
+        var text = "{\n"
+        for (index, pair) in objects.enumerated() {
+            let pairString = presentPair(from: pair, stage: stage)
+            text += pairString
+            if index != objects.count - 1 {
+                text += ","
+            }
+            text += "\n"
+        }
+        text += "\(tab(at: stage == 1 ? 0 : stage - 1))}"
+        return text
+    }
+    
+    func presentPair(from pair: [String:JSONValueType], stage: Int) -> String {
+        var text = ""
+        let keys = pair.keys.map {String($0)}
+        let values = pair.values.map {$0}
+        
+        switch values[0] {
+        case .array(_), .object(_):
+            text += "\(tab(at: stage))\(keys[0]) : \(values[0].presentableValue(stage: stage + 1))"
+        default:
+            text += "\(tab(at: stage))\(keys[0]) : \(values[0].presentableValue(stage: stage))"
+        }
+        return text
     }
 }
