7662f3d9b7a2ae1fb87737c38c33dc4a495bad4a
popsmile <popsmile7@gmail.com>
2018-10-19 11:24:57 +0900

    Create InputView structure

---
+++ JSONParser/JSONParser/InputView.swift
@@ -0,0 +1,19 @@
+//
+//  InputView.swift
+//  JSONParser
+//
+//  Created by 윤지영 on 18/10/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    static private let guideMessage = "분석할 JSON 데이터를 입력하세요."
+    
+    static func readInput() -> String {
+        print(guideMessage)
+        guard let input: String = readLine() else { return String() }
+        return input
+    }
+}
