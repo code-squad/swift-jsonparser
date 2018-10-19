38a01232a2e3de27b835cd5cb3ea69082911e992
popsmile <popsmile7@gmail.com>
2018-10-19 15:07:05 +0900

    Create Unit Tests for JSONParser

---
+++ JSONParser/JSONParserUnitTest/UnitTestExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  UnitTestExtensions.swift
+//  JSONParserUnitTest
+//
+//  Created by 윤지영 on 19/10/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import XCTest
+
+class UnitTestExtensions: XCTestCase {
+    override func setUp() {}
+    override func tearDown() {}
+
+    func testExample() {}
+}
