231d518caa97288573de6d0f1d6b6d68fbf45dc5
popsmile <popsmile7@gmail.com>
2018-10-19 12:34:03 +0900

    Add a function not to print zero count of type

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -9,11 +9,30 @@
 import Foundation
 
 struct OutputView {
+    private static func countType(in stringArray: [String]) -> [Int] {
+        var typeCount = [Int]()
+        typeCount.append(StringInspector.countType(in: stringArray))
+        typeCount.append(DoubleInspector.countType(in: stringArray))
+        typeCount.append(BooleanInspector.countType(in: stringArray))
+        return typeCount
+    }
+    
+    private static func addTypeName(in stringArray: [String]) -> String {
+        var typeName: [String?] = ["문자열", "숫자", "부울"]
+        let typeCount = countType(in: stringArray)
+        for index in 0..<3 {
+            if (typeCount[index]==0) {
+                typeName[index] = nil
+                continue
+            }
+            typeName[index]?.append(" \(typeCount[index])개")
+        }
+        return typeName.compactMap({$0}).joined(separator: ", ")
+    }
+    
     static func showTypeCountOf(JSON stringArray: [String]) {
         let totalCount = stringArray.count
-        let stringCout = StringInspector.countType(in: stringArray)
-        let doubleCount = DoubleInspector.countType(in: stringArray)
-        let boolCount = BooleanInspector.countType(in: stringArray)
-        print("총 \(totalCount)개의 데이터 중에 문자열 \(stringCout)개, 숫자 \(doubleCount)개, 부울 \(boolCount)개가 포함되어 있습니다.")
+        let typeCount = addTypeName(in: stringArray)
+        print("총 \(totalCount)개의 데이터 중에 \(typeCount)가 포함되어 있습니다.")
     }
 }
