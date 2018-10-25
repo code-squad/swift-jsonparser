7b47af4c75f58ae51f1de1b2ccf4bae85c793775
popsmile <popsmile7@gmail.com>
2018-10-25 19:46:52 +0900

    Add makeJSONObject method
    Depending on the prefix of the parameter String, JSONObject or JSONArray is made by parsing the string.

--- JSONParser/JSONParser/JSONParser.swift
+++ JSONParser/JSONParser/JSONParser.swift
@@ -9,26 +9,62 @@
 import Foundation
 
 struct JSONParser {
-    private static let stringArray = { (jsonString: String) -> [String] in return jsonString.removeWhiteSpaces().trimSquareBrackets().splitByComma() }
-    
-    private static func extractStringArray(from jsonString: String) -> [String]? {
-        guard jsonString.hasSideSquareBrackets() else { return nil }
-        return stringArray(jsonString)
-    }
-    
-    private static func typeCast(from string: String) -> Any? {
-        guard string.hasDoubleQuotation() == false else { return string.trimDoubleQuotation() }
-        guard Int(string) == nil else { return Int(string) }
-        guard Bool(string) == nil else { return Bool(string) }
+    private static func typeCast(from string: String) -> JSONValue? {
+        if string.hasDoubleQuotation() { return JSONValue.string(string.trimDoubleQuotation()) }
+        if let int = Int(string) { return JSONValue.int(int) }
+        if let bool = Bool(string) { return JSONValue.bool(bool) }
         return nil
     }
     
-    static func makeJSONArray(from jsonString: String) -> [Any?]? {
-        guard let stringArray = extractStringArray(from: jsonString) else { return nil }
-        var anyArray = [Any?]()
-        for string in stringArray {
-            anyArray.append(typeCast(from: string))
+    private static func makeJSONObject(from jsonString: String) -> JSONValue? {
+        var jsonObject = [String: JSONValue]()
+        let jsonStringTrimmedBrackets = jsonString.trimWhiteSpaces().trimCurlyBrackets()
+        let jsonValues = jsonStringTrimmedBrackets.splitByComma()
+        for jsonValue in jsonValues {
+            guard jsonValue.contains(":") else { return nil }
+            let objectKeyValue = jsonValue.splitByColon().map({ $0.trimWhiteSpaces() })
+            guard objectKeyValue.count > 1 else { return nil }
+            guard objectKeyValue[0].hasDoubleQuotation() else { return nil }
+            let key: String = objectKeyValue[0].trimDoubleQuotation()
+            guard let value: JSONValue = typeCast(from: objectKeyValue[1]) else { continue }
+            jsonObject[key] = value
         }
-        return anyArray
+        return JSONValue.object(jsonObject)
+    }
+    
+    private static func makeJSONArray(from jsonString: String) -> JSONValue? {
+        var jsonArray = [JSONValue]()
+        let jsonStringTrimmedBrackes = jsonString.trimWhiteSpaces().trimSquareBrackets()
+        let str = jsonStringTrimmedBrackes
+        var iterateIndex = str.startIndex
+        for index in str.indices {
+            if (str[index]==",") {
+                let a = String(str[iterateIndex..<index])
+                guard let b = typeCast(from: a) else { continue }
+                jsonArray.append(b)
+                iterateIndex = str.index(after: index)
+                continue
+            } else if (str[index]=="{") {
+                let a = str[iterateIndex...]
+                guard let b = a.firstIndex(of: "}") else { return nil }
+                let c = String(str[iterateIndex...b])
+                guard let d = makeJSONObject(from: c) else { return nil}
+                jsonArray.append(d)
+                iterateIndex = str.index(after: index)
+                continue
+            } else if (index==str.endIndex) {
+                let a = String(str[iterateIndex...index])
+                guard let b = typeCast(from: a) else { continue }
+                jsonArray.append(b)
+                continue
+            }
+        }
+        return JSONValue.array(jsonArray)
+    }
+    
+    static func parse(_ jsonString: String) -> JSONValue? {
+        if jsonString.hasSideSquareBrackets() { return makeJSONArray(from: jsonString) }
+        if jsonString.hasSideCurlyBrackets() { return makeJSONObject(from: jsonString) }
+        return nil
     }
 }
