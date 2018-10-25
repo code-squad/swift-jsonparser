7fa3286cdae6d37efd25876320ac1a80799f731f
popsmile <popsmile7@gmail.com>
2018-10-25 19:42:50 +0900

    Create an enum JSONValue
    The enum JSONValue only includes the types allowed to be 'value' of JSON data.

---
+++ JSONParser/JSONParser/JSONValue.swift
@@ -0,0 +1,17 @@
+//
+//  JSONType.swift
+//  JSONParser
+//
+//  Created by 윤지영 on 23/10/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+public enum JSONValue {
+    case string(String)
+    case int(Int)
+    case bool(Bool)
+    case object([String : JSONValue])
+    case array([JSONValue])
+}
