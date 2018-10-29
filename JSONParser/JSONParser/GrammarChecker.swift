0919c7fe9c9fc39e5028267bcc4064bccbe15281
popsmile <popsmile7@gmail.com>
2018-10-29 14:24:58 +0900

    Create structs GrammarChecker and JSONRegex
    They check whether a string has a valid grammar of JSON Data or not.

---
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -0,0 +1,21 @@
+//
+//  GrammarChecker.swift
+//  JSONParser
+//
+//  Created by 윤지영 on 29/10/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct GrammarChecker {
+
+    static func isValid(jsonString: String, for pattern: String) -> Bool {
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: []) else { return false }
+        let fullRange = NSRange(jsonString.startIndex..., in: jsonString)
+        let matchRange = regex.rangeOfFirstMatch(in: jsonString, options: [], range: fullRange)
+        guard fullRange == matchRange else { return false }
+        return true
+    }
+
+}
---
+++ JSONParser/JSONParser/JSONRegex.swift
@@ -0,0 +1,30 @@
+//
+//  JSONRegex.swift
+//  JSONParser
+//
+//  Created by 윤지영 on 29/10/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct JSONRegex {
+    private static let space = "\\s"
+    private static let colon = ":"
+    private static let leftCurlyBracket = "\\{"
+    private static let rightCurlyBracket = "\\}"
+    private static let leftSquareBracket = "\\["
+    private static let rightSquareBracket = "\\]"
+
+    private static let string = "\"[\\w_\(space)]+\""
+    private static let int = "[0-9]+"
+    private static let bool = "(?:true|false)"
+
+    private static let values = "\(string)|\(int)|\(bool)"
+    private static let keyValue = "\(string)\(space)*\(colon)\(space)*(?:\(values))"
+    private static let object = "\(leftCurlyBracket)\(space)*\(keyValue)\(space)*(?:,\(space)*\(keyValue)\(space)*)*\(rightCurlyBracket)"
+    static let jsonObject = "^\(object)$"
+
+    private static let valuesIncludingObject = "\(values)|\(object)"
+    static let jsonArray = "^\(leftSquareBracket)\(space)*(?:\(valuesIncludingObject))\(space)*(?:,\(space)*(?:\(valuesIncludingObject))\(space)*)*\(rightSquareBracket)$"
+}
