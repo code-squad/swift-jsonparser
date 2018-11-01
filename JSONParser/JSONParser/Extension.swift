b621d378fb8ba67134f22f1ec7c12f1db1df09f6
hngfu <djsl1018@naver.com>
2018-11-01 17:27:57 +0900

    [Extension] isWhatForm() 생성

--- JSONParser/JSONParser/Extension.swift
+++ JSONParser/JSONParser/Extension.swift
@@ -16,26 +16,34 @@ extension String {
     private func removeSquareBracket() -> String {
         return trimmingCharacters(in: ["[","]"])
     }
-    
     private func removeSpace() -> String {
         return replacingOccurrences(of: " ", with: "")
     }
-    
     func extractData() -> [String] {
         let removedSquareBracket = self.removeSquareBracket()
         let removedSpace = removedSquareBracket.removeSpace()
         return removedSpace.split(separator: ",").map{String($0)}
     }
     
-    func isNumberForm() -> Bool {
+    private func isNumberForm() -> Bool {
         return !isEmpty && range(of: "^[^0-9]+$", options: .regularExpression) == nil
     }
-    
-    func isStringForm() -> Bool {
+    private func isStringForm() -> Bool {
         return (hasPrefix("\"")) && (hasSuffix("\""))
     }
-    
-    func isBoolForm() -> Bool {
+    private func isBoolForm() -> Bool {
         return (uppercased() == "TRUE") || (uppercased() == "FALSE")
+    }
+    func isWhatForm() -> String {
+        if isBoolForm() {
+            return "Bool"
+        }
+        if isStringForm() {
+            return "String"
+        }
+        if isNumberForm() {
+            return "Number"
+        }
+        return "none"
     }
 }
