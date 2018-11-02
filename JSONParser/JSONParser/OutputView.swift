838e68847c5ccbed4b952da45d26448f34ba0710
hngfu <djsl1018@naver.com>
2018-11-02 12:15:25 +0900

    [OutputView] showResult() 생성

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -10,6 +10,31 @@ import Foundation
 
 struct OutputView {
     static func showNumberOfData(_ data:SwiftArray) {
-        print("총 7개의 데이터 중에 문자열 3개, 숫자 3개, 부울 1개가 포함되어 있습니다.")
+        var outputArray = [String]()
+        let numberOfAll = data.readStrings().count + data.readNumbers().count + data.readBools().count
+        
+        outputArray.append("총 \(numberOfAll)개의 데이터 중에")
+        if data.readStrings().count > 0 {
+            outputArray.append(" 문자열 \(data.readStrings().count)개")
+        }
+        if data.readNumbers().count > 0 {
+            outputArray.append(" 숫자 \(data.readNumbers().count)개")
+        }
+        if data.readBools().count > 0 {
+            outputArray.append(" 부울 \(data.readBools().count)개")
+        }
+        outputArray.append("가 포함되어 있습니다.")
+        showResult(outputArray)
+    }
+    
+    static private func showResult(_ result:[String]) {
+        for i in result {
+            print(i, terminator: "")
+            guard i != result[result.startIndex] else {continue}
+            guard i != result[result.endIndex - 1] else {continue}
+            guard i != result[result.endIndex - 2] else {continue}
+            print(",", terminator: "")
+        }
+        print("")
     }
 }
