e253c1b72aa0d5227c761bc3b12ef89b6bd3a300
hngfu <djsl1018@naver.com>
2018-11-02 10:20:26 +0900

    [SwiftType] 생성

--- JSONParser/JSONParser/Sorter.swift
+++ JSONParser/JSONParser/Sorter.swift
@@ -12,7 +12,16 @@ struct Sorter {
     static func sortByTypeIntoArray(_ inputData:[String]) -> SwiftArray {
         var swiftArr = SwiftArray()
         for data in inputData {
-            
+            switch data.isWhatForm() {
+            case "String" :
+                swiftArr.insertIntoStrings(data.removeDoubleQuotationMarks())
+            case "Number" :
+                swiftArr.insertIntoNumbers(Double(data) ?? 0)
+            case "Bool" :
+                swiftArr.insertIntoBools(<#T##bool: Bool##Bool#>)
+            default:
+                <#code#>
+            }
         }
         return swiftArr
     }
