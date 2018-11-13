b029dc1fc77814b381e34a2d9a1e3a8dd644017a
김장수 <jangsookim.me@gmail.com>
2018-11-13 10:57:51 +0900

    매개변수로 프로토콜을 넘겨주는 부분을 DTO로 수정

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -25,7 +25,7 @@ func main() {
     
     // 에러 처리된 입력을 문자열 변환하고 출력
     let jsonData = jsonParser.parse(from: input)
-    outputView.printResult(by: jsonData)
+    outputView.printResult(by: jsonData.getDTO())
 }
 
 main()
--- JSONParser/JSONParser/outputView.swift
+++ JSONParser/JSONParser/outputView.swift
@@ -10,19 +10,20 @@ import Foundation
 
 struct OutputView {
     // 문자열, 부울, 숫자 각자의 갯수에 맞게 출력
-    private func printByType(_ counts: (ints: [Int], bools: [Bool], strings: [String], totalCount: Int)) -> String {
+    private func printByType(_ counts: DTO) -> String {
         var result = String()
+        let json = counts.json
         
-        if counts.strings.count > 0 { result += "문자열 \(counts.strings.count)개 " }
-        if counts.ints.count > 0 { result += "숫자 \(counts.ints.count)개 " }
-        if counts.bools.count > 0 { result += "부울 \(counts.bools.count)개 " }
+        if json.strings.count > 0 { result += "문자열 \(json.strings.count)개 " }
+        if json.ints.count > 0 { result += "숫자 \(json.ints.count)개 " }
+        if json.bools.count > 0 { result += "부울 \(json.bools.count)개 " }
         
-        return "총 \(counts.totalCount)개의 데이터 중에 \(result)포함되어 있습니다."
+        return "총 \(json.totalCount)개의 데이터 중에 \(result)포함되어 있습니다."
     }
     
     // JSONParser에서 전달받은 데이터 세트를 출력
-    public func printResult(by counts: JSONData) {
-        let result = printByType(counts.getDTO().json)
+    public func printResult(by counts: DTO) {
+        let result = printByType(counts)
         print(result)
     }
 }
