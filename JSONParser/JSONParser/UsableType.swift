144bb32ac275eff227ea3c52420ffe31d484a7af
hngfu <djsl1018@naver.com>
2018-11-13 14:25:38 +0900

    [UsableType] 생성
    [프로토콜] 사용 가능한 타입인 String, Double, Bool 값을 묶음

---
+++ JSONParser/JSONParser/UsableType.swift
@@ -0,0 +1,16 @@
+//
+//  UsableType.swift
+//  JSONParser
+//
+//  Created by 조재흥 on 18. 11. 13..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol UsableType {
+}
+
+extension String : UsableType {}
+extension Double : UsableType {}
+extension Bool : UsableType {}
