494c8a224b846414c6149a362b8d0d5bb8d8957b
hngfu <djsl1018@naver.com>
2018-11-16 15:33:15 +0900

    [SwiftType] 추가
    enum타입

---
+++ JSONParser/JSONParser/SwiftType.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftType.swift
+//  JSONParser
+//
+//  Created by 조재흥 on 18. 11. 16..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+enum SwiftType {
+    case string
+    case number
+    case bool
+    case object
+}
