1c20f04e32a8b32726eb643550e08e222626bcda
hngfu <djsl1018@naver.com>
2018-11-19 19:50:22 +0900

    [JsonBool] 생성

---
+++ JSONParser/JSONParser/JsonType/JsonBool.swift
@@ -0,0 +1,18 @@
+//
+//  JsonBool.swift
+//  JSONParser
+//
+//  Created by 조재흥 on 18. 11. 19..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct JsonBool {
+    private let bool : Bool
+    
+    init(bool:Bool) {
+        self.bool = bool
+    }
+    
+}
