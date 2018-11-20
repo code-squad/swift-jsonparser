43ab83f54d0b373a34e63a1c110b1ec45e09dac0
김장수 <jangsookim.me@gmail.com>
2018-11-20 23:15:49 +0900

    텍스트 익스텐션 재수정

--- JSONParser/JSONParser/textExtension.swift
+++ JSONParser/JSONParser/textExtension.swift
@@ -16,16 +16,6 @@ extension String {
         return String(self.dropFirst().dropLast())
     }
     
-    // 콤마(,)로 배열로 변환 & 공백(blank) 제거
-    var separateByComma: [String] {
-        return self.split(separator: ",").map { String($0).trimmingCharacters(in: .whitespaces) }
-    }
-    
-    // 콜론(:)으로 배열로 변환 & 공백(blank) 제거
-    var separateByColumn: [String] {
-        return self.split(separator: ":").map { String($0).trimmingCharacters(in: .whitespaces) }
-    }
-    
     // 문자열인지 확인 & 부울 타입 제외
     var isString: Bool {
         return self.first == "\"" && self.last == "\"" 
@@ -39,5 +29,17 @@ extension String {
     // 부울인지 확인
     var isBoolean: Bool {
         return Bool(self) != nil
+    }
+    
+    // 배열인지 확인
+    var isArray: Bool {
+        guard self.first == "[" && self.last == "]" else { return false }
+        return true
+    }
+    
+    // 객체인지 확인
+    var isObject: Bool {
+        guard self.first == "{" && self.last == "}" else { return false }
+        return true
     }
 }
