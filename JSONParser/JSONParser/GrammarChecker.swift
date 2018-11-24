1dda3d89d16b14c4029aeb08dd5c52d5f22c20bf
hngfu <djsl1018@naver.com>
2018-11-24 15:03:30 +0900

    문법체크를 간소화 시켰습니다.

--- JSONParser/JSONParser/GrammarChecker.swift
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -9,17 +9,10 @@
 import Foundation
 
 struct GrammarChecker {
-    static private let s = "\\s*"
-    static private let d = "\\d"
-    static private let w = "\\w"
-    static private let bool = "(true|false)"
-    static private let string = "\"\(s)(\(w)|\(s)|\(d)|\\{|\\}|\\[|\\])+\(s)\""
-    
     static func checkValidOfGrammar(string:String) -> Bool {
         guard !self.checkColonInArray(string: string) else {return  false}
-        if string.hasPrefix("{") && string.hasSuffix("}") {
-            guard checkObject(string: string) else {return false}
-        }
+        guard !self.checkArrayInObject(string: string) else {return false}
+        guard !self.checkObjectInObject(string: string) else {return false}
         return true
     }
     
@@ -27,21 +20,15 @@ struct GrammarChecker {
         let regex = "\\[\\s*\"(\\w|\\s|\\d|\\{|\\}|\\[|\\])+\"\\s*:\\s*(\"(\\w|\\s|\\d|\\{|\\}|\\[|\\])+\"|\\d+|true|false)\\s*\\]"
         return string.range(of: regex, options: .regularExpression) != nil
     }
-    
-    static private func checkObject(string:String) -> Bool {
-        let removedSquare = string.trimmingCharacters(in: ["{","}"])
-        guard !checkInArray(string: removedSquare) else {return false}
-        guard !checkInObject(string: removedSquare) else {return false}
-        return true
-    }
-    
-    static private func checkInObject(string:String) -> Bool {
-        let regex = "\\{\(s)(\(s)(\(s)\(string)\(s):\(s)(\(string)|\(d)+|\(bool))\(s))\(s),?)+\(s)\\}"
+
+    static private func checkArrayInObject(string:String) -> Bool {
+        let regex = "(?<=:.{0,10})(\\[.*?\\])(?=.*?\\})"
         return string.range(of: regex, options: .regularExpression) != nil
     }
     
-    static private func checkInArray(string:String) -> Bool {
-        let regex = "\\[\(s)(\(s)((\(s)\"\(s)(\\w|\\s|\\d|\\{|\\}|\\[|\\])+\(s)\"|\\d+|true|false))\(s),?\(s))+\(s)\\]"
+    static private func checkObjectInObject(string:String) -> Bool {
+        let regex = "(?<=:.{0,10})({.*?})(?=.*?\\})"
         return string.range(of: regex, options: .regularExpression) != nil
     }
 }
+
