64cfd032942f037bf59e0bf96de68aa8483990fa
hngfu <djsl1018@naver.com>
2018-11-26 16:28:58 +0900

    [GrammarCheck] Json Value에 들어갈 수 있는 값들을 문법체크에 추가

--- JSONParser/JSONParser/GrammarChecker.swift
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -11,6 +11,7 @@ import Foundation
 struct GrammarChecker {
     static func checkValidOfGrammar(string:String) -> Bool {
         guard !self.checkColonInArray(string: string) else {return  false}
+        guard self.checkBasicGrammar(string: string) else {return false}
         return true
     }
     
@@ -18,5 +19,38 @@ struct GrammarChecker {
         let regex = "\\[\\s*\"(\\w|\\s|\\d|\\{|\\}|\\[|\\])+\"\\s*:\\s*(\"(\\w|\\s|\\d|\\{|\\}|\\[|\\])+\"|\\d+|true|false)\\s*\\]"
         return string.range(of: regex, options: .regularExpression) != nil
     }
+    
+    static private func checkBasicGrammar(string:String) -> Bool {
+        guard !checkString(string: string) else {return true}
+        guard !checkNumber(string: string) else {return true}
+        guard !checkBool(string: string) else {return true}
+        guard !checkArray(string: string) else {return true}
+        guard !checkObject(string: string) else {return true}
+        return false
+    }
+    
+    static private func checkString(string:String) -> Bool {
+        let regex = "^\"(.+)\"$"
+        return string.range(of: regex, options: .regularExpression) != nil
+    }
+    
+    static private func checkNumber(string:String) -> Bool {
+        let regex = "^\\d+$"
+        return string.range(of: regex, options: .regularExpression) != nil
+    }
+    
+    static private func checkBool(string:String) -> Bool {
+        let regex = "TRUE|FALSE"
+        return string.uppercased().range(of: regex, options: .regularExpression) != nil
+    }
+    
+    static private func checkArray(string:String) -> Bool {
+        let regex = "^\\[.+\\]$"
+        return string.range(of: regex, options: .regularExpression) != nil
+    }
+    
+    static private func checkObject(string:String) -> Bool {
+        let regex = "^\\{.+\\}$"
+        return string.range(of: regex, options: .regularExpression) != nil
+    }
 }
-
