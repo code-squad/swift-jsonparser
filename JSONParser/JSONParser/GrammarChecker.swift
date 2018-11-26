e62561f19fe5ac4121b59b85078905312486a927
hngfu <djsl1018@naver.com>
2018-11-26 11:06:41 +0900

    [GrammarCheck] object안에 object or array를 걸러내는 체크 삭제

--- JSONParser/JSONParser/GrammarChecker.swift
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -11,23 +11,11 @@ import Foundation
 struct GrammarChecker {
     static func checkValidOfGrammar(string:String) -> Bool {
         guard !self.checkColonInArray(string: string) else {return  false}
-        guard !self.checkArrayInObject(string: string) else {return false}
-        guard !self.checkObjectInObject(string: string) else {return false}
         return true
     }
     
     static private func checkColonInArray(string:String) -> Bool {
         let regex = "\\[\\s*\"(\\w|\\s|\\d|\\{|\\}|\\[|\\])+\"\\s*:\\s*(\"(\\w|\\s|\\d|\\{|\\}|\\[|\\])+\"|\\d+|true|false)\\s*\\]"
-        return string.range(of: regex, options: .regularExpression) != nil
-    }
-
-    static private func checkArrayInObject(string:String) -> Bool {
-        let regex = "(?<=:.{0,10})(\\[.*?\\])(?=.*?\\})"
-        return string.range(of: regex, options: .regularExpression) != nil
-    }
-    
-    static private func checkObjectInObject(string:String) -> Bool {
-        let regex = "(?<=:.{0,10})({.*?})(?=.*?\\})"
         return string.range(of: regex, options: .regularExpression) != nil
     }
 }
