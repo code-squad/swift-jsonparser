28eff53bf2e939a2c6a054c3767b8a3ffbfa0c48
hngfu <djsl1018@naver.com>
2018-11-26 12:09:31 +0900

    [RegularExpression] 쓸 데 없는 정규표현식 전부 삭제 후 값 추출하는 정규표현식 추가

--- JSONParser/JSONParser/RegularExpression.swift
+++ JSONParser/JSONParser/RegularExpression.swift
@@ -9,14 +9,7 @@
 import Foundation
 
 struct RegularExpression {
-    static private let s = "\\s*"
-    static private let d = "\\d"
-    static private let w = "\\w"
-    static private let bool = "(true|false)"
-    static private let string = "\"\(s)(\(w)|\(s)|\(d)|\\{|\\}|\\[|\\])+\(s)\""
-    static private let object = "\\{\(s)(\(s)(\(s)\(string)\(s):\(s)(\(string)|\(d)+|\(bool))\(s))\(s),?)+\(s)\\}"
-    static private let array = "\\[\(s)(\(s)((\(s)\"\(s)(\\w|\\s|\\d|\\{|\\}|\\[|\\])+\(s)\"|\\d+|true|false))\(s),?\(s))+\(s)\\]"
-    static private let allData = "(\(object)|\(array)|\(string)|\(d)+|\(bool))"
+    static private let allData = "\"(.+?)\"|\\d+|true|false|\\[(.+?)\\]|\\{(.+?)\\}"
     
     static private func regex(pattern:String, string:String) -> [String] {
         guard let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) else {return []}
