4e1c527c2911bc3bd4df8cbe05b9b84983ad244b
김장수 <jangsookim.me@gmail.com>
2018-11-27 18:40:27 +0900

    사용법 & 네이밍 수준을 비슷한 수준으로 통일

--- JSONParser/JSONParser/regularExpression.swift
+++ JSONParser/JSONParser/regularExpression.swift
@@ -18,11 +18,11 @@ struct JSONRegex {
     private static let object = "\\{\(blank)\(keyAndValue)\(blank)(?:,\(blank)\(keyAndValue)\(blank))*\\}"
 
     // 중첩 구조의 입력에서 사용
-    private static let nestedKeyAndValue = "\(str)\(blank):\(blank)(?:\(basic)|\(object)|\(array))"
+    private static let objectFactor = "\(str)\(blank):\(blank)(?:\(basic)|\(object)|\(array))"
     private static let arrayFactor = "\(basic)|\(object)|\(array)"
     
     // GrammarCheck 구조체에서 규칙 검사에 사용
-    private static let objectPattern = "^\\{\(blank)\(nestedKeyAndValue)\(blank)(?:,\(blank)\(nestedKeyAndValue)\(blank))*\\}$"
+    private static let objectPattern = "^\\{\(blank)\(objectFactor)\(blank)(?:,\(blank)\(objectFactor)\(blank))*\\}$"
     private static let arrayPattern = "^\\[\(blank)(?:\(arrayFactor))\(blank)(?:,\(blank)(?:\(arrayFactor))\(blank))*\\]$"
     
     
@@ -30,7 +30,7 @@ struct JSONRegex {
     // 규칙 검사를 통과한 입력에서 JSONType 요소를 배열로 변환
     func extractData(from rawData: String) -> [String] {
         if rawData.isArray { return classify(rawData.removeBracket, with: JSONRegex.arrayFactor) }
-        if rawData.isObject { return classify(rawData, with: JSONRegex.nestedKeyAndValue) }
+        if rawData.isObject { return classify(rawData.removeBracket, with: JSONRegex.objectFactor) }
         return []
     }
     
