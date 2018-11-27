bd9ddfceb3c5ba28b73c84261d6ba02dfabf09ce
김장수 <jangsookim.me@gmail.com>
2018-11-27 17:38:55 +0900

    중첩 구조를 분석하고 파싱하도록 수정

--- JSONParser/JSONParser/regularExpression.swift
+++ JSONParser/JSONParser/regularExpression.swift
@@ -19,18 +19,17 @@ struct JSONRegex {
 
     // 중첩 구조의 입력에서 사용
     private static let nestedKeyAndValue = "\(str)\(blank):\(blank)(?:\(basic)|\(object)|\(array))"
-    private static let objectFactor = "\\{\(blank)\(nestedKeyAndValue)\(blank)(?:,\(blank)\(nestedKeyAndValue)\(blank))*\\}"
     private static let arrayFactor = "\(basic)|\(object)|\(array)"
     
     // GrammarCheck 구조체에서 규칙 검사에 사용
-    private static let objectPattern = "^\(objectFactor)$"
+    private static let objectPattern = "^\\{\(blank)\(nestedKeyAndValue)\(blank)(?:,\(blank)\(nestedKeyAndValue)\(blank))*\\}$"
     private static let arrayPattern = "^\\[\(blank)(?:\(arrayFactor))\(blank)(?:,\(blank)(?:\(arrayFactor))\(blank))*\\]$"
     
     
     
     // 규칙 검사를 통과한 입력에서 JSONType 요소를 배열로 변환
     func extractData(from rawData: String) -> [String] {
-        if rawData.isArray { return classify(rawData, with: JSONRegex.arrayFactor) }
+        if rawData.isArray { return classify(rawData.removeBracket, with: JSONRegex.arrayFactor) }
         if rawData.isObject { return classify(rawData, with: JSONRegex.nestedKeyAndValue) }
         return []
     }
