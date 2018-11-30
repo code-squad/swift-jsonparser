d8014b6f24c16b8d371174216aeb0a0fdb1ea91d
hngfu <djsl1018@naver.com>
2018-11-30 23:11:26 +0900

    [InputView] readFile() 완성
    이 메소드로 인해 ./JSONParser input.json 이라고 입력하면 바탕화면에 있는 input.json 파일의 정보를 읽음
    기본 경로가 바탕화면이라는게 중요

--- JSONParser/JSONParser/InputView.swift
+++ JSONParser/JSONParser/InputView.swift
@@ -15,7 +15,12 @@ struct InputView {
     }
     
     static func readFile() -> String {
+        let arguments = CommandLine.arguments
+        let fileManager = FileManager()
+        let desktop = fileManager.urls(for: .desktopDirectory, in: .userDomainMask).first!
+        let path = desktop.appendingPathComponent("\(arguments[1])")
+        let json = try? String(contentsOf: path, encoding: .utf8)
         
-        return ""
+        return json ?? ""
     }
 }
