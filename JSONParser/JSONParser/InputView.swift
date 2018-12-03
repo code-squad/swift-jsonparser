3855780a894ff0f21156bc762cb67679054835f6
hngfu <djsl1018@naver.com>
2018-12-03 17:00:11 +0900

    main에서 CommandLine값을 받고 Input방법을 선택해서 사용하도록 수정

--- JSONParser/JSONParser/InputView.swift
+++ JSONParser/JSONParser/InputView.swift
@@ -9,22 +9,12 @@
 import Foundation
 
 struct InputView {
-    static func readInput(arguments:[String]) -> (String,URL?) {
-        if arguments.count == 1 {
-            return readEnter(ment: "분석할 JSON 데이터를 입력하세요.")
-        }
-        if arguments.count == 2 {
-            return readContent(readFileName: arguments[1], saveFileName: "output.json")
-        }
-        return readContent(readFileName: arguments[1], saveFileName: arguments[2])
-    }
-    
-     static private func readEnter(ment:String) -> (String,URL?) {
+    static func readEnter(ment:String) -> (String,URL?) {
         print(ment)
         return (readLine() ?? "", nil)
     }
     
-    static private func readContent(readFileName:String, saveFileName:String) -> (String,URL?) {
+    static func readContent(readFileName:String, saveFileName:String) -> (String,URL?) {
         let fileManager = FileManager()
         guard let desktop = fileManager.urls(for: .desktopDirectory, in: .userDomainMask).first else {return ("",nil)}
         let path = desktop.appendingPathComponent("\(readFileName)")
--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -9,8 +9,16 @@
 import Foundation
 
 func main() {
-    let (input,url) = InputView.readInput(arguments: CommandLine.arguments)
-
+    let arguments = CommandLine.arguments
+    var (input,url) : (String,URL?)
+    if arguments.count == 1 {
+        (input,url) = InputView.readEnter(ment: "분석할 JSON 데이터를 입력하세요.")
+    } else if arguments.count == 2 {
+        (input,url) = InputView.readContent(readFileName: arguments[1], saveFileName: "output.json")
+    } else {
+        (input,url) = InputView.readContent(readFileName: arguments[1], saveFileName: arguments[2])
+    }
+    
     guard GrammarChecker.checkInputData(data: input) else {print("지원하지 않는 형식을 포함하고 있습니다.");return}
     guard let jsonData = Parser.convert(string: input) else {return}
     
