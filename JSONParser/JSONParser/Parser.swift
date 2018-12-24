9e5557bf9fec0ae32c8240d73d38a61146e4ab79
conyconydev <conyconydev@gmail.com>
2018-12-24 11:37:21 +0900

    update Parser : is validData

--- JSONParser/JSONParser/Parser.swift
+++ JSONParser/JSONParser/Parser.swift
@@ -70,7 +70,7 @@ struct Parser{
                 resultDataString.append(popdata)
             }else if isBoolType(popdata) {
                 resultDataBool.append(popdata)
-            }else if isNumber(popdata) {
+            }else if isNumber(popdata) && isNumber(popdata) {
                 resultDataInt.append(popdata)
             }
         }
@@ -79,6 +79,10 @@ struct Parser{
     
     private static func isNumber (_ popData : String) -> Bool {
         return popData.components(separatedBy: CharacterSet.decimalDigits).count != 0
+    }
+    private func isValidCharacter(_ popData : String) -> Bool {
+        let validCharacter = CharacterSet.init(charactersIn: "0123456789")
+        return (popData.rangeOfCharacter(from: validCharacter.inverted) == nil)
     }
     
     private static func isStringType (_ popData : String) -> Bool {
