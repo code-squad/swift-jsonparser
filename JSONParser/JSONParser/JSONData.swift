d062fee8a42864adc4b1dd8ca00b32f552f603b1
NORIKIM <jinagold.k@gmail.com>
2019-01-10 11:43:51 +0900

    사용자의 입력값을 부울,문자열,숫자로 분리해서 가져옴

---
+++ JSONParser/JSONParser/JSONData.swift
@@ -0,0 +1,37 @@
+//
+//  JSONData.swift
+//  JSONParser
+//
+//  Created by JINA on 08/01/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct JSONData {
+    var typeBoolean: Int
+    var typeString: Int
+    var typeNumber: Int
+    
+    init(typeBoolean: Int, typeString: Int, typeNumber: Int){
+        self.typeBoolean = typeBoolean
+        self.typeString = typeString
+        self.typeNumber = typeNumber
+    }
+    
+    var data: [String:Int] {
+        return ["부울":typeBoolean,"문자열":typeString,"숫자":typeNumber]
+    }
+    
+    static func makeJSON(from splitInput: [String]) -> JSONData {
+        let typeBoolean = RegularExpression.bringData(from: splitInput, regex: "(false|true)")
+        let typeString = RegularExpression.bringData(from: splitInput, regex: "\"{1}+[A-Z0-9a-z]+\"{1}")
+        let typeNumber = splitInput.count - (typeBoolean.count + typeString.count)
+        
+        let data = JSONData.init(typeBoolean: typeBoolean.count, typeString: typeString.count, typeNumber: typeNumber)
+        return data
+    }
+    
+}
+
+
