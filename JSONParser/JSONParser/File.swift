61f8c749baeb270972b5369f69c651638d634232
conyconydev <conyconydev@gmail.com>
2019-01-20 16:54:24 +0900

    File 생성 파일 입출력구현하려고 만들었음

---
+++ JSONParser/JSONParser/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  JSONParser
+//
+//  Created by Elena on 18/01/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct File {
+    func main() {
+        let arguments = CommandLine.arguments
+        print(arguments[1])
+    }
+}
