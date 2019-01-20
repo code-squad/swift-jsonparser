06b89a41ab6de5e120d1eaa68b8b93f39ce13883
김지나 <jinagold.k@gmail.com>
2019-01-20 16:42:16 +0900

    프로토콜을 JsonValue를 이용하여 다양한 타입을 저장하도록 JSONData생성

--- JSONParser/JSONParser/JSONData.swift
+++ JSONParser/JSONParser/JSONData.swift
@@ -8,30 +8,22 @@
 
 import Foundation
 
-struct JSONData {
-    private var typeBoolean: Int
-    private var typeString: Int
-    private var typeNumber: Int
+class JSONData {
+    var values: [JsonValue] = []
     
-    init(typeBoolean: Int, typeString: Int, typeNumber: Int){
-        self.typeBoolean = typeBoolean
-        self.typeString = typeString
-        self.typeNumber = typeNumber
-    }
-    
-    var data: [String:Int] {
-        return ["부울":typeBoolean,"문자열":typeString,"숫자":typeNumber]
-    }
-    
-    static func makeJSON(from splitInput: [String]) -> JSONData {
-        let typeBoolean = RegularExpression.bringData(from: splitInput, regex: "(false|true)")
-        let typeString = RegularExpression.bringData(from: splitInput, regex: "\"{1}+[A-Z0-9a-z]+\"{1}")
-        let typeNumber = splitInput.count - (typeBoolean.count + typeString.count)
-        
-        let data = JSONData.init(typeBoolean: typeBoolean.count, typeString: typeString.count, typeNumber: typeNumber)
-        return data
-    }
-    
+    init(_ values: JsonValue...) {
+        self.values = values
+    } 
 }
+
+
+
+
+
+
+
+
+
+
 
 
