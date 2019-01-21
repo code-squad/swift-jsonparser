25bc4512c8de8ffb3c9a1229a8f0b5be0e46b313
conyconydev <conyconydev@gmail.com>
2019-01-21 15:38:33 +0900

    출력문 변경

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -9,21 +9,24 @@
 import Foundation
 
 struct OutputView {
-    private struct Message {
-        static let invalidForm = "지원하지 않는 형식을 포함하고 있습니다."
+    struct Message {
         
+        enum Error: String {
+            case invalidForm = "지원하지 않는 형식을 포함하고 있습니다."
+            case noInvalidSave = "저장되지 않음"
+        }
+
         struct countResult {
             static let noCount = 0
             static let countUnit = "개"
             static let comma = ","
             
-            static func parserResultData(with totalCount: Int, _ dataForm: String, and typeCount: String) -> String {
+            static func parserResultData(_ totalCount: Int, _ dataForm: String, and typeCount: String) -> String {
                 return "총 \(totalCount)개의 \(dataForm) 데이터 중에 \(typeCount)가 포함되어 있습니다."
             }
-            
         }
     }
-    
+   
     private static func addTypeName(_ data: JSONDataForm) -> String {
         let typeCount = data.countValue()
         let countNameAdded = typeCount.map { (key, value) in "\(key) \(value)\(Message.countResult.countUnit)" }
@@ -34,13 +37,24 @@ struct OutputView {
         let totalCount = data.totalCount
         let typeName = data.typeName
         let typeCount = addTypeName(data)
-        print(Message.countResult.parserResultData(with: totalCount, typeName, and: typeCount))
+        print(Message.countResult.parserResultData(totalCount, typeName, and: typeCount))
     }
+    
     static func showJSONTypeData(_ data: JSONDataForm) {
         print(data.typeData)
     }
     
-    static func errorResult() {
-        print(Message.invalidForm)
+    static func errorResult(_ error: Message.Error) {
+        print(error.rawValue)
+    }
+    
+    static func writeShowJSONTypeData(_ jsonDataForm: JSONDataForm,_ file: String) {
+        let data = jsonDataForm.typeData
+        guard let fileURL = File.getURL(file) else { return }
+        do {
+            try data.write(to: fileURL, atomically: true, encoding: .utf8)
+        } catch {
+            errorResult(.noInvalidSave)
+        }
     }
 }
