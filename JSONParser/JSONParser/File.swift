9bb0d7840c6892f7c0fc8449898e127337de00db
conyconydev <conyconydev@gmail.com>
2019-01-21 15:37:21 +0900

    File.swift Command Line Argument 값에 따라 처리하도록 구현

--- JSONParser/JSONParser/File.swift
+++ JSONParser/JSONParser/File.swift
@@ -8,9 +8,72 @@
 
 import Foundation
 
-struct File {
-    func main() {
-        let arguments = CommandLine.arguments
-        print(arguments[1])
+protocol JSONParserReadWrite {
+    func readUserString() -> String?
+    func writeResult(_: JSONDataForm)
+}
+
+struct CommandLineRead {
+    enum Arguments: Int {
+        case projectAddress = 0, inputFile, outputFile
+    }
+    
+    static func readArgument(_ index: Arguments) -> String? {
+        let index = index.rawValue
+        let commandArgs = CommandLine.arguments
+        guard index < commandArgs.count else { return nil }
+        return commandArgs[index]
+    }
+    
+    static func selectArgument(_ index: Arguments) -> Bool {
+        guard readArgument(index) != nil else { return false }
+        return true
     }
 }
+
+struct File {
+    
+    static func getURL(_ file: String? = nil) -> URL? {
+        let fileURL = FileManager.default.urls(for: .desktopDirectory,in: .userDomainMask)[0]
+        guard let file = file else { return fileURL }
+        let fileURLAppendingPath = fileURL.appendingPathComponent(file)
+        return fileURLAppendingPath
+    }
+    
+    static func readData(_ file: String) -> String? {
+        guard let fileURL = getURL(file) else { return nil }
+        guard let data = try? String(contentsOf: fileURL) else { return nil }
+        return data
+    }
+}
+
+struct JSONLineParser: JSONParserReadWrite {
+    
+    func readUserString() -> String? {
+        let jsonString = InputView.getUserString()
+        return jsonString
+    }
+    
+    func writeResult(_ jsonDataForm: JSONDataForm) {
+        OutputView.showJSONTypeData(jsonDataForm)
+    }
+    
+}
+
+struct JSONFileParser: JSONParserReadWrite {
+    private let defaultName = "defaultName.json"
+    
+    func readUserString() -> String? {
+        guard let file = CommandLineRead.readArgument(.inputFile) else { return nil }
+        guard let jsonString = File.readData(file) else { return nil }
+        return jsonString
+    }
+
+    func writeResult(_ jsonDataForm: JSONDataForm) {
+        let file = CommandLineRead.readArgument(.outputFile) ?? defaultName
+        OutputView.writeShowJSONTypeData(jsonDataForm,file)
+    }
+    
+}
+
+
