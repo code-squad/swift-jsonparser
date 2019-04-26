1494f6fda7a5c495eb6d2dea688b035ca89b8fd2
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-27 00:58:49 +0900

    숫자 분석기 첫번째 분석 함수 생성

--- JSONParser/JSONParser/NumberParser.swift
+++ JSONParser/JSONParser/NumberParser.swift
@@ -1,37 +1,49 @@
 import Foundation
 
 struct NumberParser: Parser {
+    
+    var result: SupportedType
+    
     var buffer = ""
     var isDataToParse = false
-    var hasMinusSign = false
+    var hasDoneFirstParse = false
     var isZero = false
     
-    mutating func parse(character: Character) throws -> Double? {
+    mutating func parse(_ character: Character) throws -> Bool {
+        
+    }
+    
+    mutating func firstParse(_ character: Character) throws {
         switch character {
         case "-":
-            if hasMinusSign {
-                throw NumberParsingError.duplicatedMinusSign
-            } else {
-                hasMinusSign = !hasMinusSign
-                buffer.append(character)
-            }
-        case "0":
-            if isZero {
-                throw NumberParsingError.duplicatedNumberZero
-            } else {
-                isZero = !isZero
-            }
-        default:
             buffer.append(character)
+        case "0":
+            buffer.append(character)
+            hasDoneFirstParse = true
+        case "1", "2", "3", "4", "5", "6", "7", "8", "9":
+            buffer.append(character)
+            hasDoneFirstParse = true
+        default:
+            throw NumberParsingError.invalidNumberFormat
+        }
+    }
+    
+    mutating func secondParse(_ character: Character) throws {
+        switch <#value#> {
+        case <#pattern#>:
+            <#code#>
+        default:
+            <#code#>
         }
         
-        return nil
     }
+    
+    
     
 }
 
 enum NumberParsingError: Error {
     case duplicatedMinusSign
     case duplicatedNumberZero
-    case invalidNumber
+    case invalidNumberFormat
 }
