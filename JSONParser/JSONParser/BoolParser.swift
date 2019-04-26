375589d8a3216311db6b122ad891838185c624b1
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-27 00:38:02 +0900

    부울을 결정하면 다른 부울의 문자 배열 원소를 전부 제거함

--- JSONParser/JSONParser/BoolParser.swift
+++ JSONParser/JSONParser/BoolParser.swift
@@ -7,29 +7,23 @@ struct BoolParser: Parser {
     
     var trueCharacters: [Character] = ["t", "r", "u", "e"]
     var falseCharacters: [Character] = ["f", "a", "l", "s", "e"]
-
+    
     mutating func parse(_ character: Character) throws -> Bool {
         switch character {
         case trueCharacters.first:
             if didDetermineBoolValue {
-                if result as! Bool {
-                    trueCharacters.removeFirst()
-                } else {
-                    throw BoolParsingError.cannotFindBoolFormat
-                }
+                throw BoolParsingError.cannotFindBoolFormat
             } else {
                 result = true
+                falseCharacters.removeAll()
             }
             return true
         case falseCharacters.first:
             if didDetermineBoolValue {
-                if !(result as! Bool) {
-                    falseCharacters.removeFirst()
-                } else {
-                    throw BoolParsingError.cannotFindBoolFormat
-                }
+                throw BoolParsingError.cannotFindBoolFormat
             } else {
                 result = false
+                trueCharacters.removeAll()
             }
             return true
         case ",", " ":
@@ -38,9 +32,6 @@ struct BoolParser: Parser {
             throw BoolParsingError.cannotFindBoolFormat
         }
     }
-    
-    
-    
     
 }
 
