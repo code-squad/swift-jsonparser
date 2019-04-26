bb8e92efcaabd1b58d0b5bc9dd1eab5f179f5745
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-26 22:43:58 +0900

    문자열 분석기 구조체 생성

--- JSONParser/JSONParser/JSONParser.swift
+++ JSONParser/JSONParser/JSONParser.swift
@@ -6,90 +6,61 @@ struct JSONParser {
     
     
     
-    mutating func parse(JSON: String) throws {
+    
+    
+    
+    
+    mutating func parse(JSON: String) {
         
-        var currentEncodedOffset = 0
-        var currentStringIndex = String.Index(encodedOffset: currentEncodedOffset)
+        var buffer = ""
+        var isDataToParse = false
         
+        for character in JSON {
+            
+            if character == "," {
+                isDataToParse = !isDataToParse
+            }
+            if isDataToParse {
+                buffer.append(character)
+            } else {
+                data.string.append(buffer)
+            }
+            
+        }
         
+    }
+    
+    
+    
+    struct StringParser {
+        var buffer = ""
+        var isDataToParse = false
+        var isEscapeSequence = false
         
-        
-        func collectStringData(firstCharacter: Character) {
-            var string = "\(firstCharacter)"
-            while currentEncodedOffset < JSON.count - 1 {
-                if JSON[currentStringIndex] == "\"" {
-                    currentEncodedOffset += 1
-                    break
+        mutating func parse(character: Character) -> String? {
+            switch character {
+            case "\\":
+                if isEscapeSequence {
+                    buffer.append(character)
+                } else {
+                    isEscapeSequence = !isEscapeSequence
                 }
-                string.append(JSON[currentStringIndex])
-                currentEncodedOffset += 1
-            }
-            data.string.append(string)
-        }
-        
-        
-        func collectNumberData(firstCharacter: Character) throws {
-            let digits: [Character] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-            var numberString = "\(firstCharacter)"
-            
-            if numberString == "0", JSON[currentStringIndex] == "0" {
-                throw ParsingError.invalidNumber
-            }
-            
-            
-            if JSON[currentStringIndex] == "-" {
-                numberString.append(JSON[currentStringIndex])
-                currentEncodedOffset += 1
-            }
-            
-            
-            while currentEncodedOffset < JSON.count - 1 {
-                
-                
-                currentEncodedOffset += 1
-            }
-            
-        }
-        
-        
-        func collectBoolData() {
-            while currentEncodedOffset < JSON.count - 1 {
-                
-                
-                currentEncodedOffset += 1
-            }
-            
-        }
-        
-        
-        while currentEncodedOffset < JSON.count - 1 {
-            switch JSON[currentStringIndex] {
-            case "[":
-                ""
+            case "\"":
+                if isEscapeSequence {
+                    buffer.append(character)
+                    isEscapeSequence = !isEscapeSequence
+                } else {
+                    isDataToParse = !isDataToParse
+                }
             default:
-                ""
+                if isDataToParse {
+                    buffer.append(character)
+                } else {
+                    return buffer
+                }
             }
-            
-            
-            
-            currentEncodedOffset += 1
+            return nil
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        defer {
-            currentEncodedOffset = 0
-        }
-        
-        
-        
-        
     }
     
     
