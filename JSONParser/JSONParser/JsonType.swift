6d506e9b480edeedc3d7ca198352691dfb97b013
kiljh <kiljh@me.com>
2019-04-29 17:41:25 +0900

    Create JsonType

---
+++ JSONParser/JSONParser/JsonType.swift
@@ -0,0 +1,15 @@
+//
+//  JsonType.swift
+//  JSONParser
+//
+//  Created by joon-ho kil on 4/29/19.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum JsonType {
+    case int(Int)
+    case string(String)
+    case bool(Bool)
+}
