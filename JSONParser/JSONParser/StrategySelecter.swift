8814e2920cd2413e14bd08057fdacfb158f7a721
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-29 10:03:30 +0900

    전략 선택기로 이름 변경

--- JSONParser/JSONParser/StrategySelecter.swift
+++ JSONParser/JSONParser/StrategySelecter.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct TypeSelecter {
+struct StrategySelecter {
     
     private var parsingStrategy: ParsingStrategy? = nil
     private var hasDetectedType = false
@@ -15,14 +15,14 @@ struct TypeSelecter {
     
     private mutating func detectType(_ character: Character) throws {
         switch character {
+        case " ":
+            return
         case "\"":
             parsingStrategy = StringParsingStrategy()
         case "t", "f":
             parsingStrategy = BoolParsingStrategy()
         case "-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
             parsingStrategy = NumberParsingStrategy()
-        case " ":
-            return
         case "[":
             parsingStrategy = ArrayParsingStrategy()
         default:
