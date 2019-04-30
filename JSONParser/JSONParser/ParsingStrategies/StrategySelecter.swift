ac431ea51b9d6fcf97303fe83a7e74155854e9b1
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-30 17:04:28 +0900

    detectType이 문자를 분석하고 반환하도록 변경

--- JSONParser/JSONParser/ParsingStrategies/StrategySelecter.swift
+++ JSONParser/JSONParser/ParsingStrategies/StrategySelecter.swift
@@ -4,6 +4,7 @@ struct StrategySelecter: ParsingStrategy {
     
     private(set) var parsingStrategy: ParsingStrategy!
     
+    
     mutating func result() throws -> Type {
         let result = try parsingStrategy.result()
         parsingStrategy = nil
@@ -12,7 +13,7 @@ struct StrategySelecter: ParsingStrategy {
     
     mutating func parse(_ charcater: Character) throws -> ParsingState {
         if parsingStrategy == nil {
-            try detectType(charcater)
+            return try detectType(charcater)
         }
         guard parsingStrategy != nil else {
             return ParsingState.isNotDone
@@ -20,25 +21,42 @@ struct StrategySelecter: ParsingStrategy {
         return try parsingStrategy.parse(charcater)
     }
     
-    private mutating func detectType(_ character: Character) throws {
-        switch character {
-        case " ":
-            return
-        case "\"":
-            parsingStrategy = StringParsingStrategy()
-        case "t", "f":
-            parsingStrategy = BoolParsingStrategy()
-        case "-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
-            parsingStrategy = NumberParsingStrategy()
-        case "[":
-            parsingStrategy = ArrayParsingStrategy()
+    
+    private func loopParsingStrategy(index: Int) -> ParsingStrategy? {
+        switch index {
+        case 1:
+            return ArrayParsingStrategy()
+        case 2:
+            return StringParsingStrategy()
+        case 3:
+            return BoolParsingStrategy()
+        case 4:
+            return NumberParsingStrategy()
         default:
-            throw TypeSeltectionError.unsupportedCharacter
+            return nil
         }
     }
+    
+    private mutating func detectType(_ character: Character) throws -> ParsingState {
+        if character == " " { return ParsingState.isNotDone }
+        
+        for index in 1...4 {
+            guard var parsingStrategy = loopParsingStrategy(index: index) else {
+                throw TypeSeltectionError.invalidIndex
+            }
+            if let state = try? parsingStrategy.parse(character) {
+                self.parsingStrategy = parsingStrategy
+                return state
+            }
+        }
+        throw TypeSeltectionError.unsupportedCharacter
+    }
+    
+    
     
 }
 
 enum TypeSeltectionError: Error {
     case unsupportedCharacter
+    case invalidIndex
 }
