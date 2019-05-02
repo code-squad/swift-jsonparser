e3cf089c388b9b1e09dd6c852f68510f2157d75f
gukjin jang <jgj455@gmail.com>
2019-05-02 15:21:31 +0900

    Add : InputView, JsonParser, OutputView 구조체 생성

---
+++ JSONParser/JSONParser/InputView.swift
@@ -0,0 +1,13 @@
+//
+//  InputView.swift
+//  JSONParser
+//
+//  Created by jang gukjin on 02/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    
+}
