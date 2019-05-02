f375a570f1891d606618b3fccc8a4f3f61dc7416
Sunghyun Kim <sunghyun_k@icloud.com>
2019-05-02 15:35:50 +0900

    지원하는 유형으로 오브젝트를 추가함

--- JSONParser/JSONParser/Type.swift
+++ JSONParser/JSONParser/Type.swift
@@ -2,7 +2,18 @@ import Foundation
 
 protocol Type { var description: String { get } }
 
-extension String: Type { var description: String { return "문자열" } }
-extension Double: Type { var description: String { return "숫자" } }
-extension Bool: Type { var description: String { return "부울" } }
-extension Array: Type where Element == Type { var description: String { return "배열" } }
+extension String: Type {
+    var description: String { return "문자열" }
+}
+extension Double: Type {
+    var description: String { return "숫자" }
+}
+extension Bool: Type {
+    var description: String { return "부울" }
+}
+extension Array: Type where Element == Type {
+    var description: String { return "배열" }
+}
+extension Dictionary: Type where Value == Type {
+    var description: String { return "오브젝트" }
+}
