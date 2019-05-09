6eb7343301677527b34f49afb43bf30cdf7b38bf
hwj0623 <hwj0623@naver.com>
2019-05-09 16:17:44 +0900

    STEP 7-1 (7) Lexer 추가

---
+++ JSONParser/JSONParser/Lexer.swift
@@ -0,0 +1,62 @@
+//
+//  Lexer.swift
+//  JSONParser
+//
+//  Created by hw on 08/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+// A lexer is basically a tokenizer, but it usually attaches extra context to the tokens -- this token is a number, that token is a string literal, this other token is an equality operator.
+// https://www.quora.com/Whats-the-difference-between-a-tokenizer-lexer-and-parser
+// A lexer does the same plus attachs extra information to each token. If we tokenize into words, a lexer would attach tags like number, word, punctuation etc.
+
+import Foundation
+enum LexicalType: String{
+    case integerNumber = "Int"
+    case bool = "Bool"
+    case string = "String"
+}
+
+typealias LexPair = (type: LexicalType , content: String)
+
+struct Lexer {
+    static func doLexcialAnalysis (tokenList : [String]) throws -> [LexPair]{
+        var result = [LexPair]()
+        for token in tokenList {
+            let testStringResult = isString(token)
+            let testNumberResult = isNumeric(token)
+            let testBooleanResult = isBoolean(token)
+            let lexicalType = try decideElementType(string: testStringResult, number: testNumberResult, bool: testBooleanResult)
+            result.append(LexPair(type: lexicalType, content: token))
+        }
+        return result
+    }
+
+    static func decideElementType( string: Bool, number: Bool, bool : Bool ) throws -> LexicalType {
+        var result: LexicalType? = string ? LexicalType.string : nil
+        result = number ? LexicalType.integerNumber : result
+        result = bool ? LexicalType.bool : result
+        guard let resultType = result else {
+            throw ErrorCode.lexicalTypeError
+        }
+        return resultType
+    }
+    
+    static private func isString (_ token : String) -> Bool {
+        return token.hasPrefix("\"") && token.hasSuffix("\"") ? true : false
+    }
+    
+    static private func isNumeric (_ token : String) -> Bool {
+        guard let _: Int = Int(token) else {
+            return false
+        }
+        return true
+    }
+    
+    static private func isBoolean (_ token : String) -> Bool {
+        guard let _: Bool = Bool(token) else {
+            return false
+        }
+        return true
+    }
+}
