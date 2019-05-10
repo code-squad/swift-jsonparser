ef25e47078c736ab9ad1f7487503aa1a30d4688e
O-O-wl <ldcpaul@hanmail.net>
2019-05-11 03:44:33 +0900

    Modifie : 타입체크하는 객체 테스트

---
+++ JSONParser/JSONParser/TypeChecker.swift
@@ -0,0 +1,9 @@
+//
+//  TypeChecker.swift
+//  JSONParser
+//
+//  Created by 이동영 on 11/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
---
+++ JSONParser/JSONParserTests/TypeCheckerTest.swift
@@ -0,0 +1,24 @@
+//
+//  TypeCheckerTest.swift
+//  JSONParserTests
+//
+//  Created by 이동영 on 11/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import XCTest
+
+class TypeCheckerTest: XCTestCase {
+
+    var typeChecker : TypeChecker!
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.typeChecker = TypeChecker()
+    }
+    
+    func testCheckType(){
+        
+    }
+
+
+}
