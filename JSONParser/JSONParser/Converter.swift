54a5b1eda3a416eae90fe000feba41692c6bf684
O-O-wl <ldcpaul@hanmail.net>
2019-05-11 05:15:18 +0900

    Modifie : 배열로 변환하는 메소드 수정

--- JSONParser/JSONParser/Converter.swift
+++ JSONParser/JSONParser/Converter.swift
@@ -11,11 +11,20 @@ import Foundation
 struct Converter {
     static let separator = ","
     
-    static func convert(string:String) throws -> [String]{
-        let stringArray = string.components(separatedBy: self.separator )
+    static func convert(string:String) throws -> [String] {
+        let values = try Converter.getValues(string)
+        let stringArray = values.components(separatedBy: self.separator )
         guard stringArray.count != 0 else {
             throw Exception.wrongFormat
         }
         return stringArray
+    }
+    
+    static func getValues(_ string:String) throws -> String {
+        guard try TypeChecker().check(string, type: .Array) else {
+            return string
+        }
+        let unnecessaryCharacters:[Character] = ["[","]"," "]
+        return string.filter{ !unnecessaryCharacters.contains($0) }
     }
 }
