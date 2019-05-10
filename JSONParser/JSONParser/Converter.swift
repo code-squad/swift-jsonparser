e28e86d337af54f59b0f2760921c5a0cb23cd7cb
O-O-wl <ldcpaul@hanmail.net>
2019-05-11 03:29:18 +0900

    Modifie : 문자열을 문자열의 배열로 바꾸는 함수 정의

--- JSONParser/JSONParser/Converter.swift
+++ JSONParser/JSONParser/Converter.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+struct Converter {
+    static let separator = ","
+    
+    static func convert(string:String) throws -> [String]{
+        let stringArray = string.components(separatedBy: self.separator )
+        guard stringArray.count != 0 else {
+            throw Exception.wrongFormat
+        }
+        return stringArray
+    }
+}
