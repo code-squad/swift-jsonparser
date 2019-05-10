fd4b1eda18bf2a563386901029be15abe41247c0
O-O-wl <ldcpaul@hanmail.net>
2019-05-11 05:22:58 +0900

    Modifie : 험수 네이밍 수정에 따른 테스트메소드 수정

--- JSONParser/JSONParserTests/ConverterTests.swift
+++ JSONParser/JSONParserTests/ConverterTests.swift
@@ -12,12 +12,13 @@ class ConverterTest: XCTestCase {
     
     func testConvertComposedOfIntegerData() {
         let input = "[ 10, 21, 4, 314, 99, 0, 72 ]"
-        let array = try! Converter.convert(string: input)
+        let array = try! Converter.stringToArray(string: input)
         XCTAssertEqual(array,["10","21","4","314","99","0","72"],"정수형 배열로의 변환에 성공합니다.")
     }
+    
     func testConvertComposedOfMixedData() {
         let input =  "[ 10, \"jk\", 4, \"314\", 99, \"crong\", false ]"
-        let array = try! Converter.convert(string: input)
+        let array = try! Converter.stringToArray(string: input)
         XCTAssertEqual(array,[ "10", "\"jk\""
             , "4", "\"314\"", "99", "\"crong\"", "false" ],"혼합된 데이터타입의 배열로의 변환에 성공합니다.")
     }
