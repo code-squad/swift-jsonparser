f16a30ec275fde97e0de0d25e058e666cb747386
O-O-wl <ldcpaul@hanmail.net>
2019-05-11 02:23:11 +0900

    Created: InputView 구조체 생성

---
+++ JSONParser/JSONParser/InputView.swift
@@ -0,0 +1,30 @@
+//
+//  InputView.swift
+//  JSONParser
+//
+//  Created by 이동영 on 11/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    enum Question : String {
+        case aboutJSON = "분석할 JSON 데이터를 입력하세요."
+    }
+    
+    private func ask(_ q:Question) {
+        print(q.rawValue)
+    }
+    
+    private func fetchInput() -> String {
+        let input = readLine() ?? ""
+        return input
+    }
+    
+    func run() -> String {
+        ask(Question.aboutJSON)
+        return fetchInput()
+    }
+    
+}
