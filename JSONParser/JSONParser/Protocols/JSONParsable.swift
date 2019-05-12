4936670acd2a1007a4aaa68ec22e7413c4c23d7d
O-O-wl <ldcpaul@hanmail.net>
2019-05-12 14:29:32 +0900

    Created: JSONParsable 프로토콜 정의

---
+++ JSONParser/JSONParser/Protocols/JSONParsable.swift
@@ -0,0 +1,13 @@
+//
+//  JSONParsable.swift
+//  JSONParser
+//
+//  Created by 이동영 on 12/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol JSONParsable {
+    <#requirements#>
+}
