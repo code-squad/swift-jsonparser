72b4059c84d7bc21a9d8a43be77ea4cfe3bc8d9a
cmindy <minjidev@naver.com>
2019-05-13 17:07:24 +0900

    feat: Add InputView struct

---
+++ JSONParser/JSONParser/InputView.swift
@@ -0,0 +1,13 @@
+//
+//  InputView.swift
+//  JSONParser
+//
+//  Created by CHOMINJI on 13/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+
+}
