b6afb4a1336f67524f863fc1e9c5d619707e3009
kiljh <kiljh@me.com>
2019-05-14 16:11:18 +0900

    생성 - JSONParserTest 테스트 생성

---
+++ JSONParser/JSONParserTest/JSONParserTest.swift
@@ -0,0 +1,16 @@
+//
+//  JSONParserTest.swift
+//  JSONParserTest
+//
+//  Created by joon-ho kil on 5/14/19.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import XCTest
+
+class JSONParserTest: XCTestCase {
+    func test() {
+        
+    }
+
+}
