3a19984794ae33d6cd3d26589feb8adc1c163afd
Sunghyun Kim <sunghyun_k@icloud.com>
2019-05-17 23:49:25 +0900

    Token 구조체 작성

---
+++ JSONParser/JSONParser/Token.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Token {
+    static let BeginArray = "["
+    static let EndArray = "]"
+    static let BeginObject = "{"
+    static let EndObject = "}"
+    static let NameSeparator = ":"
+    static let ValueSeparator = ","
+    static let QuotationMark = "\""
+    static let Escape = "\\"
+}
