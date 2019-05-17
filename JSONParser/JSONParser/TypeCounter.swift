dc83f811ed22237237a352d7ab5c63c3cd7e3313
Daheen Lee <allwhite.dev@gmail.com>
2019-05-17 19:49:44 +0900

    feat: Add TypeCounter with method to get total count of each type

--- JSONParser/JSONParser/TypeCounter.swift
+++ JSONParser/JSONParser/TypeCounter.swift
@@ -8,4 +8,31 @@
 
 import Foundation
 
-
+struct TypeCounter {
+    static func getTotalTypeCount(of values: [JSONValue]) -> [(String, Int)] {
+        let boolCount   = countBool(in: values)
+        let numberCount = countNumber(in: values)
+        let stringCount = countString(in: values)
+        
+        var totalCountPair: [(String, Int)] = []
+        totalCountPair.append((Bool.typeDescription, boolCount))
+        totalCountPair.append((Int.typeDescription, numberCount))
+        totalCountPair.append((String.typeDescription, stringCount))
+        return totalCountPair
+    }
+    
+    static func countBool(in values: [JSONValue]) -> Int {
+        let boolElements = values.filter({ value in value is Bool })
+        return boolElements.count
+    }
+    
+    static func countNumber(in values: [JSONValue]) -> Int {
+        let numberElements = values.filter({ value in value is Int })
+        return numberElements.count
+    }
+    
+    static func countString(in values: [JSONValue]) -> Int {
+        let stringElements = values.filter({ value in value is String })
+        return stringElements.count
+    }
+}
