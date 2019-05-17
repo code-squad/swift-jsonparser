303ef4017dcdfe21f97ee44adc3c9806ef3e5328
gukjin jang <jgj455@gmail.com>
2019-05-17 16:11:47 +0900

    Modify: Bracket 열거형을 구조체 Sign으로 변경하고 ",,,:,| 를 내부프로퍼티에 저장

--- JSONParser/JSONParser/JsonParser.swift
+++ JSONParser/JSONParser/JsonParser.swift
@@ -9,10 +9,6 @@
 import Foundation
 
 struct JsonParser {
-    let frontCurlyBracket = Bracket.frontCurly.rawValue
-    let backCurlyBracket = Bracket.backCurly.rawValue
-    let frontSquareBracket = Bracket.frontSquare.rawValue
-    let backSquareBracket = Bracket.backSquare.rawValue
     /// 입력받은 문자열이 nil인지 아닌지 판단 및 옵셔널 바인딩 함수
     private func distinctNil(input: String?) throws -> String {
         guard let notOptionalText: String = input else { throw ErrorMessage.wrongValue }
@@ -28,8 +24,8 @@ struct JsonParser {
     /// 입력받은 String의 양끝에 "[","]" 또는 "{","}" 유무를 판단하여 정보를 넘기는 함수
     private func distinctArray(inputData: String) throws -> (input: String, distinctMent: String) {
         switch (inputData.first, inputData.last) {
-        case (frontCurlyBracket,backCurlyBracket): return (inputData, DataMent.dictionaryMent.rawValue)
-        case(frontSquareBracket,backSquareBracket): return (inputData, DataMent.arrayMent.rawValue)
+        case (Sign.frontCurlyBracket,Sign.backCurlyBracket): return (inputData, DataMent.dictionaryMent.rawValue)
+        case(Sign.frontSquareBracket,Sign.backSquareBracket): return (inputData, DataMent.arrayMent.rawValue)
         default: throw ErrorMessage.notArray
         }
     }
@@ -37,10 +33,10 @@ struct JsonParser {
     func parsingData(beforeData : String) throws -> Json {
         var convertedElement: Json
         let afterData = beforeData.trimmingCharacters(in: .whitespacesAndNewlines)
-        if afterData.first == frontCurlyBracket, afterData.last == backCurlyBracket {
+        if afterData.first == Sign.frontCurlyBracket, afterData.last == Sign.backCurlyBracket {
             let distinctAfterData = try distinctArray(inputData: afterData)
             let dictionaryData = isolatingString(input: distinctAfterData.input)
-            let isolatedDictionaryData = dictionaryData.components(separatedBy: ",").filter{ $0 != "" }
+            let isolatedDictionaryData = dictionaryData.components(separatedBy: "\(Sign.comma)").filter{ $0 != "" }
             for dictionaryDatum in isolatedDictionaryData {
                 _ = try valueOfArrayOrDistinct(dataElement: dictionaryDatum, dataMent: distinctAfterData.distinctMent)
             }
@@ -49,7 +45,7 @@ struct JsonParser {
             convertedElement = TypeInt.init(json: afterData)
         } else if let _ = Bool(afterData) {
             convertedElement = TypeBool.init(json: afterData)
-        } else if afterData.first == ("\""),afterData.last == ("\"") {
+        } else if afterData.first == Sign.doubleQuote, afterData.last == Sign.doubleQuote {
             convertedElement = TypeString.init(json: afterData)
         } else {
             throw ErrorMessage.wrongValue
@@ -58,9 +54,9 @@ struct JsonParser {
     }
     /// 입력된 원소에 key값이 있는 데이터인 경우 값만을 리턴하는 함수
     func ifKeyExist(dictionaryDataElement: String) throws -> String{
-        let refinedDatum = dictionaryDataElement.components(separatedBy: ":")
+        let refinedDatum = dictionaryDataElement.components(separatedBy: "\(Sign.colon)")
         let removeBlankDatum = refinedDatum[0].trimmingCharacters(in: .whitespacesAndNewlines)
-        if removeBlankDatum.first != ("\"") || removeBlankDatum.last != ("\"") {
+        if removeBlankDatum.first != Sign.doubleQuote || removeBlankDatum.last != Sign.doubleQuote {
             throw ErrorMessage.wrongKey
         } else if refinedDatum.count != 2 {
             throw ErrorMessage.wrongValue
@@ -71,7 +67,7 @@ struct JsonParser {
     /// 입력된 문자열이 배열인지 딕셔너리인지 판단하고, 이에 맞는 원소들을 확인하는 함수
     private func valueOfArrayOrDistinct(dataElement: String, dataMent: String) throws -> String{
         var refinedDatum: String
-        if dataElement.contains(":"), dataMent == DataMent.dictionaryMent.rawValue {
+        if dataElement.contains(Sign.colon), dataMent == DataMent.dictionaryMent.rawValue {
             refinedDatum = try ifKeyExist(dictionaryDataElement: dataElement)
         } else if dataMent == DataMent.arrayMent.rawValue {
             refinedDatum = dataElement
@@ -82,19 +78,22 @@ struct JsonParser {
     }
     /// 입력한 데이터의 내부에 객체가 존재할 경우 데이터를 나누는 함수
     private func dictionaryInArray(input: String) -> [String] {
-        var dictionaryInData = input.components(separatedBy: ["{","}"]).filter{ $0 != "" }
+        let frontCurlyNumber = Sign.frontCurlyBracket.unicodeScalars.map { $0.value }.reduce(0, +)
+        let backCurlyNumber = Sign.backCurlyBracket.unicodeScalars.map { $0.value }.reduce(0, +)
+        let curlyBrackets : CharacterSet = [Unicode.Scalar(frontCurlyNumber) ?? " ", Unicode.Scalar(backCurlyNumber) ?? " "]
+        var dictionaryInData = input.components(separatedBy: curlyBrackets).filter{ $0 != "" }
         for index in 0..<dictionaryInData.count {
-            if dictionaryInData[index].first == "," || dictionaryInData[index].last == "," {
-                dictionaryInData[index] = dictionaryInData[index].replacingOccurrences(of: ",", with: "|")
+            if dictionaryInData[index].first == Sign.comma || dictionaryInData[index].last == Sign.comma {
+                dictionaryInData[index] = dictionaryInData[index].replacingOccurrences(of: "\(Sign.comma)", with: "\(Sign.pipe)")
             }
         }
         for index in 0..<dictionaryInData.count {
-            if dictionaryInData[index].contains(":") {
-                dictionaryInData[index] = "\(frontCurlyBracket)" + dictionaryInData[index] + "\(backCurlyBracket)"
+            if dictionaryInData[index].contains(Sign.colon) {
+                dictionaryInData[index] = "\(Sign.frontCurlyBracket)" + dictionaryInData[index] + "\(Sign.backCurlyBracket)"
             }
         }
         let dataReJoined = dictionaryInData.joined()
-        dictionaryInData = dataReJoined.components(separatedBy: "|")
+        dictionaryInData = dataReJoined.components(separatedBy: "\(Sign.pipe)")
         return dictionaryInData
     }
     /// [Json] 타입의 배열을 생성하는 함수
@@ -104,10 +103,10 @@ struct JsonParser {
         let input = try distinctNil(input: inputData)
         let data = try distinctArray(inputData: input)
         let refinedData = isolatingString(input: data.input)
-        if refinedData.contains(frontCurlyBracket), refinedData.contains(backCurlyBracket) {
+        if refinedData.contains(Sign.frontCurlyBracket), refinedData.contains(Sign.backCurlyBracket) {
             isolatedData = dictionaryInArray(input: refinedData).filter{ $0 != "" }
         } else {
-            isolatedData = refinedData.components(separatedBy: ",").filter{ $0 != "" }
+            isolatedData = refinedData.components(separatedBy: "\(Sign.comma)").filter{ $0 != "" }
         }
         for dataElement in isolatedData {
             let refinedDatum = try valueOfArrayOrDistinct(dataElement: dataElement, dataMent: data.distinctMent)
---
+++ JSONParser/JSONParser/Sign.swift
@@ -0,0 +1,20 @@
+//
+//  Bracket.swift
+//  JSONParser
+//
+//  Created by jang gukjin on 14/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct Sign {
+    static let frontCurlyBracket: Character = "{"
+    static let backCurlyBracket: Character = "}"
+    static let frontSquareBracket: Character = "["
+    static let backSquareBracket: Character = "]"
+    static let comma: Character = ","
+    static let doubleQuote: Character = "\""
+    static let colon: Character = ":"
+    static let pipe: Character = "|"
+}
