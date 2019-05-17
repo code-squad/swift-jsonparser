38ff4174dbc6d8d830a9592af9b77f7f30e1035b
Daheen Lee <allwhite.dev@gmail.com>
2019-05-17 16:29:13 +0900

    refactor: Add printing instruction in execute() method of InputView

--- JSONParser/JSONParser/InputView.swift
+++ JSONParser/JSONParser/InputView.swift
@@ -9,7 +9,10 @@
 import Foundation
 
 struct IntputView {
+    static private let instruction = "분석할 JSON 데이터를 입력하세요."
+    
     static func read() -> String {
+        print(instruction)
         guard let input = readLine() else {
             return ""
         }
