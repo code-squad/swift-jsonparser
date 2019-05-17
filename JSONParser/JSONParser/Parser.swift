5dae82d98fcdd24b3ffe070621327c322e61e0fc
Daheen Lee <allwhite.dev@gmail.com>
2019-05-17 17:01:25 +0900

    feat: Add parse() method in Parser

--- JSONParser/JSONParser/Parser.swift
+++ JSONParser/JSONParser/Parser.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+struct Parser {
+    
+    static func parse(_ tokens: [String]) -> [JSONValue] {
+        
+        var values: [JSONValue] = []
+        
+        if isWrappedWithBrackets(using: tokens) {
+            let tokensWithoutBracket = tokens.filter() { token in
+                token != JSONSymbols.openBracket && token != JSONSymbols.closedBracket }
+            
+            values = parseIntoJSONValues(using: tokensWithoutBracket)
+        }
+        
+        return values
+        
+    }
+    
+    static private func isWrappedWithBrackets(using tokens: [String]) -> Bool {
+        return tokens[0] == JSONSymbols.openBracket
+            && tokens[tokens.count-1] == JSONSymbols.closedBracket
+    }
+    
+    static private func parseIntoJSONValues(using tokens: [String]) -> [JSONValue] {
+        var values: [JSONValue] = []
+        
+        for token in tokens {
+            let value = JSONValueFactory.make(token: token)
+            values.append(value)
+        }
+        return values
+    }
+    
+}
+
+
