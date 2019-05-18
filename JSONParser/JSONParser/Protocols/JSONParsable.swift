172c386985fe63553cc8e5221b96c3c43062fe0d
O-O-wl <ldcpaul@hanmail.net>
2019-05-18 23:53:49 +0900

    Created: Stack 생성

---
+++ JSONParser/JSONParser/Protocols/JSONParsable.swift
@@ -0,0 +1,9 @@
+//
+//  JSONParsable.swift
+//  JSONParser
+//
+//  Created by 이동영 on 18/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
--- JSONParser/JSONParser/Structs/Stack.swift
+++ JSONParser/JSONParser/Structs/Stack.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+struct Stack<T>{
+    private var elements:[T]
+    
+    init(){
+        self.elements = [T]()
+    }
+    
+    func isEmpty() -> Bool {
+        return elements.count > 0 ? false : true
+    }
+    
+    mutating func push(_ e:T){
+        self.elements.insert(e, at: 0)
+    }
+    
+    mutating func pop() throws -> T {
+        guard let lastElement = self.elements.popLast() else {
+            throw Exception.Empty
+        }
+        return lastElement
+    }
+    
+    func peek() throws -> T {
+        guard let lastElement = self.elements.last else {
+            throw Exception.Empty
+        }
+        return lastElement
+    }
+    
+    func size() -> Int {
+        return self.elements.count
+    }
+    
+    func show(){
+        print(self.elements)
+    }
+    
+    enum Exception: String,Error,CustomStringConvertible{
+        var description: String { return self.rawValue}
+        
+        case Empty = "스택이 비었습니다."
+    }
+}
+
