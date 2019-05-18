f8c920d234f67d68c55640067d537d159a6a8e91
Sunghyun Kim <sunghyun_k@icloud.com>
2019-05-18 15:13:27 +0900

    배열과 오브젝트 문법 확인기를 나눔

--- JSONParser/JSONParser/GrammarChecker.swift
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -2,12 +2,19 @@ import Foundation
 
 struct GrammarChecker {
     
-    private static let pattern = "^\\[\\s*(((\".*\"|[^,\"\\s]+)\\s*)(,\\s*(\".*\"|[^,\"\\s]+)\\s*)*)?\\]$|^\\{\\s*(((\"[^\"]*\")\\s*:\\s*(\"[^\"]*\"|[^,\"\\s]+)\\s*)(,\\s*((\"[^\"]*\")\\s*:\\s*(\"[^\"]*\"|[^,\"\\s]+))\\s*)*)?\\}$"
+    private static let arrayPattern = "^\\[\\s*(((\".*\"|[^,\"\\s]+)\\s*)(,\\s*(\".*\"|[^,\"\\s]+)\\s*)*)?\\]$"
+    private static let objectPattern = "^\\{\\s*(((\"[^\"]*\")\\s*:\\s*(\"[^\"]*\"|[^,\"\\s]+)\\s*)(,\\s*((\"[^\"]*\")\\s*:\\s*(\"[^\"]*\"|[^,\"\\s]+))\\s*)*)?\\}$"
     
-    private static var regularExpression = try! NSRegularExpression(pattern: pattern, options: [])
+    private static var arrayRegularExpression = try! NSRegularExpression(pattern: arrayPattern, options: [])
     
-    static func checkJSONGrammar(_ input: String) -> Bool {
-        return regularExpression.numberOfMatches(in: input, options: [], range: NSRange(location: 0, length: input.count)) == 1
+    private static var objectRegularExpression = try! NSRegularExpression(pattern: objectPattern, options: [])
+    
+    static func checkArrayGrammar(_ input: String) -> Bool {
+        return arrayRegularExpression.numberOfMatches(in: input, options: [], range: NSRange(location: 0, length: input.count)) == 1
+    }
+    
+    static func checkObjectGrammar(_ input: String) -> Bool {
+        return objectRegularExpression.numberOfMatches(in: input, options: [], range: NSRange(location: 0, length: input.count)) == 1
     }
     
 }
