6d6aa4091709840d3bb165536c305944f8c2591e
kiljh <kiljh@me.com>
2019-05-19 23:51:03 +0900

    수정 - 테스트 유형 별 작성

--- JSONParser/JSONParserTest/JSONParserTest.swift
+++ JSONParser/JSONParserTest/JSONParserTest.swift
@@ -9,16 +9,24 @@
 import XCTest
 
 class JSONParserTest: XCTestCase {
-    func testInputError() {
+    func testArraySuccess() {
         XCTAssertNoThrow(try GrammarChecker.checkJsonGrammar("[ 10, 21, 4, 314, 99, 0, 72 ]"))
         XCTAssertNoThrow(try GrammarChecker.checkJsonGrammar("[ 10, \"jk\", 4, \"314\", 99, \"crong\", false ]"))
-        XCTAssertNoThrow(try GrammarChecker.checkJsonGrammar("{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }"))
-        XCTAssertThrowsError(try GrammarChecker.checkJsonGrammar("[ \"name\" : \"KIM JUNG\" ]"))
         XCTAssertNoThrow(try GrammarChecker.checkJsonGrammar("[ 72 ]"))
         XCTAssertNoThrow(try GrammarChecker.checkJsonGrammar("[ { \"name\" : \"dominic\" } ]"))
+        XCTAssertNoThrow(try GrammarChecker.checkJsonGrammar("[ { \"name\" : \"master's course\", \"opened\" : true }, [ \"java\", \"javascript\", \"swift\" ] ]"))
+    }
+    func testArrayFail() {
+        XCTAssertThrowsError(try GrammarChecker.checkJsonGrammar("[ \"name\" : \"KIM JUNG\" ]"))
         XCTAssertThrowsError(try GrammarChecker.checkJsonGrammar("[ \"name\" : \"KIM JUNG\" }"))
         XCTAssertThrowsError(try GrammarChecker.checkJsonGrammar("[ { \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5 }, { \"name\" : \"YAGOM\" } "))
+    }
+    func testObjectSuccess() {
+        XCTAssertNoThrow(try GrammarChecker.checkJsonGrammar("{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }"))
         XCTAssertNoThrow(try GrammarChecker.checkJsonGrammar("{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"children\" : [\"hana\", \"hayul\", \"haun\"] }"))
-        XCTAssertNoThrow(try GrammarChecker.checkJsonGrammar("[ { \"name\" : \"master's course\", \"opened\" : true }, [ \"java\", \"javascript\", \"swift\" ] ]"))
+    }
+    func testObjectFail() {
+        XCTAssertThrowsError(try GrammarChecker.checkJsonGrammar("{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5 }, { \"name\" : \"YAGOM\" } "))
+        XCTAssertThrowsError(try GrammarChecker.checkJsonGrammar("{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : fale }"))
     }
 }
