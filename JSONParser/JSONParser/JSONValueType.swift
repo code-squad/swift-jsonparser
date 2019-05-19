67a1624274de9deb66a84b96101cb24a33abd0e6
cmindy <minjidev@naver.com>
2019-05-19 23:03:31 +0900

    feat: Add JSONValueType protocol

---
+++ JSONParser/JSONParser/JSONValueType.swift
@@ -0,0 +1,13 @@
+//
+//  JSONValueType.swift
+//  JSONParser
+//
+//  Created by CHOMINJI on 19/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol JSONValueType {
+    
+}
