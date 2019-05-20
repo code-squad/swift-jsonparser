b911de0832c4e6cb651fb7a93aac53ce5b1d2228
cmindy <minjidev@naver.com>
2019-05-20 14:59:21 +0900

    feat: Modify String, Int, Bool to conform JSONValueType protocol

--- JSONParser/JSONParser/JSONValueType.swift
+++ JSONParser/JSONParser/JSONValueType.swift
@@ -9,5 +9,24 @@
 import Foundation
 
 protocol JSONValueType {
-    
+    var type: String { get }
+}
+
+extension String: JSONValueType {
+    var type: String {
+        return "문자열"
+    }
+}
+
+typealias Number = Int
+extension Number: JSONValueType {
+    var type: String {
+        return "숫자"
+    }
+}
+
+extension Bool: JSONValueType {
+    var type: String {
+        return "부울"
+    }
 }
