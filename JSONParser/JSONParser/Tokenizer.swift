232e9a7ac73cb7f563943a1db17af4448cbc17f8
cmindy <minjidev@naver.com>
2019-05-20 14:50:57 +0900

    feat: Add tokenize() for tokenize input of prompt

--- JSONParser/JSONParser/Tokenizer.swift
+++ JSONParser/JSONParser/Tokenizer.swift
@@ -10,4 +10,22 @@ import Foundation
 
 struct Tokenizer {
     
+    static func tokenize(from prompt: String) -> [String] {
+        let trimmed = prompt.split(separator: " ").joined()
+        let input = trimmed.splitByLeftBracket().splitByRightBracket()
+        let tokens = input.components(separatedBy: ",")
+        print(tokens)
+        return tokens
+    }
 }
+
+extension String {
+    func splitByLeftBracket() -> String {
+        return self.split(separator: "[").joined()
+    }
+    
+    func splitByRightBracket() -> String {
+        return self.split(separator: "]").joined()
+    }
+}
+
