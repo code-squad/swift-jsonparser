cdbddd8c944b9512b37ddc207e1bb8ce8ca56d42
Daheen Lee <allwhite.dev@gmail.com>
2019-05-20 17:56:11 +0900

    feat: Add TokenView struct with utility methods for token

--- JSONParser/JSONParser/TokenView.swift
+++ JSONParser/JSONParser/TokenView.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+struct TokenView {
+    private var index = 0
+    private var tokens: [String]
+    
+    init(tokens: [String]) {
+        self.tokens = tokens
+    }
+    
+    func hasNext() -> Bool {
+        let nextIndex = index + 1
+        return nextIndex < tokens.count
+    }
+    
+    mutating func next() -> String? {
+        var nextToken: String?
+        let nextIndex = index + 1
+        
+        if (nextIndex < tokens.count) {
+            nextToken = tokens[nextIndex]
+            index += 1
+        }
+        return nextToken
+    }
+}
