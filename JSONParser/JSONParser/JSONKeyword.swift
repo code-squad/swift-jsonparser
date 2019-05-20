5cbf30d74a60579b427c163b47df1f35bebde8b5
cmindy <minjidev@naver.com>
2019-05-20 16:20:30 +0900

    feat: Add JSONKeyword struct

---
+++ JSONParser/JSONParser/JSONKeyword.swift
@@ -0,0 +1,13 @@
+//
+//  JSONKeyword.swift
+//  JSONParser
+//
+//  Created by CHOMINJI on 20/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct JSONKeyword {
+    
+}
