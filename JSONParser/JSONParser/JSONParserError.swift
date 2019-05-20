dd00c8d680eccb3f2d522b3c1aa2179b431d4eba
Daheen Lee <allwhite.dev@gmail.com>
2019-05-20 15:29:23 +0900

    refactor: Unify error type into JSONError type

--- JSONParser/JSONParser/JSONParserError.swift
+++ JSONParser/JSONParser/JSONParserError.swift
@@ -8,36 +8,14 @@
 
 import Foundation
 
-protocol JSONParserError: Error {
-    var message: String { get }
-}
-
-enum InputError: String, JSONParserError {
-    case invalidFormat = "잘못된 입력입니다."
-    var message: String {
-        return self.rawValue
-    }
-}
-
-enum TokenizerError: String, JSONParserError {
+enum JSONError: String, Error {
+    case invalidInputFormat = "잘못된 입력입니다."
     case impossibleToTokenize = "입력 문자열을 나눌 수 없습니다."
-    var message: String {
-        return self.rawValue
-    }
-}
-
-enum ParserError: String, JSONParserError {
     case invalidFormatToParse = "파싱할 수 없는 형식입니다."
     case impossibleToParse = "의미있는 type으로 변환할 수 없습니다."
-    var message: String {
-        return self.rawValue
-    }
-}
-
-enum ValueFactoryError: String, JSONParserError {
     case impossibleToCreateJSONValue = "JSON value를 생성할 수 없습니다."
+    
     var message: String {
         return self.rawValue
     }
 }
-
