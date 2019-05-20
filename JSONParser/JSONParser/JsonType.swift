b85de4d3863dd011dd0bc20229d00b1e73c6506d
Kroisttt <yago0315@gmail.com>
2019-05-20 17:16:07 +0900

    JsonType 프로토콜 추가

---
+++ JSONParser/JSONParser/JsonType.swift
@@ -0,0 +1,44 @@
+//
+//  JsonType.swift
+//  JSONParser
+//
+//  Created by 이진영 on 20/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum TypeName: String {
+    case array = "배열"
+    case number = "숫자"
+    case string = "문자열"
+    case bool = "부울"
+}
+
+protocol JsonType {
+    var typeDescription: String { get }
+}
+
+extension Double: JsonType {
+    var typeDescription: String {
+        return TypeName.number.rawValue
+    }
+}
+
+extension String: JsonType {
+    var typeDescription: String {
+        return TypeName.string.rawValue
+    }
+}
+
+extension Bool: JsonType {
+    var typeDescription: String {
+        return TypeName.bool.rawValue
+    }
+}
+
+extension Array: JsonType where Element: JsonType {
+    var typeDescription: String {
+        return TypeName.array.rawValue
+    }
+}
