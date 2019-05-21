92cd8edd3e11e04b9f7b8631d88979b5e717b65a
Sunghyun Kim <sunghyun_k@icloud.com>
2019-05-21 11:00:34 +0900

    InputView에서 오류를 발생시키지 않도록 수정함

--- JSONParser/JSONParser/Views/InputView.swift
+++ JSONParser/JSONParser/Views/InputView.swift
@@ -2,16 +2,9 @@ import Foundation
 
 struct InputView {
     
-    static func ask(about question: String) throws -> String {
+    static func ask(about question: String) -> String {
         print(question)
-        guard let input = readLine() else {
-            throw InputError.invalidError
-        }
-        return input.trimmingCharacters(in: Token.whitespace)
+        return (readLine() ?? "").trimmingCharacters(in: Token.whitespace)
     }
     
-}
-
-enum InputError: Error {
-    case invalidError
 }
--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -1,13 +1,12 @@
 import Foundation
 
 func main() {
+    
+    let input = InputView.ask(about: "분석할 JSON 데이터를 입력하세요.")
 
     do {
-
-        let input = try InputView.ask(about: "분석할 JSON 데이터를 입력하세요.")
         
         let data = try Parser.parseValue(input)
-        
         OutputView.printTypeCount(data: data)
         
     } catch {
