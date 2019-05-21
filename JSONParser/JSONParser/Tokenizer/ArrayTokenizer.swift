06f085ef23455a6c37c12f6b592cad6ca6f5d9ab
Sunghyun Kim <sunghyun_k@icloud.com>
2019-05-21 11:25:46 +0900

    들여쓰기를 줄이기 위하여 함수 분리함

--- JSONParser/JSONParser/Tokenizer/ArrayTokenizer.swift
+++ JSONParser/JSONParser/Tokenizer/ArrayTokenizer.swift
@@ -9,11 +9,9 @@ struct ArrayTokenizer {
         case bufferIsEmpty
     }
     
+    /// 처음과 끝의 괄호가 제거된 후에 사용해야 함.
     static func tokenize(_ input: String) throws -> [String] {
         
-        guard input.first == Token.beginArray, input.last == Token.endArray else {
-            throw TokenizingError.cannotDetectBeginOrEndOfArray
-        }
         var input = input
         input.removeFirst()
         input.removeLast()
@@ -23,55 +21,60 @@ struct ArrayTokenizer {
             throw TokenizingError.invalidArrayGrammar
         }
         
-        var tokenizedInput = [String]()
+        var result = [String]()
         var buffer = ""
         var isParsingString = false
         var nestedArrayCount: UInt = 0
         var nestedObjectCount: UInt = 0
         
+        func moveBufferToResult() throws {
+            guard !buffer.isEmpty else {
+                throw TokenizingError.bufferIsEmpty
+            }
+            result.append(buffer.trimmingCharacters(in: Token.whitespace))
+            buffer.removeAll()
+        }
+        
+        func parse(character: Character) throws {
+            switch character {
+            case Token.endArray:
+                guard nestedArrayCount > 0 else {
+                    throw TokenizingError.invalidNestedStructure
+                }
+                nestedArrayCount -= 1
+            case Token.beginArray:
+                nestedArrayCount += 1
+            case Token.endObject:
+                guard nestedObjectCount > 0 else {
+                    throw TokenizingError.invalidNestedStructure
+                }
+                nestedObjectCount -= 1
+            case Token.beginObject:
+                nestedObjectCount += 1
+            case Token.valueSeparator:
+                guard nestedArrayCount == 0, nestedObjectCount == 0 else { break }
+                try moveBufferToResult()
+                return
+            default:
+                break
+            }
+        }
+        
         for character in values {
             if character == Token.quotationMark {
                 isParsingString.toggle()
             } else if !isParsingString {
-                switch character {
-                case Token.endArray:
-                    guard nestedArrayCount > 0 else {
-                        throw TokenizingError.invalidNestedStructure
-                    }
-                    nestedArrayCount -= 1
-                case Token.beginArray:
-                    nestedArrayCount += 1
-                case Token.endObject:
-                    guard nestedObjectCount > 0 else {
-                        throw TokenizingError.invalidNestedStructure
-                    }
-                    nestedObjectCount -= 1
-                case Token.beginObject:
-                    nestedObjectCount += 1
-                case Token.valueSeparator:
-                    guard nestedArrayCount == 0, nestedObjectCount == 0 else { break }
-                    try move(fromBuffer: &buffer, to: &tokenizedInput)
-                    continue
-                default:
-                    break
-                }
+                try parse(character: character)
             }
             buffer.append(character)
         }
         guard !buffer.isEmpty else {
-            return tokenizedInput
+            return result
         }
-        try move(fromBuffer: &buffer, to: &tokenizedInput)
-        return tokenizedInput
+        try moveBufferToResult()
+        return result
     }
     
-    private static func move(fromBuffer buffer: inout String, to result: inout [String]) throws {
-        guard !buffer.isEmpty else {
-            throw TokenizingError.bufferIsEmpty
-        }
-        result.append(buffer.trimmingCharacters(in: Token.whitespace))
-        buffer.removeAll()
-    }
     
     
 }
--- JSONParser/JSONParser/Tokenizer/Tokenizer.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct Tokenizer {
-    
-    
-    
-}
