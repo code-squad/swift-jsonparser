1232a97634ce4ec5384fd8f112d392a0c3099c26
cmindy <minjidev@naver.com>
2019-05-24 01:24:17 +0900

    feat: Add TypeCounter struct

---
+++ JSONParser/JSONParser/TypeCounter.swift
@@ -0,0 +1,13 @@
+//
+//  TypeCounter.swift
+//  JSONParser
+//
+//  Created by CHOMINJI on 24/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct TypeCounter {
+    
+}
