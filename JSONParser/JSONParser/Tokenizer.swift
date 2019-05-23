a98dfbd2d122eeaf38e50bdfa32273581d18190e
hwj0623 <hwj0623@naver.com>
2019-05-23 18:15:24 +0900

    STEP 7-2 (8) Tokenizer 수정
    * 1)  토큰화 과정은 JsonObject인지, JsonArray인지에 따라 달리 진행
    * 2) JsonArray의 경우, 토큰, 데이터 타입 분석, 저장 과정에서  내부 요소가 JsonObject라면 1)의 JsonObject 토큰분석과 관련된 메소드를 호출
    * 3) 위 과정은 1) 이나 2)의 요소 값들이 전부 Bool, String, Int 인 경우에 데이터를 저장하며 재귀탈출
    * 4) 재귀 로직상 Parser로직을 Tokenizer 로직에 통합

--- JSONParser/JSONParser/Tokenizer.swift
+++ JSONParser/JSONParser/Tokenizer.swift
@@ -16,11 +16,135 @@ import Foundation
 
 struct Tokenizer {
     
-    static  func tokenize (_ input: String) -> [String] {
-        let lowerBound = String.Index.init(encodedOffset: 1)
-        let uppderBound = String.Index.init(encodedOffset: input.count-1)
-        let removeSquareBracketInput = input[lowerBound..<uppderBound]
-        let output = removeSquareBracketInput.split(separator: ",").map { (value) in return String(value).trimmingCharacters(in: .whitespacesAndNewlines)}
-        return output
+    func tokenize(_ input : String) throws -> JsonParsable {
+        var result : JsonParsable
+        let type = Lexer.checkInputType(input)
+        switch type {
+        case .jsonObject:
+            result = try handleStringAsJsonObject(input)
+            break
+        case .jsonArray:
+            let jsonArray = handleStringAsJsonArray(input)
+            result = try convertJsonArrayElementDataType(tokenList: jsonArray)
+            break
+        default :
+            throw ErrorCode.invalidJsonFormat
+        }
+        return result
     }
+    
+    private func handleStringAsJsonObject(_ input: String) throws -> JsonParsable {
+        let trimmedInput = input[1..<input.count-1]
+        let keyValueList = trimmedInput.components(separatedBy: ",")
+        var keys: [String] = [String]()
+        var values: [JsonValue] = [JsonValue]()
+        for eachKeyValue in keyValueList{
+            let keyValuePair: [String] = eachKeyValue.components(separatedBy: ":").map { $0.trimmingCharacters(in: .whitespacesAndNewlines)}
+            let valueDataType = try convertJsonObjectStringValueToElementDataType(keyValuePair[1])
+            keys.append(keyValuePair[0])
+            values.append(valueDataType)
+        }
+        let jsonObject: JsonObject = JsonObject.init(keys: keys, values: values)
+        return jsonObject
+    }
+    
+    /// subroutine of jsonObject
+    private func convertJsonObjectStringValueToElementDataType(_ tokenValue : String ) throws -> JsonValue {
+        let lexicalType = try Lexer.confirmTokenDataType(tokenValue)
+        let result : JsonValue = createJsonValueFromJsonObject(type: lexicalType, token: tokenValue)
+        return result
+    }
+    
+    /// subroutine of jsonArray
+    private func convertJsonArrayElementDataType (tokenList : [String]) throws -> JsonParsable{
+        var result = JsonArray()
+        for token in tokenList {
+            let lexicalType = try Lexer.confirmTokenDataType(token)
+            try saveJsonValueInJsonArray(jsonArray: &result, token: token, type: lexicalType)
+        }
+        return result
+    }
+    
+    private func createJsonValueFromJsonObject (type: LexicalType, token : String ) -> JsonValue {
+        var result : JsonValue = JsonValue.init(token)
+        switch type {
+        case .bool :
+            if let boolValue = Bool(token) {
+                result = JsonValue.init(boolValue)
+            }
+        case .intNumber:
+            if let intValue = Int(token) {
+                result = JsonValue.init(intValue)
+            }
+        default :
+            break
+        }
+        return result
+    }
+    
+    private func saveJsonValueInJsonArray (jsonArray: inout JsonArray, token: String,  type: LexicalType) throws {
+        switch type {
+        case .bool :
+            if let boolValue = Bool(token) {
+                jsonArray.add(value: JsonValue.init(boolValue))
+            }
+            break
+        case .intNumber:
+            if let intValue = Int(token) {
+                jsonArray.add(value: JsonValue.init(intValue))
+            }
+        case .string:
+            jsonArray.add(value: JsonValue.init(token))
+        case .jsonObject:
+            let jsonObject =  try handleStringAsJsonObject(token)
+            jsonArray.add(value: JsonValue.init(jsonObject))
+        case .jsonArray:
+            break
+        }
+    }
+    
+    private func handleStringAsJsonArray (_ input: String) -> [String] {
+        var queue : Queue <String> = Queue <String>()
+        var index = 0
+        var element = ""
+        while index <= input.count-2 {
+            index += 1
+            if input[index] == "{" {
+                let result = concatenateJsonObjectString(inputString: input, outerIndex: index)
+                index = result.innerIndex+1
+                queue.add(result.jsonObject)
+                continue
+            }
+            if isTokenCommaBlank(input: input, index: index) && isValidElement(element) {
+                queue.add(element)
+                element = ""
+                continue
+            }
+            if input[index] != " " {
+                element += input[index]
+            }
+        }
+        let jsonValueList = queue.toArray()
+        return jsonValueList
+    }
+    
+    func concatenateJsonObjectString (inputString : String, outerIndex: Int) -> (jsonObject: String, innerIndex: Int){
+        var offset = outerIndex
+        var jsonObject = ""
+        while inputString[offset] != "}" {
+            jsonObject += inputString[offset]
+            offset += 1
+        }
+        jsonObject += inputString[offset]
+        return (jsonObject, offset)
+    }
+    
+    func isTokenCommaBlank(input: String, index: Int ) -> Bool {
+        return (input[index] == "," || input[index] == " " )
+    }
+    
+    func isValidElement(_ element: String) -> Bool {
+        return element != " " && element != ""
+    }
+    
 }
