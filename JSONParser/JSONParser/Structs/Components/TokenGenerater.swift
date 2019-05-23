9a4325c5ca615d901da22ba3d0523a270852344f
O-O-wl <ldcpaul@hanmail.net>
2019-05-23 15:42:04 +0900

    Modified: TokenGenerater  부분 메소드 구현 , 은닉

--- JSONParser/JSONParser/Structs/Components/TokenGenerater.swift
+++ JSONParser/JSONParser/Structs/Components/TokenGenerater.swift
@@ -9,7 +9,35 @@
 import Foundation
 
 struct TokenGenerater {
-    static public func createToken(_ string:String) -> Token {
-        return Token.init(type: .String, value: string)
+    
+    static public func createToken(_ string: String) -> Token {
+        let nomalized = self.normalize(string)
+        let type = determinType(value: nomalized)
+        return Token.init(type: type, value: nomalized)
     }
+    
+    private static func normalize(_ string: String) -> String {
+        var normalized = string
+        normalizeFirst(&normalized)
+        normalizeLast(&normalized)
+        return normalized
+    }
+    
+    private static func normalizeFirst(_ string: inout String){
+        if(Symbols.whiteSpace == string.first){
+            string.removeFirst()
+        }
+    }
+    
+    private static func normalizeLast(_ string: inout String){
+        if([Symbols.comma,Symbols.whiteSpace].contains(string.last)){
+            string.removeLast()
+        }
+    }
+    
+    private static func determinType(value:String) -> TokenType {
+        return TokenType.of(value)
+    }
+    
+    
 }
