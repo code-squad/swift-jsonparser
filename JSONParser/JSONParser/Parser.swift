91a3090b1ea30326640d79cd38acf5d9cf7ab2bc
cmindy <minjidev@naver.com>
2019-05-24 01:23:25 +0900

    feat: Add parse feature to Parser

--- JSONParser/JSONParser/Parser.swift
+++ JSONParser/JSONParser/Parser.swift
@@ -10,4 +10,49 @@ import Foundation
 
 struct Parser {
     
+    private var validator: Validatable
+    
+    init(validator: Validatable) {
+        self.validator = validator
+    }
+    
+    func parse(tokens: [String]) -> [JSONValueType] {
+        var values: [JSONValueType] = []
+        
+        for token in tokens {
+            if validator.hasQuotation(in: token) {
+                let stringToken = removeQuotation(in: token)
+                values.append(stringToken)
+            }
+            
+            if validator.isBool(of: token) {
+                let boolToken = matchBool(in: token)
+                values.append(boolToken)
+            }
+            
+            if validator.isNumber(of: token) {
+                let numberToken = toNumber(in: token)
+                values.append(numberToken)
+            }
+        }
+        return values
+    }
+    
+    private func removeQuotation(in token: String) -> String {
+        let trimmedToken = token.dropFirst().dropLast()
+        return String(trimmedToken)
+    }
+    
+    private func matchBool(in token: String) -> Bool {
+        if token == JSONKeyword.true {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    private func toNumber(in token: String) -> Number {
+        let number = Number(token) ?? -1
+        return number
+    }
 }
