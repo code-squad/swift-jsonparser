a5ee1c2d6151b3568156438461a02f14c1e71dae
O-O-wl <ldcpaul@hanmail.net>
2019-05-23 15:52:13 +0900

    Modified: 타입확인 메소드 추가

--- JSONParser/JSONParser/Enums/TokenType.swift
+++ JSONParser/JSONParser/Enums/TokenType.swift
@@ -12,5 +12,33 @@ enum TokenType {
     case Array
     case Number
     case String
-    case Bool
+    case Boolean
+    case None
+    
+    static func of(_ string: String) -> TokenType {
+        if isArray(string) { return .Array }
+        if isNumber(string) { return .Number }
+        if isString(string) { return .String }
+        if isBoolean(string) { return .Boolean }
+        return .None
+    }
+    
+    static func isArray(_ string: String) -> Bool {
+        return ["[","]"].contains(string)
+    }
+
+    static func isNumber(_ string: String) -> Bool {
+        guard Int.init(string) != nil else { return false }
+        return true
+    }
+    
+    static func isString(_ string: String) -> Bool {
+        return string.first == "\"" && string.last == "\""
+    }
+    
+    static func isBoolean(_ string: String) -> Bool {
+        guard Bool.init(string) != nil else { return false }
+        return true
+    }
+    
 }
