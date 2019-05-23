172da06994bf17e1448424c81cccbdeb7f1be9a2
hwj0623 <hwj0623@naver.com>
2019-05-23 18:03:55 +0900

    STEP 7-2 (6) enum LexicalType 별도 파일로 분리 및 수정

--- JSONParser/JSONParser/LexicalType.swift
+++ JSONParser/JSONParser/LexicalType.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+enum LexicalType: CustomStringConvertible, CaseIterable {
+    
+    static var allCases : [LexicalType]{
+        return [ .intNumber, .bool, .string, .jsonObject, .jsonArray]
+    }
+    
+    case intNumber
+    case bool
+    case string
+    case jsonObject
+    case jsonArray
+    
+    var description: String {
+        switch self {
+        case .intNumber :
+            return "숫자"
+        case .string :
+            return "문자열"
+        case .bool :
+            return "부울"
+        case .jsonObject :
+            return "객체"
+        case .jsonArray :
+            return "배열"
+        }
+    }
+}
