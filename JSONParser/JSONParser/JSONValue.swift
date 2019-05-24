f1d9d2750d3eb5541d815d57f09e935646a0623c
Daheen Lee <allwhite.dev@gmail.com>
2019-05-24 13:47:52 +0900

    refactor: Change typeDescription as instance property

--- JSONParser/JSONParser/JSONValue.swift
+++ JSONParser/JSONParser/JSONValue.swift
@@ -9,17 +9,17 @@
 import Foundation
 
 protocol JSONValue {
-    static var typeDescription: String { get }
+    var typeDescription: String { get }
 }
 
 extension Bool: JSONValue {
-    static var typeDescription: String { return "부울" }
+    var typeDescription: String { return "부울" }
 }
 
 extension Int: JSONValue {
-    static var typeDescription: String { return "숫자" }
+    var typeDescription: String { return "숫자" }
 }
 
 extension String: JSONValue {
-    static var typeDescription: String { return "문자열" }
+    var typeDescription: String { return "문자열" }
 }
