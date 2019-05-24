70a2edd14028662e348f7511323846d38f3bd030
Daheen Lee <allwhite.dev@gmail.com>
2019-05-24 12:19:55 +0900

    refactor: Specify JSONError types

--- JSONParser/JSONParser/JSONParserError.swift
+++ JSONParser/JSONParser/JSONParserError.swift
@@ -8,14 +8,50 @@
 
 import Foundation
 
-enum JSONError: String, Error {
+protocol JSONError: Error {
+    var message: String { get }
+}
+
+enum InputError: String, JSONError {
     case invalidInputFormat = "잘못된 입력입니다."
-    case impossibleToTokenize = "입력 문자열을 나눌 수 없습니다."
-    case invalidFormatToParse = "파싱할 수 없는 형식입니다."
-    case impossibleToParse = "의미있는 type으로 변환할 수 없습니다."
-    case impossibleToCreateJSONValue = "JSON value를 생성할 수 없습니다."
     
     var message: String {
         return self.rawValue
     }
 }
+
+enum TokenizerError: String, JSONError {
+    case impossibleToTokenize = "입력 문자열을 나눌 수 없습니다."
+    
+    var message: String {
+        return self.rawValue
+    }
+}
+
+enum TokenReaderError: String, JSONError {
+    case hasNoNextToken = "더 이상 읽을 token이 없습니다."
+    
+    var message: String {
+        return self.rawValue
+    }
+}
+
+enum ParserError : String, JSONError {
+    case invalidSymbolToParse = "JSON Data가 허용하지 않는 문자가 포함되어 있습니다."
+    case invalidFirstSymbolOfJSONData = "JSON 데이터가 { 혹은 [로 시작하지 않습니다."
+    case impossibleToParse = "JSON Value로 파싱할 수 없습니다."
+    
+    var message: String {
+        return self.rawValue
+    }
+}
+
+enum JSONValueFactoryError: String, JSONError {
+    case impossibleToConvertIntoBool = "주어진 token을 Bool로 변환할 수 없습니다."
+    case impossibleToconvertIntoNumber = "주어진 token을 숫자로 변환할 수 없습니다."
+    
+    var message: String {
+    return self.rawValue
+    }
+}
+
