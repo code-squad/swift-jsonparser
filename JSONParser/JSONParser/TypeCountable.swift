f0f6c31f412ca6b48aa1957a9e6ebaf94696590d
Daheen Lee <allwhite.dev@gmail.com>
2019-05-24 12:35:32 +0900

    feat: Add TypeCountable protocol that defines functionality to express count of elements that JSONValue has

---
+++ JSONParser/JSONParser/TypeCountable.swift
@@ -0,0 +1,13 @@
+//
+//  TypeCountable.swift
+//  JSONParser
+//
+//  Created by Daheen Lee on 24/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol TypeCountable {
+    var elementCount: Int { get }
+}
