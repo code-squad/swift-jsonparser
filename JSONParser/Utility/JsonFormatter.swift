41796b973142e22e12eaef21e6d3d223243900ac
hwj0623 <hwj0623@naver.com>
2019-05-24 15:30:25 +0900

    REV (5) countDictionary[] 사전의 키를 LexicalType 으로 변경하여 문자열 비교 피하기

--- JSONParser/Utility/JsonFormatter.swift
+++ JSONParser/Utility/JsonFormatter.swift
@@ -11,20 +11,20 @@ import Foundation
 struct JsonFormatter{
     
     private var jsonData : JsonParsable
-    private (set) var countDictionary: [String: Int]
+    private (set) var countDictionary: [LexicalType: Int]
     private (set) var totalCount = 0
-    private (set) var descendingOrderByValueCountInformation : [(key: String, value: Int)] = [(key:String, value: Int)]()
+    private (set) var descendingOrderByValueCountInformation : [(key: LexicalType, value: Int)] = [(key:LexicalType, value: Int)]()
     
     init(jsonData: JsonParsable) throws {
         self.jsonData = jsonData
-        countDictionary = [String: Int]()
+        countDictionary = [LexicalType: Int]()
         try countDataTypeOfElementInJsonData()
-        self.descendingOrderByValueCountInformation = self.countDictionary.sorted{ $0.value > $1.value }
+        self.descendingOrderByValueCountInformation = self.countDictionary.sorted { $0.value > $1.value }
     }
     
     mutating private func initiateCountDictionary () {
         for element in LexicalType.allCases {
-            self.countDictionary.updateValue(0, forKey: element.description)
+            self.countDictionary.updateValue(0, forKey: element)
         }
     }
     
@@ -45,20 +45,20 @@ struct JsonFormatter{
         self.totalCount += 1
         switch element.jsonValue{
         case is Bool:
-            let currentCount = countDictionary[LexicalType.bool.description] ?? 0
-            countDictionary[LexicalType.bool.description] = currentCount + 1
+            let currentCount = countDictionary[LexicalType.bool] ?? 0
+            countDictionary[LexicalType.bool] = currentCount + 1
         case is Int:
-            let currentCount = countDictionary[LexicalType.intNumber.description] ?? 0
-            countDictionary[LexicalType.intNumber.description] = currentCount + 1
+            let currentCount = countDictionary[LexicalType.intNumber] ?? 0
+            countDictionary[LexicalType.intNumber] = currentCount + 1
         case is JsonObject:
-            let currentCount = countDictionary[LexicalType.jsonObject.description] ?? 0
-            countDictionary[LexicalType.jsonObject.description] = currentCount + 1
+            let currentCount = countDictionary[LexicalType.jsonObject] ?? 0
+            countDictionary[LexicalType.jsonObject] = currentCount + 1
         case is JsonArray:
-            let currentCount = countDictionary[LexicalType.jsonArray.description] ?? 0
-            countDictionary[LexicalType.jsonArray.description] = currentCount + 1
+            let currentCount = countDictionary[LexicalType.jsonArray] ?? 0
+            countDictionary[LexicalType.jsonArray] = currentCount + 1
         case is String :
-            let currentCount = countDictionary[LexicalType.string.description] ?? 0
-            countDictionary[LexicalType.string.description] = currentCount + 1
+            let currentCount = countDictionary[LexicalType.string] ?? 0
+            countDictionary[LexicalType.string] = currentCount + 1
         default:
             break
         }
