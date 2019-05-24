d430da0ba35adb7d92f895b3b60202c5b0970cfa
Daheen Lee <allwhite.dev@gmail.com>
2019-05-24 11:15:00 +0900

    refactor: Add initializer with input in Tokenizer and modify tokenize() method logic
    tokenize() method splits input string by blank unless token is reading string value.

--- JSONParser/JSONParser/Tokenizer.swift
+++ JSONParser/JSONParser/Tokenizer.swift
@@ -9,13 +9,38 @@
 import Foundation
 
 struct Tokenizer {
-    static let emptyString = ""
-    static let blankWithComma = " ,"
+    static let commaWithBlank = " ,"
     
-    static func execute(using input: String) throws -> [String] {
-        let blankAddedInput = input.replacingOccurrences(of: JSONSymbols.comma, with: blankWithComma)
-        let tokens = blankAddedInput.components(separatedBy: JSONSymbols.blank)
+    private let input: String
+    
+    init(input: String) {
+        self.input = input.replacingOccurrences(of: JSONSymbols.comma.rawValue, with: Tokenizer.commaWithBlank)
+    }
+    
+    func execute() throws -> [String] {
+        let tokens = try tokenize()
         if tokens.isEmpty { throw JSONError.impossibleToTokenize }
+        return tokens
+    }
+    
+    private func tokenize() throws -> [String] {
+        var tokens = [String]()
+        var token = String()
+        var isOutOfString = true
+        
+        for index in input.indices {
+            let character = input[index]
+            if JSONSymbols.doubleQuotation.equals(character) {
+                isOutOfString = !isOutOfString
+            }
+            if JSONSymbols.blank.equals(character) && isOutOfString {
+                tokens.append(token)
+                token = String()
+                continue
+            }
+            token += String(character)
+        }
+        tokens.append(token)
         return tokens
     }
     
