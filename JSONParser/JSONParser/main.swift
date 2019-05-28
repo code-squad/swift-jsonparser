60502feb0514a556fbfd30edd183a96bb283667d
Kroisttt <yago0315@gmail.com>
2019-05-28 16:30:50 +0900

    main() 추가

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -8,3 +8,23 @@
 
 import Foundation
 
+func main() {
+    do {
+        let input = InputView.readInput()
+        
+        let tokens = try Tokenizer.tokenize(input: input)
+        let parseResult = try Parser.parse(tokens: tokens)
+        
+        OutputView.printParseResult(result: parseResult)
+    } catch let error as TypeError {
+        print(error.description)
+    } catch let error as TokenizeError {
+        print(error.description)
+    } catch let error as ParseError{
+        print(error.description)
+    } catch {
+        return
+    }
+}
+
+main()
