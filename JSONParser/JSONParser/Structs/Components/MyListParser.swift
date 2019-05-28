461f5c32ef5712d597a86f734fd0af9b4ec14611
O-O-wl <ldcpaul@hanmail.net>
2019-05-29 06:02:20 +0900

    Modified: MyListParser , 토큰 문자열 병합 메소드 , 분석 메소드 구현

--- JSONParser/JSONParser/Structs/Components/MyListParser.swift
+++ JSONParser/JSONParser/Structs/Components/MyListParser.swift
@@ -9,8 +9,39 @@
 import Foundation
 
 struct MyListParser: Parser {
-    func parse() -> Dictionary<Token, Int> {
-        return Dictionary<Token, Int>()
+    private var statistics: Dictionary<Token, Int>
+    private var tokens: Array<Token>
+    
+    init(tokens: Array<Token>) {
+        self.tokens = tokens
+        self.statistics = Dictionary<Token, Int>()
     }
-   
+    
+    mutating func parse() -> Dictionary<Token, Int> {
+        self.assembleToken()
+        _ = self.tokens.map{ self.statistics[$0] = (self.statistics[$0] ?? 0) + 1 }
+       return self.statistics.filter{ !Token.symbols.contains($0.key) }
+    }
+    
+    private mutating func assembleToken() {
+        var isString = false
+        var indexs = MyStack<Int>()
+        for (index,token) in self.tokens.enumerated() {
+            if isString {
+                indexs.push(index)
+            }
+            if(token == .DoubleQuotation){
+                isString.toggle()
+                self.tokens[index] = .String
+            }
+        }
+        while !indexs.isEmpty(){
+            guard let index = indexs.pop() else { return }
+            self.tokens.remove(at: index )
+        }
+    }
+    
 }
+
+
+
