652a6bc2a18c18b2bf64054e52f534dee3c4a37a
Kroisttt <yago0315@gmail.com>
2019-05-28 16:30:37 +0900

    OutputView 추가

---
+++ JSONParser/JSONParser/Views/OutputView.swift
@@ -0,0 +1,15 @@
+//
+//  OutputView.swift
+//  JSONParser
+//
+//  Created by 이진영 on 28/05/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    static func printParseResult(result: JsonType) {
+        print(result.typeDescription)
+    }
+}
