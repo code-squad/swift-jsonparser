36543a2033fb42233ba1fda9f3e891cc6ff9c45a
junyng <pom0894@naver.com>
2019-05-29 11:00:48 +0900

    Lexer 구조체 추가, index 프로퍼티 위치의 문자를 가져오는 메소드 및 탐색중인 위치를 증가시키는 메소드 추가

---
+++ JSONParser/JSONParser/Lexer.swift
@@ -0,0 +1,25 @@
+//
+//  Lexer.swift
+//  JSONParser
+//
+//  Created by BLU on 2019. 5. 29..
+//  Copyright © 2019년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct Lexer {
+    let input: String
+    var position: String.Index
+    
+    func peek() -> Character? {
+        guard position < input.endIndex else {
+            return nil
+        }
+        return input[position]
+    }
+    
+    mutating func advance() {
+        position = input.index(after: position)
+    }
+}
