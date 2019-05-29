407e6b74ccce038dab4dfe0572246fc92040167c
junyng <pom0894@naver.com>
2019-05-29 11:05:40 +0900

    Token 열거형 정의

---
+++ JSONParser/JSONParser/Token.swift
@@ -0,0 +1,16 @@
+//
+//  Token.swift
+//  JSONParser
+//
+//  Created by BLU on 2019. 5. 29..
+//  Copyright © 2019년 JK. All rights reserved.
+//
+
+import Foundation
+
+enum Token {
+    case doubleQuotation
+    case comma
+    case string(String)
+    case number(Int)
+}
