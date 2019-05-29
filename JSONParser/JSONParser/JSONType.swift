a30ba224b63f04252e20a78f4f0900851f4dc2d7
Sunghyun Kim <sunghyun_k@icloud.com>
2019-05-29 18:06:13 +0900

    JSONType이 countInfo를 사용하여 유형 포함 여부를 확인하도록 익스텐션 추가

--- JSONParser/JSONParser/JSONType.swift
+++ JSONParser/JSONParser/JSONType.swift
@@ -30,17 +30,30 @@ extension Array: JSONType where Element == JSONType {
     
     var typeDescription: String { return "배열" }
     
+    private var countInfo: [String: Int] {
+        var countInfo = [String: Int]()
+        for item in self {
+            countInfo[item.typeDescription] = (countInfo[item.typeDescription] ?? 0) + 1
+        }
+        return countInfo
+    }
+    
     var countDescription: String {
         if self.isEmpty {
             return "빈 배열입니다."
         }
-        var counts = [String: Int]()
-        for item in self {
-            counts[item.typeDescription] = (counts[item.typeDescription] ?? 0) + 1
-        }
         
-        let countsString = counts.map { "\($0.key) \($0.value)개" }
+        let countsString = countInfo.map { "\($0.key) \($0.value)개" }
         return "이 배열에는 총 \(self.count)개의 데이터 중에 \(countsString.joined(separator: ", "))가 포함되어 있습니다."
+    }
+    
+    func contains(typeDescription: String) -> Bool {
+        for (key, _) in countInfo {
+            if key == typeDescription  {
+                return true
+            }
+        }
+        return false
     }
     
 }
@@ -50,17 +63,30 @@ extension Object: JSONType where Key == String ,Value == JSONType {
     
     var typeDescription: String { return "오브젝트" }
     
+    private var countInfo: [String: Int] {
+        var countInfo = [String: Int]()
+        for (_, value) in self {
+            countInfo[value.typeDescription] = (countInfo[value.typeDescription] ?? 0) + 1
+        }
+        return countInfo
+    }
+    
     var countDescription: String {
         if self.isEmpty {
             return "빈 오브젝트입니다."
         }
-        var counts = [String: Int]()
-        for (_, value) in self {
-            counts[value.typeDescription] = (counts[value.typeDescription] ?? 0) + 1
-        }
         
-        let countsString = counts.map { "\($0.key) \($0.value)개" }
+        let countsString = countInfo.map { "\($0.key) \($0.value)개" }
         return "이 오브젝트에는 총 \(self.count)개의 데이터 중에 \(countsString.joined(separator: ", "))가 포함되어 있습니다."
+    }
+    
+    func contains(typeDescription: String) -> Bool {
+        for (key, _) in countInfo {
+            if key == typeDescription  {
+                return true
+            }
+        }
+        return false
     }
     
 }
