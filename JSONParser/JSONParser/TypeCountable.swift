e6eae6dd5aabb8a7f202d411c0ea95d265ef17e5
Daheen Lee <allwhite.dev@gmail.com>
2019-05-31 00:07:47 +0900

    feat: Make Array and Dictionary conditionally conform to TypeCountable when type parameter is JSONValue

--- JSONParser/JSONParser/TypeCountable.swift
+++ JSONParser/JSONParser/TypeCountable.swift
@@ -12,3 +12,22 @@ protocol TypeCountable {
     var elementCount: Int { get }
     var elements: [JSONValue] { get }
 }
+
+extension Array: TypeCountable where Element == JSONValue {
+    var elementCount: Int {
+        return self.count
+    }
+    var elements: [JSONValue] {
+        return self
+    }
+}
+
+extension Dictionary: TypeCountable where Key == String, Value == JSONValue {
+    var elementCount: Int {
+        return self.count
+    }
+    
+    var elements: [JSONValue] {
+        return Array(self.values)
+    }
+}
