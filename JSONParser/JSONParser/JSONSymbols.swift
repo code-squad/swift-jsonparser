d7ff7cd6aebbe52336a42d03332027a6e01496be
Daheen Lee <allwhite.dev@gmail.com>
2019-05-31 01:32:48 +0900

    refactor: Change type of JSONSymbol Struct

--- JSONParser/JSONParser/JSONSymbols.swift
+++ JSONParser/JSONParser/JSONSymbols.swift
@@ -8,30 +8,20 @@
 
 import Foundation
 
-enum JSONSymbols: String {
-    case openBracket  = "["
-    case closedBracket  = "]"
-    case openBrace = "{"
-    case closedBrace = "}"
-    case comma = ","
-    case blank = " "
-    case doubleQuotation = "\""
-    case colon = ":"
-    
-    func equals(_ token: String) -> Bool {
-        return self.rawValue == token
-    }
-    
-    func equals(_ token: Character) -> Bool {
-        let convertedToken = String(token)
-        return equals(convertedToken)
-    }
-    
-    func notEquals(_ token: String) -> Bool {
-        return !equals(token)
-    }
-    
-    func notEquals(_ token: Character) -> Bool {
-        return !equals(token)
+struct JSONSymbols {
+    static let openBracket: Character = "["
+    static let closedBracket: Character = "]"
+    static let openBrace: Character = "{"
+    static let closedBrace: Character = "}"
+    static let comma: Character = ","
+    static let blank: Character = " "
+    static let doubleQuotation: Character = "\""
+    static let colon: Character = ":"
+}
+
+extension Character {
+    var inString: String {
+        return String(self)
     }
 }
+
