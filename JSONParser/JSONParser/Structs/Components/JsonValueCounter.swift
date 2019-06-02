d5cc83effe83b3ecf5ca3c3717f17fd4e65cbc63
O-O-wl <ldcpaul@hanmail.net>
2019-06-02 17:26:46 +0900

    Modified: Counter 구체클래스 정의

--- JSONParser/JSONParser/Structs/Components/JsonValueCounter.swift
+++ JSONParser/JSONParser/Structs/Components/JsonValueCounter.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+struct JsonValueCounter: Counter {
+  
+    typealias T = JsonValue
+    
+    func count(target: JsonValue) -> Dictionary<String, Int> {
+        var numOf = Dictionary<String, Int>()
+        if let object = target as? JsonObject {
+            numOf = self.count(in: object)
+        }
+        if let list = target as? JsonList {
+            numOf = self.count(in: list)
+        }
+        return numOf
+    }
+    
+    func count(in object: JsonObject) -> Dictionary<String, Int> {
+        var numOf = Dictionary<String, Int>()
+        _ = object.map {
+            numOf[$0.value.describeType()] = ( numOf[$0.value.describeType()] ?? 0 ) + 1
+        }
+        return numOf
+    }
+    
+    func count(in list: JsonList) -> Dictionary<String, Int> {
+        var numOf = Dictionary<String, Int>()
+        _ = list.map {
+            numOf[$0.describeType()] = ( numOf[$0.describeType()] ?? 0 ) + 1
+        }
+        return numOf
+    }
+}
