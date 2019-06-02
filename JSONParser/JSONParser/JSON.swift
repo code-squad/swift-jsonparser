9339c130d1213dc54dd2337e86665bfcb349a6ba
junyng <pom0894@naver.com>
2019-06-02 15:46:37 +0900

    JSON 결과로 나올 수 있는 타입에 대한 열거형 추가

---
+++ JSONParser/JSONParser/JSON.swift
@@ -0,0 +1,26 @@
+//
+//  JSON.swift
+//  JSONParser
+//
+//  Created by BLU on 2019. 6. 2..
+//  Copyright © 2019년 JK. All rights reserved.
+//
+
+import Foundation
+
+enum JSON {
+    case string(String)
+    case number(Int)
+    case bool(Bool)
+    
+    var valueDescription: String {
+        switch self {
+        case .string(let string):
+            return string
+        case .number(let int):
+            return String(int)
+        case .bool(let bool):
+            return String(bool)
+        }
+    }
+}
