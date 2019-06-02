05d70589c6a7ebe42274f6a4ed7f474a5ec81bee
O-O-wl <ldcpaul@hanmail.net>
2019-06-02 17:26:17 +0900

    Modified: Counter 테스트메소드 추가

--- JSONParser/JSONParserTests/JsonValueCounterTests.swift
+++ JSONParser/JSONParserTests/JsonValueCounterTests.swift
@@ -9,25 +9,34 @@
 import XCTest
 
 class JsonValueCounterTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    //Given
+    let counter = JsonValueCounter()
+    
+    func testObjectCount(){
+        //Given
+        let object: JsonObject =  ["name":"부엉이" , "age" : 27]
+        
+        //When
+        let numOf = self.counter.count(target: object)
+        
+        //Then
+        XCTAssertEqual(numOf["문자열"], 1)
+        XCTAssertEqual(numOf["숫자"], 1)
+    }
+    
+    func testListCount(){
+        //Given
+        let list: JsonList =  ["name", true, "부엉이" , "age" , 27]
+        
+        //When
+        let numOf = self.counter.count(target: list)
+        
+        //Then
+        XCTAssertEqual(numOf["문자열"], 3)
+        XCTAssertEqual(numOf["숫자"], 1)
+        XCTAssertEqual(numOf["부울"], 1)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
