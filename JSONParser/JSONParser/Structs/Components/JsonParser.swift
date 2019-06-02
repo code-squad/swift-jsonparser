1a8f82ec5b097404f4a1b0efec906478cd3138ec
O-O-wl <ldcpaul@hanmail.net>
2019-06-02 15:33:17 +0900

    Modified: ",value," -> string 토큰으로 변환하는 메소드 추가

--- JSONParser/JSONParser/Structs/Components/JsonParser.swift
+++ JSONParser/JSONParser/Structs/Components/JsonParser.swift
@@ -9,16 +9,48 @@
 import Foundation
 
 struct JsonParser {
-    var tokens: Array<Token>
+    private var tokens: Array<Token>
     var strategy: JsonParsingStrategy
     
-    mutating func parse() -> JsonValue {
+    init(tokens: Array<Token>, strategy: JsonParsingStrategy = JsonListParsingStrategy()) {
+        self.tokens = tokens
+        self.strategy = strategy
+    }
+    
+    public mutating func parse() -> JsonValue {
         self.nomalizeTokens()
         return self.strategy.parse(tokens: self.tokens)
     }
     
-    mutating func nomalizeTokens() {
-        
+    private mutating func nomalizeTokens() {
+        var contents = ""
+        var removingTokenIndex = MyStack<Int>()
+        var isString = false
+        for (index, token) in self.tokens.enumerated() {
+            if token == .DoubleQuotation {
+                isString.toggle()
+                if isString == false {
+                    self.tokens[index] = .String(contents)
+                    contents = ""
+                }
+                continue
+            }
+            if isString {
+                contents += token.getValue()
+                removingTokenIndex.push(index)
+            }
+        }
+        self.remove(indexs: &removingTokenIndex)
+    }
+    
+    private mutating func remove(indexs: inout MyStack<Int>){
+        while let removingIndex = indexs.pop() {
+            self.tokens.remove(at: removingIndex)
+        }
+    }
+    
+    private mutating func set(strategy: JsonParsingStrategy) {
+        self.strategy = strategy
     }
     
 }
