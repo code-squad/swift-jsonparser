4f3bcf359fa73dab0324f5d6bb0ca1b987d6ae87
junyng <pom0894@naver.com>
2019-06-03 11:56:04 +0900

    Token 열거형 타입에 Equatable 프로토콜 함수 정의

--- JSONParser/JSONParser/Token.swift
+++ JSONParser/JSONParser/Token.swift
@@ -35,4 +35,22 @@ enum Token: Equatable {
             return String(number)
         }
     }
+    
+    static func ==(lhs: Token, rhs: Token) -> Bool {
+        switch (lhs, rhs) {
+        case (.openSquareBracket, .openSquareBracket),
+             (.closeSquareBracket, .closeSquareBracket),
+             (.doubleQuotation, .doubleQuotation),
+             (.comma, .comma):
+            return true
+        case (.bool(let lb), .bool(let rb)):
+            return lb == rb
+        case (.string(let ls), .string(let rs)):
+            return ls == rs
+        case (.number(let ln), .number(let rn)):
+            return ln == rn
+        default:
+            return false
+        }
+    }
 }
