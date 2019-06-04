c4c1009189324dc940ff82126daf3f1d591464ab
junyng <pom0894@naver.com>
2019-06-05 03:22:09 +0900

    메인 함수 정의, 함수 내부에 do-try-catch 에러 처리 적용

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -8,3 +8,23 @@
 
 import Foundation
 
+func main() {
+    do {
+        let input = try InputView.readJSONData()
+        var lexer = Lexer(input: input)
+        let tokens = try lexer.tokenize()
+        var parser = Parser(tokens: tokens)
+        let jsonValues = try parser.parse()
+        OutputView.printJSONValues(jsonValues)
+    } catch let error as InputView.Error {
+        print(error.localizedDescription)
+    } catch let error as Lexer.Error {
+        print(error.localizedDescription)
+    } catch let error as Parser.Error {
+        print(error.localizedDescription)
+    } catch {
+        print("예상치 못한 에러 발생: \(error))"
+    }
+}
+
+main()
