7172fa96d148e7c2c8cd84182510bb80a39f997f
kimjieun <zieunv@gmail.com>
2019-06-05 16:38:43 +0900

    input값을 요소별로 쪼갬

--- JSONParser/JSONParser/JSONTokenizer.swift
+++ JSONParser/JSONParser/JSONTokenizer.swift
@@ -9,7 +9,27 @@
 import Foundation
 
 struct JSONTokenizer {
-    static func tokenize(data: String) -> [String] {
-        return data.split(separator: ",").map { String($0) }
+    static func tokenize(data: String) throws -> [String] {
+        let tokens: [Character] = [",", "[", "]"]
+        var result = [String]()
+        var buffer = ""
+        
+        func readToken(character: Character) {
+            if tokens.contains(character) {
+                if !buffer.isEmpty {
+                    result.append(buffer)
+                    buffer.removeAll()
+                }
+                result.append(String(character))
+            } else {
+                buffer.append(character)
+            }
+        }
+        
+        for character in data {
+            readToken(character: character)
+        }
+        return result
     }
+    
 }
