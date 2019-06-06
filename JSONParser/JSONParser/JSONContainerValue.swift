90a04258423d96409736856fbc42855b1ec1d2f5
junyng <pom0894@naver.com>
2019-06-06 16:27:43 +0900

    JSONContainerValue 프로토콜 추가

---
+++ JSONParser/JSONParser/JSONContainerValue.swift
@@ -0,0 +1,13 @@
+//
+//  JSONValueContainable.swift
+//  JSONParser
+//
+//  Created by BLU on 2019. 6. 6..
+//  Copyright © 2019년 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol JSONContainerValue {
+    var containerDescription: String { get }
+}
