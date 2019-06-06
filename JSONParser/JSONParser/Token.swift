c6b1917dbb27ae9ef2b39863844184ab0913f31f
kimjieun <zieunv@gmail.com>
2019-06-06 15:01:43 +0900

    하드코딩을 없애기 위한 Token 구조체 생성

---
+++ JSONParser/JSONParser/Token.swift
@@ -0,0 +1,15 @@
+//
+//  Token.swift
+//  JSONParser
+//
+//  Created by JieunKim on 05/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct Token {
+    static let beginArray = "["
+    static let endArray = "]"
+    static let valueSeparator = ","
+}
