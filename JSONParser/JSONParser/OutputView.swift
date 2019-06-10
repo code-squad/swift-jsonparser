607b02589e161ff2fb388675e0102fa412ca9e2a
chan <clown4014@gmail.com>
2019-06-11 00:33:22 +0900

    outputView구조체 생성

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+struct OutputView {
+    static func printResult(countNumber:CountNumbers) {
+        let dataOfInt = countNumber.intData
+        let dataOfBool = countNumber.boolData
+        let dataOfString = countNumber.stringData
+        print("총\(dataOfInt + dataOfBool + dataOfString) 개의 데이터 중에 문자열 \(dataOfString)개, 숫자 \(dataOfInt)개, 부울 \(dataOfBool)개가 포함되어 있습니다.")
+    }
+}
