60a4029a149c530ff281adde47ba0907b03e2a8c
박준희 <junibibi04@gmail.com>
2019-06-11 00:23:22 +0900

    문법에 문제없는지 확인하는 구조체를 만들고 메소드 이름정함.

---
+++ JSONParser/JSONParser/SyntaxAnalyzer.swift
@@ -0,0 +1,17 @@
+//
+//  SyntaxAnalyzer.swift
+//  JSONParser
+//
+//  Created by JH on 08/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct SyntaxAnalyzer {
+    
+    func analyzeSyntax(tokens: [String]) -> [String] {
+        //2번->,,두번 연속으로 나오면 안된다, 처음에 [시작 ]끝 확인해야함
+        return []
+    }
+}
