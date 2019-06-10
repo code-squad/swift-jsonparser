5f62fb54bcea70d610792289a2a6a3f65157c4b1
박준희 <junibibi04@gmail.com>
2019-06-11 00:20:26 +0900

    입력받은 문자열을 쪼개는 구조체 생성하고 메소드 이름정함

---
+++ JSONParser/JSONParser/Tokenizer.swift
@@ -0,0 +1,17 @@
+//
+//  Tokenizer.swift
+//  JSONParser
+//
+//  Created by JH on 08/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct Tokenizer {
+    
+    func tokenize(input: String) -> [String] {
+        //1번
+        return []
+    }
+}
