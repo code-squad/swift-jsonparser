baacac33c807c1201002c0fd38fd23a0eb9b92d2
kimjieun <zieunv@gmail.com>
2019-06-10 21:36:41 +0900

    dictionary를 활용한 key와 value값으로 data분류

--- JSONParser/JSONParser/JSONDataCount.swift
+++ JSONParser/JSONParser/JSONDataCount.swift
@@ -9,15 +9,13 @@
 import Foundation
 
 struct JSONDataCount {
-    static func JSONTypeCount(arrayData: [String]) throws -> (string: Int, number: Int, bool: Int) {
-        let parsedInput = try arrayData.map {try JSONParser.parseValue(data: $0)}
-        let stringCount = parsedInput.filter{$0 is String}.count
-        let numberCount = parsedInput.filter{$0 is Number}.count
-        let boolCount = parsedInput.filter{$0 is Bool}.count
-        let result = (stringCount,numberCount,boolCount)
-        return result
+    static func NEWJSONTypeCount(arrayData: [JSONDataType]) throws -> [String: Int] {
+        var counts: [String: Int] = [:]
+        for item in arrayData {
+            counts[item.typeDescription] = (counts[item.typeDescription] ?? 0) + 1
+        }
+        return counts
     }
-    
 }
 
 
