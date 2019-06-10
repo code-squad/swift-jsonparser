a57148c9027dd56e52cbee650ede4beddc7c0f9c
kimjieun <zieunv@gmail.com>
2019-06-10 22:14:53 +0900

    main을 함수로 묶음

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -8,14 +8,18 @@
 
 import Foundation
 
-do {
-    let input = try InputView.ask(for: .request)
-    let tokenizedInput = try JSONTokenizer.tokenize(data: input)
-    let analyzedInput = try JSONAnalyzer.analyze(data: tokenizedInput)
-    let parsedInput = try JSONParser.parseArray(data: analyzedInput)
-    try OutputView.printDescription(input: parsedInput)
-} catch let error as JSONError {
-    print(error)
-} catch {
-    print(error)
+func main(){
+    do {
+        let input = try InputView.ask(for: .request)
+        let tokenizedInput = try JSONTokenizer.tokenize(data: input)
+        let analyzedInput = try JSONAnalyzer.analyze(data: tokenizedInput)
+        let parsedInput = try JSONParser.parseArray(data: analyzedInput)
+        try OutputView.printDescription(input: parsedInput)
+    } catch let error as JSONError {
+        print(error)
+    } catch {
+        print(error)
+    }
 }
+
+main()
