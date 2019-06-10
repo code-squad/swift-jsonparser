714cbc961af8bd3207fec8283cd615456a6798e8
kimjieun <zieunv@gmail.com>
2019-06-10 22:07:26 +0900

    Outputview 생성

---
+++ JSONParser/JSONParser/OutputView.swift
@@ -0,0 +1,27 @@
+//
+//  OutputView.swift
+//  JSONParser
+//
+//  Created by JieunKim on 06/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+
+struct OutputView {
+    static func printDescription(input: [JSONDataType]) throws {
+        let description = try NEWprintDataCount(arrayData: input)
+        print(description)
+    }
+    
+    static private func NEWprintDataCount(arrayData: [JSONDataType]) throws -> String {
+        var result: String = ""
+        let totalCount = arrayData.count
+        let counts = try JSONDataCount.NEWJSONTypeCount(arrayData: arrayData)
+        for (key, value) in counts {
+            result += "\(key)이 \(value)개 "
+        }
+        return "총 \(totalCount)개의 데이터 중에 \(result)포함되어 있습니다."
+    }
+}
