f966f45204ec4b1d2184b5e4246767e7893551fd
chan <clown4014@gmail.com>
2019-06-11 17:06:54 +0900

    JSON data parsing 함수 추가

--- JSONParser/JSONParser/Converter.swift
+++ JSONParser/JSONParser/Converter.swift
@@ -10,6 +10,13 @@ import Foundation
 
 struct converter {
     
+    private let dataTypeCriterion = DataTypeCriterion()
+    private var countInt = 0
+    private var countBool = 0
+    private var countString = 0
+    private var countCanNotConvertData = 0
+    private var canNotConvertData:[String] = []
+    
     static func convertToArray(JSON:String) -> [String] {
         
         let removedJSONArrayFormatInput = JSON.trimmingCharacters(in: FormatItem.JSONArrayFormat)
@@ -18,4 +25,43 @@ struct converter {
         
     }
     
+    mutating func parsing(JSON:[String])throws -> [JSONValue] {
+        
+        var JSONParserValue:[JSONValue] = []
+        
+        for item in JSON {
+            
+            if dataTypeCriterion.distinguishingString.isStrictSubset(of: CharacterSet(charactersIn: item)) {
+                JSONParserValue.append(String(item))
+                countString += 1
+                continue
+            }
+            
+            if CharacterSet(charactersIn: item).isStrictSubset(of: dataTypeCriterion.distinguishingInt){
+                JSONParserValue.append(Int(item)!)
+                countInt += 1
+                continue
+            }
+            
+            if item == dataTypeCriterion.distinguishingBool.true || item == dataTypeCriterion.distinguishingBool.false {
+                JSONParserValue.append(Bool(item)!)
+                countBool += 1
+                continue
+            }
+            canNotConvertData.append(item)
+            countCanNotConvertData += 1
+        }
+        
+        guard countCanNotConvertData == 0 else{
+            print(canNotConvertData)
+            throw ConvertError.canNotCovertData
+        }
+        
+        return JSONParserValue
+        
+    }
+    
 }
+
+
+
