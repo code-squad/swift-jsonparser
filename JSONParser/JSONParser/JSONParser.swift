1ea1b38d6854a3ab550a02c613fa771bf15f6463
chan <clown4014@gmail.com>
2019-06-11 18:20:36 +0900

    JSON을 의미있는 데이터로 변환하여 생성할 JSONParser구조체 선언

---
+++ JSONParser/JSONParser/JSONParser.swift
@@ -0,0 +1,18 @@
+//
+//  JSONParser.swift
+//  JSONParser
+//
+//  Created by 이희찬 on 11/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct JSONParser {
+    
+    var JSONParserValue:[JSONValue] = []
+    var countInt = 0
+    var countBool = 0
+    var countString = 0
+    
+}
