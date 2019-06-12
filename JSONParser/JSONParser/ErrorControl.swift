3001fd73c1e18c2deb89840bdee91218e2ae07b5
kimjieun <zieunv@gmail.com>
2019-06-12 12:16:15 +0900

    ErrorControl을 custom에서 localizedDescription으로 변경

--- JSONParser/JSONParser/ErrorControl.swift
+++ JSONParser/JSONParser/ErrorControl.swift
@@ -8,10 +8,22 @@
 
 import Foundation
 
-enum JSONError: String, Error, CustomStringConvertible {
-    var description: String { return self.rawValue }
-        case wrongValue = "입력값이 유효하지 않습니다."
-        case emptyBuffer = "buffer가 비었습니다."
-        case notArray = "Array가 아닙니다."
-        case unexpectedSeperator = "예상치 못한 ,가 나왔습니다."
+enum JSONError: Error {
+    case wrongValue
+    case emptyBuffer
+    case notArray
+    case unexpectedSeperator
+    
+    var localizedDescription: String {
+        switch self {
+        case .wrongValue:
+            return "입력값이 유효하지 않습니다."
+        case .emptyBuffer:
+            return "buffer가 비었습니다."
+        case .notArray:
+            return "Array가 아닙니다."
+        case .unexpectedSeperator:
+            return "예상치 못한 ,가 나왔습니다."
+        }
+    }
 }
