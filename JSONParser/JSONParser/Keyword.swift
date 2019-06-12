57476d892143413d36dfe8f2d46db4502b1ff9d2
junyng <pom0894@naver.com>
2019-06-12 10:24:32 +0900

    Keyword 구조체 추가

---
+++ JSONParser/JSONParser/Keyword.swift
@@ -0,0 +1,21 @@
+//
+//  SymbolLetter.swift
+//  JSONParser
+//
+//  Created by BLU on 12/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct Keyword {
+    static let openSquareBracket: Character = "["
+    static let closeSquareBracket: Character = "]"
+    static let doubleQuotation: Character = "\""
+    static let comma: Character = "\""
+    static let emptySpace: Character = " "
+    static let `true` = "true"
+    static let `false` = "false"
+    static let alphanumerics = "a" ... "b"
+    static let decimalDigits = "0" ... "9"
+}
