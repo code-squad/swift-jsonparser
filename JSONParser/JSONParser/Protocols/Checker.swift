c7f6b228d18cae175aee8d7e38e1df9ec5b61248
O-O-wl <ldcpaul@hanmail.net>
2019-06-17 19:49:50 +0900

    Created: define  Checker protocol, GrammerChecker struct

---
+++ JSONParser/JSONParser/Protocols/Checker.swift
@@ -0,0 +1,15 @@
+//
+//  Checker.swift
+//  JSONParser
+//
+//  Created by 이동영 on 17/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol Checker {
+    associatedtype T
+    
+    func check(format: T) -> Bool
+}
---
+++ JSONParser/JSONParser/Structs/Components/GrammerChecker.swift
@@ -0,0 +1,19 @@
+//
+//  GrammerChecker.swift
+//  JSONParser
+//
+//  Created by 이동영 on 17/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct GrammerChecker: Checker {
+    typealias T = String
+    
+    func check(format: String) -> Bool {
+        return false
+        
+    }
+    
+}
