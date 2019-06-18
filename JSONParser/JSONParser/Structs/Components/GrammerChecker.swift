44931ad1b2d34c64fbaad90939a47874a6394fd9
O-O-wl <ldcpaul@hanmail.net>
2019-06-18 23:42:35 +0900

    feat: Add check method

--- JSONParser/JSONParser/Structs/Components/GrammerChecker.swift
+++ JSONParser/JSONParser/Structs/Components/GrammerChecker.swift
@@ -12,8 +12,30 @@ struct GrammerChecker: Checker {
     typealias T = String
     
     func check(format: String) -> Bool {
+        let maxDepth = format.filter{
+            return ["[","{"].contains($0)
+            }.count
+        return checkList(format: format,depth: maxDepth) || checkObject(format: format, depth: maxDepth)
+    }
+    
+    private func checkObject(format: String, depth: Int) -> Bool {
+        let regex = JsonPattern.object.getRegex(depth)
+        return check(string: format, pattern: regex)
+    }
+    
+    private func checkList(format: String, depth: Int) -> Bool {
+        let regex = JsonPattern.list.getRegex(depth)
+        return check(string: format, pattern: regex)
+    }
+    
+    public func check(string: String, pattern: String) -> Bool {
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+        if let match = regex.firstMatch(in: string, options: [], range: string.range) {
+            let result = NSString.init(string:string).substring(with: (match.range))
+            print(result)
+            return result.range == string.range
+        }
         return false
-        
     }
     
 }
