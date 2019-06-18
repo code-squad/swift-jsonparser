8b3a6ecb3706850323a364258e50a14a3823e3da
cmindy <minjidev@naver.com>
2019-06-18 21:40:02 +0900

    test: Add testArray() for testing array

--- JSONParser/JSONParserTests/GrammerCheckerTests.swift
+++ JSONParser/JSONParserTests/GrammerCheckerTests.swift
@@ -10,4 +10,29 @@ import XCTest
 
 class GrammerCheckerTests: XCTestCase {
     
+    func testArray() {
+        // given
+        let multiObject = "[ { \"name\" : \"MINJI CHO\", \"alias\" : \"mindy\", \"level\" : 2, \"married\" : false }, { \"name\" : \"HELLO HI\", \"alias\" : \"hi\", \"level\" : 4, \"married\" : true } ]"
+        let multiValuesIncludeObject = "[ 15, false, { \"name\" : \"MINJI CHO\", \"alias\" : \"mindy\", \"level\" : 2, \"married\" : false },true, \"hihi\", false, { \"name\" : \"HELLO HI\", \"alias\" : \"hi\", \"level\" : 4, \"married\" : true } ]"
+        
+        // then
+        XCTAssertTrue(GrammerChecker.isJSONFormat(of: multiObject))
+        XCTAssertTrue(GrammerChecker.isJSONFormat(of: multiValuesIncludeObject))
+    }
+    
+    func testArray_whenHasKeyValue() {
+        // given
+        let invalidInput = "[ \"name\" : \"mindy\" ]"
+        
+        // then
+        XCTAssertFalse(GrammerChecker.isJSONFormat(of: invalidInput))
+    }
+    
+    func testArray_whenHasEmptyValue() {
+        // given
+        let emptyArray = "[ ]"
+        
+        // then
+        XCTAssertTrue(GrammerChecker.isJSONFormat(of: emptyArray))
+    }
 }
