20630f900d0ac8405a52832abea104c9d5644398
cmindy <minjidev@naver.com>
2019-06-18 23:34:42 +0900

    test: Add testObject() for testing object input

--- JSONParser/JSONParserTests/GrammerCheckerTests.swift
+++ JSONParser/JSONParserTests/GrammerCheckerTests.swift
@@ -35,4 +35,44 @@ class GrammerCheckerTests: XCTestCase {
         // then
         XCTAssertTrue(GrammerChecker.isJSONFormat(of: emptyArray))
     }
+    
+    func testObject() {
+        //given
+        let object = "{ \"name\" : \"MINJI CHO\", \"alias\" : \"mindy\", \"level\" : 2, \"married\" : false }"
+        
+        //then
+        XCTAssertTrue(GrammerChecker.isJSONFormat(of: object))
+    }
+    
+    func testObject_whenKeyIsNotString() {
+        // given
+        let keyIsNotString = "{ name : \"Cho Minji\", \"alias\" : \"mindy\" }"
+        
+        // then
+        XCTAssertFalse(GrammerChecker.isJSONFormat(of: keyIsNotString))
+    }
+    
+    func testObject_whenObjectHasNotColon() {
+        // given
+        let hasNotColon = "{ \"name\" \"Lee Dong Young\", \"alias\" : \"Owl\" }"
+        
+        // then
+        XCTAssertFalse(GrammerChecker.isJSONFormat(of: hasNotColon))
+    }
+    
+    func testObject_whenHasArray() {
+        // given
+        let invalidInput = "{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"children\" : [\"hana\", \"hayul\", \"haun\"] }"
+        
+        // then
+        XCTAssertFalse(GrammerChecker.isJSONFormat(of: invalidInput))
+    }
+    
+    func testObject_whenHasEmptyValue() {
+        // given
+        let emptyArray = "{ }"
+        
+        // then
+        XCTAssertTrue(GrammerChecker.isJSONFormat(of: emptyArray))
+    }
 }
