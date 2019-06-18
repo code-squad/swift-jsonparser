bd39f60aba621f61ba1e3ca5da5e3766198794bc
cmindy <minjidev@naver.com>
2019-06-18 17:58:25 +0900

    feat: Add a GrammerChecker to main for run

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -8,15 +8,24 @@
 
 import Foundation
 
-let userInput = InputView.readPrompt(for: "분석할 JSON 데이터를 입력하세요.")
-
-let tokens = Tokenizer.tokenize(from: userInput)
-let tokenScanner = TokenScanner(tokens: tokens)
-
-var parser = Parser(tokens: tokens, scanner: tokenScanner)
-if let jsonData = parser.parse() {
-    OutputView.printDescription(of: jsonData)
+func main() {
+    let userInput = InputView.readPrompt(for: "분석할 JSON 데이터를 입력하세요.")
+    
+    guard GrammerChecker.isJSONFormat(of: userInput) else {
+        print("지원하지 않는 형식이 포함되어 있습니다.")
+        return
+    }
+    
+    let tokens = Tokenizer.tokenize(from: userInput)
+    let tokenScanner = TokenScanner(tokens: tokens)
+    
+    let parser = Parser(tokens: tokens, scanner: tokenScanner)
+    if let jsonData = parser.parse() {
+        OutputView.printDescription(of: jsonData)
+    }
 }
+
+main()
 
 // { "name" : "MINJI CHO", "alias" : "mindy", "level" : 2, "married" : false }
 
