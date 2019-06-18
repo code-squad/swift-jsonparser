9a9177e3bdd3f8b4f142a9fe43227d7f43cc701d
박준희 <junibibi04@gmail.com>
2019-06-18 15:59:49 +0900

    문법체크하는 메소드 생성

--- JSONParser/JSONParser/SyntaxAnalyzer.swift
+++ JSONParser/JSONParser/SyntaxAnalyzer.swift
@@ -8,10 +8,34 @@
 
 import Foundation
 
+enum ErrorMessage: Error {
+    case grammarError
+}
+
 struct SyntaxAnalyzer {
     
-    func analyzeSyntax(tokens: [String]) -> [String] {
-        //2번->,,두번 연속으로 나오면 안된다, 처음에 [시작 ]끝 확인해야함
-        return []
+    func analyzeSyntax(tokens: [String]) throws -> [String] {
+        guard tokens[0] == "[" && tokens[tokens.count-1] == "]" else {
+            throw ErrorMessage.grammarError
+        }
+        return tokens
     }
 }
+
+
+/*
+
+ -------------------------------------------------------------
+ 1.input [ 이면 arraycharacter에 append
+         " ", "," 이면 다음 index확인
+ 2. 다음 index가 " 이면 다시 " 나올때까지 arraystring에 append
+ 
+ 3. "없는 문자이면 ,과 " " 나올때까지 arr에 append
+ 4. arr이 Int로 변환되면 intarray에 append
+ 5. arr이 true거나 false이면 boolarray에 append
+ --------------------------------------------------------------
+ 
+ 
+ */
+
+
