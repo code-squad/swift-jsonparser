ae8462433fea2dc315ee3711c2e44df9f33e57e7
cmindy <minjidev@naver.com>
2019-06-18 17:53:09 +0900

    feat: Add Pattern to GrammerChecker

--- JSONParser/JSONParser/GrammerChecker.swift
+++ JSONParser/JSONParser/GrammerChecker.swift
@@ -8,4 +8,17 @@
 
 import Foundation
 
-
+struct GrammerChecker {
+    
+    struct Pattern {
+        static let bool = "(true|false)"
+        static let string = "\"[\\w\\s]+\""
+        static let number = "[0-9]+"
+        static let whiteSpace = "[\\s]*"
+        static let value = "\(bool)|\(number)|\(string)"
+        static let keyValue = "\(whiteSpace)\(string)\(whiteSpace):\(whiteSpace)[(\(number)|\(string)|\(bool)]*\(whiteSpace)"
+        static let object = "\\{[[\(keyValue)][,]?]+\\}"
+        static let extendedValue = "\(value)|\(object)"
+        static let array = "(\\[(\(whiteSpace)(\(extendedValue))*\(whiteSpace))*])"
+    }
+}
