5593380e0f63c308941ee3bfc7f99c43b3b9df98
박준희 <junibibi04@gmail.com>
2019-06-18 14:21:28 +0900

    입력받은 문자열을 쪼개서 배열로 반환하는 메소드 생성

--- JSONParser/JSONParser/Tokenizer.swift
+++ JSONParser/JSONParser/Tokenizer.swift
@@ -9,9 +9,14 @@
 import Foundation
 
 struct Tokenizer {
-    
+    /// 입력받은 문자열을 모두쪼개서 배열로반환하는 메소드
     func tokenize(input: String) -> [String] {
-        //1번
-        return []
+        var tokens = Array<String>()
+        
+        for token in input {
+            tokens.append(String(token))
+        }
+        
+        return tokens
     }
 }
