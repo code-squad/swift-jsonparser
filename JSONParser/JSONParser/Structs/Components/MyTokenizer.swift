7223355d83dfe0ff5bec6cce072366cc640db37c
O-O-wl <ldcpaul@hanmail.net>
2019-06-19 22:22:11 +0900

    refactor: Modify Split() , and Remove dependent method

--- JSONParser/JSONParser/Structs/Components/MyTokenizer.swift
+++ JSONParser/JSONParser/Structs/Components/MyTokenizer.swift
@@ -11,74 +11,29 @@ import Foundation
 struct MyTokenizer: Tokenizer {
     private let factory = TokenFactory()
     private var scanner: Scanner
+    private var string: String
     
     init(_ string: String) {
+        self.string = string
         self.scanner = MyScanner.init(string: string)
     }
     
-    mutating func tokenize() throws -> [Token] {
-        let units = try self.split()
+    mutating func tokenize() -> [Token] {
+        let units = self.split()
         let tokens = units.map {
             self.factory.create(string: $0)
         }
         return tokens
     }
     
-    private mutating func split() throws -> [String] {
-        var units = [String]()
-        while let character = self.scanner.next() {
-            switch character {
-            case "[","]",","," ",":","{","}":
-                units.append(String(character))
-            case "0"..."9":
-                units.append(getNumber())
-                self.scanner.backCurser()
-            case "\"":
-                units.append(getString())
-            default:
-                units.append(getValue())
-                self.scanner.backCurser()
-            }
+    private func split() -> [String] {
+        let pattern = Pattern.tokens
+        
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+        let matches = regex.matches(in: self.string, options: [], range: self.string.range)
+        let units = matches.map {
+            NSString.init(string: self.string).substring(with: ($0.range))
         }
         return units
     }
-    
-    private mutating func getNumber() -> String {
-        var number = ""
-        self.scanner.backCurser()
-        while let character = self.scanner.next() {
-            if (character.isNumber) {
-                number = "\(number)\(character)"
-                continue
-            }
-            break
-        }
-        return number
-    }
-    
-    private mutating func getString() -> String {
-        var string = "\""
-        while let character = self.scanner.next() {
-            if (character != "\"") {
-                string = "\(string)\(character)"
-                continue
-            }
-            break
-        }
-        return string + "\""
-    }
-    
-    private mutating func getValue() -> String {
-        var value = ""
-        self.scanner.backCurser()
-        while let character = self.scanner.next() {
-            if !(character.isWhitespace || character.isPunctuation){
-                value = "\(value)\(character)"
-                continue
-            }
-            break
-        }
-        return value
-    }
-    
 }
