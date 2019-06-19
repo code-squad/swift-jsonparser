d28333ec055d67ac315f105a2ef268f15a2ab76f
O-O-wl <ldcpaul@hanmail.net>
2019-06-19 22:17:32 +0900

    feat: Implement Checker protocol method

--- JSONParser/JSONParser/Structs/Components/GrammerChecker.swift
+++ JSONParser/JSONParser/Structs/Components/GrammerChecker.swift
@@ -11,31 +11,53 @@ import Foundation
 struct GrammerChecker: Checker {
     typealias T = String
     
-    func check(format: String) -> Bool {
+    func check(format: String) throws {
         let maxDepth = format.filter{
             return ["[","{"].contains($0)
             }.count
-        return checkList(format: format,depth: maxDepth) || checkObject(format: format, depth: maxDepth)
+        let isPosslible =  try checkList(format: format,depth: maxDepth) || ( try checkObject(format: format, depth: maxDepth) )
+        guard isPosslible else {
+            throw Error.unsupportedFormat
+        }
+        return 
     }
     
-    private func checkObject(format: String, depth: Int) -> Bool {
-        let regex = JsonPattern.object.getRegex(depth)
-        return check(string: format, pattern: regex)
+    private func checkObject(format: String, depth: Int = 0) throws -> Bool {
+        let regex = Pattern.object
+        return try check(string: format, pattern: regex)
     }
     
-    private func checkList(format: String, depth: Int) -> Bool {
-        let regex = JsonPattern.list.getRegex(depth)
-        return check(string: format, pattern: regex)
+    private func checkList(format: String, depth: Int = 0) throws -> Bool {
+        let regex = Pattern.list
+        return try check(string: format, pattern: regex)
     }
     
-    public func check(string: String, pattern: String) -> Bool {
-        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+    private func check(string: String, pattern: String) throws -> Bool {
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: []) else {
+            throw Error.unsupportedRegex
+        }
         if let match = regex.firstMatch(in: string, options: [], range: string.range) {
             let result = NSString.init(string:string).substring(with: (match.range))
-            print(result)
             return result.range == string.range
         }
         return false
     }
     
+}
+// -MARK: - + Error
+extension GrammerChecker {
+    enum Error: Swift.Error,LocalizedError {
+        case unsupportedFormat
+        case unsupportedRegex
+        
+        var errorDescription: String? {
+            switch self {
+            case .unsupportedFormat:
+                return "지원하지않는 JSON 형식입니다."
+            case .unsupportedRegex:
+                return "정규표현식 형식이 올바르지 않습니다."
+            }
+            
+        }
+    }
 }
