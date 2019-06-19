ec50e7a38984fde4df03bbaea4914d0ae7747e1a
Daheen Lee <allwhite.dev@gmail.com>
2019-06-19 15:12:50 +0900

    refactor: Modify main function to print notice when input does not match JSON pattern

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -13,15 +13,16 @@ func main() {
     do {
         let input = try InputView.read()
         let isJSONPattern = try GrammarChecker.check(input: input)
-        if !isJSONPattern {
-            throw GrammarCheckerError.matchNoPattern
+        if isJSONPattern {
+            let tokens = try Tokenizer.execute(jsonData: input)
+            var jsonParser = Parser(tokens: tokens)
+            let parsedValue = try jsonParser.parse()
+            try OutputView.printJSONDescription(of: parsedValue)
+        } else {
+            print("지원하지 않는 형식입니다.")
         }
-        let tokens = try Tokenizer.execute(jsonData: input)
-        var jsonParser = Parser(tokens: tokens)
-        let parsedValue = try jsonParser.parse()
-        try OutputView.printJSONDescription(of: parsedValue)
-    } catch let e as JSONError {
-        print(e.message)
+    } catch let error as JSONError {
+        print(error.message)
         return
     } catch {
         print("Other Unexpected Error")
