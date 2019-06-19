06875c4a570419fc65277e6ed81ba905f231fdf3
junyng <pom0894@naver.com>
2019-06-19 17:15:43 +0900

    Dictionary 기본 타입 JSONContainerValue 프로토콜 준수

--- JSONParser/JSONParser/JSONContainerValue.swift
+++ JSONParser/JSONParser/JSONContainerValue.swift
@@ -22,8 +22,15 @@ extension Array: JSONValue, JSONContainerValue where Element == JSONValue {
     }
 }
 
-extension Dictionary: JSONValue where Key == String, Value == JSONValue {
+extension Dictionary: JSONValue, JSONContainerValue where Key == String, Value == JSONValue {
     var typeDescription: String {
         return "객체"
+    }
+    var containerDescription: String {
+        var groupedJSONValues = [String: Int]()
+        for value in self.values {
+            groupedJSONValues[value.typeDescription] = (groupedJSONValues[value.typeDescription] ?? 0) + 1
+        }
+        return "총 \(self.count)개의 데이터 중에 \(groupedJSONValues.map { "\($0) \($1)개" }.joined(separator: ","))가 포함되어 있습니다."
     }
 }
