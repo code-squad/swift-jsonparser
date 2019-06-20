8272f0684ccd029dbfaf19c0f36bd96365767e7c
kimjieun <zieunv@gmail.com>
2019-06-21 03:54:25 +0900

    변수명 수정

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -8,28 +8,26 @@
 
 import Foundation
 
-
 struct OutputView {
-    static func printDescription(input: JSONDataType) throws {
+    static func printOut(_ input: JSONDataType) throws {
         if input is Array<JSONDataType> {
             let description = try printDataCount(arrayData: input)
             print(description)
         } else {
-            
+            print(input)
         }
     }
     
     static private func printDataCount(arrayData: JSONDataType) throws -> String {
         var result: String = ""
-        
         guard let array = arrayData as? Array<JSONDataType> else {
             throw JSONError.emptyBuffer
         }
         
         let totalCount = array.count
-        let counts = try JSONDataCount.JSONTypeCount(arrayData: array)
+        let counts = try Counter.count(jsonData: array)
         for (key, value) in counts {
-            result += "\(key)이 \(value)개 "
+            result += "\(key)가 \(value)개 "
         }
         return "총 \(totalCount)개의 데이터 중에 \(result)포함되어 있습니다."
     }
