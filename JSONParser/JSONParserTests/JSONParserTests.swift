21a6a94aa27d3e44732610948f19ee6371602289
kimjieun <zieunv@gmail.com>
2019-06-21 17:35:04 +0900

    testcode 작성

--- JSONParser/JSONParserTests/JSONParserTests.swift
+++ JSONParser/JSONParserTests/JSONParserTests.swift
@@ -11,16 +11,25 @@ import XCTest
 class JSONParserTests: XCTestCase {
 
     func testParseArray() {
-        let tokens = ["1","true","\"name\""]
-        let result =  JSONParser.parseArray(tokens: tokens)!
-        let expected: [JSONDataType] = [1,true,"name"]
         
+        let tokens = ["[","1","true","\"name\"","]"]
+        var parser =  JSONParser(tokens: tokens)
+        let expected: [JSONDataType] = [1,true,"name"]
+        let result = ( try! parser.parse() ) as! [JSONDataType]
         for (index,_) in result.enumerated() {
             XCTAssertEqual(result[index].typeDescription,                      expected[index].typeDescription)
         }
     }
     
-    
+    func testParseObject() {
+        let tokens = ["{","\"zieunv\"",":", "true", "\"mindy\"",":" ,"23", "\"부엉이\"",":", "27" ,"}"]
+        var parser = JSONParser(tokens: tokens)
+        let expected: [ String : JSONDataType] = ["zieunv" : true,"mindy":23, "부엉이": 27]
+        let result = (try! parser.parse() ) as! [JSONDataType]
+        for(index,_) in result.enumerated() {
+            XCTAssertEqual(result[index].typeDescription, expected.typeDescription)
+        }
+    }
     
 
 }
