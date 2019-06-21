1a14e650b83c9d6496a702adc3c83dbf67abaa65
Daheen Lee <allwhite.dev@gmail.com>
2019-06-21 19:14:47 +0900

    feat: Extends String.StringInterpolation for typeCountDescription of JSONValue

--- JSONParser/JSONParser/StringInterpolation.swift
+++ JSONParser/JSONParser/StringInterpolation.swift
@@ -8,3 +8,21 @@
 
 import Foundation
 
+extension String.StringInterpolation {
+    mutating func appendInterpolation(typeCountDescription value: JSONValue & TypeCountable) {
+        let prefixDescription = "총 \(value.elementCount)개의 \(value.typeDescription) 데이터 중에 "
+        let suffixDescription = "가 포함되어 있습니다."
+        var elementDescription = String()
+        let seperator = ", "
+        var prefix = String()
+        let totalTypeCountPair = TypeCounter.getTotalTypeCount(of: value)
+        for (typeDescription, count) in totalTypeCountPair {
+            elementDescription += prefix
+            elementDescription += "\(typeDescription) \(count)개"
+            prefix = seperator
+        }
+        appendLiteral("\(prefixDescription)\(elementDescription)\(suffixDescription)")
+    }
+    
+
+}
