1f8fb51bb11ec8659d5773c854843c0b128663d8
Kroisttt <yago0315@gmail.com>
2019-06-21 18:36:53 +0900

    GrammarChecker 수정
    static으로 수정, matches()에서 input이 array나 object중 하나라면 true가 반환되도록 수정

--- JSONParser/JSONParser/GrammarChecker.swift
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -9,16 +9,19 @@
 import Foundation
 
 struct GrammarChecker {
-    private let objectRegex = "^\\{ ?((|, ?)(\"[a-zA-Z0-9]+\" ?: ?)(\"[a-zA-Z0-9 ]+\"|[0-9]+|true|false))+ ?\\}$"
-    private let arrayRegex = "^\\[ ?((|, ?)(\"[a-zA-Z0-9 ]+\"|[0-9]+|true|false|(\\{ ?((|, ?)(\"[a-zA-Z0-9]+\" ?: ?)(\"[a-zA-Z0-9 ]+\"|[0-9]+|true|false))+ ?\\})))+ ?\\]$"
+    private static let objectRegex = "^\\{ ?((|, ?)(\"[a-zA-Z0-9]+\" ?: ?)(\"[a-zA-Z0-9 ]+\"|[0-9]+|true|false))+ ?\\}$"
+    private static let arrayRegex = "^\\[ ?((|, ?)(\"[a-zA-Z0-9 ]+\"|[0-9]+|true|false|(\\{ ?((|, ?)(\"[a-zA-Z0-9]+\" ?: ?)(\"[a-zA-Z0-9 ]+\"|[0-9]+|true|false))+ ?\\})))+ ?\\]$"
     
-    func checkGrammar(input: String) throws {
+    static func checkGrammar(input: String) throws {
         if !matches(input) {
             throw GrammarCheckError.noMatchesPattern
         }
     }
     
-    func matches(_ value: String) -> Bool {
-        return value.range(of: objectRegex, options: .regularExpression) != nil
+    private static func matches(_ value: String) -> Bool {
+        let isArray = value.range(of: arrayRegex, options: .regularExpression) != nil
+        let isObject = value.range(of: objectRegex, options: .regularExpression) != nil
+        
+        return isArray || isObject
     }
 }
