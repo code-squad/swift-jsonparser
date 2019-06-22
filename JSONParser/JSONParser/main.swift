bf98b8b51b7a3d6b3979dd7e272b60c84178973d
O-O-wl <ldcpaul@hanmail.net>
2019-06-23 00:32:28 +0900

    refactor: Add Error handing into main

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -7,21 +7,4 @@
 //
 //
 
-
-import Foundation
-do {
-    let inputView = InputView()
-    let userInput = inputView.run()
-    let checker = GrammerChecker()
-    guard checker.check(format: userInput) else {
-        throw GrammerChecker.Error.unsupportedFormat
-    }
-    var tokenizer = MyTokenizer.init(userInput)
-    let tokens = tokenizer.tokenize()
-    let result = JsonParser.run(tokens: tokens)
-    let jsonContainer = result.value
-    let outputView = OutputView.init()
-    outputView.output(jsonContainer)
-}catch {
-    ErrorHandler.handle(error: error)
-}
+JSONAnalyer().run()
