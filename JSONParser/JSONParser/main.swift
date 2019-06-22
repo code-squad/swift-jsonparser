0e574e424aafa52af7632871933390d27e14bfa7
박준희 <junibibi04@gmail.com>
2019-06-22 19:01:35 +0900

    main파일에서 메소드호출함

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -8,3 +8,18 @@
 
 import Foundation
 
+var inputView = InputView()
+var outputView = OutputView()
+var tokenizer = Tokenizer()
+var parser = Parser()
+
+outputView.show(string: "Json데이터를 입력하세요")
+let input = inputView.ask(data: "Json데이터")
+let tokens = tokenizer.tokenize(input: input)
+
+do {
+    let jsonData = try parser.startParsing(tokens: tokens)
+    outputView.showCount(json: jsonData)
+} catch {
+    outputView.show(string: "\(error)")
+}
