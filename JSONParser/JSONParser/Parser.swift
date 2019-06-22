e399c746e1e6efca918970d8ef2ab959ec47121f
박준희 <junibibi04@gmail.com>
2019-06-22 15:23:32 +0900

    옵셔널바인딩해서 사용하는 부분이 반복되어서 옵셔널말고 오류를 던지는 코드로 수정함 hasNext범위 수정함

--- JSONParser/JSONParser/Parser.swift
+++ JSONParser/JSONParser/Parser.swift
@@ -32,13 +32,13 @@ struct Parser {
     
     //다음인덱스를 확인함(return에 있는 조건이 실패할경우 false반환함)
     func hasNext(index: Int) -> Bool {
-        return index < tokens.endIndex
+        return index < tokens.endIndex - 1
     }
     
     //현재인덱스를 받아서 다음인덱스와 다음인덱스의 값을 반환함
-    func readToken(index: Int) -> (index: Int, token: String)? {
+    func readToken(index: Int) throws -> (index: Int, token: String) {
         guard hasNext(index: index) else {
-            return nil
+            throw ParsingError.error
         }
         let nextIndex = index + 1
         return (index: nextIndex, token: tokens[nextIndex])
@@ -46,9 +46,8 @@ struct Parser {
     
     
     func parseString(index: Int) throws -> (index: Int, value: String)? {
-        guard let (index, token) = readToken(index: index) else {
-            throw ParsingError.error
-        }
+        let (index, token) = try readToken(index: index)
+        
         guard token.first == "\"" && token.last == "\"" else {
             return nil
         }
@@ -60,9 +59,8 @@ struct Parser {
     }
     
     func parseNumber(index: Int) throws -> (index: Int, value: Double)? {
-        guard let (index, token) = readToken(index: index) else {
-            throw ParsingError.error
-        }
+        let (index, token) = try readToken(index: index)
+
         guard let number = Double(token) else {
             return nil
         }
@@ -70,9 +68,8 @@ struct Parser {
     }
     
     func parseBool(index: Int) throws -> (index: Int, value: Bool)? {
-        guard let (index, token) = readToken(index: index) else {
-            throw ParsingError.error
-        }
+        let (index, token) = try readToken(index: index)
+
         guard let bool = Bool(token) else {
             return nil
         }
@@ -80,6 +77,34 @@ struct Parser {
     }
     
     func parseArray(index: Int) throws -> (index: Int, value: [JsonType])? {
+        var resultArray = [JsonType]()
+        //parseArray가 처리할수있는 값인지 확인해준다.
+        let (index, token) = try readToken(index: index)
+
+        guard token == "[" else {
+            return nil
+        }
+        while true {
+            let (index, value) = try parseValue(index: index)
+            resultArray.append(value)
+            
+            guard let (index, token) = readToken(index: index) else {
+                return nil
+            }
+            guard token == "]" else {
+                return (index, resultArray)
+            }
+        }
+        
+        
+        
+        처음index값이 [ 인지 확인하고, 아니면 nil 반환
+        ]를 만날때까지 반복한다.
+        index + 1 으로 이동해서 parse 진행하고 배열에 append한다
+        ,면 index+1하고 계속 반복한다.
+
+        ]나오면, append한 배열을 반환한다.
+        
         return (0, [])
     }
 
