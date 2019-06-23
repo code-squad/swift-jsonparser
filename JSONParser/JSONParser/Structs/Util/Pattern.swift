fdc7b9e0b6f3a1aadb70935cd0e072ab50f18034
O-O-wl <ldcpaul@hanmail.net>
2019-06-24 00:10:46 +0900

    refactor: Modify Access Modifier , method positon

--- JSONParser/JSONParser/Structs/Util/Pattern.swift
+++ JSONParser/JSONParser/Structs/Util/Pattern.swift
@@ -21,26 +21,31 @@ extension Pattern {
 }
 // - MARK: - Collection
 extension Pattern {
-    static let keyValue = "\(string)(\(ws))?:(\(ws))?(\(value))"
-    static let object = "(\(ws))?\\{((\(ws))?\(keyValue)(\(ws))?(,)?)*\\}(\(ws))?"
-    static let list = "(\(ws))?\\[((\(ws))?(\(value)|(\(object)))(\(ws))?(,)?(\(ws))?)*\\](\(ws))?"
+    private static let keyValue = "\(string)(\(ws))?:(\(ws))?(\(value))"
+    private static let object = "(\(ws))?\\{((\(ws))?\(keyValue)(\(ws))?(,)?)*\\}(\(ws))?"
+    private static let list = "(\(ws))?\\[((\(ws))?(\(value)|(\(object)))(\(ws))?(,)?(\(ws))?)*\\](\(ws))?"
 }
 extension Pattern {
-    static func getValue(depth: Int) -> String {
+    
+    static func getObject(depth: Int) -> String {
+        return "(\(ws))?\\{((\(ws))?\(getKeyValue(depth: depth))(\(ws))?(,)?)*\\}(\(ws))?"
+    }
+    
+    static func getList(depth: Int) -> String {
+        return "(\(ws))?\\[((\(ws))?\(getValue(depth: depth))(\(ws))?(,)?(\(ws))?)*\\](\(ws))?"
+    }
+    
+    private static func getKeyValue(depth: Int) -> String {
+        return "\(string)(\(ws))?:(\(ws))?(\(getValue(depth: depth)))"
+    }
+    
+    private static func getValue(depth: Int) -> String {
         var depth = depth
         if depth > 0 {
             depth-=1
             return "((\(getObject(depth: depth))|(\(getList(depth: depth)))|(\(value))))"
         }
-    return "(\(value))"
+        return "(\(value))"
     }
-    static func getKeyValue(depth: Int) -> String {
-        return "\(string)(\(ws))?:(\(ws))?(\(getValue(depth: depth)))"
-    }
-    static func getObject(depth: Int) -> String {
-        return "(\(ws))?\\{((\(ws))?\(getKeyValue(depth: depth))(\(ws))?(,)?)*\\}(\(ws))?"
-    }
-    static func getList(depth: Int) -> String {
-        return "(\(ws))?\\[((\(ws))?\(getValue(depth: depth))(\(ws))?(,)?(\(ws))?)*\\](\(ws))?"
-    }
+    
 }
