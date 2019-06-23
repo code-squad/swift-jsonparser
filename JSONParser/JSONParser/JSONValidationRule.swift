7cd91716ee796693213a437466a0a5c54b1a86ac
junyng <pom0894@naver.com>
2019-06-23 15:07:02 +0900

    문자열 확장 함수 matches, 사용자 정의 연산자 =~ 정의 ValidationRule 파일 내 정의로 변경

--- JSONParser/JSONParser/JSONValidationRule.swift
+++ JSONParser/JSONParser/JSONValidationRule.swift
@@ -13,22 +13,3 @@ struct JSONValidationRule: ValidationRule {
         return value =~ Regex.jsonObject || value =~ Regex.jsonArray
     }
 }
-
-extension String {
-    func matches(_ pattern: String) -> Bool {
-        return self.range(of: pattern, options: .regularExpression) != nil
-    }
-}
-
-precedencegroup RegexPrecedence {
-    higherThan: LogicalConjunctionPrecedence
-}
-/// 정규식 패턴에 매치하는 사용자 정의 연산자
-infix operator =~: RegexPrecedence
-/// - Parameters:
-///   - string: 입력할 문자열
-///   - pattern: 정규식 문자열
-/// - Returns: 정규식에 일치 여부에 따라 true or false
-func =~ (string: String, pattern: String) -> Bool {
-    return string.matches(pattern)
-}
