cc46334eb61e43c2d7a475085041c100b0faec57
junyng <pom0894@naver.com>
2019-06-23 15:02:46 +0900

    정규식 일치 여부를 리턴하는 사용자 정의 연산 =~ 추가 및 적용

--- JSONParser/JSONParser/JSONValidationRule.swift
+++ JSONParser/JSONParser/JSONValidationRule.swift
@@ -9,9 +9,8 @@
 import Foundation
 
 struct JSONValidationRule: ValidationRule {
-    
     func isValid(_ value: String) -> Bool {
-        return value.matches(Regex.jsonObject) || value.matches(Regex.jsonArray)
+        return value =~ Regex.jsonObject || value =~ Regex.jsonArray
     }
 }
 
@@ -19,4 +18,17 @@ extension String {
     func matches(_ pattern: String) -> Bool {
         return self.range(of: pattern, options: .regularExpression) != nil
     }
+}
+
+precedencegroup RegexPrecedence {
+    higherThan: LogicalConjunctionPrecedence
+}
+/// 정규식 패턴에 매치하는 사용자 정의 연산자
+infix operator =~: RegexPrecedence
+/// - Parameters:
+///   - string: 입력할 문자열
+///   - pattern: 정규식 문자열
+/// - Returns: 정규식에 일치 여부에 따라 true or false
+func =~ (string: String, pattern: String) -> Bool {
+    return string.matches(pattern)
 }
