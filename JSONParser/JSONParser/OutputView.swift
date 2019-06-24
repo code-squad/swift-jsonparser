9add94375c74710c081dc56d31284be9d54bd28c
kimjieun <zieunv@gmail.com>
2019-06-25 03:10:09 +0900

    단일 object에 대한 count 함수 구현

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -13,9 +13,25 @@ struct OutputView {
         if input is Array<JSONDataType> {
             let description = try printDataCount(arrayData: input)
             print(description)
-        } else {
-            print(input)
+        } else if input is Dictionary <String, JSONDataType> {
+            let description = try printObjectCount(objectData: input)
+            print(description)
         }
+    }
+    
+  
+    static private func printObjectCount(objectData: JSONDataType) throws -> String {
+        var result: String = ""
+        guard let object = objectData as? [String: JSONDataType] else {
+            throw JSONError.notObject
+        }
+        let objectCount = object.count
+        let counts =  Counter.objectCount(object: object)
+        
+        for (key, value) in counts {
+            result += "\(key)가 \(value)개 "
+        }
+        return "총 \(objectCount)개의 데이터 중에 \(result)포함되어 있습니다."
     }
     
     static private func printDataCount(arrayData: JSONDataType) throws -> String {
@@ -25,7 +41,7 @@ struct OutputView {
         }
         
         let totalCount = array.count
-        let counts = try Counter.count(jsonData: array)
+        let counts = Counter.count(jsonData: array)
         for (key, value) in counts {
             result += "\(key)가 \(value)개 "
         }
