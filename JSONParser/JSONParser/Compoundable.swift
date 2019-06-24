f89b0970be66386e25abb20e213fb6c1b29d857c
Daheen Lee <allwhite.dev@gmail.com>
2019-06-24 22:20:37 +0900

    feat: Add new conformance of protocol Compoundable to Array and Dictionary

--- JSONParser/JSONParser/Compoundable.swift
+++ JSONParser/JSONParser/Compoundable.swift
@@ -15,6 +15,15 @@ protocol Compoundable {
     func getFormatted(element: JSONValue, indent: Int) -> String
 }
 
+extension Compoundable {
+    func getFormatted(element: JSONValue, indent: Int) -> String {
+        switch element {
+        case let compound as Compoundable :
+            return "\(compound.formatted(indentLevel: indent))"
+        default:
+            return "\(element)"
+        }
+    }
 }
 
 extension Array: Compoundable where Element == JSONValue {
@@ -24,6 +33,17 @@ extension Array: Compoundable where Element == JSONValue {
     
     var elements: [JSONValue] {
         return self
+    }
+    
+    func formatted(indentLevel: Int) -> String {
+        var arrayString = String()
+        var prefix = "\(JSONSymbols.openBracket)"
+        for element in self {
+            arrayString += "\(prefix)\(getFormatted(element: element, indent: indentLevel))"
+            prefix = JSONSymbols.seperator
+        }
+        arrayString += "\(JSONSymbols.closedBracket)"
+        return arrayString
     }
 }
 
@@ -36,4 +56,17 @@ extension Dictionary: Compoundable where Key == String, Value == JSONValue {
         return Array(self.values)
     }
     
+    func formatted(indentLevel: Int) -> String {
+        var objectString = String()
+        var prefix = "\(JSONSymbols.openBrace)\(JSONSymbols.newline)"
+        let seperator = "\(JSONSymbols.seperator)\(JSONSymbols.newline)"
+        var indentation = String(repeating: JSONSymbols.tab, count: indentLevel + 1)
+        for (key, value) in self {
+            objectString += "\(prefix)\(indentation)\(key) : \(getFormatted(element: value, indent: indentLevel + 1))"
+            prefix = seperator
+        }
+        indentation = String(repeating: JSONSymbols.tab, count: indentLevel)
+        objectString += "\(JSONSymbols.newline)\(indentation)\(JSONSymbols.closedBrace)"
+        return objectString
+    }
 }
