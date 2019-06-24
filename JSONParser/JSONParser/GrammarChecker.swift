74e10fdd86c581ed27e7d4504c2b9a250371bbdd
Kroisttt <yago0315@gmail.com>
2019-06-24 13:31:40 +0900

    정규표현식 수정

--- JSONParser/JSONParser/GrammarChecker.swift
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -9,15 +9,16 @@
 import Foundation
 
 struct GrammarChecker {
-    private static let string = "\"[a-zA-Z0-9 ]+\""
+    private static let string = "\"[a-zA-Z0-9', ]+\""
     private static let number = "[0-9]+"
     private static let bool = "(true|false)"
     private static let ws = " ?"
     
     private static let nestedObject = "\\{\(ws)((|,\(ws))(\(string)\(ws):\(ws))(\(string)|\(number)|\(bool)))+\(ws)\\}"
+    private static let nestedArray = "\\[\(ws)((|,\(ws))(\(string)|\(number)|\(bool)))+\(ws)\\]"
     
-    private static let objectRegex = "^\\{\(ws)((|,\(ws))(\(string)\(ws):\(ws))(\(string)|\(number)|\(bool)))+\(ws)\\}$"
-    private static let arrayRegex = "^\\[\(ws)((|,\(ws))(\(string)|\(number)|\(bool)|(\(nestedObject))))+\(ws)\\]$"
+    private static let objectRegex = "^\\{\(ws)((|,\(ws))(\(string)\(ws):\(ws))(\(string)|\(number)|\(bool)|(\(nestedArray))|(\(nestedObject))))+\(ws)\\}$"
+    private static let arrayRegex = "^\\[\(ws)((|,\(ws))(\(string)|\(number)|\(bool)|(\(nestedObject))|(\(nestedArray))))+\(ws)\\]$"
     
     static func checkGrammar(input: String) -> Bool {
         let isArray = input.range(of: arrayRegex, options: .regularExpression) != nil
