66dda1aa1a6d49e173b47019f54133be22659841
Daheen Lee <allwhite.dev@gmail.com>
2019-06-24 22:47:48 +0900

    refactor: Remove format related methods from TypeCountable

--- JSONParser/JSONParser/TypeCountable.swift
+++ JSONParser/JSONParser/TypeCountable.swift
@@ -11,62 +11,22 @@ import Foundation
 protocol TypeCountable {
     var elementCount: Int { get }
     var elements: [JSONValue] { get }
-    func formatted(indentLevel: Int) -> String
-    func getFormatted(element: JSONValue, indent: Int) -> String
 }
 
-extension TypeCountable {
-    func getFormatted(element: JSONValue, indent: Int) -> String {
-        switch element {
-        case let compound as Compoundable :
-            return "\(compound.formatted(indentLevel: indent))"
-        default:
-            return "\(element)"
-        }
-    }
-}
-
-extension Array: Compoundable where Element == JSONValue {
+extension Array: TypeCountable where Element == JSONValue {
     var elementCount: Int {
         return self.count
     }
-    
     var elements: [JSONValue] {
         return self
     }
-    
-    func formatted(indentLevel: Int) -> String {
-        var arrayString = String()
-        var prefix = "\(JSONSymbols.openBracket)"
-        for element in self {
-            arrayString += "\(prefix)\(getFormatted(element: element, indent: indentLevel))"
-            prefix = JSONSymbols.seperator
-        }
-        arrayString += "\(JSONSymbols.closedBracket)"
-        return arrayString
-    }
 }
 
-extension Dictionary: Compoundable where Key == String, Value == JSONValue {
+extension Dictionary: TypeCountable where Key == String, Value == JSONValue {
     var elementCount: Int {
         return self.count
     }
-    
     var elements: [JSONValue] {
         return Array(self.values)
-    }
-    
-    func formatted(indentLevel: Int) -> String {
-        var objectString = String()
-        var prefix = "\(JSONSymbols.openBrace)\(JSONSymbols.newline)"
-        let seperator = "\(JSONSymbols.seperator)\(JSONSymbols.newline)"
-        var indentation = String(repeating: JSONSymbols.tab, count: indentLevel + 1)
-        for (key, value) in self {
-            objectString += "\(prefix)\(indentation)\(key) : \(getFormatted(element: value, indent: indentLevel + 1))"
-            prefix = seperator
-        }
-        indentation = String(repeating: JSONSymbols.tab, count: indentLevel)
-        objectString += "\(JSONSymbols.newline)\(indentation)\(JSONSymbols.closedBrace)"
-        return objectString
     }
 }
