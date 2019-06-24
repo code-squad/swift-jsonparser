da1f971e24fe1f944b7ab9a19b9b0a491ddc0e08
박준희 <junibibi04@gmail.com>
2019-06-24 17:02:42 +0900

    주석리/띄어쓰기 수정함

--- JSONParser/JSONParser/JsonType.swift
+++ JSONParser/JSONParser/JsonType.swift
@@ -13,7 +13,7 @@ protocol JsonType {
     
     var countInfo: [String: Int] { get }
     
-    var count: Int {get}
+    var count: Int { get }
 }
 
 //기본구현함
@@ -38,6 +38,9 @@ extension String: JsonType {
     var typeName: String {
         return "문자열"
     }
+    var count: Int {
+        return 1
+    }
 }
 
 
@@ -51,11 +54,14 @@ extension Array: JsonType where Element == JsonType {
     var typeName: String {
         return "배열"
     }
-    
+    //배열은 element갯수가 1개이상일 수 있기 때문에 따로 정의해줌.
     var countInfo: [String : Int] {
-        
+        //typeName별로 그룹화해줌
         let byName = Dictionary(grouping: self, by: { $0.typeName })
+        //그룹화된 갯수를 반환함
         return byName.compactMapValues({ $0.count })
-        
     }
 }
+
+
+
