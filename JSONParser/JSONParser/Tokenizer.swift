23983bb5b33151235fe3f3461d03fefa26af069b
박준희 <junibibi04@gmail.com>
2019-06-24 17:00:02 +0900

    기호들을 가지고 있는 구조체이름 수정함 문자를 쪼개는 중에 오류가 있어 코드 수정함

--- JSONParser/JSONParser/Tokenizer.swift
+++ JSONParser/JSONParser/Tokenizer.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Structure {
+struct Mark {
     static let startArray: Character = "["
     static let endArray: Character = "]"
     static let comma: Character = ","
@@ -29,22 +29,29 @@ struct Tokenizer {
         }
     }
     
-    
     mutating func tokenize(input: String) -> [String] {
         
-        let structures: [Character] = [Structure.startArray, Structure.endArray, Structure.comma]
-        var isString = false //"이 나와야 string이기때문에 그 전까지 기본값은 false
+        let structures: [Character] = [Mark.startArray, Mark.endArray, Mark.comma]
+        var isString = false //"나와야 string이기때문에 그 전까지 기본값은 false
         
         for character in input {
             
-            if character == Structure.quotationMark { //""안에서는 문자를 나누지 않도록 조건을 설정함
+            if character == Mark.quotationMark { //""안에서는 문자를 나누지 않도록 조건을 설정함
+                if isString {
+                    tokens.append(String(Mark.quotationMark) + temp + String(Mark.quotationMark))
+                    temp.removeAll()
+                } else {
+                    tokens.append(temp)
+                    temp.removeAll()
+                }
+                
                 isString.toggle()
             }
-            
-            if isString {
+            else if isString {
                 temp.append(character)
+                
             } else {
-                guard character != Structure.whitespace else { //공백은 무시하고 다음 루프로 이동함
+                guard character != Mark.whitespace else { //공백은 무시하고 다음 루프로 이동함
                     continue
                 }
                 if structures.contains(character) {
@@ -54,7 +61,6 @@ struct Tokenizer {
                     temp.append(character)
                 }
             }
-            
         }
         tempToTokens()
         // 반환하기전에 tokens배열을 비워줌
