ba7554ad4d21caad6de8e9114ac899c88b7b159c
cmindy <minjidev@naver.com>
2019-06-24 18:08:48 +0900

    feat: Add prettyJSON for serialize json pretty

--- JSONParser/JSONParser/PrettyFormatter.swift
+++ JSONParser/JSONParser/PrettyFormatter.swift
@@ -7,3 +7,93 @@
 //
 
 import Foundation
+
+class Formatter {
+    var jsonString = ""
+    private var indent = 0, depth = 0
+    
+    init() { }
+    
+    func prettyJSON(data: JSONValueType) -> String {
+        serializeJSON(data: data)
+        return jsonString
+    }
+    
+    private func serializeJSON(data: JSONValueType) {
+        switch data {
+        case let object as Object:
+            depth += 1
+            serializeObject(data: object)
+        case let array as JSONArray:
+            depth += 1
+            serializeArray(data: array)
+        default:
+            jsonString += "\(data)"
+        }
+    }
+    
+    private func serializeObject(data: Object) {
+        jsonString += "{"
+        jsonString += "\n"
+        incAndWriteIndent()
+        
+        var first = true
+        for (key, value) in data {
+            if first {
+                first = false
+            } else {
+                jsonString += ",\n"
+                writeIndent()
+            }
+            serializeJSON(data: key)
+            jsonString += ": "
+            serializeJSON(data: value)
+        }
+        jsonString += "\n"
+        
+        if depth > 2 {
+            writeIndent()
+        }
+        
+        decAndWriteIndent()
+        jsonString += "}"
+    }
+    
+    private func serializeArray(data: JSONArray) {
+        if depth > 2 {
+            jsonString += "\n"
+            incAndWriteIndent()
+        }
+        
+        jsonString += "["
+        var first = true
+        for element in data {
+            if first {
+                first = false
+            } else {
+                jsonString += ", "
+            }
+            serializeJSON(data: element)
+        }
+        jsonString += "]"
+        
+        if depth > 2 {
+            jsonString += "\n"
+        }
+    }
+    
+    private func incAndWriteIndent() {
+        indent += 1
+        writeIndent()
+    }
+    
+    private func decAndWriteIndent() {
+        indent -= 1
+        writeIndent()
+    }
+    
+    private func writeIndent() {
+        let indentation = String.init(repeating: "\t", count: indent)
+        jsonString += indentation
+    }
+}
