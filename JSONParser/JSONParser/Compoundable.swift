dcc8a67ee0a96aafc081f6df3c97589130e6a9b2
Daheen Lee <allwhite.dev@gmail.com>
2019-06-24 22:17:02 +0900

    refactor: Rename protocol TypeCountable Compoundable

--- JSONParser/JSONParser/Compoundable.swift
+++ JSONParser/JSONParser/Compoundable.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+protocol Compoundable {
+    var elementCount: Int { get }
+    var elements: [JSONValue] { get }
+    
+}
+
+extension Array: Compoundable where Element == JSONValue {
+    var elementCount: Int {
+        return self.count
+    }
+    
+    var elements: [JSONValue] {
+        return self
+    }
+}
+
+extension Dictionary: Compoundable where Key == String, Value == JSONValue {
+    var elementCount: Int {
+        return self.count
+    }
+    
+    var elements: [JSONValue] {
+        return Array(self.values)
+    }
+    
+}
