9e1030e81781ed758c2f559d462b9e5297a79cbe
박준희 <junibibi04@gmail.com>
2019-07-01 22:25:08 +0900

    main함수를 만들어 코드를 안에 넣어줌

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -8,21 +8,30 @@
 
 import Foundation
 
-var inputView = InputView()
-var outputView = OutputView()
-var tokenizer = Tokenizer()
-var parser = Parser()
-
-outputView.show(string: "Json데이터를 입력하세요")
-let input = inputView.ask(data: "Json데이터")
-let tokens = tokenizer.tokenize(input: input)
-
-do {
-    let jsonData = try parser.startParsing(tokens: tokens)
-    if let countableData = jsonData as? Countable {
-        outputView.showCount(typeName: jsonData.typeName, countInfo: countableData.countInfo )
+func main() {
+    var inputView = InputView()
+    var outputView = OutputView()
+    
+    var grammarChecker = GrammarChecker()
+    var tokenizer = Tokenizer()
+    var parser = Parser()
+    
+    outputView.show(string: "Json데이터를 입력하세요")
+    let input = inputView.ask(data: "Json데이터")
+    guard grammarChecker.checkGrammar(input: input) else {
+        outputView.show(string: "지원하지 않는 형식을 포함하고 있습니다.")
+        return
     }
-} catch {
-    outputView.show(string: "\(error)")
+    let tokens = tokenizer.tokenize(input: input)
+    
+    do {
+        let jsonData = try parser.startParsing(tokens: tokens)
+        if let countableData = jsonData as? Countable {
+            outputView.showCount(typeName: jsonData.typeName, countInfo: countableData.countInfo )
+        }
+    } catch {
+        outputView.show(string: "\(error)")
+    }
 }
 
+main()
