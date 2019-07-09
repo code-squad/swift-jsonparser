3f1faaebbffa13049bfe9662aa954d0206a48ede
박준희 <junibibi04@gmail.com>
2019-07-09 21:55:02 +0900

    프로토콜을 구현할때 필요한 변수들을 상위로 위치변경함. Serializable프로토콜 선언함. Serializable기본구현함. Array와 오브젝트가 Serializable프로토콜을 채택하고 extention해줌 Serializer구조체 위치를 아래로 변경함.

--- JSONParser/JSONParser/Serializer.swift
+++ JSONParser/JSONParser/Serializer.swift
@@ -8,57 +8,55 @@
 
 import Foundation
 
-struct Serializer {
-    //indentCount에따라 공백이 추가되는 코드
-    private var indentCount = 0
-    private func makeIndent(count: Int) -> String {
-        var result = ""
-        for _ in 0 ..< count {
-            result.append("    ")
-        }
-        return result
+private var indentCount = 0
+private func makeIndent(count: Int) -> String {
+    var result = ""
+    for _ in 0 ..< count {
+        result.append("    ")
     }
-    private var indent: String {
-        return makeIndent(count: indentCount)
+    return result
+}
+private var indent: String {
+    return makeIndent(count: indentCount)
+}
+
+
+protocol Serializable {
+    func serialized() -> String
+}
+
+extension Serializable {
+    func serialized() -> String {
+        return "\(self)"
     }
-    
-    //jsonValue 직렬화하는 메소드
-    mutating func serializeValue(_ jsonData: JsonType) -> String {
-        switch jsonData {
-        case let object as [String: JsonType]:
-            return serializeObject(object)
-        case let array as [JsonType]:
-            return serializeArray(array)
-        default:
-            return ("\(jsonData)")
-        }
-    }
-    
-    //jsonType배열 직렬화하는 메소드
-    mutating func serializeArray(_ array: [JsonType]) -> String {
+}
+
+extension Array: Serializable where Element == JsonType {
+    func serialized() -> String {
         var result = ""
         var isFirst = true
         result.append("[")
-        array.forEach { element in
+        self.forEach { element in
             if isFirst {
                 isFirst = false
             } else {
                 result.append(", ")
             }
-            result.append(serializeValue(element))
+            result.append(element.serialized())
         }
         result.append("]")
-
+        
         return result
     }
-    
-    //jsonType오브젝트 직렬화하는 메소드
-    mutating func serializeObject(_ object: [String: JsonType]) -> String {
+}
+
+extension Dictionary: Serializable where Key == String, Value == JsonType {
+    func serialized() -> String {
         var result = ""
         var isFirst = true
         result.append("{")
         indentCount += 1
-        object.forEach { (key, value) in
+        self.forEach { (key, value) in
             if isFirst {
                 isFirst = false
             } else {
@@ -66,12 +64,19 @@ struct Serializer {
             }
             result.append("\n")
             result.append(indent)
-            result.append("\(key): \(serializeValue(value))")
+            result.append("\(key.serialized()): \(value.serialized())")
         }
         indentCount -= 1
         result.append("\n")
         result.append(indent)
         result.append("}")
         return result
+    }
+}
+
+struct Serializer {
+    //jsonValue 직렬화하는 메소드
+    func serializeValue(_ jsonData: JsonType) -> String {
+        return jsonData.serialized()
     }
 }
